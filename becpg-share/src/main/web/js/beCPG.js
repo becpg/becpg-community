c6e1b910a95e317d3cc47f4caf597b7910b82640
Laborie Matthieu <matthieu.laborie@linagora.com>
2011-09-13 10:19:00 +0200

    Add logo to becpg types

--- becpg-share/src/main/web/js/beCPG.js
+++ becpg-share/src/main/web/js/beCPG.js
@@ -41,3 +41,98 @@ beCPG.widget = beCPG.widget || {};
  */
 beCPG.util = beCPG.util || {};
 
+
+/**
+ * Given a filename, returns either a filetype icon or generic icon file stem
+ *
+ * @method Alfresco.util.getFileIcon
+ * @param p_fileName {string} File to find icon for
+ * @param p_fileType {string} Optional: Filetype to offer further hinting
+ * @param p_iconSize {int} Icon size: 32
+ * @return {string} The icon name, e.g. doc-file-32.png
+ * @static
+ */
+beCPG.util.getFileIcon = function(p_fileName, p_fileType, p_iconSize)
+{
+   // Mapping from extn to icon name for cm:content
+   var extns = 
+   {
+      "doc": "doc",
+      "docx": "doc",
+      "ppt": "ppt",
+      "pptx": "ppt",
+      "xls": "xls",
+      "xlsx": "xls",
+      "pdf": "pdf",
+      "bmp": "img",
+      "gif": "img",
+      "jpg": "img",
+      "jpeg": "img",
+      "png": "img",
+      "txt": "text"
+   };
+
+   
+  
+   var prefix = "generic",
+      fileType = typeof p_fileType === "string" ? p_fileType : "cm:content",
+      iconSize = typeof p_iconSize === "number" ? p_iconSize : 32;
+   
+   // If type = cm:content, then use extn look-up
+   var type = beCPG.util.getFileIcon.types[fileType];
+   if (type === "file")
+   {
+      var extn = p_fileName.substring(p_fileName.lastIndexOf(".") + 1).toLowerCase();
+      if (extn in extns)
+      {
+         prefix = extns[extn];
+      }
+   }
+   else if (typeof type == "undefined")
+   {
+      type = "file";
+   }
+   return prefix + "-" + type + "-" + iconSize + ".png";
+};
+beCPG.util.getFileIcon.types =
+{
+   "{http://www.alfresco.org/model/content/1.0}cmobject": "file",
+   "cm:cmobject": "file",
+   "{http://www.alfresco.org/model/content/1.0}content": "file",
+   "cm:content": "file",
+   "{http://www.alfresco.org/model/content/1.0}thumbnail": "file",
+   "cm:thumbnail": "file",
+   "{http://www.alfresco.org/model/content/1.0}folder": "folder",
+   "cm:folder": "folder",
+   "{http://www.alfresco.org/model/content/1.0}category": "category",
+   "cm:category": "category",
+   "{http://www.alfresco.org/model/content/1.0}person": "user",
+   "cm:person": "user",
+   "{http://www.alfresco.org/model/content/1.0}authorityContainer": "group",
+   "cm:authorityContainer": "group",
+   "tag": "tag",
+   "{http://www.alfresco.org/model/site/1.0}sites": "site",
+   "st:sites": "site",
+   "{http://www.alfresco.org/model/site/1.0}site": "site",
+   "st:site": "site",
+   "{http://www.alfresco.org/model/transfer/1.0}transferGroup": "server-group",
+   "trx:transferGroup": "server-group",
+   "{http://www.alfresco.org/model/transfer/1.0}transferTarget": "server",
+   "trx:transferTarget": "server",
+   "{http://www.bcpg.fr/model/security/1.0}aclGroup" : "acl",
+   "sec:aclGroup":"acl",
+   "{http://www.bcpg.fr/model/becpg/1.0}cost":"cost",
+   "bcpg:cost":"cost",
+   "{http://www.bcpg.fr/model/becpg/1.0}microbio":"microbio",
+   "bcpg:microbio":"microbio",
+   "{http://www.bcpg.fr/model/becpg/1.0}physicoChem":"physicoChem",
+   "bcpg:physicoChem":"physicoChem",
+   "{http://www.bcpg.fr/model/becpg/1.0}allergen":"allergen",
+   "bcpg:allergen":"allergen",
+  "{http://www.bcpg.fr/model/becpg/1.0}organo":"organo",
+   "bcpg:organo":"organo",
+   "{http://www.bcpg.fr/model/becpg/1.0}ing":"ing",
+   "bcpg:ing":"ing",
+   "{http://www.bcpg.fr/model/becpg/1.0}nut":"nut",
+   "bcpg:nut":"nut"
+};
