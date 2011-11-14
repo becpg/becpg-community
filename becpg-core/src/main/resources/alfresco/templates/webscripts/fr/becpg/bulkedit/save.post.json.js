75261a4f209c1476b35453ef472e8989751e9750
Laborie Matthieu <matthieu.laborie@becpg.fr>
2011-11-14 13:27:35 +0100

    BUGFIX

--- becpg-core/src/main/resources/alfresco/templates/webscripts/fr/becpg/bulkedit/save.post.json.js
+++ becpg-core/src/main/resources/alfresco/templates/webscripts/fr/becpg/bulkedit/save.post.json.js
@@ -3,13 +3,26 @@ var field = json.get("field");
 var value = json.get("value");
 var nodeRef = json.get("nodeRef");
 
+var savedValue = "";
+
+if(value!=null && value.length != null && !(value instanceof Date)){
+	for(var i in value){
+		if(savedValue.length>0){
+			savedValue+=",";
+		}
+		savedValue+=value[i];
+	}
+} else {
+	savedValue = value;
+}
+
 var node = search.findNode(nodeRef);
 if(node!=null && field.contains("prop_")){
 	
-	node.properties[field.replace("prop_","").replace("_",":")] =  	value;
+	node.properties[field.replace("prop_","").replace("_",":")] =  	savedValue;
 	node.save();
 	
 }
 
 
-model.newValue = value;
+model.newValue = savedValue;
