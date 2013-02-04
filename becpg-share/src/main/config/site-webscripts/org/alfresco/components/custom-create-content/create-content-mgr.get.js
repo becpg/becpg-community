be2f86eacfcae865858405ca255780b67b94ad6e
Laborie Matthieu <matthieu.laborie@becpg.fr>
2013-02-04 09:24:13 +0100

    refs #286

--- becpg-share/src/main/config/site-webscripts/org/alfresco/components/custom-create-content/create-content-mgr.get.js
+++ becpg-share/src/main/config/site-webscripts/org/alfresco/components/custom-create-content/create-content-mgr.get.js
@@ -1,8 +1,8 @@
-// Find the default DocumentList widget and replace it with the custom widget
+// Find the default CreateContentMgr widget and replace it with the custom widget
 for (var i=0; i<model.widgets.length; i++)
 {
   if (model.widgets[i].id == "CreateContentMgr")
   {
-    model.widgets[i].options.isEntity == ((page.url.args.isEntity != null) ? page.url.args.isEntity : "false");
+    model.widgets[i].options.isEntity = "true" == ((page.url.args.isEntity != null) ? page.url.args.isEntity : "false");
   }
 }
