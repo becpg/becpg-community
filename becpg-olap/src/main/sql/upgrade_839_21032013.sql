42d03eeba34703837fe76df14086582894d0a534
Laborie Matthieu <matthieu.laborie@becpg.fr>
2013-03-25 08:54:19 +0100

    #204

--- becpg-olap/src/main/sql/upgrade_839_21032013.sql
+++ becpg-olap/src/main/sql/upgrade_839_21032013.sql
@@ -8,10 +8,23 @@ UPDATE becpg_entity  SET is_last_version = TRUE WHERE
  id IN (
  	SELECT MAX(entity.id) as id  FROM  (SELECT * FROM becpg_entity) AS entity GROUP BY entity.entity_id
 );
-					
 
-UPDATE becpg_datalist SET is_last_version = TRUE WHERE
-  id IN (
- 		SELECT MAX(datalist.id) as id  FROM  (SELECT * FROM becpg_datalist) AS datalist GROUP BY datalist.datalist_id
-);
-			
+CREATE TEMPORARY TABLE `temp_becpg_datalist` (
+  `id` BIGINT(20) NOT NULL,
+  `datalist_id` VARCHAR(62) NOT NULL, 
+  PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
+
+  
+INSERT INTO `temp_becpg_datalist` (id,datalist_id) SELECT id,datalist_id FROM becpg_datalist;
+
+UPDATE becpg_datalist as t1 INNER JOIN
+(
+  SELECT MAX(datalist.id) as id  FROM  temp_becpg_datalist AS datalist 
+  GROUP BY datalist.datalist_id
+) as t2 
+ON  t1.id = t2.id
+SET t1.is_last_version = TRUE; 
+
+
+DROP TABLE temp_becpg_datalist;
