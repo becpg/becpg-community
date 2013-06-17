de489c82bc06fcfad5b2876d1b024361bddeba9c
Laborie Matthieu <matthieu.laborie@becpg.fr>
2013-06-17 10:39:42 +0200

    Fix security

--- becpg-core/src/main/java/fr/becpg/repo/security/aop/SecurityMethodBeforeAdvice.java
+++ becpg-core/src/main/java/fr/becpg/repo/security/aop/SecurityMethodBeforeAdvice.java
@@ -14,36 +14,37 @@ import fr.becpg.repo.security.BeCPGAccessDeniedException;
 import fr.becpg.repo.security.SecurityService;
 
 public class SecurityMethodBeforeAdvice implements MethodBeforeAdvice {
-	
+
 	private SecurityService securityService;
-	
+
 	private NamespaceService namespaceService;
-	
+
 	private static Log logger = LogFactory.getLog(SecurityMethodBeforeAdvice.class);
-	
+
 	public void setSecurityService(SecurityService securityService) {
 		this.securityService = securityService;
 	}
-	
+
 	public void setNamespaceService(NamespaceService namespaceService) {
 		this.namespaceService = namespaceService;
 	}
 
-
 	@Override
 	public void before(Method m, Object[] args, Object target) throws Throwable {
-		String methodQName = m.getAnnotation(AlfQname.class).qname();
-		if(methodQName!=null){
-			String classQName = target.getClass().getAnnotation(AlfQname.class).qname();
-			if(classQName!=null){
-				if(securityService.computeAccessMode(QName.createQName(classQName, namespaceService), methodQName) != SecurityService.WRITE_ACCESS){
-					logger.info("Access denied to field "+methodQName+" for "+AuthenticationUtil.getFullyAuthenticatedUser());
-					throw new BeCPGAccessDeniedException(methodQName);
+
+		if (m.isAnnotationPresent(AlfQname.class)) {
+			String methodQName = m.getAnnotation(AlfQname.class).qname();
+			if (methodQName != null) {
+				String classQName = target.getClass().getAnnotation(AlfQname.class).qname();
+				if (classQName != null) {
+					if (securityService.computeAccessMode(QName.createQName(classQName, namespaceService), methodQName) != SecurityService.WRITE_ACCESS) {
+						logger.info("Access denied to field " + methodQName + " for " + AuthenticationUtil.getFullyAuthenticatedUser());
+						throw new BeCPGAccessDeniedException(methodQName);
+					}
 				}
+
 			}
-			
 		}
-		
 
 	}
 
