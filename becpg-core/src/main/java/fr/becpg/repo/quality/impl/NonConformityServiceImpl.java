eafedfed2828ee0253b9af572bff43f6c0390546
Laborie Matthieu <matthieu.laborie@becpg.fr>
2013-08-21 10:32:52 +0200

    fix #562 #564

--- becpg-core/src/main/java/fr/becpg/repo/quality/impl/NonConformityServiceImpl.java
+++ becpg-core/src/main/java/fr/becpg/repo/quality/impl/NonConformityServiceImpl.java
@@ -1,10 +1,13 @@
 package fr.becpg.repo.quality.impl;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.alfresco.repo.model.Repository;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.workflow.WorkflowInstance;
+import org.alfresco.service.cmr.workflow.WorkflowService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -19,9 +22,14 @@ public class NonConformityServiceImpl implements NonConformityService {
 
 	private RepoService repoService;
 	private Repository repositoryHelper;
+	private WorkflowService workflowService;
 
 	public void setRepoService(RepoService repoService) {
 		this.repoService = repoService;
+	}
+
+	public void setWorkflowService(WorkflowService workflowService) {
+		this.workflowService = workflowService;
 	}
 
 	public void setRepositoryHelper(Repository repositoryHelper) {
@@ -36,8 +44,7 @@ public class NonConformityServiceImpl implements NonConformityService {
 		// calculate dest node
 		if (productNodeRef != null) {
 
-			destFolderNodeRef = repoService.getOrCreateFolderByPath(productNodeRef, RepoConsts.PATH_NC,
-					TranslateHelper.getTranslatedPath(RepoConsts.PATH_NC));
+			destFolderNodeRef = repoService.getOrCreateFolderByPath(productNodeRef, RepoConsts.PATH_NC, TranslateHelper.getTranslatedPath(RepoConsts.PATH_NC));
 		}
 
 		// default folder
@@ -55,10 +62,36 @@ public class NonConformityServiceImpl implements NonConformityService {
 
 	@Override
 	public void classifyNC(NodeRef ncNodeRef, NodeRef productNodeRef) {
-
 		logger.debug("Classify NC");
 		NodeRef destFolderNodeRef = getStorageFolder(productNodeRef);
-		repoService.moveNode(ncNodeRef, destFolderNodeRef);				
+		repoService.moveNode(ncNodeRef, destFolderNodeRef);
+	}
+
+	@Override
+	public List<String> getAssociatedWorkflow(NodeRef ncNodeRef) {
+
+		List<String> ret = new ArrayList<>();
+		List<WorkflowInstance> workflows = workflowService.getWorkflowsForContent(ncNodeRef, true);
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("Found " + workflows.size() + " associated workflows ");
+		}
+
+		for (WorkflowInstance workflowInstance : workflows) {
+			ret.add(workflowInstance.getId());
+		}
+
+		return ret;
+	}
+
+	@Override
+	public void deleteWorkflows(List<String> instanceIds) {
+		if (logger.isDebugEnabled()) {
+			logger.debug(instanceIds.size() + " workflows to delete");
+		}
+
+		workflowService.cancelWorkflows(instanceIds);
+
 	}
 
 }
