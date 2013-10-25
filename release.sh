fdb901b4e99fea3daf4b1bb110da25acdb816793
Laborie Matthieu <matthieu.laborie@becpg.fr>
2013-10-25 14:06:28 +0200

    refs #626

--- release.sh
+++ release.sh
@@ -2,8 +2,6 @@
 
 . ./common.sh
 
-
-
 echo "**********************************************************"
 echo "Build AMP"
 echo "**********************************************************"
@@ -18,6 +16,8 @@ echo "**********************************************************"
 cd $BECPG_ROOT/distribution/target
 scp -P 22222 becpg-*-distribution.tar.gz  root@www.becpg.fr:/srv/becpg-data/becpg-repo/releases
 
-cd $BECPG_OLAP_ROOT/target
-scp -P 22222 becpg-olap-*-distribution.tar.gz  root@www.becpg.fr:/srv/becpg-data/becpg-repo/releases
-
+read -p "Release report server (y/n)" ans
+if [ "$ans" = "y" ]; then
+	cd $BECPG_ROOT/becpg-report/target
+	scp -P 22222  becpg-report-*.war root@www.becpg.fr:/srv/becpg-data/becpg-repo/releases/addons
+fi
