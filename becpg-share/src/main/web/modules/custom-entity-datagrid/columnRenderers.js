2e4d927f417cceef7c45106e4274a35ed135a8fa
Laborie Matthieu <matthieu.laborie@becpg.fr>
2013-11-18 17:30:40 +0100

    fix #754

--- becpg-share/src/main/web/modules/custom-entity-datagrid/columnRenderers.js
+++ becpg-share/src/main/web/modules/custom-entity-datagrid/columnRenderers.js
@@ -167,6 +167,13 @@ if (beCPG.module.EntityDataGridRenderers) {
       propertyName : [ "bcpg:cost", "bcpg:allergen", "bcpg:nut", "bcpg:ing", "bcpg:geoOrigin", "bcpg:bioOrigin",
             "bcpg:geo", "bcpg:microbio", "bcpg:physicoChem", "bcpg:organo" ],
       renderer : function(oRecord, data, label, scope) {
+    
+         if (oRecord.getData("itemData")["prop_bcpg_depthLevel"] != null) {
+            var padding = (oRecord.getData("itemData")["prop_bcpg_depthLevel"].value - 1) * 15;
+            return '<span class="' + data.metadata + '" style="margin-left:' + padding + 'px;">' + Alfresco.util
+                  .encodeHTML(data.displayValue) + '</span>';
+         }
+         
          return '<span class="' + data.metadata + '" >' + Alfresco.util.encodeHTML(data.displayValue) + '</span>';
       }
 
