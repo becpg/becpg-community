d434e9af373ab137ed9618935b4d4a284e4bf4e4
Laborie Matthieu <matthieu.laborie@becpg.fr>
2013-12-03 13:53:38 +0100

    fix patch

--- becpg-core/src/main/java/fr/becpg/repo/admin/patch/NutParentLevelPatch.java
+++ becpg-core/src/main/java/fr/becpg/repo/admin/patch/NutParentLevelPatch.java
@@ -35,6 +35,7 @@ public class NutParentLevelPatch extends AbstractBeCPGPatch {
 
 	private BehaviourFilter policyBehaviourFilter;
 
+
 	public void setBeCPGSearchService(BeCPGSearchService beCPGSearchService) {
 		this.beCPGSearchService = beCPGSearchService;
 	}
@@ -52,22 +53,24 @@ public class NutParentLevelPatch extends AbstractBeCPGPatch {
 
 		logger.info("Apply NutParentLevelPatch");
 
-		try {
-			policyBehaviourFilter.disableBehaviour(BeCPGModel.ASPECT_DEPTH_LEVEL);
+		List<NodeRef> dataListNodeRefs = beCPGSearchService.luceneSearch("+TYPE:\"bcpg:nutList\" NOT ASPECT:\"bcpg:depthLevelAspect\" ");
+		logger.info("EntitySortableListPatch add sort in bcpg:entityListItem, size: " + dataListNodeRefs.size());
 
-			List<NodeRef> dataListNodeRefs = beCPGSearchService.luceneSearch("+TYPE:\"bcpg:nutList\" NOT ASPECT:\"bcpg:depthLevelAspect\" ");
-			logger.info("EntitySortableListPatch add sort in bcpg:entityListItem, size: " + dataListNodeRefs.size());
+		int i = 0;
+		List<List<NodeRef>> batches = Lists.partition(dataListNodeRefs, 500);
+		for (final List<NodeRef> subList : batches) {
+			try {
+				i++;
+				logger.info("Batch " + i + " over " + batches.size());
+				transactionService.getRetryingTransactionHelper().doInTransaction(new RetryingTransactionCallback<Void>() {
 
-			int i=0; 
-			List<List<NodeRef>> batches =  Lists.partition(dataListNodeRefs, 300);
-			for (final List<NodeRef> subList : batches) {
-				try {
-					i++;
-					logger.info("Batch "+i+" over " +batches.size());
-					transactionService.getRetryingTransactionHelper().doInTransaction(new RetryingTransactionCallback<Void>() {
+					@Override
+					public Void execute() throws Throwable {
 
-						@Override
-						public Void execute() throws Throwable {
+						policyBehaviourFilter.disableBehaviour(BeCPGModel.ASPECT_DEPTH_LEVEL);
+						policyBehaviourFilter.disableBehaviour(BeCPGModel.ASPECT_SORTABLE_LIST);
+
+						try {
 							for (NodeRef dataListNodeRef : subList) {
 								if (nodeService.exists(dataListNodeRef)) {
 									Map<QName, Serializable> properties = new HashMap<>();
@@ -78,78 +81,80 @@ public class NutParentLevelPatch extends AbstractBeCPGPatch {
 								}
 							}
 							return null;
+
+						} finally {
+							policyBehaviourFilter.enableBehaviour(BeCPGModel.ASPECT_DEPTH_LEVEL);
+							policyBehaviourFilter.enableBehaviour(BeCPGModel.ASPECT_SORTABLE_LIST);
 						}
 
-					}, false, true);
-				} catch (Throwable e) {
-					logger.warn(e, e);
-				}
+					}
+
+				}, false, true);
+			} catch (Throwable e) {
+				logger.warn(e, e);
 			}
-
-			i = 1;
-			updateParents();
-			applySort("Energie kJ, étiquetage", i++);
-			applySort("Energie kcal, étiquetage", i++);
-			applySort("Energie kJ", i++);
-			applySort("Energie kcal", i++);
-			applySort("Lipides", i++);
-			applySort("AG saturés", i++);
-			applySort("AG monoinsaturés", i++);
-			applySort("AG polyinsaturés", i++);
-			applySort("Glucides", i++);
-			applySort("Sucres", i++);
-			applySort("Amidon", i++);
-			applySort("Polyols totaux", i++);
-			applySort("Fibres alimentaires", i++);
-			applySort("Iode", i++);
-			applySort("Protéines", i++);
-			applySort("Protéines brutes (N x 6.25)", i++);
-			applySort("Eau", i++);
-			applySort("Sodium", i++);
-			applySort("Sel", i++);
-			applySort("Magnésium", i++);
-			applySort("Phosphore", i++);
-			applySort("Potassium", i++);
-			applySort("Calcium", i++);
-			applySort("Manganèse", i++);
-			applySort("Fer total", i++);
-			applySort("Cuivre", i++);
-			applySort("Zinc", i++);
-			applySort("Sélénium", i++);
-			applySort("Rétinol", i++);
-			applySort("Bêta-carotène", i++);
-			applySort("Vitamine A", i++);
-			applySort("Vitamine D", i++);
-			applySort("Activité vitaminique E (en équivalents alpha-tocophérol)", i++);
-			applySort("Vitamine C totale", i++);
-			applySort("Vitamine B1 ou Thiamine", i++);
-			applySort("Vitamine B2 ou Riboflavine", i++);
-			applySort("Vitamine B3 ou PP ou Niacine", i++);
-			applySort("Vitamine B5 ou Acide pantothénique", i++);
-			applySort("Vitamine B6 ou Pyridoxine", i++);
-			applySort("Vitamine B12 ou Cobalamines", i++);
-			applySort("Vitamine B9 ou Folates totaux", i++);
-			applySort("Alcool (éthanol)", i++);
-			applySort("Acides organiques", i++);
-			applySort("Cholestérol", i++);
-			applySort("AG 4:0, butyrique", i++);
-			applySort("AG 6:0, caproïque", i++);
-			applySort("AG 8:0, caprylique", i++);
-			applySort("AG 10:0, caprique", i++);
-			applySort("AG 12:0, laurique", i++);
-			applySort("AG 14:0, myristique", i++);
-			applySort("AG 16:0, palmitique", i++);
-			applySort("AG 18:0, stéarique", i++);
-			applySort("AG 18:1 9c (n-9), oléique", i++);
-			applySort("AG 18:2 9c,12c (n-6), linoléique", i++);
-			applySort("AG 18:3 c9,c12,c15 (n-3), alpha-linolénique", i++);
-			applySort("AG 20:4 5c,8c,11c,14c (n-6), arachidonique", i++);
-			applySort("AG 20:5 5c,8c,11c,14c,17c (n-3), EPA", i++);
-			applySort("AG 22:6 4c,7c,10c,13c,16c,19c (n-3), DHA", i++);
-
-		} finally {
-			policyBehaviourFilter.enableBehaviour(BeCPGModel.ASPECT_DEPTH_LEVEL);
 		}
+
+		i = 1;
+		updateParents();
+		applySort("Energie kJ, étiquetage", i++);
+		applySort("Energie kcal, étiquetage", i++);
+		applySort("Energie kJ", i++);
+		applySort("Energie kcal", i++);
+		applySort("Lipides", i++);
+		applySort("AG saturés", i++);
+		applySort("AG monoinsaturés", i++);
+		applySort("AG polyinsaturés", i++);
+		applySort("Glucides", i++);
+		applySort("Sucres", i++);
+		applySort("Amidon", i++);
+		applySort("Polyols totaux", i++);
+		applySort("Fibres alimentaires", i++);
+		applySort("Iode", i++);
+		applySort("Protéines", i++);
+		applySort("Protéines brutes (N x 6.25)", i++);
+		applySort("Eau", i++);
+		applySort("Sodium", i++);
+		applySort("Sel", i++);
+		applySort("Magnésium", i++);
+		applySort("Phosphore", i++);
+		applySort("Potassium", i++);
+		applySort("Calcium", i++);
+		applySort("Manganèse", i++);
+		applySort("Fer total", i++);
+		applySort("Cuivre", i++);
+		applySort("Zinc", i++);
+		applySort("Sélénium", i++);
+		applySort("Rétinol", i++);
+		applySort("Bêta-carotène", i++);
+		applySort("Vitamine A", i++);
+		applySort("Vitamine D", i++);
+		applySort("Activité vitaminique E (en équivalents alpha-tocophérol)", i++);
+		applySort("Vitamine C totale", i++);
+		applySort("Vitamine B1 ou Thiamine", i++);
+		applySort("Vitamine B2 ou Riboflavine", i++);
+		applySort("Vitamine B3 ou PP ou Niacine", i++);
+		applySort("Vitamine B5 ou Acide pantothénique", i++);
+		applySort("Vitamine B6 ou Pyridoxine", i++);
+		applySort("Vitamine B12 ou Cobalamines", i++);
+		applySort("Vitamine B9 ou Folates totaux", i++);
+		applySort("Alcool (éthanol)", i++);
+		applySort("Acides organiques", i++);
+		applySort("Cholestérol", i++);
+		applySort("AG 4:0, butyrique", i++);
+		applySort("AG 6:0, caproïque", i++);
+		applySort("AG 8:0, caprylique", i++);
+		applySort("AG 10:0, caprique", i++);
+		applySort("AG 12:0, laurique", i++);
+		applySort("AG 14:0, myristique", i++);
+		applySort("AG 16:0, palmitique", i++);
+		applySort("AG 18:0, stéarique", i++);
+		applySort("AG 18:1 9c (n-9), oléique", i++);
+		applySort("AG 18:2 9c,12c (n-6), linoléique", i++);
+		applySort("AG 18:3 c9,c12,c15 (n-3), alpha-linolénique", i++);
+		applySort("AG 20:4 5c,8c,11c,14c (n-6), arachidonique", i++);
+		applySort("AG 20:5 5c,8c,11c,14c,17c (n-3), EPA", i++);
+		applySort("AG 22:6 4c,7c,10c,13c,16c,19c (n-3), DHA", i++);
 
 		return I18NUtil.getMessage(MSG_SUCCESS);
 	}
@@ -170,14 +175,23 @@ public class NutParentLevelPatch extends AbstractBeCPGPatch {
 
 				@Override
 				public Void execute() throws Throwable {
-					NodeRef parent = firstOrNull(beCPGSearchService.luceneSearch("+TYPE:\"bcpg:nut\" AND +@cm\\:name:\"" + nutName + "\" "));
-					if (parent != null) {
-						List<NodeRef> parents = associationService.getSourcesAssocs(parent, BeCPGModel.ASSOC_NUTLIST_NUT);
-						for (NodeRef item : parents) {
-							nodeService.setProperty(item, BeCPGModel.PROP_SORT, sort);
+					policyBehaviourFilter.disableBehaviour(BeCPGModel.ASPECT_DEPTH_LEVEL);
+					policyBehaviourFilter.disableBehaviour(BeCPGModel.ASPECT_SORTABLE_LIST);
+
+					try {
+
+						NodeRef parent = firstOrNull(beCPGSearchService.luceneSearch("+TYPE:\"bcpg:nut\" AND +@cm\\:name:\"" + nutName + "\" "));
+						if (parent != null) {
+							List<NodeRef> parents = associationService.getSourcesAssocs(parent, BeCPGModel.ASSOC_NUTLIST_NUT);
+							for (NodeRef item : parents) {
+								nodeService.setProperty(item, BeCPGModel.PROP_SORT, sort);
+							}
 						}
+						return null;
+					} finally {
+						policyBehaviourFilter.enableBehaviour(BeCPGModel.ASPECT_DEPTH_LEVEL);
+						policyBehaviourFilter.enableBehaviour(BeCPGModel.ASPECT_SORTABLE_LIST);
 					}
-					return null;
 				}
 
 			}, false, true);
@@ -218,17 +232,24 @@ public class NutParentLevelPatch extends AbstractBeCPGPatch {
 
 							@Override
 							public Void execute() throws Throwable {
+								policyBehaviourFilter.disableBehaviour(BeCPGModel.ASPECT_DEPTH_LEVEL);
+								policyBehaviourFilter.disableBehaviour(BeCPGModel.ASPECT_SORTABLE_LIST);
 
-								for (NodeRef check : parents) {
-									for (NodeRef item : subList) {
-										if (nodeService.getPrimaryParent(item).getParentRef().equals((nodeService.getPrimaryParent(check)).getParentRef())) {
-											logger.info("Updating parent for nut" + nutName + " " + item + " with " + check);
-											nodeService.setProperty(item, BeCPGModel.PROP_PARENT_LEVEL, check);
-											nodeService.setProperty(item, BeCPGModel.PROP_DEPTH_LEVEL, 2);
+								try {
+									for (NodeRef check : parents) {
+										for (NodeRef item : subList) {
+											if (nodeService.getPrimaryParent(item).getParentRef().equals((nodeService.getPrimaryParent(check)).getParentRef())) {
+												logger.info("Updating parent for nut" + nutName + " " + item + " with " + check);
+												nodeService.setProperty(item, BeCPGModel.PROP_PARENT_LEVEL, check);
+												nodeService.setProperty(item, BeCPGModel.PROP_DEPTH_LEVEL, 2);
+											}
 										}
 									}
+									return null;
+								} finally {
+									policyBehaviourFilter.enableBehaviour(BeCPGModel.ASPECT_DEPTH_LEVEL);
+									policyBehaviourFilter.enableBehaviour(BeCPGModel.ASPECT_SORTABLE_LIST);
 								}
-								return null;
 							}
 
 						}, false, true);
