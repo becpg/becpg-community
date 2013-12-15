a73e0a73a2a55f57d141ec202ea4b1e1368897f3
Philippe QUERE <philippe.quere@becpg.fr>
2013-12-15 18:33:05 +0100

    Sort ReqCtrlList by type

--- becpg-core/src/main/java/fr/becpg/repo/product/formulation/MergeReqCtrlFormulationHandler.java
+++ becpg-core/src/main/java/fr/becpg/repo/product/formulation/MergeReqCtrlFormulationHandler.java
@@ -1,5 +1,7 @@
 package fr.becpg.repo.product.formulation;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +14,13 @@ import fr.becpg.repo.formulation.FormulateException;
 import fr.becpg.repo.formulation.FormulationBaseHandler;
 import fr.becpg.repo.product.data.ProductData;
 import fr.becpg.repo.product.data.productList.ReqCtrlListDataItem;
+import fr.becpg.repo.product.data.productList.RequirementType;
 
+/**
+ * Merge ReqCtrlListDataItem to avoid duplication of items and sort them
+ * @author quere
+ *
+ */
 @Service
 public class MergeReqCtrlFormulationHandler extends FormulationBaseHandler<ProductData> {
 
@@ -54,6 +62,59 @@ public class MergeReqCtrlFormulationHandler extends FormulationBaseHandler<Produ
 				dbKV.getValue().setSources(newReqCtrlListDataItem.getSources());
 				reqCtrlList.remove(newReqCtrlListDataItem);		
 			}
+		}
+		
+		//sort
+		sort(reqCtrlList);
+	}
+	
+	/**
+	 * Sort by type
+	 *
+	 */
+	private void sort(List<ReqCtrlListDataItem> reqCtrlList){
+		
+		Collections.sort(reqCtrlList, new Comparator<ReqCtrlListDataItem>(){
+       	
+		final int BEFORE = -1;
+   	    final int EQUAL = 0;
+   	    final int AFTER = 1;	
+			
+			@Override
+			public int compare(ReqCtrlListDataItem r1, ReqCtrlListDataItem r2) {
+				
+				if (r1.getReqType() != null && r1.getReqType() != null) {
+					if (r1.getReqType().equals(r2.getReqType())) {
+						return EQUAL;
+					}
+					else if(r1.getReqType().equals(RequirementType.Forbidden)){
+						return BEFORE;
+					}
+					else if(r2.getReqType().equals(RequirementType.Forbidden)){
+						return AFTER;
+					}
+					else if(r1.getReqType().equals(RequirementType.Tolerated)){
+						return BEFORE;
+					}
+					else{
+						return AFTER;
+					}
+				}
+				else if(r1.getReqType() != null){
+					return BEFORE;
+				}
+				else if(r2.getReqType() != null){
+					return AFTER;
+				}
+				
+				return EQUAL;
+			}
+       });  
+		
+		int i = 0;
+		for(ReqCtrlListDataItem r : reqCtrlList){
+			r.setSort(i);
+			i++;
 		}
 	}
 }
