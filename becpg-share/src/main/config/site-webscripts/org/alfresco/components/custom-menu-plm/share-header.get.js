50b2f0f67da97ee2f83f64cd3060ed3a3cd14ee4
Laborie Matthieu <matthieu.laborie@becpg.fr>
2014-01-10 16:09:32 +0100

    Patch migration 1.6

--- becpg-share/src/main/config/site-webscripts/org/alfresco/components/custom-menu-plm/share-header.get.js
+++ becpg-share/src/main/config/site-webscripts/org/alfresco/components/custom-menu-plm/share-header.get.js
@@ -2,13 +2,63 @@
 
 var beCPGMenu = getOrCreateBeCPGMenu();
 
+
 if (beCPGMenu != null) {
+   
+      var  nodeRef = page.url.args.nodeRef, dockbarUrl = "/becpg/dockbar";
+      if (nodeRef !== null && nodeRef.length > 0) {
+         dockbarUrl += "?entityNodeRef=" + nodeRef;
+      }
+      
+      var result = remote.call(dockbarUrl);
+      if (result.status.code == status.STATUS_OK)
+      {
+         results = eval('(' + result + ')');
+
+         beCPGMenu.config.widgets.push( {
+            name : "alfresco/menus/AlfMenuGroup",
+            config : {
+               label : "header.becpg.recents",
+            }
+         } );
+         
+         for (var i=0; i < results.items.length; i++){
+            var item = results.items[i];
+            
+            var targetUrl = "entity-details?nodeRef=" +item.nodeRef ;
+            
+            if(item.site){
+               targetUrl = "site/" + item.site.shortName+"/"+targetUrl ;
+            }
+            
+            beCPGMenu.config.widgets.push( {
+               name : "alfresco/header/AlfMenuItem",
+               config : {
+                  label : item.displayName,
+                  iconClass : item.itemType.split(":")[1],
+                  targetUrl : targetUrl
+               }
+            });
+            
+         }
+                  
+      }
+
+      beCPGMenu.config.widgets.push( {
+         name : "alfresco/menus/AlfMenuGroup",
+         config : {
+            label : "header.becpg.tools",
+         }
+      } );   
+      
 
    beCPGMenu.config.widgets.push( {
       name : "alfresco/header/AlfMenuItem",
       config : {
          label : "header.project-list.label",
+         iconClass : "project-list-header",
          targetUrl : "project-list#filter=projects|InProgress"
+            
       }
    });
    
@@ -16,16 +66,30 @@ if (beCPGMenu != null) {
       name : "alfresco/header/AlfMenuItem",
       config : {
          label : "header.nc-list.label",
+         iconClass : "nc-list-header",
          targetUrl : "nc-list#filter=ncs|Claim"
       }
    });
    
-   beCPGMenu.config.widgets.push({
-      name : "alfresco/header/AlfMenuItem",
-      config : {
-         label : "header.becpg-admin.label",
-         targetUrl : "console/admin-console/becpg-admin"
-      }
-   });
+   if (user.isAdmin)
+   {
+   
+      beCPGMenu.config.widgets.push( {
+         name : "alfresco/menus/AlfMenuGroup",
+         config : {
+            label : "header.becpg.admin",
+         }
+      } );   
+      
+      beCPGMenu.config.widgets.push({
+         name : "alfresco/header/AlfMenuItem",
+         config : {
+            label : "header.becpg-admin.label",
+            iconClass : "becpg-admin-header",
+            targetUrl : "console/admin-console/becpg-admin"
+         }
+      });
+   
+   }
 
 }
