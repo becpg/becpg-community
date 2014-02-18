5ff0ae3d34317671f9e10a7426d0bbd0cb0e775b
Laborie Matthieu <matthieu.laborie@becpg.fr>
2014-02-18 16:07:44 +0100

    Create enterprise version

---
+++ becpg-tools/src/main/java/fr/becpg/tools/http/GetEntityCommand.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (C) 2010-2014 beCPG. 
+ *  
+ * This file is part of beCPG 
+ *  
+ * beCPG is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ *  
+ * beCPG is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public License along with beCPG. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package fr.becpg.tools.http;
+
+
+
+public class GetEntityCommand  extends AbstractHttpCommand {
+
+
+	private static String COMMAND_URL_TEMPLATE = "/becpg/remote/entity?nodeRef=%s";
+
+	
+	public GetEntityCommand(String serverUrl) {
+		super(serverUrl);
+	}
+
+	@Override
+	public String getHttpUrl(Object... params) {
+		return getServerUrl() + String.format(COMMAND_URL_TEMPLATE, encodeParams(params) );
+	}
+
+
+	
+}
---
+++ becpg-tools/src/main/java/fr/becpg/tools/jdbc/JdbcUtils.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (C) 2010-2014 beCPG. 
+ *  
+ * This file is part of beCPG 
+ *  
+ * beCPG is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ *  
+ * beCPG is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public License along with beCPG. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package fr.becpg.tools.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcUtils {
+	public static Long update(Connection connection, String sql, Object[] objects) throws SQLException {
+
+		try (PreparedStatement pst = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
+
+			for (int i = 0; i < objects.length; i++) {
+				pst.setObject(i + 1, objects[i]);
+			}
+			pst.executeUpdate();
+			try (ResultSet rs = pst.getGeneratedKeys();) {
+				if (rs != null && rs.next()) {
+					return rs.getLong(1);
+				}
+			}
+		}
+
+	return -1L;
+}
+}
