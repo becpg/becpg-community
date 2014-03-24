b365c772162dbe037684df4140eeb7cb3c473b68
Philippe QUERE <philippe.quere@becpg.fr>
2014-03-24 18:45:05 +0100

    #896 : unit tests

--- becpg-core/src/test/java/fr/becpg/test/InitializationTest.java
+++ becpg-core/src/test/java/fr/becpg/test/InitializationTest.java
@@ -1,7 +1,18 @@
 package fr.becpg.test;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.alfresco.model.ContentModel;
 import org.alfresco.repo.dictionary.DictionaryDAO;
 import org.alfresco.repo.domain.qname.QNameDAO;
+import org.alfresco.repo.model.Repository;
+import org.alfresco.repo.transaction.RetryingTransactionHelper.RetryingTransactionCallback;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.namespace.NamespaceService;
+import org.alfresco.service.namespace.QName;
+import org.alfresco.service.transaction.TransactionService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,11 +32,44 @@ public class InitializationTest {
 	@Autowired
 	DictionaryDAO dictionaryDAO;
 	
+	@Autowired
+	Repository repositoryHelper;
+	
+	@Autowired
+	NodeService nodeService;
+	
+	@Autowired
+	TransactionService transactionService;
+	
 	@Test
-	public void testInit(){
+	public void testInitWithoutCreateNode(){
 		
 		dictionaryDAO.reset();
-		//Patch for H2 ????
+		
+		//Doesn't work
+		Assert.assertNotNull(qNameDAO.getQName(BeCPGModel.PROP_SORT));
+	}
+	
+	@Test
+	public void testInitWithCreateNode(){
+		
+		dictionaryDAO.reset();
+		
+		transactionService.getRetryingTransactionHelper().doInTransaction(new RetryingTransactionCallback<Boolean>() {
+			public Boolean execute() throws Throwable {
+
+				Map<QName, Serializable> properties = new HashMap<QName, Serializable>();
+				properties.put(ContentModel.PROP_NAME, "name");
+				properties.put(BeCPGModel.PROP_SORT, 1);
+				nodeService.createNode(repositoryHelper.getCompanyHome(), ContentModel.ASSOC_CONTAINS,
+						QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, (String) properties.get(ContentModel.PROP_NAME)),
+						ContentModel.TYPE_CONTENT, properties);
+				return false;
+
+			}
+		}, false, true);
+		
+		//Works
 		Assert.assertNotNull(qNameDAO.getQName(BeCPGModel.PROP_SORT));
 	}
 	
