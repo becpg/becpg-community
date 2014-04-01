dd347a2b45e19f2cd3d6db7e163dc7a24d6eb1a9
Philippe QUERE <philippe.quere@becpg.fr>
2014-04-01 09:43:57 +0200

    unit tests

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/policy/ProductPolicy.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/policy/ProductPolicy.java
@@ -1,16 +1,8 @@
 package fr.becpg.repo.product.policy;
 
-import java.io.Serializable;
-import java.util.Map;
-
-import org.alfresco.repo.copy.CopyBehaviourCallback;
-import org.alfresco.repo.copy.CopyDetails;
-import org.alfresco.repo.copy.CopyServicePolicies;
-import org.alfresco.repo.copy.DefaultCopyBehaviourCallback;
-import org.alfresco.repo.node.NodeServicePolicies;
+import org.alfresco.repo.coci.CheckOutCheckInServicePolicies;
 import org.alfresco.repo.policy.JavaBehaviour;
-import org.alfresco.service.cmr.repository.ChildAssociationRef;
-import org.alfresco.service.namespace.QName;
+import org.alfresco.service.cmr.repository.NodeRef;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -26,24 +18,24 @@ import fr.becpg.repo.policy.AbstractBeCPGPolicy;
  * 
  */
 public class ProductPolicy extends AbstractBeCPGPolicy implements 
-			NodeServicePolicies.OnCreateNodePolicy{
+		CheckOutCheckInServicePolicies.OnCheckOut{
 
 	private static Log logger = LogFactory.getLog(ProductPolicy.class);
 
 	@Override
 	public void doInit() {
 		logger.debug("Init ProductPolicy...");
-		policyComponent.bindClassBehaviour(NodeServicePolicies.OnCreateNodePolicy.QNAME, PLMModel.TYPE_PRODUCT,
-				new JavaBehaviour(this, "onCreateNode"));
+		policyComponent.bindClassBehaviour(CheckOutCheckInServicePolicies.OnCheckOut.QNAME, PLMModel.TYPE_PRODUCT,
+				new JavaBehaviour(this, "onCheckOut"));
 	}
 
 	@Override
-	public void onCreateNode(ChildAssociationRef childAssocRef) {
+	public void onCheckOut(NodeRef workingCopyNodeRef) {
+		nodeService.setProperty(workingCopyNodeRef, PLMModel.PROP_PRODUCT_STATE, SystemState.Simulation);
 		
-		nodeService.setProperty(childAssocRef.getChildRef(), PLMModel.PROP_PRODUCT_STATE, SystemState.Simulation);
-		
-		if (nodeService.hasAspect(childAssocRef.getChildRef(), PLMWorkflowModel.ASPECT_PRODUCT_VALIDATION_ASPECT)) {
-			nodeService.removeAspect(childAssocRef.getChildRef(), PLMWorkflowModel.ASPECT_PRODUCT_VALIDATION_ASPECT);
-		}
-	}		
+		if (nodeService.hasAspect(workingCopyNodeRef, PLMWorkflowModel.ASPECT_PRODUCT_VALIDATION_ASPECT)) {
+			nodeService.removeAspect(workingCopyNodeRef, PLMWorkflowModel.ASPECT_PRODUCT_VALIDATION_ASPECT);
+		}		
+	}
+	
 }
