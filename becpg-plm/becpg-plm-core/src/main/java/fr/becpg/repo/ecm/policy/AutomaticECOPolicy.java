aec425989d750cd2440e50d8611df60734a5bbc3
Laborie Matthieu <matthieu.laborie@becpg.fr>
2014-05-15 18:18:13 +0200

    Bugfix

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/ecm/policy/AutomaticECOPolicy.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/ecm/policy/AutomaticECOPolicy.java
@@ -1,10 +1,14 @@
 package fr.becpg.repo.ecm.policy;
 
+import java.io.Serializable;
+import java.util.Map;
 import java.util.Set;
 
+import org.alfresco.model.ContentModel;
 import org.alfresco.repo.node.NodeServicePolicies;
 import org.alfresco.repo.policy.JavaBehaviour;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.QName;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -15,16 +19,13 @@ import fr.becpg.repo.repository.L2CacheSupport;
 
 /**
  * 
- * @author matthieu
- * Add automatic change order
+ * @author matthieu Add automatic change order
  */
-public class AutomaticECOPolicy extends AbstractBeCPGPolicy implements  NodeServicePolicies.OnUpdateNodePolicy {
+public class AutomaticECOPolicy extends AbstractBeCPGPolicy implements NodeServicePolicies.OnUpdatePropertiesPolicy {
 
 	private AutomaticECOService automaticECOService;
-	
+
 	private static Log logger = LogFactory.getLog(AutomaticECOPolicy.class);
-	
-	
 
 	public void setAutomaticECOService(AutomaticECOService automaticECOService) {
 		this.automaticECOService = automaticECOService;
@@ -32,27 +33,31 @@ public class AutomaticECOPolicy extends AbstractBeCPGPolicy implements  NodeServ
 
 	@Override
 	public void doInit() {
-		policyComponent.bindClassBehaviour(NodeServicePolicies.OnUpdateNodePolicy.QNAME, PLMModel.TYPE_PRODUCT, new JavaBehaviour(this,	"onUpdateNode"));
-	}
-	
-	@Override
-	public void onUpdateNode(NodeRef nodeRef) {
-		if(L2CacheSupport.isThreadLockEnable()){
-			logger.debug("Entity is locked by ECM :"+nodeRef);
-			return;
-		}
-		queueNode(nodeRef);
+		policyComponent.bindClassBehaviour(NodeServicePolicies.OnUpdatePropertiesPolicy.QNAME, PLMModel.TYPE_PRODUCT, new JavaBehaviour(this,
+				"onUpdateProperties"));
 	}
 
-	
+	@Override
+	public void onUpdateProperties(NodeRef nodeRef, Map<QName, Serializable> before, Map<QName, Serializable> after) {
+		if (before.containsKey(ContentModel.PROP_MODIFIED) && after.containsKey(ContentModel.PROP_MODIFIED)
+				&& !before.get(ContentModel.PROP_MODIFIED).equals(after.get(ContentModel.PROP_MODIFIED))) {
+
+			if (L2CacheSupport.isThreadLockEnable()) {
+				logger.debug("Entity is locked by ECM :" + nodeRef);
+				return;
+			}
+			queueNode(nodeRef);
+		}
+
+	}
+
 	@Override
 	protected void doBeforeCommit(String key, Set<NodeRef> pendingNodes) {
 		for (NodeRef nodeRef : pendingNodes) {
-			if (isNotLocked(nodeRef) && !isWorkingCopyOrVersion(nodeRef) && !isBeCPGVersion(nodeRef) ) {
+			if (isNotLocked(nodeRef) && !isWorkingCopyOrVersion(nodeRef) && !isBeCPGVersion(nodeRef)) {
 				automaticECOService.addAutomaticChangeEntry(nodeRef);
 			}
-		}		
+		}
 	}
-	
 
 }
