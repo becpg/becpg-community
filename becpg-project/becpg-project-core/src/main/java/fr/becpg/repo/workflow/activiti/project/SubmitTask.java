63cfce8f09a26a46f482cc2730986e4d4ce9815e
Laborie Matthieu <matthieu.laborie@becpg.fr>
2014-10-16 17:12:50 +0200

    refs #1237

--- becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/workflow/activiti/project/SubmitTask.java
+++ becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/workflow/activiti/project/SubmitTask.java
@@ -1,6 +1,7 @@
 package fr.becpg.repo.workflow.activiti.project;
 
 import org.activiti.engine.delegate.DelegateTask;
+import org.alfresco.repo.forum.CommentService;
 import org.alfresco.repo.workflow.activiti.ActivitiScriptNode;
 import org.alfresco.repo.workflow.activiti.tasklistener.ScriptTaskListener;
 import org.alfresco.service.cmr.repository.NodeRef;
@@ -27,24 +28,27 @@ public class SubmitTask extends ScriptTaskListener {
 	
 	private ProjectService projectService;
 	
-	public void setNodeService(NodeService nodeService) {
-		this.nodeService = nodeService;
-	}
-
-	public void setProjectService(ProjectService projectService) {
-		this.projectService = projectService;
-	}
+	private CommentService commentService;
+	
 
 	@Override
 	public void notify(final DelegateTask task) {
 
 		nodeService = getServiceRegistry().getNodeService();		
 		projectService = (ProjectService)ApplicationContextHelper.getApplicationContext().getBean("projectService");
+		commentService =  (CommentService)ApplicationContextHelper.getApplicationContext().getBean("commentService");
 		
 		ActivitiScriptNode taskNode = (ActivitiScriptNode) task.getVariable("pjt_workflowTask");
 		if (taskNode != null) {
+			
 			logger.debug("taskNode exist " + taskNode.getNodeRef());
 			NodeRef taskNodeRef = taskNode.getNodeRef();
+			
+			String taskComment = (String) task.getVariable("bpm_comment");
+			if(taskComment!=null && !taskComment.isEmpty()){
+				commentService.createComment(taskNodeRef, "", taskComment, false);
+			}
+			
 			if(nodeService.exists(taskNodeRef)){	            	
             	projectService.submitTask(taskNodeRef);
 			}
