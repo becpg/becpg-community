bd4065c61f2cfd0aa0d7c39bf3397bd046d4a213
Laborie Matthieu <matthieu.laborie@becpg.fr>
2015-02-23 17:17:05 +0100

    Revert project policy

--- becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/policy/ProjectPolicy.java
+++ becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/policy/ProjectPolicy.java
@@ -88,16 +88,12 @@ public class ProjectPolicy extends AbstractBeCPGPolicy implements
 			
 			if (afterState.equals(ProjectState.InProgress.toString())) {
 				logger.debug("onUpdateProperties:start project");
-				
-				if(nodeService.getProperty(nodeRef, ProjectModel.PROP_PROJECT_START_DATE) == null){
-				
 					Date startDate = ProjectHelper.removeTime(new Date());
 					nodeService.setProperty(nodeRef, ProjectModel.PROP_PROJECT_START_DATE, startDate);
 					ProjectData projectData = alfrescoRepository.findOne(nodeRef);
 					for(TaskListDataItem taskListDataItem : ProjectHelper.getNextTasks(projectData, null)){
 						nodeService.setProperty(taskListDataItem.getNodeRef(), ProjectModel.PROP_TL_START, startDate);
 					}
-				}
 				formulateProject = true;
 			} else if (afterState.equals(ProjectState.Cancelled.toString())) {
 				logger.debug("onUpdateProperties:cancel project");
