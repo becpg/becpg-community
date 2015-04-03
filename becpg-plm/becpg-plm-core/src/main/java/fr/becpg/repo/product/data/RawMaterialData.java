460922bfc8ba4bfd2825815a1b264de9730bbfe4
Laborie Matthieu <matthieu.laborie@becpg.fr>
2015-04-03 12:33:49 +0200

    Supplier plants

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/data/RawMaterialData.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/data/RawMaterialData.java
@@ -17,6 +17,8 @@ import fr.becpg.repo.repository.annotation.AlfType;
 public class RawMaterialData extends ProductData {
 
 	private List<NodeRef> suppliers = new ArrayList<NodeRef>();	
+	
+	private List<NodeRef> supplierPlants = new ArrayList<NodeRef>();	
 
 	@AlfMultiAssoc
 	@AlfQname(qname="bcpg:suppliers")
@@ -28,10 +30,21 @@ public class RawMaterialData extends ProductData {
 		this.suppliers = suppliers;
 	}
 
+	@AlfMultiAssoc
+	@AlfQname(qname="bcpg:supplierPlants")
+	public List<NodeRef> getSupplierPlants() {
+		return supplierPlants;
+	}
+
+	public void setSupplierPlants(List<NodeRef> supplierPlants) {
+		this.supplierPlants = supplierPlants;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
+		result = prime * result + ((supplierPlants == null) ? 0 : supplierPlants.hashCode());
 		result = prime * result + ((suppliers == null) ? 0 : suppliers.hashCode());
 		return result;
 	}
@@ -45,6 +58,11 @@ public class RawMaterialData extends ProductData {
 		if (getClass() != obj.getClass())
 			return false;
 		RawMaterialData other = (RawMaterialData) obj;
+		if (supplierPlants == null) {
+			if (other.supplierPlants != null)
+				return false;
+		} else if (!supplierPlants.equals(other.supplierPlants))
+			return false;
 		if (suppliers == null) {
 			if (other.suppliers != null)
 				return false;
