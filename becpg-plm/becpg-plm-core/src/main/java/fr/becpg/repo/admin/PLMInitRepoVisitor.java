958790559217d290baeab397a2a9a622eb128d3c
sername=mounia azibi <sername=mounia.azibi@becpg.fr>
2015-04-13 09:34:26 +0200

    Add standard packaging aspect

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/admin/PLMInitRepoVisitor.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/admin/PLMInitRepoVisitor.java
@@ -467,6 +467,10 @@ public class PLMInitRepoVisitor extends AbstractInitVisitorImpl {
 		entityLists.put(PlmRepoConsts.PATH_LABELCLAIMS_TYPES, BeCPGModel.TYPE_LIST_VALUE);
 		entityLists.put(PlmRepoConsts.PATH_MICROBIO_CONTROL_STEPS, BeCPGModel.TYPE_LIST_VALUE);
 		entityLists.put(PlmRepoConsts.PATH_RESOURCE_PARAM_TYPES, BeCPGModel.TYPE_LIST_VALUE);
+		
+		entityLists.put(PlmRepoConsts.PATH_PM_MATERIALS, BeCPGModel.TYPE_LIST_VALUE);
+		entityLists.put(PlmRepoConsts.PATH_PM_PRINT_TYPES, BeCPGModel.TYPE_LIST_VALUE);
+		entityLists.put(PlmRepoConsts.PATH_PM_PRINT_VANISHS, BeCPGModel.TYPE_LIST_VALUE);
 
 		return entitySystemService.createSystemEntity(parentNodeRef, path, entityLists);
 	}
