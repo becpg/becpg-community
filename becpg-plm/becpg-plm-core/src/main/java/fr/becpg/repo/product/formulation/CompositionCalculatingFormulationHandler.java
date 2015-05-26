92c546c7fb0f951bbc049a1549a4b96d94932ebd
Philippe QUERE <philippe.quere@becpg.fr>
2015-05-26 17:27:08 +0200

    #1560

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/CompositionCalculatingFormulationHandler.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/CompositionCalculatingFormulationHandler.java
@@ -34,7 +34,6 @@ import fr.becpg.repo.product.data.EffectiveFilters;
 import fr.becpg.repo.product.data.ProductData;
 import fr.becpg.repo.product.data.RawMaterialData;
 import fr.becpg.repo.product.data.constraints.CompoListUnit;
-import fr.becpg.repo.product.data.constraints.ProductUnit;
 import fr.becpg.repo.product.data.productList.CompoListDataItem;
 import fr.becpg.repo.repository.AlfrescoRepository;
 import fr.becpg.repo.variant.filters.VariantFilters;
@@ -66,22 +65,11 @@ public class CompositionCalculatingFormulationHandler extends FormulationBaseHan
 			logger.debug("no compo => no formulation");
 			return true;
 		}
-
-		// Take in account net weight
-		if (formulatedProduct.getQty() != null && formulatedProduct.getUnit() != null) {
-			if (ProductUnit.g.equals(formulatedProduct.getUnit())) {
-				formulatedProduct.setNetWeight(formulatedProduct.getQty() / 1000);
-			} else if (ProductUnit.kg.equals(formulatedProduct.getUnit())) {
-				formulatedProduct.setNetWeight(formulatedProduct.getQty());
-			}
-		}
+		
 		Double netWeight = formulatedProduct.getNetWeight();
 		Composite<CompoListDataItem> compositeAll = CompositeHelper.getHierarchicalCompoList(formulatedProduct.getCompoList(EffectiveFilters.ALL));
 		Composite<CompoListDataItem> compositeDefaultVariant = CompositeHelper.getHierarchicalCompoList(formulatedProduct.getCompoList(
 				EffectiveFilters.ALL, VariantFilters.DEFAULT_VARIANT));
-
-		// calculate on every item
-		visitQtyChildren(formulatedProduct, netWeight, compositeAll);
 
 		// Yield
 		visitYieldChildren(formulatedProduct, compositeDefaultVariant);
@@ -115,62 +103,6 @@ public class CompositionCalculatingFormulationHandler extends FormulationBaseHan
 		}
 
 		return true;
-	}
-
-	private void visitQtyChildren(ProductData formulatedProduct, Double parentQty, Composite<CompoListDataItem> composite) throws FormulateException {
-
-		for (Composite<CompoListDataItem> component : composite.getChildren()) {
-
-			Double qtyInKg = calculateQtyInKg(component.getData());
-			logger.debug("qtySubFormula: " + qtyInKg + " parentQty: " + parentQty);
-			if (qtyInKg != null) {
-
-				// take in account percentage
-				if (CompoListUnit.Perc.equals(component.getData().getCompoListUnit()) && parentQty != null && !parentQty.equals(0d)) {
-					qtyInKg = qtyInKg * parentQty / 100;
-				}
-
-				// Take in account yield that is defined on component
-				Double qty = null;
-				if (component.isLeaf()) {
-					qty = qtyInKg * 100 / FormulationHelper.getYield(component.getData());
-				} else {
-					qty = qtyInKg;
-				}
-				component.getData().setQty(qty);
-			}
-
-			// calculate volume ?
-			Double volume = FormulationHelper.getNetVolume(component.getData(), nodeService);
-			component.getData().setVolume(volume);
-
-			// calculate children
-			if (!component.isLeaf()) {
-
-				// take in account percentage
-				if (CompoListUnit.Perc.equals(component.getData().getCompoListUnit()) && parentQty != null && !parentQty.equals(0d)) {
-
-					visitQtyChildren(formulatedProduct, parentQty, component);
-
-					// no yield but calculate % of composite
-					Double compositePerc = 0d;
-					boolean isUnitPerc = true;
-					for (Composite<CompoListDataItem> child : component.getChildren()) {
-						compositePerc += child.getData().getQtySubFormula();
-						isUnitPerc = isUnitPerc && CompoListUnit.Perc.equals(child.getData().getCompoListUnit());
-						if (!isUnitPerc) {
-							break;
-						}
-					}
-					if (isUnitPerc) {
-						component.getData().setQtySubFormula(compositePerc);
-						component.getData().setQty(compositePerc * parentQty / 100);
-					}
-				} else {
-					visitQtyChildren(formulatedProduct, component.getData().getQty(), component);
-				}
-			}
-		}
 	}
 
 	private void visitYieldChildren(ProductData formulatedProduct, Composite<CompoListDataItem> composite) throws FormulateException {
@@ -259,10 +191,10 @@ public class CompositionCalculatingFormulationHandler extends FormulationBaseHan
 				component.getData().setVolume(value);
 
 			} else {
-				value = (Double) component.getData().getVolume();
-				value = value != null ? value : 0d;
+				value = FormulationHelper.getNetVolume(component.getData(), nodeService);
+				component.getData().setVolume(value);
 			}
-			volume += value;
+			volume += (value != null ? value : 0d);
 		}
 		return volume;
 	}
