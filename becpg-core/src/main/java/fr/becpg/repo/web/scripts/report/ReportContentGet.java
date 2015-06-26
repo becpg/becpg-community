6b01d2d4158b8e88e6081dd152f33312bcc0f253
Laborie Matthieu <matthieu.laborie@becpg.fr>
2015-06-26 08:37:35 +0200

    fix #1613

--- becpg-core/src/main/java/fr/becpg/repo/web/scripts/report/ReportContentGet.java
+++ becpg-core/src/main/java/fr/becpg/repo/web/scripts/report/ReportContentGet.java
@@ -19,11 +19,15 @@
 package fr.becpg.repo.web.scripts.report;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.alfresco.model.ContentModel;
 import org.alfresco.repo.web.scripts.content.ContentGet;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.NamespaceService;
+import org.alfresco.service.namespace.QName;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.extensions.webscripts.WebScriptException;
@@ -43,8 +47,18 @@ public class ReportContentGet extends ContentGet {
 	private static final Log logger = LogFactory.getLog(ReportContentGet.class);
 
 	private static final String PARAM_ENTITY_NODEREF = "entityNodeRef";
+	protected static final String PARAM_STORE_TYPE = "store_type";
+	protected static final String PARAM_STORE_ID = "store_id";
+	protected static final String PARAM_ID = "id";
+	protected static final String PARAM_PROPERTY = "property";
 
 	private EntityReportService entityReportService;
+
+	private NamespaceService namespaceService;
+
+	public void setNamespaceService(NamespaceService namespaceService) {
+		this.namespaceService = namespaceService;
+	}
 
 	public void setEntityReportService(EntityReportService entityReportService) {
 		this.entityReportService = entityReportService;
@@ -58,6 +72,22 @@ public class ReportContentGet extends ContentGet {
 			entityNodeRef = new NodeRef(entityNodeRefParam);
 		}
 
+		NodeRef nodeRef = null;
+
+		// create map of template vars
+		Map<String, String> templateArgs = req.getServiceMatch().getTemplateVars();
+		if (templateArgs != null) {
+			String storeType = templateArgs.get(PARAM_STORE_TYPE);
+			String storeId = templateArgs.get(PARAM_STORE_ID);
+			String nodeId = templateArgs.get(PARAM_ID);
+			if (storeType != null && storeId != null && nodeId != null) {
+				nodeRef = new NodeRef(storeType, storeId, nodeId);
+			}
+		}
+		if (nodeRef == null) {
+			throw new WebScriptException(HttpServletResponse.SC_NOT_FOUND, "No report provided");
+		}
+
 		if (entityNodeRef == null) {
 			throw new WebScriptException(HttpServletResponse.SC_NOT_FOUND, "No entity provided");
 		}
@@ -67,7 +97,28 @@ public class ReportContentGet extends ContentGet {
 			entityReportService.generateReport(entityNodeRef);
 		}
 
-		super.execute(req, res);
+		if (!nodeService.exists(nodeRef)) {
+			nodeRef = entityReportService.getSelectedReport(entityNodeRef);
+		}
+
+		// determine attachment
+		boolean attach = Boolean.valueOf(req.getParameter("a"));
+
+		// render content
+		QName propertyQName = ContentModel.PROP_CONTENT;
+		String contentPart = templateArgs.get(PARAM_PROPERTY);
+		if (contentPart.length() > 0 && contentPart.charAt(0) == ';') {
+			if (contentPart.length() < 2) {
+				throw new WebScriptException(HttpServletResponse.SC_BAD_REQUEST, "Content property malformed");
+			}
+			String propertyName = contentPart.substring(1);
+			if (propertyName.length() > 0) {
+				propertyQName = QName.createQName(propertyName, namespaceService);
+			}
+		}
+
+		// Stream the content
+		streamContentLocal(req, res, nodeRef, attach, propertyQName, null);
 
 	}
 
