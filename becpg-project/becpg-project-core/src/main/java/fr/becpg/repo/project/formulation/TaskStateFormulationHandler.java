79a8223cb2c9405a6c20168af4b90fbd6f3fbee2
Laborie Matthieu <matthieu.laborie@becpg.fr>
2015-07-10 10:27:25 +0200

    refs #1484

--- becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/formulation/TaskStateFormulationHandler.java
+++ becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/formulation/TaskStateFormulationHandler.java
@@ -250,12 +250,14 @@ public class TaskStateFormulationHandler extends FormulationBaseHandler<ProjectD
 								nextDeliverable.setContent(new NodeRef(nextDeliverable.getUrl().substring(DeliverableUrl.CONTENT_URL_PREFIX.length())));
 								nextDeliverable.setUrl(null);
 							}
-							
-							if(DeliverableScriptOrder.Pre.equals(nextDeliverable.getScriptOrder())){
-								projectService.runScript(projectData, nextTask, nextDeliverable.getContent());
-								nextDeliverable.setState(DeliverableState.Completed);
-							} 
+
 						}
+						
+						if( DeliverableState.InProgress.equals(nextDeliverable.getState()) 
+								&& DeliverableScriptOrder.Pre.equals(nextDeliverable.getScriptOrder())){
+							projectService.runScript(projectData, nextTask, nextDeliverable.getContent());
+							nextDeliverable.setState(DeliverableState.Completed);
+						} 
 						
 					}
 					
