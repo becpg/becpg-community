13942aee49759206b73209ac31417c294be2dfb2
Laborie Matthieu <matthieu.laborie@becpg.fr>
2015-10-15 12:34:08 +0200

    refs 1717

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/data/CharactDetails.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/data/CharactDetails.java
@@ -1,71 +1,76 @@
 /*******************************************************************************
- * Copyright (C) 2010-2015 beCPG. 
- *  
- * This file is part of beCPG 
- *  
- * beCPG is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version. 
- *  
- * beCPG is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * GNU Lesser General Public License for more details. 
- *  
+ * Copyright (C) 2010-2015 beCPG.
+ *
+ * This file is part of beCPG
+ *
+ * beCPG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * beCPG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License along with beCPG. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package fr.becpg.repo.product.data;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.alfresco.service.cmr.repository.NodeRef;
 
 /**
  * Contains charact details
+ *
  * @author matthieu
  *
  */
 public class CharactDetails {
 
-	Map<NodeRef,String> computedCharacts = null;
+	List<NodeRef> computedCharacts = null;
 	
-	final Map<NodeRef,Map<NodeRef,Double>> data = new LinkedHashMap<>();
-	
-	public CharactDetails(Map<NodeRef,String> computedCharacts) {
+	final Map<NodeRef, Map<NodeRef, CharactDetailsValue>> data = new LinkedHashMap<>();
+
+	public CharactDetails(List<NodeRef> computedCharacts ) {
 		super();
 		this.computedCharacts = computedCharacts;
 	}
 
-	public void addKeyValue(NodeRef charactNodeRef, NodeRef key, Double value) {
-		Map<NodeRef,Double> tmp = data.get(charactNodeRef);
-		if(tmp==null){
+	public void addKeyValue(NodeRef charactNodeRef, CharactDetailsValue value) {
+		Map<NodeRef, CharactDetailsValue> tmp = data.get(charactNodeRef);
+		if(tmp == null){
 			tmp = new LinkedHashMap<>();
-		}		
-		double v = (value != null ? value : 0d) + (tmp.get(key) != null ? tmp.get(key) : 0d);
-		tmp.put(key, v != 0d ? v : null);		
+		}
+		
+		if (tmp.containsKey(value.getKeyNodeRef())) {
+			tmp.get(value.getKeyNodeRef()).add(value.getValue());
+		} else {
+			tmp.put(value.getKeyNodeRef(), value);
+		}
+
 		data.put(charactNodeRef, tmp);
 	}
 
 	public boolean hasElement(NodeRef charactNodeRef) {
-		return computedCharacts==null || computedCharacts.isEmpty() || computedCharacts.containsKey(charactNodeRef);
+		return computedCharacts==null || computedCharacts.isEmpty() || computedCharacts.contains(charactNodeRef);
 	}
 
-	public Map<NodeRef, Map<NodeRef, Double>> getData() {
+	public Map<NodeRef, Map<NodeRef, CharactDetailsValue>> getData() {
 		return data;
 	}
 
 	@Override
 	public String toString() {
-		return "CharactDetails [computedCharacts=" + computedCharacts + ", data=" + data + "]";
+		return "CharactDetails [ data=" + data + "]";
 	}
 
-	public String getUnit(NodeRef key) {
-		return computedCharacts.get(key);
+	public void initCharact(NodeRef charactNodeRef) {
+		data.put(charactNodeRef, new LinkedHashMap<>());
+		
 	}
-	
-	
-	
 
 }
