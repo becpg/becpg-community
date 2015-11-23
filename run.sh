d7a6ab01348cedbfc4e0b63689e99148b25b1ded
Laborie Matthieu <matthieu.laborie@becpg.fr>
2015-11-23 17:18:41 +0100

    fix junit tests and units

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # Downloads the spring-loaded lib if not existing and runs the full all-in-one
 # (Alfresco + Share + Solr) using the runner project
 
-MAVEN_OPTS="-Xms512m -Xmx2G" mvn test-compile install -Prun
+MAVEN_OPTS="-Xms512m -Xmx2G" mvn test-compile install -Prun -DskipTests=true
