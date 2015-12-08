b437e0c621e074024c2ee63a473b367eb7f48a42
Laborie Matthieu <matthieu.laborie@becpg.fr>
2015-12-08 17:55:04 +0100

    Document view

--- becpg-share/src/main/amp/config/alfresco/module/becpg-share/config/site-webscripts/org/alfresco/components/entity-charact-views/documents-view.get.js
+++ becpg-share/src/main/amp/config/alfresco/module/becpg-share/config/site-webscripts/org/alfresco/components/entity-charact-views/documents-view.get.js
@@ -1,30 +1,51 @@
+<import resource="classpath:/alfresco/site-webscripts/org/alfresco/components/documentlibrary/include/toolbar.lib.js">
+<import resource="classpath:/alfresco/site-webscripts/org/alfresco/components/upload/uploadable.lib.js">
 <import resource="classpath:/alfresco/site-webscripts/org/alfresco/components/documentlibrary/include/documentlist.lib.js">
 
 doclibCommon();
 
-function main()
+function widgets()
 {
+ 
+   var docListToolbar = {
+      id: "DocListToolbar", 
+      name: "Alfresco.DocListToolbar",
+      assignTo: "docListToolbar",
+      options: {
+         siteId: "",
+    	 rootNode : page.url.args.nodeRef!=null ?page.url.args.nodeRef : "null",
+         hideNavBar: false,
+         repositoryBrowsing: false,
+         useTitle: false,
+         syncMode: model.syncMode != null ? model.syncMode : "",
+         createContentByTemplateEnabled: model.createContentByTemplateEnabled,
+         createContentActions: model.createContent
+      }
+   };
    
    var documentList = {
       id : "DocumentList", 
       name : "Alfresco.DocumentList",
       options : {
          syncMode : model.syncMode != null ? model.syncMode : "",         
-         siteId : (page.url.templateArgs.site != null) ? page.url.templateArgs.site : "",
+         siteId : "",
          containerId : template.properties.container != null ? template.properties.container : "documentLibrary",
-         rootNode : model.rootNode != null ? model.rootNode : "null",
-         usePagination : (args.pagination == "true"),
+         rootNode : page.url.args.nodeRef!=null ?page.url.args.nodeRef : "null",
+         usePagination : true,
          sortAscending : (model.preferences.sortAscending != null ? model.preferences.sortAscending : true),
          sortField : model.preferences.sortField != null ? model.preferences.sortField : "cm:name",
          showFolders : (model.preferences.showFolders != null ? model.preferences.showFolders : true),
+         hideNavBar: (model.preferences.hideNavBar != null ? model.preferences.hideNavBar : false),
          simpleView : model.preferences.simpleView != null ? model.preferences.simpleView : "null",
+         viewRenderers: model.viewRenderers,
          viewRendererName : model.preferences.viewRendererName != null ? model.preferences.viewRendererName : "detailed",
-         viewRendererNames : model.viewRendererNames != null ? model.viewRendererNames : ["simple", "detailed", "gallery", "filmstrip"],
+         viewRendererNames : model.viewRendererNames != null ? model.viewRendererNames : ["simple", "detailed", "gallery"],
          highlightFile : page.url.args["file"] != null ? page.url.args["file"] : "",
          replicationUrlMapping : model.replicationUrlMapping != null ? model.replicationUrlMapping : "{}",
-         repositoryBrowsing : model.rootNode != null, 
-         useTitle : (model.useTitle != null ? model.useTitle == "true" : true),
-         userIsSiteManager : model.userIsSiteManager
+         repositoryBrowsing : false, 
+         useTitle : false,
+         userIsSiteManager : model.userIsSiteManager,
+         associatedToolbar: { _alfValue: "docListToolbar", _alfType: "REFERENCE" }
       }
    };
    if (model.repositoryUrl != null)
@@ -32,8 +53,15 @@ function main()
       documentList.options.repositoryUrl = model.repositoryUrl;
    }
    
-   model.widgets = [documentList];
+   var documentsView = {
+		id : "DocumentsView",
+		name : "beCPG.component.DocumentsView",
+		options: {
+			currVersionNodeRef : page.url.args.nodeRef!=null ?page.url.args.nodeRef : null
+		}   
+   };
+   
+   model.widgets = [documentsView, docListToolbar, documentList];
 }
 
-main();
-
+widgets();
