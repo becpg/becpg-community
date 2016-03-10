a3ae095dac89bd288b6712c4af206942da5e1f0f
Laborie Matthieu <matthieu.laborie@becpg.fr>
2016-03-10 18:44:10 +0100

    bugfix gs1 tare

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/TareFormulationHandler.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/TareFormulationHandler.java
@@ -1,18 +1,18 @@
 /*******************************************************************************
- * Copyright (C) 2010-2015 beCPG. 
- *  
- * This file is part of beCPG 
- *  
- * beCPG is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version. 
- *  
- * beCPG is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * GNU Lesser General Public License for more details. 
- *  
+ * Copyright (C) 2010-2015 beCPG.
+ *
+ * This file is part of beCPG
+ *
+ * beCPG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * beCPG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License along with beCPG. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package fr.becpg.repo.product.formulation;
@@ -64,16 +64,10 @@ public class TareFormulationHandler extends FormulationBaseHandler<ProductData> 
 		tarePrimary = tarePrimary.add(calculateTareOfPackaging(formulatedProduct));
 		formulatedProduct.setTareUnit(TareUnit.kg);
 
-		if (tarePrimary.doubleValue() < 1) {
-			logger.debug("Calculating tare in g: " + tarePrimary);
-			tarePrimary = tarePrimary.multiply(new BigDecimal(1000d));
-			formulatedProduct.setTareUnit(TareUnit.g);
-		}
-		formulatedProduct.setTare(tarePrimary.doubleValue());
-
 		if (formulatedProduct.getAspects().contains(GS1Model.ASPECT_MEASURES_ASPECT)) {
-			
-			BigDecimal netWeightPrimary = new BigDecimal(FormulationHelper.getNetWeight(formulatedProduct, FormulationHelper.DEFAULT_NET_WEIGHT).toString());
+
+			BigDecimal netWeightPrimary = new BigDecimal(
+					FormulationHelper.getNetWeight(formulatedProduct, FormulationHelper.DEFAULT_NET_WEIGHT).toString());
 
 			BigDecimal weightPrimary = tarePrimary.add(netWeightPrimary);
 
@@ -110,6 +104,13 @@ public class TareFormulationHandler extends FormulationBaseHandler<ProductData> 
 
 		}
 
+		if (tarePrimary.doubleValue() < 1) {
+			logger.debug("Calculating tare in g: " + tarePrimary);
+			tarePrimary = tarePrimary.multiply(new BigDecimal(1000d));
+			formulatedProduct.setTareUnit(TareUnit.g);
+		}
+		formulatedProduct.setTare(tarePrimary.doubleValue());
+
 		return true;
 	}
 
@@ -132,7 +133,7 @@ public class TareFormulationHandler extends FormulationBaseHandler<ProductData> 
 				level = PackagingLevel.Secondary;
 			}
 
-			if (packList.getPkgLevel() != null && packList.getPkgLevel().equals(level)) {
+			if ((packList.getPkgLevel() != null) && packList.getPkgLevel().equals(level)) {
 				totalTare = totalTare.add(FormulationHelper.getTareInKg(packList, nodeService));
 			}
 		}
