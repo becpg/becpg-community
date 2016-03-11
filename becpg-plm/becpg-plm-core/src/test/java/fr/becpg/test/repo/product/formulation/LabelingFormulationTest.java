0ec5b902d5fe82d107ac92840cb91eafe4080c2e
Laborie Matthieu <matthieu.laborie@becpg.fr>
2016-03-11 15:18:04 +0100

    Bugfix in food data

--- becpg-plm/becpg-plm-core/src/test/java/fr/becpg/test/repo/product/formulation/LabelingFormulationTest.java
+++ becpg-plm/becpg-plm-core/src/test/java/fr/becpg/test/repo/product/formulation/LabelingFormulationTest.java
@@ -23,6 +23,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Calendar;
 import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.Callable;
@@ -46,7 +47,6 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.extensions.surf.util.I18NUtil;
 
-import com.ibm.icu.util.Calendar;
 
 import fr.becpg.model.BeCPGModel;
 import fr.becpg.model.PLMModel;
