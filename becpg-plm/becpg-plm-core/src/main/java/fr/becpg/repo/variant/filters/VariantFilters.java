e368f49117668a682a3e051fe3f5da5436cff45a
Laborie Matthieu <matthieu.laborie@becpg.fr>
2016-04-22 17:44:49 +0200

    refs #2175

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/variant/filters/VariantFilters.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/variant/filters/VariantFilters.java
@@ -1,18 +1,18 @@
 /*******************************************************************************
- * Copyright (C) 2010-2016 beCPG. 
- *  
- * This file is part of beCPG 
- *  
- * beCPG is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version. 
- *  
- * beCPG is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * GNU Lesser General Public License for more details. 
- *  
+ * Copyright (C) 2010-2016 beCPG.
+ *
+ * This file is part of beCPG
+ *
+ * beCPG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * beCPG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License along with beCPG. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package fr.becpg.repo.variant.filters;
@@ -26,17 +26,16 @@ import fr.becpg.repo.repository.filters.DataListFilter;
 import fr.becpg.repo.variant.model.VariantData;
 import fr.becpg.repo.variant.model.VariantDataItem;
 
-public class VariantFilters<T extends VariantDataItem> implements DataListFilter<ProductData,T> {
+public class VariantFilters<T extends VariantDataItem> implements DataListFilter<ProductData, T> {
 
 	private NodeRef variantNodeRef;
 
 	private Boolean isDefaultVariant = true;
 
-	
-	public VariantFilters(){
+	public VariantFilters() {
 		super();
 	}
-	
+
 	public VariantFilters(NodeRef variantNodeRef) {
 		super();
 		this.variantNodeRef = variantNodeRef;
@@ -51,7 +50,7 @@ public class VariantFilters<T extends VariantDataItem> implements DataListFilter
 	@Override
 	public Predicate<T> createPredicate(final ProductData entity) {
 
-		if (variantNodeRef == null && entity.getVariants()!=null) {
+		if ((variantNodeRef == null) && (entity.getVariants() != null)) {
 			for (VariantData variant : entity.getVariants()) {
 				if (variant.getIsDefaultVariant()) {
 					this.variantNodeRef = variant.getNodeRef();
@@ -60,16 +59,15 @@ public class VariantFilters<T extends VariantDataItem> implements DataListFilter
 			}
 		}
 
-		return new Predicate<T>() {
-			@Override
-			public boolean test(VariantDataItem obj) {
-				if (variantNodeRef!=null && obj instanceof VariantDataItem) {
-					VariantDataItem item = ((VariantDataItem) obj);
+		return obj -> {
+			if (obj instanceof VariantDataItem) {
+				VariantDataItem item = (obj);
 
+				if (variantNodeRef != null) {
 					if (isDefaultVariant != null) {
 
-						if (item.getVariants() == null || item.getVariants().isEmpty()) {
-							if (isDefaultVariant != null && isDefaultVariant) {
+						if ((item.getVariants() == null) || item.getVariants().isEmpty()) {
+							if ((isDefaultVariant != null) && isDefaultVariant) {
 								return true;
 							}
 						}
@@ -78,13 +76,15 @@ public class VariantFilters<T extends VariantDataItem> implements DataListFilter
 							return true;
 						}
 
-					} else if (variantNodeRef != null && item.getVariants().contains(variantNodeRef)) {
+					} else if ((variantNodeRef != null) && item.getVariants().contains(variantNodeRef)) {
 						return true;
 					}
 					return false;
+				} else if ((item.getVariants() != null) && !item.getVariants().isEmpty()) {
+					return false;
 				}
-				return true;
 			}
+			return true;
 		};
 	}
 
