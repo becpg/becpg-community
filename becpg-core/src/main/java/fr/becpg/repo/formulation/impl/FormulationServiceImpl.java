7624f545d642a91dda53bdac2cfb9f8cf5f898d4
Laborie Matthieu <matthieu.laborie@becpg.fr>
2016-07-22 08:21:21 +0200

    fix NPE

--- becpg-core/src/main/java/fr/becpg/repo/formulation/impl/FormulationServiceImpl.java
+++ becpg-core/src/main/java/fr/becpg/repo/formulation/impl/FormulationServiceImpl.java
@@ -173,13 +173,13 @@ public class FormulationServiceImpl<T extends FormulatedEntity> implements Formu
 			}
 		} catch (Exception e) {
 
+			logger.error(e,e);
+			
 			if (e instanceof FormulateException) {
 				throw (FormulateException) e;
 			} else if (e instanceof ConcurrencyFailureException) {
 				throw (ConcurrencyFailureException) e;
 			}
-			logger.error(e,e);
-			
 			throw new FormulateException(I18NUtil.getMessage("message.formulate.failure", repositoryEntity.getNodeRef()), e);
 
 		}
