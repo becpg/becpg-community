b643aeb0a1c2008f9d131dbae862b88cdf836501
Laborie Matthieu <matthieu.laborie@becpg.fr>
2016-10-03 11:20:22 +0200

    fix #2515

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/labeling/LabelingFormulationHandler.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/labeling/LabelingFormulationHandler.java
@@ -606,11 +606,11 @@ public class LabelingFormulationHandler extends FormulationBaseHandler<ProductDa
 												toAdd.put(aggregateRuleNodeRef, current);
 											}
 
-											if (qty != null) {
+											if (qty != null && current.getQty()!=null) {
 												current.setQty(current.getQty() + qty);
 											}
 
-											if (volume != null) {
+											if (volume != null && current.getVolume()!=null) {
 												current.setVolume(current.getVolume() + volume);
 											}
 
@@ -694,9 +694,9 @@ public class LabelingFormulationHandler extends FormulationBaseHandler<ProductDa
 										}
 										iterator.remove();
 										break;
-									} else if (qty != null) {
+									} else if (qty != null && component.getQty()!=null) {
 										component.setQty(component.getQty() - qty);
-										if (volume != null) {
+										if (volume != null && component.getVolume()!=null) {
 											component.setVolume(component.getVolume() - volume);
 										}
 									}
