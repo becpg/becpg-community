b7c79c83f87400d704416aa61a9568c8e0923bae
Steven Bihoue <steven.bihoue@becpg.fr>
2017-02-21 11:54:29 +0100

    refs #2854

--- becpg-tools/src/main/java/fr/becpg/tools/http/GetDocumentCommand.java
+++ becpg-tools/src/main/java/fr/becpg/tools/http/GetDocumentCommand.java
@@ -2,7 +2,8 @@ package fr.becpg.tools.http;
 
 public class GetDocumentCommand extends AbstractHttpCommand {
 
-	private static final String COMMAND_URL_TEMPLATE = "/api/-default-/public/alfresco/versions/1/nodes/%s/content";
+//	private static final String COMMAND_URL_TEMPLATE = "/api/-default-/public/alfresco/versions/1/nodes/%s/content";
+	private static final String COMMAND_URL_TEMPLATE = "/api/-default-/public/cmis/versions/1.1/atom/content?id=%s";
 	
 	public GetDocumentCommand(String serverUrl) {
 		super(serverUrl.replace("/service", "") );
