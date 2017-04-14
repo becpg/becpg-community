1be664625cc828289c59be46e7e4fbd850ab0858
Steven BihouÃ© <steven.bihoue@becpg.fr>
2017-04-14 10:10:06 +0200

    fix #2998

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/data/CharactDetailsValue.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/data/CharactDetailsValue.java
@@ -7,8 +7,13 @@ public class CharactDetailsValue {
 	private NodeRef parentNodeRef;
 	private NodeRef keyNodeRef;
 	private Double value;
+	private Double previousValue;
+	private Double futureValue;
+	private Double mini;
+	private Double maxi;
 	private Integer level;
 	private String unit;
+	private String name;
 
 	public CharactDetailsValue(NodeRef parentNodeRef, NodeRef keyNodeRef, Double value, Integer level, String unit) {
 		super();
@@ -35,6 +40,46 @@ public class CharactDetailsValue {
 		return unit;
 	}
 
+	public Double getPreviousValue() {
+		return previousValue;
+	}
+
+	public void setPreviousValue(Double previousValue) {
+		this.previousValue = previousValue;
+	}
+
+	public Double getFutureValue() {
+		return futureValue;
+	}
+
+	public void setFutureValue(Double futureValue) {
+		this.futureValue = futureValue;
+	}
+
+	public Double getMini() {
+		return mini;
+	}
+
+	public void setMini(Double mini) {
+		this.mini = mini;
+	}
+
+	public Double getMaxi() {
+		return maxi;
+	}
+
+	public void setMaxi(Double maxi) {
+		this.maxi = maxi;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public void add(Double val) {
 		if ((value != null) && (val != null)) {
 			value += val;
@@ -45,9 +90,16 @@ public class CharactDetailsValue {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((futureValue == null) ? 0 : futureValue.hashCode());
 		result = prime * result + ((keyNodeRef == null) ? 0 : keyNodeRef.hashCode());
 		result = prime * result + ((level == null) ? 0 : level.hashCode());
+		result = prime * result + ((maxi == null) ? 0 : maxi.hashCode());
+		result = prime * result + ((mini == null) ? 0 : mini.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((parentNodeRef == null) ? 0 : parentNodeRef.hashCode());
+		result = prime * result + ((previousValue == null) ? 0 : previousValue.hashCode());
+		result = prime * result + ((unit == null) ? 0 : unit.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
 		return result;
 	}
 
@@ -63,6 +115,11 @@ public class CharactDetailsValue {
 		if (getClass() != obj.getClass())
 			return false;
 		CharactDetailsValue other = (CharactDetailsValue) obj;
+		if (futureValue == null) {
+			if (other.futureValue != null)
+				return false;
+		} else if (!futureValue.equals(other.futureValue))
+			return false;
 		if (keyNodeRef == null) {
 			if (other.keyNodeRef != null)
 				return false;
@@ -73,12 +130,49 @@ public class CharactDetailsValue {
 				return false;
 		} else if (!level.equals(other.level))
 			return false;
+		if (maxi == null) {
+			if (other.maxi != null)
+				return false;
+		} else if (!maxi.equals(other.maxi))
+			return false;
+		if (mini == null) {
+			if (other.mini != null)
+				return false;
+		} else if (!mini.equals(other.mini))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
 		if (parentNodeRef == null) {
 			if (other.parentNodeRef != null)
 				return false;
 		} else if (!parentNodeRef.equals(other.parentNodeRef))
 			return false;
+		if (previousValue == null) {
+			if (other.previousValue != null)
+				return false;
+		} else if (!previousValue.equals(other.previousValue))
+			return false;
+		if (unit == null) {
+			if (other.unit != null)
+				return false;
+		} else if (!unit.equals(other.unit))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "CharactDetailsValue [parentNodeRef=" + parentNodeRef + ", keyNodeRef=" + keyNodeRef + ", value=" + value + ", previousValue="
+				+ previousValue + ", futureValue=" + futureValue + ", mini=" + mini + ", maxi=" + maxi + ", level=" + level + ", unit=" + unit
+				+ ", name=" + name + "]";
 	}
 
 	
