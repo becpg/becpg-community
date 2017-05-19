1158394a3b8678312df30748bc731b2bc34f7d67
Steven BihouÃ© <steven.bihoue@becpg.fr>
2017-05-19 17:33:02 +0200

    refs #3126

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/helper/Nutrient5CHelper.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/helper/Nutrient5CHelper.java
@@ -3,11 +3,11 @@ package fr.becpg.repo.product.helper;
 import java.util.List;
 
 /**
- * Helper to compute 5C Nutrient Profile Score
- *
- * @author matthieu
- *
- */
+* Helper to compute 5C Nutrient Profile Score
+*
+* @author matthieu
+*
+*/
 public class Nutrient5CHelper {
 
 	enum NutrientCategory {
@@ -15,195 +15,231 @@ public class Nutrient5CHelper {
 	}
 
 	private static final double[][] othersACategories = new double[][] {
-	    { 3350d, 3015d, 2680d, 2345d, 2010d, 1675d, 1340d, 1005d, 670d, 335d },
+		{ 3350d, 3015d, 2680d, 2345d, 2010d, 1675d, 1340d, 1005d, 670d, 335d },
 		{ 10d, 9d, 8d, 7d, 6d, 5d, 4d, 3d, 2d, 1d },
 		{ 45d, 40d, 36d, 31d, 27d, 22.5d, 18d, 13.5d, 9d, 4.5d },
 		{ 900d, 810d, 720d, 630d, 540d, 450d, 360d, 270d, 180d, 90d }
 	};
 
 	private static final double[][] othersCCategories = new double[][] {
-		    { -1d, -1d, -1d , -1d, -1d, 80d, -1d, -1d, 60d, 40d },
-		    { 4.7d, 3.7d, 2.8d, 1.9d, 0.9d },
-			{ 8.0d, 6.4d, 4.8d, 3.2d, 1.6d } };
-	
-	private static final double[][] cheeseACategories = new double[][] {
-		    { 3350d, 3015d, 2680d, 2345d, 2010d, 1675d, 1340d, 1005d, 670d, 335d },
+		{ -1d, -1d, -1d , -1d, -1d, 80d, -1d, -1d, 60d, 40d },
+		{ 3.5d, 2.8d, 2.1d, 1.4d, 0.7d },
+		{ 4.7d, 3.7d, 2.8d, 1.9d, 0.9d },
+		{ 8.0d, 6.4d, 4.8d, 3.2d, 1.6d } };
+
+		private static final double[][] cheeseACategories = new double[][] {
+			{ 3350d, 3015d, 2680d, 2345d, 2010d, 1675d, 1340d, 1005d, 670d, 335d },
 			{ 10d, 9d, 8d, 7d, 6d, 5d, 4d, 3d, 2d, 1d }, { 45d, 40d, 36d, 31d, 27d, 22.5d, 18d, 13.5d, 9d, 4.5d},
 			{ 900d, 810d, 720d, 630d, 540d, 450d, 360d, 270d, 180d, 90d } };
-	
-	private static final double[][] cheeseCCategories = new double[][] { { -1d, -1d, -1d , -1d, -1d, 80d, -1d, -1d, 60d, 40d }, {4.7d, 3.7d, 2.8d, 1.9d, 0.9d },
-			{8.0d, 6.4d, 4.8d, 3.2d, 1.6d } };
-	
-	private static final double[][] fatsACategories = new double[][] { { 3350d, 3015d, 2680d, 2345d, 2010d, 1675d, 1340d, 1005d, 670d, 335d },
-			{ 63d, 57d, 51d, 45d, 39d, 33d, 27d, 21d, 15d, 9d }, {45d, 40d, 36d, 31d, 27d, 22.5d, 18d, 13.5d, 9d, 4.5d},
-			{  900d, 810d, 720d, 630d, 540d, 450d, 360d, 270d, 180d, 90d } };
-	
-	private static final double[][] fatsCCategories = new double[][] {
-		    { -1d, -1d, -1d , -1d, -1d, 80d, -1d, -1d, 60d, 40d },
-		    { 4.7d, 3.7d, 2.8d, 1.9d, 0.9d },
-			{ 8.0d, 6.4d, 4.8d, 3.2d, 1.6d } };
-	
-	private static final double[][] beveragesACategories = new double[][] {
-		    { 270d, 240d, 210d, 180d, 150d, 120d, 90d, 60d, 30d, 0d },
-			{ 10d, 9d, 8d, 7d, 6d, 5d, 4d, 3d, 2d, 1d },
-			{ 13.5d, 12d, 10.5d, 9d, 7.5d, 6d, 4.5d, 3d, 1.5d, 0d },
-			{ 900d, 810d, 720d, 630d, 540d, 450d, 360d, 270d, 180d, 90d} };
-	
-	private static final double[][] beveragesCCategories = new double[][] {
-		    { 80d, -1d, -1d, -1d, -1d, -1d, 60d, -1d, 40d, -1d },
-		    { 4.7d, 3.7d, 2.8d, 1.9d, 0.9d  },
-			{8.0d, 6.4d, 4.8d, 3.2d, 1.6d } };
 
-			
+			private static final double[][] cheeseCCategories = new double[][] {
+				{ -1d, -1d, -1d , -1d, -1d, 80d, -1d, -1d, 60d, 40d },
+				{ 3.5d, 2.8d, 2.1d, 1.4d, 0.7d },
+				{4.7d, 3.7d, 2.8d, 1.9d, 0.9d },
+				{8.0d, 6.4d, 4.8d, 3.2d, 1.6d } };
 
-	private static double[][] getACategory(NutrientCategory category) {
-		switch (category) {
-		case Fats:
-			return fatsACategories;
-		case Beverages:
-			return beveragesACategories;
-		case Cheeses:
-			return cheeseACategories;
-		default:
-			return othersACategories;
-		}
+				private static final double[][] fatsACategories = new double[][] {
+				{ 3350d, 3015d, 2680d, 2345d, 2010d, 1675d, 1340d, 1005d, 670d, 335d },
+				{ 63d, 57d, 51d, 45d, 39d, 33d, 27d, 21d, 15d, 9d },
+				{45d, 40d, 36d, 31d, 27d, 22.5d, 18d, 13.5d, 9d, 4.5d},
+				{  900d, 810d, 720d, 630d, 540d, 450d, 360d, 270d, 180d, 90d } };
 
-	}
+				private static final double[][] fatsCCategories = new double[][] {
+					{ -1d, -1d, -1d , -1d, -1d, 80d, -1d, -1d, 60d, 40d },
+					{ 3.5d, 2.8d, 2.1d, 1.4d, 0.7d },
+					{ 4.7d, 3.7d, 2.8d, 1.9d, 0.9d },
+					{ 8.0d, 6.4d, 4.8d, 3.2d, 1.6d } };
 
-	private static double[][] getCCategory(NutrientCategory category) {
-		switch (category) {
-		case Fats:
-			return fatsCCategories;
-		case Beverages:
-			return beveragesCCategories;
-		case Cheeses:
-			return cheeseCCategories;
-		default:
-			return othersCCategories;
-		}
-	}
+					private static final double[][] beveragesACategories = new double[][] {
+						{ 270d, 240d, 210d, 180d, 150d, 120d, 90d, 60d, 30d, 0d },
+						{ 10d, 9d, 8d, 7d, 6d, 5d, 4d, 3d, 2d, 1d },
+						{ 13.5d, 12d, 10.5d, 9d, 7.5d, 6d, 4.5d, 3d, 1.5d, 0d },
+						{ 900d, 810d, 720d, 630d, 540d, 450d, 360d, 270d, 180d, 90d} };
+
+						private static final double[][] beveragesCCategories = new double[][] {
+							{ 80d, -1d, -1d, -1d, -1d, -1d, 60d, -1d, 40d, -1d },
+							{ 3.5d, 2.8d, 2.1d, 1.4d, 0.7d },
+							{ 4.7d, 3.7d, 2.8d, 1.9d, 0.9d  },
+							{8.0d, 6.4d, 4.8d, 3.2d, 1.6d } };
 
 
-	/**
-	 * 
-	 * @param energyKj
-	 * @param satFat
-	 * @param totalSugar
-	 * @param sodium
-	 * @param percFruitsAndVetgs
-	 * @param fibre
-	 * @param protein
-	 * @param category
-	 * @return
-	 */
-	public static int compute5CScore(Double energyKj, Double satFat, Double totalSugar, Double sodium, Double percFruitsAndVetgs, Double fibre,
-			Double protein, String category) {
 
-		int aScore = 0;
-		int cScore = 0;
-		int score = 10;
+							private static double[][] getACategory(NutrientCategory category) {
+								switch (category) {
+									case Fats:
+									return fatsACategories;
+									case Beverages:
+									return beveragesACategories;
+									case Cheeses:
+									return cheeseACategories;
+									default:
+									return othersACategories;
+								}
 
-		double[][] aCategories = getACategory(NutrientCategory.valueOf(category));
-		double[][] cCategories = getCCategory(NutrientCategory.valueOf(category));
+							}
 
-		if (energyKj != null) {
+							private static double[][] getCCategory(NutrientCategory category) {
+								switch (category) {
+									case Fats:
+									return fatsCCategories;
+									case Beverages:
+									return beveragesCCategories;
+									case Cheeses:
+									return cheeseCCategories;
+									default:
+									return othersCCategories;
+								}
+							}
 
-			for (double val : aCategories[0]) {
-				if (energyKj > val && val > 0) {
-					break;
-				}
-				score--;
-			}
-			aScore += score;
-		}
 
-		if (satFat != null) {
-			score = 10;
-			for (double val : aCategories[1]) {
-				if (satFat > val && val > 0) {
-					break;
-				}
-				score--;
-			}
+							/**
+							*
+							* @param energyKj
+							* @param satFat
+							* @param totalFat
+							* @param totalSugar
+							* @param sodium
+							* @param percFruitsAndVetgs
+							* @param nspFibre
+							* @param aoacFibre
+							* @param protein
+							* @param category
+							* @return
+							*/
+							public static int compute5CScore(Double energyKj, Double satFat, Double totalFat, Double totalSugar, Double sodium, Double percFruitsAndVetgs, Double nspFibre, Double aoacFibre,
+							Double protein, String category) {
 
-			aScore += score;
-		}
+								int aScore = 0;
+								int cScore = 0;
+								int score = 10;
 
-		if (totalSugar != null) {
-			score = 10;
-			for (double val : aCategories[2]) {
-				if (totalSugar > val && val > 0) {
-					break;
-				}
-				score--;
-			}
+								double[][] aCategories = getACategory(NutrientCategory.valueOf(category));
+								double[][] cCategories = getCCategory(NutrientCategory.valueOf(category));
 
-			aScore += score;
-		}
+								if (energyKj != null) {
 
-		if (sodium != null) {
-			score = 10;
-			for (double val : aCategories[3]) {
-				if (sodium > val && val > 0) {
-					break;
-				}
-				score--;
-			}
+									for (double val : aCategories[0]) {
+										if (energyKj > val && val > 0) {
+											break;
+										}
+										score--;
+									}
+									aScore += score;
+								}
 
-			aScore += score;
-		}
+								if (NutrientCategory.Fats.equals(NutrientCategory.valueOf(category)))
+								{
+									if (satFat != null && totalFat != null) {
+										score = 10;
+										for (double val : aCategories[1]) {
+											if ((satFat/totalFat*100) > val && val > 0) {
+												break;
+											}
+											score--;
+										}
+										aScore += score;
+									}
+								} else if (satFat != null)
+									{
+										score = 10;
+										for (double val : aCategories[1]) {
+											if (satFat > val && val > 0) {
+												break;
+											}
+											score--;
+										}
 
-		if (percFruitsAndVetgs != null) {
-			score = 10;
-			for (double val : cCategories[0]) {
-				if (percFruitsAndVetgs > val && val > 0) {
-					break;
-				}
-				score--;
-			}
-			cScore += score;
-		}
+										aScore += score;
+									}
+								
 
-		if (fibre != null) {
-			score = 5;
-			for (double val : cCategories[1]) {
-				if (fibre > val && val > 0) {
-					break;
-				}
-				score--;
-			}
-			cScore += score;
-		}
+									if (totalSugar != null)
+									{
+										score = 10;
+										for (double val : aCategories[2]) {
+											if (totalSugar > val && val > 0) {
+												break;
+											}
+											score--;
+										}
 
-		if (!((aScore >= 11) && ((percFruitsAndVetgs != null) && (percFruitsAndVetgs <= 80d))
-				&& !NutrientCategory.Cheeses.equals(NutrientCategory.valueOf(category)))) {
-			if (protein != null) {
-				score = 5;
-				for (double val : cCategories[2]) {
-					if (protein > val && val > 0) {
-						break;
-					}
-					score--;
-				}
-				cScore += score;
-			}
-		}
+										aScore += score;
+									}
 
-		return aScore - cScore;
+									if (sodium != null) {
+										score = 10;
+										for (double val : aCategories[3]) {
+											if (sodium > val && val > 0) {
+												break;
+											}
+											score--;
+										}
 
-	}
+										aScore += score;
+									}
 
-	public static String buildNutrientClass(Double score, List<Double> ranges, List<String> clazz) {
-		if (score != null) {
-			for (int i = 0; i < ranges.size(); i++) {
-				if (score > ranges.get(i)) {
-					return clazz.get(i);
-				}
-			}
-			return clazz.get(clazz.size() - 1);
-		}
-		return null;
+									if (percFruitsAndVetgs != null) {
+										score = 10;
+										for (double val : cCategories[0]) {
+											if (percFruitsAndVetgs > val && val > 0) {
+												break;
+											}
+											score--;
+										}
+										cScore += score;
+									}
 
-	}
+									if (nspFibre != null) {
+										score = 5;
+										for (double val : cCategories[1]) {
+											if (nspFibre > val && val > 0) {
+												break;
+											}
+											score--;
+										}
+										cScore += score;
+									}
 
-}
+									if (aoacFibre != null) {
+										score = 5;
+										for (double val : cCategories[2]) {
+											if (aoacFibre > val && val > 0) {
+												break;
+											}
+											score--;
+										}
+										cScore += score;
+									}
+
+									if (!((aScore >= 11) && ((percFruitsAndVetgs != null) && (percFruitsAndVetgs <= 80d))
+									&& !NutrientCategory.Cheeses.equals(NutrientCategory.valueOf(category)))) {
+										if (protein != null) {
+											score = 5;
+											for (double val : cCategories[3]) {
+												if (protein > val && val > 0) {
+													break;
+												}
+												score--;
+											}
+											cScore += score;
+										}
+									}
+
+									return aScore - cScore;
+
+								}
+
+								public static String buildNutrientClass(Double score, List<Double> ranges, List<String> clazz) {
+									if (score != null) {
+										for (int i = 0; i < ranges.size(); i++) {
+											if (score > ranges.get(i)) {
+												return clazz.get(i);
+											}
+										}
+										return clazz.get(clazz.size() - 1);
+									}
+									return null;
+
+								}
+
+							}
