2db43c263cde34adfcb25edb3329d48636666138
Laborie Matthieu <matthieu.laborie@becpg.fr>
2017-05-30 16:00:40 +0200

    Update copyright

--- becpg-plm/becpg-plm-share/src/main/amp/config/alfresco/module/becpg-plm-share/web/components/entity-data-lists/rapidLink-toolbar.js
+++ becpg-plm/becpg-plm-share/src/main/amp/config/alfresco/module/becpg-plm-share/web/components/entity-data-lists/rapidLink-toolbar.js
@@ -1,5 +1,5 @@
 /***********************************************************************************************************************
- * Copyright (C) 2010-2016 beCPG. This file is part of beCPG beCPG is free software: you can redistribute it and/or
+ * Copyright (C) 2010-2017 beCPG. This file is part of beCPG beCPG is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version. beCPG is distributed in the hope that it will
  * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
