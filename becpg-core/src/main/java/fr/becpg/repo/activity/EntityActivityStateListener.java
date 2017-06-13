33c8984fb5077c2a51ba1530fd6a5842c31a10aa
rabah aoukaba <rabah.aoukaba@becpg.fr>
2017-06-13 18:06:57 +0200

    refs #2464

---
+++ becpg-core/src/main/java/fr/becpg/repo/activity/EntityActivityStateListener.java
@@ -0,0 +1,52 @@
+package fr.becpg.repo.activity;
+
+import org.alfresco.service.cmr.activities.ActivityService;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.becpg.repo.activity.data.ActivityListDataItem;
+import fr.becpg.repo.activity.data.ActivityType;
+import fr.becpg.repo.helper.AttributeExtractorService;
+import fr.becpg.repo.repository.AlfrescoRepository;
+import fr.becpg.repo.repository.RepositoryEntity;
+
+@Service
+public class EntityActivityStateListener implements EntityActivityListener {
+
+	public static final String ENTITY_STATE_ACTIVITY = "fr.becpg.entity.state-changed";
+
+	@Autowired
+	private ActivityService activityService;
+
+	@Autowired
+	private AttributeExtractorService attributeExtractorService;
+	
+	@Autowired
+	private EntityActivityPlugin[] entityActivityPlugins;
+	
+	@Autowired 
+	private NodeService nodeService;
+
+	
+	@Override
+	public void notify(NodeRef entityNodeRef, ActivityListDataItem activityListDataItem) {
+	
+		boolean isMatch = false;
+		for(EntityActivityPlugin entityActivityPlugin : entityActivityPlugins){	
+			if (entityActivityPlugin.isMatchingEntityType(nodeService.getType(entityNodeRef))){
+				isMatch = true;
+				break;
+			}
+		}
+	
+		if (activityListDataItem.getActivityType().equals(ActivityType.State) && isMatch) {
+			activityService.postActivity(ENTITY_STATE_ACTIVITY, attributeExtractorService.extractSiteId(entityNodeRef), "entity",
+					activityListDataItem.getActivityData());
+		}
+
+
+	}
+
+}
--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/activity/PlmEntityActivityPlugin.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/activity/PlmEntityActivityPlugin.java
@@ -1,15 +1,28 @@
 package fr.becpg.repo.activity;
 
 import org.alfresco.service.namespace.QName;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.becpg.model.PLMModel;
+import fr.becpg.repo.entity.EntityDictionaryService;
 
 @Service
 public class PlmEntityActivityPlugin implements EntityActivityPlugin {	
 	
+	@Autowired
+	EntityDictionaryService entityDictionaryService;
+	
+	
 	public boolean isMatchingStateProperty(QName propName){
 		return PLMModel.PROP_PRODUCT_STATE.isMatch(propName) || PLMModel.PROP_SUPPLIER_STATE.isMatch(propName);
 	}
+
+	@Override
+	public boolean isMatchingEntityType(QName entityType) {
+		
+		return entityDictionaryService.isSubClass(entityType, PLMModel.TYPE_PRODUCT) || PLMModel.TYPE_SUPPLIER.isMatch(entityType);
+	}
+
 
 }
