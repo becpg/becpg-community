9788d456396c3e38762f16391aaaa64b2996d1a1
Laborie Matthieu <matthieu.laborie@becpg.fr>
2017-09-28 18:09:49 +0200

    cleanup

--- becpg-olap/src/main/sql/upgrade_871_28032013.sql
+++
@@ -1,4 +0,0 @@
-
-UPDATE becpg_dimdate SET NDay = concat ( concat (RIGHT (concat ('0', day), 2),'/', RIGHT(concat ('0', month), 2)),'/',year);
-
-UPDATE becpg_dimdate SET NWeek = concat ( 'S', week ,'/', year);
