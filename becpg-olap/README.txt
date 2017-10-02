0718183a9cca4b02b812e51750e0f2d944fbf9bd
Laborie Matthieu <matthieu.laborie@becpg.fr>
2017-10-02 17:52:35 +0200

    Remove Olap from community edition

--- becpg-olap/README.txt
+++
@@ -1,5 +0,0 @@
-##
-## beCPG analysis README
-##
-
-Call deploy.sh <instance_path> to deploy beCPG analysis  
--- becpg-olap/src/main/bin/cron.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-TARGET_JAR="$DIR/lib/${project.artifactId}-${project.version}-jar-with-dependencies.jar"
-
-java -jar $TARGET_JAR -file=jdbc.properties
