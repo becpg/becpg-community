1d65593f94cbaa50c4bf9adbec0d0b4253fdbbbe
Laborie Matthieu <matthieu.laborie@becpg.fr>
2017-11-06 14:20:52 +0100

    fix #3624

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/workflow/activiti/npd/CreateProduct.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/workflow/activiti/npd/CreateProduct.java
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  */
 package fr.becpg.repo.workflow.activiti.npd;
 
@@ -33,16 +33,17 @@ import fr.becpg.repo.helper.RepoService;
 import fr.becpg.repo.product.ProductService;
 import fr.becpg.repo.product.data.EffectiveFilters;
 import fr.becpg.repo.product.data.ProductData;
+import fr.becpg.repo.product.data.productList.CompoListDataItem;
+import fr.becpg.repo.product.data.productList.PackagingListDataItem;
 import fr.becpg.repo.repository.AlfrescoRepository;
 
 /**
  * Create the product based on data
- * 
+ *
  * @author "Matthieu Laborie <matthieu.laborie@becpg.fr>"
- * 
+ *
  */
 public class CreateProduct extends BaseJavaDelegate {
-
 
 	private final static Log logger = LogFactory.getLog(CreateProduct.class);
 
@@ -57,7 +58,6 @@ public class CreateProduct extends BaseJavaDelegate {
 	private ProductService productService;
 
 	private RepoService repoService;
-
 
 	public void setNodeService(NodeService nodeService) {
 		this.nodeService = nodeService;
@@ -82,132 +82,132 @@ public class CreateProduct extends BaseJavaDelegate {
 	@Override
 	public void execute(final DelegateExecution task) throws Exception {
 
-		 final NodeRef pkgNodeRef = ((ActivitiScriptNode) task.getVariable("bpm_package")).getNodeRef();
+		final NodeRef pkgNodeRef = ((ActivitiScriptNode) task.getVariable("bpm_package")).getNodeRef();
 
-		RunAsWork<Object> actionRunAs = new RunAsWork<Object>() {
-			@Override
-			public Object doWork() throws Exception {
-				try {
-				
-					NodeRef projectNodeRef = null;
-					
-					List<ChildAssociationRef> childAssocs = nodeService.getChildAssocs(pkgNodeRef, WorkflowModel.ASSOC_PACKAGE_CONTAINS, RegexQNamePattern.MATCH_ALL);
-					for (ChildAssociationRef childAssoc : childAssocs) {
-						if ( nodeService.getType(childAssoc.getChildRef()).equals(ProjectModel.TYPE_PROJECT)) {
-							projectNodeRef = childAssoc.getChildRef();
-							break;
-						}
-					}
-					
-					if(projectNodeRef==null){
-						throw new NullPointerException("Cannot find project into package");
-					}
-					
-					NodeRef sourceNodeRef = null;
-					QName targetType = PLMModel.TYPE_FINISHEDPRODUCT;
-					NodeRef recipeNodeRef = null;
-					NodeRef packagingNodeRef = null;
+		RunAsWork<Object> actionRunAs = () -> {
+			try {
 
-					String entityName = (String) task.getVariable("npdwf_npdProductName");
-					
-					if(entityName == null || entityName.isEmpty()){
-						// use project name as default
-						entityName = (String)nodeService.getProperty(projectNodeRef, ContentModel.PROP_NAME);
-					}
-					
-					ActivitiScriptNode productNode = (ActivitiScriptNode) task.getVariable(
-							"npdwf_needDefinitionProduct");
-					if (productNode != null) {
-						logger.debug("Product node exist");
-						sourceNodeRef = productNode.getNodeRef();
-						targetType = nodeService.getType(sourceNodeRef);
-					}
+				NodeRef projectNodeRef = null;
 
-					ActivitiScriptNode recipeNode = (ActivitiScriptNode) task.getVariable(
-							"npdwf_needDefinitionRecipeProduct");
-					if (recipeNode != null) {
-						logger.debug("Recipe node exist");
-						recipeNodeRef = recipeNode.getNodeRef();
-					} 
-
-					ActivitiScriptNode packagingNode = (ActivitiScriptNode) task.getVariable(
-							"npdwf_needDefinitionPackagingKit");
-					if (packagingNode != null) {
-						logger.debug("Packaging node exist");
-						packagingNodeRef = packagingNode.getNodeRef();
+				List<ChildAssociationRef> childAssocs = nodeService.getChildAssocs(pkgNodeRef, WorkflowModel.ASSOC_PACKAGE_CONTAINS,
+						RegexQNamePattern.MATCH_ALL);
+				for (ChildAssociationRef childAssoc : childAssocs) {
+					if (nodeService.getType(childAssoc.getChildRef()).equals(ProjectModel.TYPE_PROJECT)) {
+						projectNodeRef = childAssoc.getChildRef();
+						break;
 					}
-										
-					String productName = repoService.getAvailableName(projectNodeRef, entityName);
-					NodeRef productNodeRef = entityService.createOrCopyFrom(sourceNodeRef, projectNodeRef, targetType,
-							productName);
-					
-					// change state
-					nodeService.setProperty(productNodeRef, PLMModel.PROP_PRODUCT_STATE, SystemState.Simulation);
-					
-					// remove entityTpl (if we choose an entityTpl as source)
-					if(nodeService.hasAspect(productNodeRef, BeCPGModel.ASPECT_ENTITY_TPL)){
-						nodeService.removeAspect(productNodeRef, BeCPGModel.ASPECT_ENTITY_TPL);
-					}
-					
-					String localName = QName.createValidLocalName(productName);
-					QName qName = QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, localName);
-					nodeService.addChild(pkgNodeRef, productNodeRef, WorkflowModel.ASSOC_PACKAGE_CONTAINS, qName);				
-	
-					// Copy datalist
-					if (recipeNodeRef != null && nodeService.exists(recipeNodeRef)) {
-						logger.debug("Copy composition dataList");
-						copyDataList(productNodeRef, recipeNodeRef, PLMModel.TYPE_COMPOLIST);
-					} else {
-						logger.debug("No recipe node found");
-					}
-
-					if (packagingNodeRef != null && nodeService.exists(packagingNodeRef)) {
-						logger.debug("Copy packaging dataList");
-						copyDataList(productNodeRef, packagingNodeRef, PLMModel.TYPE_PACKAGINGLIST);
-					} else {
-						logger.debug("No packaging node found");
-					}
-
-					
-					logger.debug("Formulate product");
-					
-					try {
-						productService.formulate(productNodeRef);
-					} catch (Exception e) {
-						logger.debug(e,e); // newly created cannot be formulate
-					}
-
-					//Assoc product to project
-					nodeService.createAssociation(projectNodeRef, productNodeRef, ProjectModel.ASSOC_PROJECT_ENTITY);					
-					
-				} catch (Exception e) {
-					if (e instanceof ConcurrencyFailureException) {
-						throw (ConcurrencyFailureException) e;
-					}
-					logger.error("Failed to create product", e);
-					throw e;
 				}
 
-				return null;
+				if (projectNodeRef == null) {
+					throw new NullPointerException("Cannot find project into package");
+				}
+
+				NodeRef sourceNodeRef = null;
+				QName targetType = PLMModel.TYPE_FINISHEDPRODUCT;
+				NodeRef recipeNodeRef = null;
+				NodeRef packagingNodeRef = null;
+
+				String entityName = (String) task.getVariable("npdwf_npdProductName");
+
+				if ((entityName == null) || entityName.isEmpty()) {
+					// use project name as default
+					entityName = (String) nodeService.getProperty(projectNodeRef, ContentModel.PROP_NAME);
+				}
+
+				ActivitiScriptNode productNode = (ActivitiScriptNode) task.getVariable("npdwf_needDefinitionProduct");
+				if (productNode != null) {
+					logger.debug("Product node exist");
+					sourceNodeRef = productNode.getNodeRef();
+					targetType = nodeService.getType(sourceNodeRef);
+				}
+
+				ActivitiScriptNode recipeNode = (ActivitiScriptNode) task.getVariable("npdwf_needDefinitionRecipeProduct");
+				if (recipeNode != null) {
+					logger.debug("Recipe node exist");
+					recipeNodeRef = recipeNode.getNodeRef();
+				}
+
+				ActivitiScriptNode packagingNode = (ActivitiScriptNode) task.getVariable("npdwf_needDefinitionPackagingKit");
+				if (packagingNode != null) {
+					logger.debug("Packaging node exist");
+					packagingNodeRef = packagingNode.getNodeRef();
+				}
+
+				String productName = repoService.getAvailableName(projectNodeRef, entityName);
+				NodeRef productNodeRef = entityService.createOrCopyFrom(sourceNodeRef, projectNodeRef, targetType, productName);
+
+				// change state
+				nodeService.setProperty(productNodeRef, PLMModel.PROP_PRODUCT_STATE, SystemState.Simulation);
+
+				// remove entityTpl (if we choose an entityTpl as source)
+				if (nodeService.hasAspect(productNodeRef, BeCPGModel.ASPECT_ENTITY_TPL)) {
+					nodeService.removeAspect(productNodeRef, BeCPGModel.ASPECT_ENTITY_TPL);
+				}
+
+				String localName = QName.createValidLocalName(productName);
+				QName qName = QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, localName);
+				nodeService.addChild(pkgNodeRef, productNodeRef, WorkflowModel.ASSOC_PACKAGE_CONTAINS, qName);
+
+				// Copy datalist
+				if ((recipeNodeRef != null) && nodeService.exists(recipeNodeRef)) {
+					logger.debug("Copy composition dataList");
+					copyDataList(productNodeRef, recipeNodeRef, PLMModel.TYPE_COMPOLIST);
+				} else {
+					logger.debug("No recipe node found");
+				}
+
+				if ((packagingNodeRef != null) && nodeService.exists(packagingNodeRef)) {
+					logger.debug("Copy packaging dataList");
+					copyDataList(productNodeRef, packagingNodeRef, PLMModel.TYPE_PACKAGINGLIST);
+				} else {
+					logger.debug("No packaging node found");
+				}
+
+				logger.debug("Formulate product");
+
+				try {
+					productService.formulate(productNodeRef);
+				} catch (Exception e1) {
+					logger.debug(e1, e1); // newly created cannot be formulate
+				}
+
+				// Assoc product to project
+				nodeService.createAssociation(projectNodeRef, productNodeRef, ProjectModel.ASSOC_PROJECT_ENTITY);
+
+			} catch (Exception e2) {
+				if (e2 instanceof ConcurrencyFailureException) {
+					throw (ConcurrencyFailureException) e2;
+				}
+				logger.error("Failed to create product", e2);
+				throw e2;
 			}
 
-
+			return null;
 		};
 		AuthenticationUtil.runAsSystem(actionRunAs);
 
 	}
 
 	private void copyDataList(NodeRef productNodeRef, NodeRef sourceNodeRef, QName typeCompolist) {
-	
-		 ProductData productData  = alfrescoRepository.findOne(productNodeRef);
-		 ProductData sourceData = alfrescoRepository.findOne(sourceNodeRef);
-		 if (typeCompolist.equals(PLMModel.TYPE_PACKAGINGLIST)) {
- 			productData.getPackagingListView().setPackagingList(sourceData.getPackagingList(new EffectiveFilters<>(EffectiveFilters.FUTUR)));
- 		 }
-		 else if (typeCompolist.equals(PLMModel.TYPE_COMPOLIST)) {
- 			productData.getCompoListView().setCompoList(sourceData.getCompoList(new EffectiveFilters<>(EffectiveFilters.FUTUR)));
- 		 }
-		 alfrescoRepository.save(productData);
+
+		ProductData productData = alfrescoRepository.findOne(productNodeRef);
+		ProductData sourceData = alfrescoRepository.findOne(sourceNodeRef);
+		if (typeCompolist.equals(PLMModel.TYPE_PACKAGINGLIST)) {
+
+			for (PackagingListDataItem packagingListDataItem : sourceData.getPackagingList(new EffectiveFilters<>(EffectiveFilters.FUTUR))) {
+				PackagingListDataItem toAdd = packagingListDataItem.clone();
+				toAdd.setNodeRef(null);
+				productData.getPackagingList().add(toAdd);
+			}
+		} else if (typeCompolist.equals(PLMModel.TYPE_COMPOLIST)) {
+
+			for (CompoListDataItem compoListDataItem : sourceData.getCompoList(new EffectiveFilters<>(EffectiveFilters.FUTUR))) {
+				CompoListDataItem toAdd = compoListDataItem.clone();
+				toAdd.setNodeRef(null);
+				productData.getCompoList().add(toAdd);
+			}
+		}
+		alfrescoRepository.save(productData);
 
 	}
 
