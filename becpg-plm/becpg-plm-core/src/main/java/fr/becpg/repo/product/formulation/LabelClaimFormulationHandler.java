2b25daf46229f9d6e2ddd92db543a3b0af5bea29
Steven BihouÃ© <steven.bihoue@becpg.fr>
2018-01-19 12:37:39 +0100

    refs #3881

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/LabelClaimFormulationHandler.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/LabelClaimFormulationHandler.java
@@ -151,7 +151,7 @@ public class LabelClaimFormulationHandler extends FormulationBaseHandler<Product
 						}
 						break;
 					case LabelClaimListDataItem.VALUE_FALSE:
-						if (labelClaimItem.getIsClaimed() || (labelClaimItem.getLabelClaimValue() == null)) {
+						if (labelClaimItem.getIsClaimed() || (labelClaimItem.getLabelClaimValue() == null) || LabelClaimListDataItem.VALUE_NA.toString().equals(labelClaimItem.getLabelClaimValue())) {
 							labelClaimItem.setIsClaimed(false);
 						}
 						if (!labelClaimItem.getIsFormulated()) {
