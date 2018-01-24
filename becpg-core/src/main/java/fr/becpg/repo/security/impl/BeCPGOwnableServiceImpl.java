601b926fefc3eec9b97e80d12c01cd354b3600b1
Laborie Matthieu <matthieu.laborie@becpg.fr>
2018-01-24 12:20:40 +0100

    fix #3874

--- becpg-core/src/main/java/fr/becpg/repo/security/impl/BeCPGOwnableServiceImpl.java
+++ becpg-core/src/main/java/fr/becpg/repo/security/impl/BeCPGOwnableServiceImpl.java
@@ -1,25 +1,130 @@
 package fr.becpg.repo.security.impl;
 
+import java.util.Collections;
+import java.util.Set;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.cache.SimpleCache;
 import org.alfresco.repo.ownable.impl.OwnableServiceImpl;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.tenant.TenantService;
+import org.alfresco.service.cmr.rendition.RenditionService;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.repository.datatype.DefaultTypeConverter;
 import org.alfresco.service.cmr.security.OwnableService;
+
+import fr.becpg.repo.entity.EntityDictionaryService;
 
 public class BeCPGOwnableServiceImpl extends OwnableServiceImpl {
 
-	
 	private boolean disableOwner = false;
 
 	public void setDisableOwner(boolean disableOwner) {
 		this.disableOwner = disableOwner;
 	}
+
+	
+	private EntityDictionaryService entityDictionaryService;
 	
 	
+	
+	public void setEntityDictionaryService(EntityDictionaryService entityDictionaryService) {
+		this.entityDictionaryService = entityDictionaryService;
+	}
+
+
+	private NodeService nodeService;
+	private SimpleCache<NodeRef, String> nodeOwnerCache;
+	private TenantService tenantService;
+	private Set<String> storesToIgnorePolicies = Collections.emptySet();
+	private RenditionService renditionService;
+	
+	
+
+	public BeCPGOwnableServiceImpl() {
+		super();
+	}
+
+	// IOC
+
+	public void setNodeService(NodeService nodeService) {
+		this.nodeService = nodeService;
+		super.setNodeService(nodeService);
+	}
+
+
+	public void setTenantService(TenantService tenantService) {
+		this.tenantService = tenantService;
+		super.setTenantService(tenantService);
+	}
+
+	public void setStoresToIgnorePolicies(Set<String> storesToIgnorePolicies) {
+		this.storesToIgnorePolicies = storesToIgnorePolicies;
+		super.setStoresToIgnorePolicies(storesToIgnorePolicies);
+	}
+
+	/**
+	 * @param ownerCache
+	 *            a transactionally-safe cache of node owners
+	 */
+	public void setNodeOwnerCache(SimpleCache<NodeRef, String> ownerCache) {
+		this.nodeOwnerCache = ownerCache;
+		super.setNodeOwnerCache(ownerCache);
+	}
+
+	/**
+	 * @param renditionService
+	 *            the renditionService to set
+	 */
+	public void setRenditionService(RenditionService renditionService) {
+		this.renditionService = renditionService;
+		super.setRenditionService(renditionService);
+	}
+
+
+	// OwnableService implementation
+
 	@Override
 	public String getOwner(NodeRef nodeRef) {
-		if(disableOwner) {
-			return OwnableService.NO_OWNER;
+		String userName = nodeOwnerCache.get(nodeRef);
+
+		if (userName == null) {
+			
+			if (userName == null) {
+
+				// If ownership is not explicitly set then we fall back to the
+				// creator
+				if (isRendition(nodeRef)) {
+					userName = getOwner(nodeService.getPrimaryParent(nodeRef).getParentRef());
+				} else if (nodeService.hasAspect(nodeRef, ContentModel.ASPECT_OWNABLE)) {
+					userName = DefaultTypeConverter.INSTANCE.convert(String.class, nodeService.getProperty(nodeRef, ContentModel.PROP_OWNER));
+				} else if(disableOwner && !entityDictionaryService.isSubClass(nodeService.getType(nodeRef),  ContentModel.TYPE_PERSON)) {
+					userName = OwnableService.NO_OWNER;
+				} else if (nodeService.hasAspect(nodeRef, ContentModel.ASPECT_AUDITABLE) ) {
+					userName = DefaultTypeConverter.INSTANCE.convert(String.class, nodeService.getProperty(nodeRef, ContentModel.PROP_CREATOR));
+				} 
+			}
+			cacheOwner(nodeRef, userName);
 		}
-		
-		return super.getOwner(nodeRef);
+
+		return userName;
 	}
+
+
+
+
+	private void cacheOwner(NodeRef nodeRef, String userName) {
+		// do not cache owners of nodes that are from stores that ignores
+		// policies
+		// to prevent mess in nodeOwnerCache
+		if (!storesToIgnorePolicies.contains(tenantService.getBaseName(nodeRef.getStoreRef()).toString())) {
+			nodeOwnerCache.put(nodeRef, userName);
+		}
+	}
+
+	private boolean isRendition(final NodeRef node) {
+		return AuthenticationUtil.runAs(() -> renditionService.isRendition(node), AuthenticationUtil.getSystemUserName());
+	}
+
 }
