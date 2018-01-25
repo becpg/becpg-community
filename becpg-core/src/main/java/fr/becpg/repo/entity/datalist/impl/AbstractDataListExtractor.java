f20e9ba1a6a65767c5bd5a90dcd532d9ee7fe07d
Laborie Matthieu <matthieu.laborie@becpg.fr>
2018-01-25 10:48:00 +0100

    refs #3889

--- becpg-core/src/main/java/fr/becpg/repo/entity/datalist/impl/AbstractDataListExtractor.java
+++ becpg-core/src/main/java/fr/becpg/repo/entity/datalist/impl/AbstractDataListExtractor.java
@@ -184,7 +184,8 @@ public abstract class AbstractDataListExtractor implements DataListExtractor {
 			userAccess.put("delete", accessRight && (permissionService.hasPermission(nodeRef, "Delete") == AccessStatus.ALLOWED));
 			userAccess.put("create", accessRight && (permissionService.hasPermission(nodeRef, "CreateChildren") == AccessStatus.ALLOWED));
 			userAccess.put("edit", accessRight && (permissionService.hasPermission(nodeRef, "Write") == AccessStatus.ALLOWED));
-			userAccess.put("sort", accessRight && nodeService.hasAspect(nodeRef, BeCPGModel.ASPECT_SORTABLE_LIST));
+			userAccess.put("sort", accessRight && (permissionService.hasPermission(nodeRef, "Write") == AccessStatus.ALLOWED) 
+					&& nodeService.hasAspect(nodeRef, BeCPGModel.ASPECT_SORTABLE_LIST));
 			userAccess.put("details", accessRight && isDetaillable(nodeRef));
 			userAccess.put("wused", accessRight);
 
