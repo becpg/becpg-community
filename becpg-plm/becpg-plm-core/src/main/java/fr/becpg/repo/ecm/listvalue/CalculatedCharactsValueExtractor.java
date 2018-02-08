0b35c97616505c19b34524856c24129fd4360f1c
Steven BihouÃ© <steven.bihoue@becpg.fr>
2018-02-08 14:46:14 +0100

    refs #3911

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/ecm/listvalue/CalculatedCharactsValueExtractor.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/ecm/listvalue/CalculatedCharactsValueExtractor.java
@@ -1,27 +1,23 @@
 package fr.becpg.repo.ecm.listvalue;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.namespace.NamespaceService;
-import org.alfresco.service.namespace.QName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import fr.becpg.model.BeCPGModel;
-import fr.becpg.model.PLMModel;
 import fr.becpg.repo.entity.EntityDictionaryService;
+import fr.becpg.repo.entity.EntityListDAO;
 import fr.becpg.repo.helper.AttributeExtractorService;
 import fr.becpg.repo.listvalue.ListValueEntry;
-import fr.becpg.repo.listvalue.ListValueExtractor;
+import fr.becpg.repo.listvalue.impl.TargetAssocValueExtractor;
 
 @Service("calculatedCharactsValueExtractor")
-public class CalculatedCharactsValueExtractor implements ListValueExtractor<NodeRef> {
+public class CalculatedCharactsValueExtractor extends TargetAssocValueExtractor {
 
 	@Autowired
 	private NodeService nodeService;
@@ -35,53 +31,35 @@ public class CalculatedCharactsValueExtractor implements ListValueExtractor<Node
 	@Autowired
 	private EntityDictionaryService entityDictionaryService;
 
+	@Autowired
+	private EntityListDAO entityListDAO;
+
 	@Override
 	public List<ListValueEntry> extract(List<NodeRef> nodeRefs) {
 		List<ListValueEntry> suggestions = new ArrayList<>();
 		if (nodeRefs != null) {
-			for (NodeRef nodeRef : nodeRefs) {
-				QName type = nodeService.getType(nodeRef);
-				String name = attributeExtractorService.extractPropName(type, nodeRef);
-				String cssClass = attributeExtractorService.extractMetadata(type, nodeRef);
-				Map<String, String> props = new HashMap<>(2);
-				props.put("type", type.toPrefixString(namespaceService));
+			suggestions = super.extract(nodeRefs);
 
-				String parentName = "";
-				if (PLMModel.TYPE_LABELINGRULELIST.equals(nodeService.getType(nodeRef))) {
-					parentName = getParentOfLabellingListValue(nodeRef) + " > ";
+			for (ListValueEntry entry : suggestions) {
+				if ("labelingRuleList".equals(entry.getCssClass())) {
+					entry.setName(getParentOfLabellingListValue(new NodeRef(entry.getValue())) + " > " + entry.getName());
 				}
-
-				if (nodeService.hasAspect(nodeRef, BeCPGModel.ASPECT_COLOR)) {
-					props.put("color", (String) nodeService.getProperty(nodeRef, BeCPGModel.PROP_COLOR));
-				}
-				
-				if (nodeService.hasAspect(nodeRef, ContentModel.ASPECT_TITLED)) {
-					props.put("title", (String) nodeService.getProperty(nodeRef, ContentModel.PROP_TITLE));
-					props.put("description", (String) nodeService.getProperty(nodeRef, ContentModel.PROP_DESCRIPTION));
-				} else {
-					name = parentName + name;
-				}
-
-				ListValueEntry entry = new ListValueEntry(nodeRef.toString(), name, cssClass, props);
-
-				suggestions.add(entry);
 			}
 		}
 		return suggestions;
 	}
 
 	private String getParentOfLabellingListValue(NodeRef value) {
-		String ret = null;
 
-		NodeRef parent = nodeService.getPrimaryParent(value).getParentRef();
-		while (!entityDictionaryService.isSubClass(nodeService.getType(parent), PLMModel.TYPE_PRODUCT)) {
-			parent = nodeService.getPrimaryParent(parent).getParentRef();
-		}
+		String ret = "";
+		NodeRef parent = entityListDAO.getEntity(value);
 
-		if (nodeService.getProperty(parent, ContentModel.PROP_TITLE) != null) {
-			ret = (String) nodeService.getProperty(parent, ContentModel.PROP_TITLE);
-		} else {
-			ret = (String) nodeService.getProperty(parent, ContentModel.PROP_NAME);
+		if (parent != null) {
+			if (nodeService.getProperty(parent, ContentModel.PROP_TITLE) != null) {
+				ret = (String) nodeService.getProperty(parent, ContentModel.PROP_TITLE);
+			} else {
+				ret = (String) nodeService.getProperty(parent, ContentModel.PROP_NAME);
+			}
 		}
 
 		return ret;
