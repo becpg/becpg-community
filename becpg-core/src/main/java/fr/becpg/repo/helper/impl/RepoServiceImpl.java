b4d029c0e1c35f774d80090e34b0d109b7259888
Laborie Matthieu <matthieu.laborie@becpg.fr>
2018-03-14 11:07:47 +0100

    fix #3433

--- becpg-core/src/main/java/fr/becpg/repo/helper/impl/RepoServiceImpl.java
+++ becpg-core/src/main/java/fr/becpg/repo/helper/impl/RepoServiceImpl.java
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  */
 package fr.becpg.repo.helper.impl;
 
@@ -26,6 +26,7 @@ import fr.becpg.repo.RepoConsts;
 import fr.becpg.repo.helper.PropertiesHelper;
 import fr.becpg.repo.helper.RepoService;
 import fr.becpg.repo.search.BeCPGQueryBuilder;
+import fr.becpg.repo.search.impl.AbstractBeCPGQueryBuilder;
 
 /**
  * The Class RepoServiceImpl.
@@ -34,73 +35,78 @@ import fr.becpg.repo.search.BeCPGQueryBuilder;
  */
 @Service("repoService")
 public class RepoServiceImpl implements RepoService {
-	
-	
+
 	/** The logger. */
 	private static final Log logger = LogFactory.getLog(RepoServiceImpl.class);
 
 	@Autowired
 	private final NodeService nodeService = null;
-	
+
 	@Autowired
 	private FileFolderService fileFolderService;
-	
+
 	@Autowired
 	private Repository repository;
 
-	
 	@Autowired
 	private NamespaceService namespaceService;
 
-
-	/* (non-Javadoc)
-	 * @see fr.becpg.repo.helper.RepoService#createFolderByPaths(org.alfresco.service.cmr.repository.NodeRef, java.util.List)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.becpg.repo.helper.RepoService#createFolderByPaths(org.alfresco.service
+	 * .cmr.repository.NodeRef, java.util.List)
 	 */
 	@Override
-	public NodeRef getOrCreateFolderByPaths(NodeRef parentNodeRef, List<String> paths) {			    
-		
-    	for(String folderName : paths){
-    		if(folderName!=null && !folderName.isEmpty()){    				    		    		
-    			parentNodeRef = getOrCreateFolderByPath(parentNodeRef, folderName, folderName);    		
-    		}
-    	}
-		
+	public NodeRef getOrCreateFolderByPaths(NodeRef parentNodeRef, List<String> paths) {
+
+		for (String folderName : paths) {
+			if ((folderName != null) && !folderName.isEmpty()) {
+				parentNodeRef = getOrCreateFolderByPath(parentNodeRef, folderName, folderName);
+			}
+		}
+
 		return parentNodeRef;
 	}
-	
-	/* (non-Javadoc)
-	 * @see fr.becpg.repo.helper.RepoService#createFolderByPath(org.alfresco.service.cmr.repository.NodeRef, java.lang.String, java.lang.String)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.becpg.repo.helper.RepoService#createFolderByPath(org.alfresco.service.
+	 * cmr.repository.NodeRef, java.lang.String, java.lang.String)
 	 */
 	@Override
 	public NodeRef getOrCreateFolderByPath(NodeRef parentNodeRef, String path, String name) {
-		
-		NodeRef folderNodeRef = getFolderByPath(parentNodeRef, path);
-		
-		if(folderNodeRef == null){
-			logger.debug("Create folder : " + name + "  ");
 
-			Map<QName, Serializable> properties = new HashMap<>();
-	    	properties.put(ContentModel.PROP_NAME, PropertiesHelper.cleanFolderName(name));	    		    	
-	    	folderNodeRef = nodeService.createNode(parentNodeRef, ContentModel.ASSOC_CONTAINS, 
-	    											QName.resolveToQName(namespaceService, path), 
-	    											ContentModel.TYPE_FOLDER, properties).getChildRef();	    		 
-		}		
+		NodeRef folderNodeRef = getFolderByPath(parentNodeRef, path);
+
+		if (folderNodeRef == null) {
+			logger.debug("Create folder : " + name + "  ");
+			folderNodeRef = nodeService.getChildByName(parentNodeRef, ContentModel.ASSOC_CONTAINS, PropertiesHelper.cleanFolderName(name));
+			if (folderNodeRef == null) {
+				Map<QName, Serializable> properties = new HashMap<>();
+				properties.put(ContentModel.PROP_NAME, PropertiesHelper.cleanFolderName(name));
+				folderNodeRef = nodeService.createNode(parentNodeRef, ContentModel.ASSOC_CONTAINS, QName.resolveToQName(namespaceService, path),
+						ContentModel.TYPE_FOLDER, properties).getChildRef();
+			}
+		}
 
 		return folderNodeRef;
 	}
 
 	@Override
 	public NodeRef getFolderByPath(NodeRef parentNodeRef, String path) {
-		
-		NodeRef ret = BeCPGQueryBuilder.createQuery().selectNodeByPath(parentNodeRef,BeCPGQueryBuilder.encodePath(path));
-		if(ret==null && !path.contains(RepoConsts.MODEL_PREFIX_SEPARATOR)){
-			//try by name...
+
+		NodeRef ret = BeCPGQueryBuilder.createQuery().selectNodeByPath(parentNodeRef, AbstractBeCPGQueryBuilder.encodePath(path));
+		if ((ret == null) && !path.contains(RepoConsts.MODEL_PREFIX_SEPARATOR)) {
+			// try by name...
 			return nodeService.getChildByName(parentNodeRef, ContentModel.ASSOC_CONTAINS, path);
 		}
-		
+
 		return ret;
 	}
-	
 
 	@Override
 	public NodeRef getFolderByPath(String path) {
@@ -111,7 +117,7 @@ public class RepoServiceImpl implements RepoService {
 	public void moveNode(NodeRef nodeRefToMove, NodeRef destionationNodeRef) {
 
 		logger.debug("start moveNode");
-		
+
 		// check the nodeRefToMove is not already moved !
 		NodeRef parentOfNodeRefToMove = nodeService.getPrimaryParent(nodeRefToMove).getParentRef();
 		if (destionationNodeRef.equals(parentOfNodeRefToMove)) {
@@ -121,7 +127,7 @@ public class RepoServiceImpl implements RepoService {
 		}
 
 		// Check there is not a node with the same name, then rename node
-		String name = getAvailableName(destionationNodeRef, (String) nodeService.getProperty(nodeRefToMove, ContentModel.PROP_NAME));		
+		String name = getAvailableName(destionationNodeRef, (String) nodeService.getProperty(nodeRefToMove, ContentModel.PROP_NAME));
 
 		if (logger.isDebugEnabled()) {
 			logger.debug(String.format("Move node '%s' in folder '%s'", name, destionationNodeRef));
@@ -139,23 +145,23 @@ public class RepoServiceImpl implements RepoService {
 
 	@Override
 	public String getAvailableName(NodeRef folderNodeRef, String name) {
-		
+
 		List<FileInfo> fileInfos = fileFolderService.list(folderNodeRef);
 		if (!fileInfos.isEmpty()) {
 			int count = 0;
 			NodeRef nodeRef = nodeService.getChildByName(folderNodeRef, ContentModel.ASSOC_CONTAINS, name);
 
-			while (nodeRef!=null) {
-				count++;				
+			while (nodeRef != null) {
+				count++;
 				String nameWithCounter = String.format("%s (%d)", name, count);
 				nodeRef = nodeService.getChildByName(folderNodeRef, ContentModel.ASSOC_CONTAINS, nameWithCounter);
 			}
-			
+
 			if (count > 0) {
 				name = String.format("%s (%d)", name, count);
 			}
 		}
-		
+
 		return name;
 	}
 
