9fddde5517022ee898016e7f855905597c5ca1ae
Laborie Matthieu <matthieu.laborie@becpg.fr>
2018-06-05 15:45:55 +0200

    refs #3824

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/rounding/NutrientRoundingRules.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/rounding/NutrientRoundingRules.java
@@ -12,396 +12,360 @@ import org.apache.commons.logging.LogFactory;
 import org.dom4j.Element;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.util.StringUtils;
 
-import fr.becpg.model.PLMModel;
 import fr.becpg.repo.helper.MLTextHelper;
 import fr.becpg.repo.product.data.productList.NutListDataItem;
 
 public class NutrientRoundingRules {
 	protected static final Log logger = LogFactory.getLog(NutrientRoundingRules.class);
-	
-	
-	
-	public enum NutrientRoundingRuleType {
-	 NRJ, Fat, SatFat, Su, Fiber, P, Na, Salt, Cholesterol, K;
-	};
-	
-	private interface RoundingRule {
-		Double round(Double value , Locale locale);
-	}
-	
-	
-	public static List<Locale>  getAvailableLocales() {
-		List<Locale> ret = new ArrayList<Locale>();		
-		ret.add(Locale.FRENCH); 
 
-		if(MLTextHelper.getSupportedLocales().contains(Locale.US)){
+	public enum NutrientRoundingRuleType {
+		NRJ, Fat, SatFat, Su, Fiber, P, Na, Salt, Cholesterol, K;
+	};
+
+	private interface RoundingRule {
+		Double round(Double value, Locale locale);
+	}
+
+	public static List<Locale> getAvailableLocales() {
+		List<Locale> ret = new ArrayList<>();
+		ret.add(Locale.FRENCH);
+
+		if (MLTextHelper.getSupportedLocales().contains(Locale.US)) {
 			ret.add(Locale.US);
 		}
-		
+
 		return ret;
 	}
-	
+
 	static Map<NutrientRoundingRuleType, RoundingRule> rules = new LinkedHashMap<>();
-	
+
 	static {
-		rules.put(NutrientRoundingRuleType.NRJ, new RoundingRule() {
-
-			@Override
-			public Double round(Double value, Locale locale) {
-				if(Locale.US.equals(locale)){
-					return nearByValueNRJUS(value);
-				}
-				return  (double) Math.round(value);
-			}		
-		});
-		
-		rules.put(NutrientRoundingRuleType.Fat, new RoundingRule() {
-
-			@Override
-			public Double round(Double value, Locale locale) {
-				
-				if(Locale.US.equals(locale)){
-					
-					return nearByValueUS(value);	
-				}
-			    return nearByValueEur(value, 0.5);
-			}	
-		});
-		
-		rules.put(NutrientRoundingRuleType.SatFat, new RoundingRule() {
-
-			@Override
-			public Double round(Double value, Locale locale) {
-				if(Locale.US.equals(locale)){
-					return nearByValueUS(value);
-				} 	
-				return nearByValueEur(value,0.1);
-			}	
-		});
-				
-		rules.put(NutrientRoundingRuleType.Su, new RoundingRule() {
-
-			@Override
-			public Double round(Double value, Locale locale) {
-				if(Locale.US.equals(locale)){
-					return nearByValueSuFiberPUS (value); 
-				}
-				return nearByValueEur(value, 0.5);
+		rules.put(NutrientRoundingRuleType.NRJ, (value, locale) -> {
+			if (Locale.US.equals(locale)) {
+				return nearByValueNRJUS(value);
 			}
+			return (double) Math.round(value);
 		});
-		
-		rules.put(NutrientRoundingRuleType.Fiber, new RoundingRule() {
 
-			@Override
-			public Double round(Double value, Locale locale) {
-				if(Locale.US.equals(locale)){
-					return nearByValueSuFiberPUS (value);					
-				}
-				return nearByValueEur(value, 0.5);
-			}	
-		});
-		
-		rules.put(NutrientRoundingRuleType.P, new RoundingRule() {
+		rules.put(NutrientRoundingRuleType.Fat, (value, locale) -> {
 
-			@Override
-			public Double round(Double value, Locale locale) {
-				if(Locale.US.equals(locale)){
-					return nearByValueSuFiberPUS (value);
-				}
-				return nearByValueEur(value, 0.5);
+			if (Locale.US.equals(locale)) {
+
+				return nearByValueUS(value);
 			}
-			
+			return nearByValueEur(value, 0.5);
 		});
-		rules.put(NutrientRoundingRuleType.Na, new RoundingRule() {
 
-			@Override
-			public Double round(Double value, Locale locale) {
-				if(Locale.US.equals(locale)){
-					return nearByValueNaUS(value);
-				}
-				return nearByValueNaSaltEur(value, 0.005);
+		rules.put(NutrientRoundingRuleType.SatFat, (value, locale) -> {
+			if (Locale.US.equals(locale)) {
+				return nearByValueUS(value);
 			}
+			return nearByValueEur(value, 0.1);
 		});
-		
-		rules.put(NutrientRoundingRuleType.Salt, new RoundingRule() {
 
-			@Override
-			public Double round(Double value, Locale locale) {
-				if(Locale.US.equals(locale)){
-					return value;
-				}
-				return nearByValueNaSaltEur(value, 0.0125 );
+		rules.put(NutrientRoundingRuleType.Su, (value, locale) -> {
+			if (Locale.US.equals(locale)) {
+				return nearByValueSuFiberPUS(value);
 			}
+			return nearByValueEur(value, 0.5);
 		});
-		rules.put(NutrientRoundingRuleType.Cholesterol, new RoundingRule() {
 
-			@Override
-			public Double round(Double value, Locale locale) {
-				if(Locale.US.equals(locale)){
-					return nearByValuecholesterolUS(value);
-				}
+		rules.put(NutrientRoundingRuleType.Fiber, (value, locale) -> {
+			if (Locale.US.equals(locale)) {
+				return nearByValueSuFiberPUS(value);
+			}
+			return nearByValueEur(value, 0.5);
+		});
+
+		rules.put(NutrientRoundingRuleType.P, (value, locale) -> {
+			if (Locale.US.equals(locale)) {
+				return nearByValueSuFiberPUS(value);
+			}
+			return nearByValueEur(value, 0.5);
+		});
+		rules.put(NutrientRoundingRuleType.Na, (value, locale) -> {
+			if (Locale.US.equals(locale)) {
+				return nearByValueNaUS(value);
+			}
+			return nearByValueNaSaltEur(value, 0.005);
+		});
+
+		rules.put(NutrientRoundingRuleType.Salt, (value, locale) -> {
+			if (Locale.US.equals(locale)) {
 				return value;
 			}
+			return nearByValueNaSaltEur(value, 0.0125);
 		});
-		
-		rules.put(NutrientRoundingRuleType.K, new RoundingRule() {
-
-			@Override
-			public Double round(Double value, Locale locale) {
-				if(Locale.US.equals(locale)){
-					return nearByValueNaUS(value);
-				}
-				return value;
+		rules.put(NutrientRoundingRuleType.Cholesterol, (value, locale) -> {
+			if (Locale.US.equals(locale)) {
+				return nearByValuecholesterolUS(value);
 			}
-		});	
-	}	
-	
-	public static void extractValue(Element nutListElt, String roundingValue){
+			return value;
+		});
 
-		Element roundedElement = nutListElt.addElement(PLMModel.PROP_NUTLIST_ROUNDED_VALUE.getLocalName());
-		JSONObject obj1 = null;
-		try {
-			obj1 = new JSONObject(roundingValue);
-			for(Iterator<?> i=obj1.keys(); i.hasNext();){
-				String key1 = (String) i.next();
-				JSONObject obj2 = new JSONObject(obj1.get(key1));
-		
-				for (Iterator<?> j = obj2.keys(); j.hasNext();) {		
-					String key2 = (String) j.next();
-				    String val = (String) obj2.get(key2);
-				    roundedElement.addAttribute(key2, val);
-				  }
+		rules.put(NutrientRoundingRuleType.K, (value, locale) -> {
+			if (Locale.US.equals(locale)) {
+				return nearByValueNaUS(value);
 			}
-		
-		}catch (JSONException e) {
-			logger.error(e,e);
-		}
+			return value;
+		});
 	}
 
-	public static String extractRoundedValue(NutListDataItem n, String roundingMode) {
-		
-		JSONObject jsonRound = new JSONObject();
-		
-		try {
-				Double nutValRound = 0.d;
-				Double nutMiniRound = 0.d;
-				Double nutMaxiRound = 0.d;
-				Double nutValuePerServingRound = 0.d;
-				Double nutGdaRound = 0.d;
-				
-				JSONObject value = new JSONObject();
-				JSONObject mini = new JSONObject();
-				JSONObject maxi = new JSONObject();
-				JSONObject valuePerServing = new JSONObject();
-				JSONObject gda = new JSONObject();
-				
-				for(Locale locale :  NutrientRoundingRules.getAvailableLocales()){
-					String nutUnit= n.getUnit();
-					nutValRound = NutrientRoundingRules.round(n.getValue(), roundingMode, locale, nutUnit);
-					nutMiniRound = NutrientRoundingRules.round(n.getMini(), roundingMode, locale, nutUnit);
-					nutMaxiRound = NutrientRoundingRules.round(n.getMaxi(), roundingMode, locale, nutUnit);
-					nutValuePerServingRound = NutrientRoundingRules.round(n.getValuePerServing(), roundingMode, locale, nutUnit);
-					nutGdaRound = NutrientRoundingRules.round(n.getGdaPerc(), roundingMode, locale, nutUnit);
-					
-					if (!Locale.FRENCH.equals(locale)){
-						
-						value.put(locale.getLanguage(),nutValRound);
-						mini.put(locale.getLanguage(), nutMiniRound);
-						maxi.put(locale.getLanguage(), nutMaxiRound);
-						valuePerServing.put(locale.getLanguage(), nutValuePerServingRound);
-						gda.put(locale.getLanguage(),  nutGdaRound);
-					}
-					else{
-						value.put("default", nutValRound);
-						mini.put("default", nutMiniRound);
-						maxi.put("default", nutMaxiRound);
-						valuePerServing.put("default", nutValuePerServingRound);
-						gda.put("default",  nutGdaRound);
+	public static Number extractValue(String roundedValue, Locale locale) {
 
-					}
-					jsonRound.put("value" ,value);
-					jsonRound.put("mini", mini);
-					jsonRound.put("maxi", maxi);
-					jsonRound.put("valuePerServing", valuePerServing);
-					jsonRound.put("gda", gda);
-					
-				}
-	
-			} catch (JSONException e) {
-				logger.error(e,e);
-			}
-		return jsonRound.toString();
+		return extractValueByKey(roundedValue, "value", locale);
 	}
 
-	
-//	RoundingRole method for Fat according to european guide	in Kcal
-	private  static Double nearByValueNRJUS (Double value){
-		
-		if (value==null){
-			return null;
+	private static Number extractValueByKey(String roundedValue, String item, Locale locale) {
+		String key = getLocalKey(locale);
+		JSONObject jsonRound;
+		try {
+			jsonRound = new JSONObject(roundedValue);
+
+			if (jsonRound.has(item)) {
+				JSONObject value = (JSONObject) jsonRound.get(item);
+				if (value.has(key)) {
+					return (Number) value.get(key);
+				}
+			}
+
+		} catch (JSONException e) {
+			logger.error(e, e);
 		}
-		else if (value >0.05){
-			return (double) (0.01* (int)(Math.ceil(value /0.01)));
-		}
-		else if (value >=5 && value <= 50){
-			return (double) (0.005* (int)(Math.ceil(value /0.005)));
-		}
-		else if (value<0.005){
-		
-			return 0.0;
-		}
-		
+
 		return null;
 	}
 
-//	RoundingRole method for Fat according to european guide in g
-	private static Double nearByValueEur (Double value, Double minValue){
-		if (value==null){
-			return null;
+	public static Number extractValuePerServing(String roundedValue, Locale locale) {
+		return extractValueByKey(roundedValue, "valuePerServing", locale);
+	}
+
+	public static Number extractMini(String roundedValue, Locale locale) {
+		return extractValueByKey(roundedValue, "mini", locale);
+	}
+
+	public static Number extractMaxi(String roundedValue, Locale locale) {
+		return extractValueByKey(roundedValue, "maxi", locale);
+	}
+
+	private static String getLocalKey(Locale locale) {
+		String key = MLTextHelper.localeKey(locale);
+		if (!Locale.FRENCH.equals(locale)) {
+			key = "default";
 		}
-		else if (value <= minValue){
+
+		return key;
+	}
+
+	public static void extractXMLAttribute(Element nutListElt, String roundedValue, Locale locale) {
+		if(roundedValue!=null) {
+			String localKey = getLocalKey(locale);
+			try {
+				JSONObject jsonRound = new JSONObject(roundedValue);
+				for (Iterator<?> i = jsonRound.keys(); i.hasNext();) {
+					String valueKey = (String) i.next();
+					JSONObject value = (JSONObject) jsonRound.get(valueKey);
+					if (value.has(localKey)) {
+						nutListElt.addAttribute("rounded"+StringUtils.capitalize(valueKey), "" + value.get(localKey));
+					}
+				}
+			} catch (JSONException e) {
+				logger.error(e, e);
+			}	
+		}
+	}
+
+	// {
+	// value : {
+	// default: 5.0,
+	// US : 6.0
+	// },
+	// mini : {
+	// default: 5.0,
+	// US : 6.0
+	// }
+	// maxi : {
+	// default: 5.0,
+	// US : 6.0
+	// }
+	// }
+	//
+	public static String extractRoundedValue(NutListDataItem n, String roundingMode) {
+
+		JSONObject jsonRound = new JSONObject();
+
+		try {
+
+			JSONObject value = new JSONObject();
+			JSONObject mini = new JSONObject();
+			JSONObject maxi = new JSONObject();
+			JSONObject valuePerServing = new JSONObject();
+
+			for (Locale locale : getAvailableLocales()) {
+
+				String key = getLocalKey(locale);
+				String nutUnit = n.getUnit();
+
+				value.put(key, round(n.getValue(), roundingMode, locale, nutUnit));
+				mini.put(key, round(n.getMini(), roundingMode, locale, nutUnit));
+				maxi.put(key, round(n.getMaxi(), roundingMode, locale, nutUnit));
+				valuePerServing.put(key, round(n.getValuePerServing(), roundingMode, locale, nutUnit));
+
+				jsonRound.put("value", value);
+				jsonRound.put("mini", mini);
+				jsonRound.put("maxi", maxi);
+				jsonRound.put("valuePerServing", valuePerServing);
+
+			}
+
+		} catch (JSONException e) {
+			logger.error(e, e);
+		}
+		return jsonRound.toString();
+	}
+
+	// RoundingRole method for Fat according to european guide in Kcal
+	private static Double nearByValueNRJUS(Double value) {
+
+		if (value == null) {
+			return null;
+		} else if (value > 0.05) {
+			return (double) (0.01 * (int) (Math.ceil(value / 0.01)));
+		} else if ((value >= 0.005) && (value <= 0.05)) {
+			return (double) (0.005 * (int) (Math.ceil(value / 0.005)));
+		} else if (value < 0.005) {
 			return 0.0;
 		}
-		else if (value >minValue && value <10){
-			return (double) Math.round(10*value)/10;
-		}
-		else if (value >=10){
+
+		return null;
+	}
+
+	// RoundingRole method for Fat according to european guide in g
+	private static Double nearByValueEur(Double value, Double minValue) {
+		if (value == null) {
+			return null;
+		} else if (value <= minValue) {
+			return 0.0;
+		} else if ((value > minValue) && (value < 10)) {
+			return (double) Math.round(10 * value) / 10;
+		} else if (value >= 10) {
 			return (double) Math.round(value);
 		}
 		return null;
 	}
-	
-//	RoundingRole method for Fat according to US guide in g
-	
-	private  static Double nearByValueUS (Double value){
-		
-		if (value==null){
-			return null;
-		}
-		else if (value >=5){
-			return (double) Math.ceil(value);
-		}
-		else if (value >=0.5 && value < 5){
-			return (double) (0.5* (int)(Math.ceil(value /0.5)));
-		}
-		else if (value<0.5){
-		
-			return 0.0;
-		}
-		
-		return null;
-	}
-	
-//RoundingRole method for Sugars/Soluble & Insoluble fiber/Protein 
-//According to the US guide
-	private static Double nearByValueSuFiberPUS (Double value){
-		
-		if (value==null){
-			return null;
-		}
-		else if (value <=0.5){
-			return 0.0;
-		}
-		else if (value >=1){
-			return (double) Math.ceil(value);
-		}
-		else if (value >0.5 && value< 1){
-			return 1.0;
-		}
-		else {
-			
-			return null;
-		}
-	}
-	
-//RoundingRole method for sodium according to US guide (Unit:g)
-	
-	private  static Double nearByValueNaUS (Double value){
-		if (value==null){
-			return null;
-		}	
-		else if (value <0.005){
-			return 0.0;
-		}
-		else if (value >=0.005 && value <0.14){
-			return (double) (0.005* (int)(Math.ceil(value /0.005)));
-		}
-		else if (value>0.14) {
-			
-			return (double) (0.01* (int)(Math.ceil(value /0.01)));	
-		}
-		else {
-			return null; 
-		}
-	}
-	
-//RoundingRole method for sodium according to european guide (unit:g)
-	private static Double nearByValueNaSaltEur (Double value, Double minValue){
-		if (value==null){
-			return null;
-		}
-		else if (value >minValue && value <1){
-			
-			return (double) Math.round(100*value)/100;
-}
-		else if (value >=1){
-			
-			return (double) Math.round(10*value)/10;			
-		}
-		else if(value <=minValue){
-			return 0.0;
-		}
-		return null;
-	}
-	
-//RoundingRole method for cholesterol according to US guide (unit:g)
 
-	private static Double nearByValuecholesterolUS (Double value){
-		if (value==null){
+	// RoundingRole method for Fat according to US guide in g
+
+	private static Double nearByValueUS(Double value) {
+
+		if (value == null) {
 			return null;
-		}
-		else if (value >0.005){
-			
-			return (double) (0.005* (int)(Math.ceil(value /0.005)));
-		}
-		
-		else if(value <=0.002){
+		} else if (value >= 5) {
+			return (double) Math.ceil(value);
+		} else if ((value >= 0.5) && (value < 5)) {
+			return (double) (0.5 * (int) (Math.ceil(value / 0.5)));
+		} else if (value < 0.5) {
+
 			return 0.0;
 		}
-		else if (value>0.002 && value<0.005){
+
+		return null;
+	}
+
+	// RoundingRole method for Sugars/Soluble & Insoluble fiber/Protein
+	// According to the US guide
+	private static Double nearByValueSuFiberPUS(Double value) {
+
+		if (value == null) {
+			return null;
+		} else if (value <= 0.5) {
+			return 0.0;
+		} else if (value >= 1) {
+			return (double) Math.ceil(value);
+		} else if ((value > 0.5) && (value < 1)) {
+			return 1.0;
+		} else {
+
+			return null;
+		}
+	}
+
+	// RoundingRole method for sodium according to US guide (Unit:g)
+
+	private static Double nearByValueNaUS(Double value) {
+		if (value == null) {
+			return null;
+		} else if (value < 0.005) {
+			return 0.0;
+		} else if ((value >= 0.005) && (value < 0.14)) {
+			return (double) (0.005 * (int) (Math.ceil(value / 0.005)));
+		} else if (value > 0.14) {
+
+			return (double) (0.01 * (int) (Math.ceil(value / 0.01)));
+		} else {
+			return null;
+		}
+	}
+
+	// RoundingRole method for sodium according to european guide (unit:g)
+	private static Double nearByValueNaSaltEur(Double value, Double minValue) {
+		if (value == null) {
+			return null;
+		} else if ((value > minValue) && (value < 1)) {
+
+			return (double) Math.round(100 * value) / 100;
+		} else if (value >= 1) {
+
+			return (double) Math.round(10 * value) / 10;
+		} else if (value <= minValue) {
+			return 0.0;
+		}
+		return null;
+	}
+
+	// RoundingRole method for cholesterol according to US guide (unit:g)
+
+	private static Double nearByValuecholesterolUS(Double value) {
+		if (value == null) {
+			return null;
+		} else if (value > 0.005) {
+
+			return (double) (0.005 * (int) (Math.ceil(value / 0.005)));
+		}
+
+		else if (value <= 0.002) {
+			return 0.0;
+		} else if ((value > 0.002) && (value < 0.005)) {
 			return 0.005;
 		}
 		return null;
 	}
-	
-	
+
 	public static Double round(Double value, String roundingRuleType, Locale locale, String nutUnit) {
-		
-		if (value == null){
-			return null; 
+
+		if (value == null) {
+			return null;
 		}
-		RoundingRule roundingRule = roundingRuleType !=null ? rules.get(NutrientRoundingRuleType.valueOf(roundingRuleType)) : null;
-		
-		
-	  if(roundingRule!=null) {
-			if (nutUnit != null && nutUnit.equals("mg/100g")){ //convert mg to g
+		RoundingRule roundingRule = roundingRuleType != null ? rules.get(NutrientRoundingRuleType.valueOf(roundingRuleType)) : null;
+
+		if (roundingRule != null) {
+			if ((nutUnit != null) && nutUnit.equals("mg/100g")) { // convert mg
+																	// to g
 				value = value / 1000;
 				value = roundingRule.round(value, locale);
 				return value * 1000;
 			}
-			if (nutUnit != null && nutUnit.equals("KJ/100g")){ //convert KJ to Kcal
-				value = value * 4.18; 
+			if ((nutUnit != null) && nutUnit.equals("KJ/100g")) { // convert KJ
+																	// to Kcal
+				value = value * 4.18;
 				value = roundingRule.round(value, locale);
 				return value / 4.18;
-			}	
+			}
 			return roundingRule.round(value, locale);
 		}
 		return value;
 	}
 
 }
-	
-	
-	
-
--- becpg-plm/becpg-plm-core/src/test/java/fr/becpg/test/repo/product/rounding/NutrientRoundingRulesTest.java
+++ becpg-plm/becpg-plm-core/src/test/java/fr/becpg/test/repo/product/rounding/NutrientRoundingRulesTest.java
@@ -1,6 +1,6 @@
 package fr.becpg.test.repo.product.rounding;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Locale;
 
@@ -11,7 +11,7 @@ import org.junit.Test;
 import fr.becpg.repo.product.formulation.rounding.NutrientRoundingRules;
 import fr.becpg.repo.product.formulation.rounding.NutrientRoundingRules.NutrientRoundingRuleType;
 
-public class NutrientRoundingRulesTest {
+public class NutrientRoundingRulesTest{
 
 	public Log logger = LogFactory.getLog(NutrientRoundingRulesTest.class);
 	
@@ -21,11 +21,9 @@ public class NutrientRoundingRulesTest {
 		
 		//Test NRJ
 		 assertEquals(NutrientRoundingRules.round(4253.6d, NutrientRoundingRuleType.NRJ.toString(), Locale.FRENCH, "kcal"), 4254d,0);
-		 assertEquals(NutrientRoundingRules.round(4d, NutrientRoundingRuleType.NRJ.toString(), Locale.US, "kcal"), 0d,0);
-		 assertEquals(NutrientRoundingRules.round(6d, NutrientRoundingRuleType.NRJ.toString(), Locale.US, "kcal"), 10d,0);
-		 assertEquals(NutrientRoundingRules.round(50.37d, NutrientRoundingRuleType.NRJ.toString(), Locale.US, "kcal"), 60d,0);
-		 assertEquals(NutrientRoundingRules.round(50.6d, NutrientRoundingRuleType.NRJ.toString(), Locale.US, "kcal"), 60d,0);
-		 
+		 assertEquals(NutrientRoundingRules.round(0.055d, NutrientRoundingRuleType.NRJ.toString(), Locale.US, "kcal"), 0.06d,0);
+		 assertEquals(NutrientRoundingRules.round(0.014d, NutrientRoundingRuleType.NRJ.toString(), Locale.US, "kcal"), 0.015d,0);
+		 assertEquals(NutrientRoundingRules.round(0.004d, NutrientRoundingRuleType.NRJ.toString(), Locale.US, "kcal"), 0d,0);
 		 
 		 //Test fat
 		 assertEquals(NutrientRoundingRules.round(null, NutrientRoundingRuleType.Fat.toString(), Locale.FRENCH, null), null);
@@ -34,10 +32,8 @@ public class NutrientRoundingRulesTest {
 		 assertEquals(NutrientRoundingRules.round(11.45d, NutrientRoundingRuleType.Fat.toString(), Locale.US, null), 12d,0);
 		 assertEquals(NutrientRoundingRules.round(0.2d, NutrientRoundingRuleType.Fat.toString(), Locale.FRENCH, null), 0d,0);
 		 assertEquals(NutrientRoundingRules.round(0.2d, NutrientRoundingRuleType.Fat.toString(), Locale.US, null), 0d,0);
-		 assertEquals(NutrientRoundingRules.round(0d, NutrientRoundingRuleType.Fat.toString(), Locale.FRENCH, null),0d,0 );
-		 assertEquals(NutrientRoundingRules.round(0d, NutrientRoundingRuleType.Fat.toString(), Locale.US, null),0d,0 );
 		 assertEquals(NutrientRoundingRules.round(4.33d, NutrientRoundingRuleType.Fat.toString(), Locale.FRENCH, null), 4.3d,0);
-		 assertEquals(NutrientRoundingRules.round(4.3d, NutrientRoundingRuleType.Fat.toString(), Locale.US, null), 4.5d,0);
+		 assertEquals(NutrientRoundingRules.round(4.33d, NutrientRoundingRuleType.Fat.toString(), Locale.US, null), 4.5d,0);
 		
 		 
 		 //Test saturated fat 
@@ -59,23 +55,44 @@ public class NutrientRoundingRulesTest {
 		 assertEquals(NutrientRoundingRules.round(10.36d, NutrientRoundingRuleType.Na.toString(), Locale.GERMAN,null), 10.4d,0);
 		 assertEquals(NutrientRoundingRules.round(0.337d, NutrientRoundingRuleType.Na.toString(), Locale.FRENCH, null), 0.34d,0);
 		 assertEquals(NutrientRoundingRules.round(0.002d, NutrientRoundingRuleType.Na.toString(), Locale.FRENCH, null), 0.0d,0);
-		 assertEquals(NutrientRoundingRules.round(0.011d, NutrientRoundingRuleType.Na.toString(), Locale.FRENCH, null), 0.01d,0);
 		 assertEquals(NutrientRoundingRules.round(0d, NutrientRoundingRuleType.Na.toString(), Locale.FRENCH, null), 0d,0);
 		 assertEquals(NutrientRoundingRules.round(null, NutrientRoundingRuleType.Na.toString(), Locale.US, null), null);
 		 assertEquals(NutrientRoundingRules.round(0.004d, NutrientRoundingRuleType.Na.toString(), Locale.US, null), 0d,0);
 		 assertEquals(NutrientRoundingRules.round(0.121d, NutrientRoundingRuleType.Na.toString(), Locale.US, null), 0.125d,0);
 		 assertEquals(NutrientRoundingRules.round(0.145d, NutrientRoundingRuleType.Na.toString(), Locale.US, null), 0.150d,0);
 		
-//		 //Test Salt
+		 //Test Salt
 		 assertEquals(NutrientRoundingRules.round(null, NutrientRoundingRuleType.Salt.toString(), Locale.FRENCH, null), null);
 		 assertEquals(NutrientRoundingRules.round(1.46d, NutrientRoundingRuleType.Salt.toString(), Locale.FRENCH, null), 1.5d,0);
 		 assertEquals(NutrientRoundingRules.round(0.037d, NutrientRoundingRuleType.Salt.toString(), Locale.FRENCH, null), 0.04d,0);
 		 assertEquals(NutrientRoundingRules.round(0.0125d, NutrientRoundingRuleType.Salt.toString(), Locale.FRENCH, null), 0.0d,0);
 		 assertEquals(NutrientRoundingRules.round(0.002d, NutrientRoundingRuleType.Salt.toString(), Locale.FRENCH, null), 0.0d,0);
+
+		 //Cholesterol
 		 
-//		 //test cholesterol
+		 assertEquals(NutrientRoundingRules.round(null, NutrientRoundingRuleType.Cholesterol.toString(), Locale.US, null), null);
+		 assertEquals(NutrientRoundingRules.round(0.002d, NutrientRoundingRuleType.Cholesterol.toString(), Locale.US, null), 0d,0);
+		 assertEquals(NutrientRoundingRules.round(0.003d, NutrientRoundingRuleType.Cholesterol.toString(), Locale.US, null), 0.005d,0);
+		 assertEquals(NutrientRoundingRules.round(0.006d, NutrientRoundingRuleType.Cholesterol.toString(), Locale.US, null), 0.01d,0);
+		 
 		 
 		 
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
