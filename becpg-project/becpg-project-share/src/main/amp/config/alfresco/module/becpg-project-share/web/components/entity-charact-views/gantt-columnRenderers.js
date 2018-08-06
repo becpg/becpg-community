e85b9400c16e1e7d5090de4a6c568fd14a012d33
Steven BihouÃ© <steven.bihoue@becpg.fr>
2018-08-06 12:14:10 +0200

    fix #4544

--- becpg-project/becpg-project-share/src/main/amp/config/alfresco/module/becpg-project-share/web/components/entity-charact-views/gantt-columnRenderers.js
+++ becpg-project/becpg-project-share/src/main/amp/config/alfresco/module/becpg-project-share/web/components/entity-charact-views/gantt-columnRenderers.js
@@ -111,7 +111,7 @@
                                                         if(task["itemData"]["assoc_pjt_tlResources"].length>0){
                                                         	taskOwner = "";
                                                         	for(var z in  task["itemData"]["assoc_pjt_tlResources"]){
-                                                        		taskOwner += '<span class="resource-title">' + task["itemData"]["assoc_pjt_tlResources"][z].displayValue + '</span>';                               	
+                                                        		taskOwner += '<span class="resource-title">' + task["itemData"]["assoc_pjt_tlResources"][z].displayValue.replace("null", "").trim() + '</span>';                               	
                                                         	}
                                                         }
                                                         
