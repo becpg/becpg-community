380a6393f182069a3abb637629b0cd161830933c
Laborie Matthieu <matthieu.laborie@becpg.fr>
2018-08-24 16:41:59 +0200

    Theme fix

--- becpg-share/src/main/amp/config/alfresco/module/becpg-share/web/modules/entity-datagrid/entity-actions.js
+++ becpg-share/src/main/amp/config/alfresco/module/becpg-share/web/modules/entity-datagrid/entity-actions.js
@@ -544,6 +544,99 @@
 			      } ]
 			   });   
 		   
+	   },
+	   
+	   
+	   onActionColumnConf : function EntityDataGrid_onActionColumnConf() {
+		   
+		   if(!this.services.preferences) {
+		   		this.services.preferences = new Alfresco.service.Preferences();
+	   		}
+	   
+    	   var me = this;
+    	   var popupKind = "columns-conf";
+    	   var html = '<div class="hd">' + this.msg("header." + popupKind + ".picker") + '</div>';
+   			html += '<div class="bd">';
+   			html += '<form  class="form-container">';
+   			html += '<div class="form-fields bulk-edit">';
+   			html += '   <div class="set">';
+   			html += '        <div class="form-field">';
+   			html += '			<div  id="'+this.id+'-columns-list" />'		
+   			html += '          </div>';
+   			html += '       </div>';
+   			html += '    </div>';
+   			html += '<div id="'+this.id+'-'+popupKind+'-ft" class="bdft">';
+   			html += '</div>';
+   			html += '</form></div>';
+   			   
+			    var containerDiv = document.createElement("div");
+				containerDiv.innerHTML = html;
+   			
+				this.widgets.columnsListPanel = Alfresco.util.createYUIPanel(containerDiv, {
+					draggable : true,
+					width : "33em"
+				});
+				
+				var hiddenColumnsInPopup = ["bcpg_startEffectivity", "bcpg_endEffectivity", "bcpg_depthLevel"];
+				
+				var itemType =  this.options.itemType != null ? this.options.itemType : this.datalistMeta.itemType;
+				var containerEl = Dom.get(this.id+'-columns-list').parentNode, html = "";
+				var colCount = 0;
+				var siteId = this.options.siteId 
+					
+				Alfresco.util.Ajax.jsonGet({
+				url : Alfresco.constants.URL_SERVICECONTEXT + "module/entity-datagrid/config/columns?mode=datagrid-prefs&itemType=" + encodeURIComponent(itemType) + "&clearCache=true" + (this.options.siteId ? "&siteId=" + this.options.siteId : ""),
+				successCallback : {
+					fn : function (response) {
+						var prefs = "fr.becpg.formulation.dashlet.custom.datagrid-prefs"+"."+itemType.replace(":","_");
+
+						for (var i = 0; i < response.json.columns.length; i++) {
+							var column = response.json.columns[i];
+							var propLabel = column.label;
+							var value = column.name.replace(":", "_");
+							var checked = column.checked ? "checked" : "";
+							
+							if (propLabel!="hidden" && propLabel && hiddenColumnsInPopup.indexOf(value) < 0) {
+								html += '<li class=""><input id="propSelected-' + i + '" type="checkbox" name="propChecked" value="'+ value +'" '+ checked + '/>' 
+										+ '<label for="propSelected-' + i + '" >' + propLabel + '</label></li>';
+							}
+						}
+
+						 html = "<span>"+this.msg("label.select-columns.title")
+						 	+"</span><br/><br/><ul style=\"width:" + ((colCount + 2) * 20) + "em;\">" + html + "</ul>";		    
+
+						containerEl.innerHTML = html;
+						 
+						var divEl = Dom.get(this.id+'-columns-conf-ft');
+						
+						divEl.innerHTML = '<input id="'+this.id+'-bulk-edit-ok" type="button" value="'+this.msg("button.ok")+'" />';
+						
+			            this.widgets.okBkButton = Alfresco.util.createYUIButton(this, "bulk-edit-ok", function (){
+
+			            	var selectedFields = Selector.query('input[type="checkbox"]', containerEl);
+
+			            	for ( var i in selectedFields) {
+					 				var fieldId = selectedFields[i].value;
+					 				var prfs = prefs + "."+fieldId
+					 				me.services.preferences.set(prfs, {checked : selectedFields[i].checked});
+					 			}
+			            	
+			            	this.widgets.columnsListPanel.hide();
+			            	
+			            	setTimeout(function(){
+			            		YAHOO.Bubbling.fire(me.scopeId + "scopedActiveDataListChanged", 
+			            	    		{extraDataParams : "&clearCache=true"}
+			            	    );
+			            	}, 1000);
+			            	
+			            });
+					},
+					scope : this
+				}
+			});
+				
+		this.widgets.columnsListPanel.show();
+     
 	   }
 
 	};
