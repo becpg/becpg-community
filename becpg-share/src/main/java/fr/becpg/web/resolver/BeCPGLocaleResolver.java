ed39b5c120c72a83d4d527aa00417926ec605687
rabah aoukaba <rabah.aoukaba@becpg.fr>
2018-10-11 16:48:25 +0200

    fix #4740

--- becpg-share/src/main/java/fr/becpg/web/resolver/BeCPGLocaleResolver.java
+++ becpg-share/src/main/java/fr/becpg/web/resolver/BeCPGLocaleResolver.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.extensions.surf.RequestContext;
 import org.springframework.extensions.surf.UserFactory;
@@ -29,19 +30,27 @@ public class BeCPGLocaleResolver extends LocaleResolver {
 
 		User user = rc.getUser();
 
-		if("true".equals(request.getParameter("resetLocale"))) {
-
-			  // allow the endpoint id to be explicitly overridden via a request attribute
-            String userEndpointId = (String) rc.getAttribute(RequestContext.USER_ENDPOINT);
-            
-            
-            UserFactory userFactory = rc.getServiceRegistry().getUserFactory();
-			try {
-				user = userFactory.initialiseUser(rc, (HttpServletRequest)request, userEndpointId, true);
-				rc.setUser(user);
-			} catch (UserFactoryException e) {}
+		if (user == null) {
+			HttpSession session = request.getSession(false);
+			if (session != null) {
+				user = (User) session.getAttribute(UserFactory.SESSION_ATTRIBUTE_KEY_USER_OBJECT);
+			}
 		}
-		
+
+		if ("true".equals(request.getParameter("resetLocale"))) {
+
+			// allow the endpoint id to be explicitly overridden via a request
+			// attribute
+			String userEndpointId = (String) rc.getAttribute(RequestContext.USER_ENDPOINT);
+
+			UserFactory userFactory = rc.getServiceRegistry().getUserFactory();
+			try {
+				user = userFactory.initialiseUser(rc, request, userEndpointId, true);
+				rc.setUser(user);
+			} catch (UserFactoryException e) {
+				//Do nothing
+			}
+		}
 
 		if (user != null) {
 
@@ -50,12 +59,12 @@ public class BeCPGLocaleResolver extends LocaleResolver {
 					locale = I18NUtil.parseLocale(entry.getKey().substring(11));
 					I18NUtil.setLocale(locale);
 				} else if (entry.getKey().startsWith("userContentLocale_")) {
-					locale = I18NUtil.parseLocale(entry.getKey().substring(18));	
+					locale = I18NUtil.parseLocale(entry.getKey().substring(18));
 					I18NUtil.setContentLocale(locale);
 				}
 			}
-			
-			if(locale!=null) {
+
+			if (locale != null) {
 				return locale;
 			}
 
