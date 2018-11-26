5c0122081ec9e3467c115da058f09a658a9cdbfb
Laborie Matthieu <matthieu.laborie@becpg.fr>
2018-11-26 16:36:09 +0100

    refs #4909

--- becpg-plm/becpg-plm-share/src/main/amp/config/alfresco/module/becpg-plm-share/web/modules/custom-entity-datagrid/product-entity-toolbar.js
+++ becpg-plm/becpg-plm-share/src/main/amp/config/alfresco/module/becpg-plm-share/web/modules/custom-entity-datagrid/product-entity-toolbar.js
@@ -432,7 +432,7 @@ YAHOO.Bubbling
                         return asset.name != null && 
                              (asset.name === "View-properties" || asset.name === "compoList" || asset.name === "processList" || asset.name === "packagingList" || asset.name === "ingLabelingList" || asset.name === "nutList" || asset.name === "labelClaimList" || asset.name === "costList" || asset.name === "physicoChemList" || asset.name === "ingList" || asset.name === "allergenList")
                                && entity != null && beCPG.util.contains(entity.aspects,
-                                       "bcpg:formulatedEntityAspect") && entity.userAccess.edit;
+                                       "bcpg:productAspect") && entity.userAccess.edit;
                      },
                      fn : function(instance) {
 
