9aacb4f11e3bb6896b632676a806987811e462c0
rabah aoukaba <rabah.aoukaba@becpg.fr>
2018-12-14 15:15:24 +0100

    refs #5037

--- becpg-core/src/main/java/fr/becpg/repo/web/scripts/report/ReportDataSourceWebscript.java
+++ becpg-core/src/main/java/fr/becpg/repo/web/scripts/report/ReportDataSourceWebscript.java
@@ -18,17 +18,21 @@
 package fr.becpg.repo.web.scripts.report;
 
 import java.io.IOException;
+import java.util.Locale;
 
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.springframework.extensions.surf.util.I18NUtil;
 import org.springframework.extensions.webscripts.AbstractWebScript;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 import org.springframework.extensions.webscripts.WebScriptResponse;
 
+import fr.becpg.repo.helper.MLTextHelper;
 import fr.becpg.repo.report.entity.EntityReportService;
 
 public class ReportDataSourceWebscript extends AbstractWebScript {
 
 	private static final String PARAM_NODEREF = "nodeRef";
+	private static final String PARAM_LOCALE = "locale";
 
 	EntityReportService entityReportService;
 
@@ -52,10 +56,18 @@ public class ReportDataSourceWebscript extends AbstractWebScript {
 	public void execute(WebScriptRequest req, WebScriptResponse res) throws IOException {
 
 		NodeRef entityNodeRef = new NodeRef(req.getParameter(PARAM_NODEREF));
+		
+		Locale locale;
+		String paramLocale = req.getParameter(PARAM_LOCALE);
+		if (paramLocale == null) {
+			locale = I18NUtil.getLocale();
+		} else {
+			locale = MLTextHelper.parseLocale(paramLocale);
+		}
 
 		res.setContentType("application/xml");
 		res.setContentEncoding("UTF-8");
-		res.getWriter().write(entityReportService.getXmlReportDataSource(entityNodeRef));
+		res.getWriter().write(entityReportService.getXmlReportDataSource(entityNodeRef, locale));
 
 	}
 
