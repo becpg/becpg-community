1f625f9c2a7326f225e482b65fd79b16a257c717
Laborie Matthieu <matthieu.laborie@becpg.fr>
2019-02-15 16:57:19 +0100

    Fix jenkins process

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# Downloads the spring-loaded lib if not existing and runs the full all-in-one
+echo -e "\e[0m888                 \e[32m.d8888b.  8888888b.   .d8888b. \e[0m" 
+echo -e "888                \e[32md88P  Y88b 888   Y88b d88P  Y88b\e[0m" 
+echo -e "888                \e[32m888    888 888    888 888    888\e[0m" 
+echo -e "88888b.   .d88b.   \e[32m888        888   d88P 888       \e[0m" 
+echo -e "888 \"88b d8P  Y8b  \e[32m888        8888888P\"  888  88888\e[0m" 
+echo -e "888  888 88888888  \e[32m888    888 888        888    888\e[0m" 
+echo -e "888 d88P Y8b.      \e[32mY88b  d88P 888        Y88b  d88P\e[0m" 
+echo -e "88888P\"   \"Y8888    \e[32m\"Y8888P\"  888         \"Y8888P88\e[0m" 
+echo -e " \e[91mCopyright (C) 2010-2019 beCPG.\e[0m"
 # (Alfresco + Share + Solr) using the runner project
 
 MAVEN_OPTS="-Xmx1024m" mvn test-compile install -Prun
