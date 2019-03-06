4c67d3e4f1d6ae28dc1a9dd67d5096335d511fc8
rabah aoukaba <rabah.aoukaba@becpg.fr>
2019-03-06 10:13:52 +0100

    refs #5205

--- becpg-core/src/main/java/fr/becpg/repo/license/BeCPGLicenseManager.java
+++ becpg-core/src/main/java/fr/becpg/repo/license/BeCPGLicenseManager.java
@@ -1,37 +1,144 @@
 package fr.becpg.repo.license;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.service.cmr.repository.ContentReader;
+import org.alfresco.service.cmr.repository.ContentService;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import fr.becpg.repo.RepoConsts;
+import fr.becpg.repo.cache.BeCPGCacheService;
+import fr.becpg.repo.helper.RepoService;
 
 @Service("becpgLicenseManager")
 public class BeCPGLicenseManager {
+
+	private static final Log logger = LogFactory.getLog(BeCPGLicenseManager.class);
+
+	public static final String LICENSE_READ_NAMED = "LICENSE_READ_NAMED";
+	public static final String LICENSE_WRITE_NAMED = "LICENSE_WRITE_NAMED";
+	public static final String LICENSE_READ_CONCURRENT = "LICENSE_READ_CONCURRENT";
+	public static final String LICENSE_WRITE_CONCURRENT = "LICENSE_WRITE_CONCURRENT";
+	public static final String LICENSE_SUPPLIER_CONCURRENT = "LICENSE_SUPPLIER_CONCURRENT";
+
+	private static final String CACHE_KEY = "LICENSE_KEY";
 
 	private long allowedConcurrentRead = -1L;
 	private long allowedConcurrentWrite = -1L;
 	private long allowedConcurrentSupplier = -1L;
 	private long allowedNamedWrite = -1L;
 	private long allowedNamedRead = -1L;
+
+	@Autowired
+	private RepoService repoService;
 	
+	@Autowired
+	private NodeService nodeService;
+	
+	@Autowired
+	private ContentService contentService;
+	
+	@Autowired
+	private BeCPGCacheService cacheService;
+
 	public long getAllowedConcurrentRead() {
 		return allowedConcurrentRead;
 	}
+
 	public long getAllowedConcurrentWrite() {
 		return allowedConcurrentWrite;
 	}
+
 	public long getAllowedConcurrentSupplier() {
 		return allowedConcurrentSupplier;
 	}
+
 	public long getAllowedNamedWrite() {
 		return allowedNamedWrite;
 	}
+
 	public long getAllowedNamedRead() {
 		return allowedNamedRead;
 	}
+
 	public String getLicenseName() {
 		return "beCPG OO License";
 	}
-	
-	//TODO look at license json file under /System/license/license.json 
-	// if no license return -1L for unlimited else cache it 
-	
-	
+
+
+	public void readLicense() {
+		try {
+			JSONObject licenseObj = getLicenseFromCache();
+			if(licenseObj != null) {
+				allowedNamedRead = licenseObj.has(LICENSE_READ_NAMED) ? licenseObj.getLong(LICENSE_READ_NAMED) : -1L;
+				allowedNamedWrite = licenseObj.has(LICENSE_WRITE_NAMED) ? licenseObj.getLong(LICENSE_WRITE_NAMED) : -1L;
+				allowedConcurrentRead = licenseObj.has(LICENSE_READ_CONCURRENT) ? licenseObj.getLong(LICENSE_READ_CONCURRENT) : -1L;
+				allowedConcurrentWrite = licenseObj.has(LICENSE_WRITE_CONCURRENT) ? licenseObj.getLong(LICENSE_WRITE_CONCURRENT) : -1L;
+				allowedConcurrentSupplier = licenseObj.has(LICENSE_SUPPLIER_CONCURRENT) ? licenseObj.getLong(LICENSE_SUPPLIER_CONCURRENT) : -1L;
+			}
+			
+		} catch (Exception e) {
+			logger.warn("Can't read license informations", e);
+		}
+	}
+
+	private JSONObject getLicenseFromCache() {
+
+		BufferedReader bfReader = null;
+		JSONObject licenseObj = null;
+		try {
+			
+		licenseObj = cacheService.getFromCache(BeCPGLicenseManager.class.getName(), CACHE_KEY);
+
+		if(licenseObj == null) {
+			NodeRef licenseProfilesFolderNodeRef = repoService
+					.getFolderByPath(RepoConsts.PATH_SYSTEM + RepoConsts.PATH_SEPARATOR + RepoConsts.PATH_LICENSE);
+			if (licenseProfilesFolderNodeRef != null) {
+				NodeRef licenseFileNodeRef = nodeService.getChildByName(licenseProfilesFolderNodeRef, ContentModel.ASSOC_CONTAINS, "license.json");
+				
+				ContentReader reader = contentService.getReader(licenseFileNodeRef, ContentModel.PROP_CONTENT);
+				
+				bfReader = new BufferedReader(new InputStreamReader(reader.getContentInputStream(), "UTF-8"));
+				StringBuilder responseStrBuilder = new StringBuilder();
+				
+				String strStream;
+				
+				while ((strStream = bfReader.readLine()) != null) {
+					responseStrBuilder.append(strStream);
+				}
+				
+				licenseObj = new JSONObject(responseStrBuilder.toString());
+				cacheService.storeInCache(BeCPGLicenseManager.class.getName(), CACHE_KEY, licenseObj);
+			}
+		}
+		
+		} catch (IOException e) {
+			logger.error("Couldn't read license file ", e);
+		}  catch (JSONException e) {
+			logger.error("Unable to serialize JSON", e);
+		}
+		
+		finally {
+			if(bfReader != null) {
+				try {
+					bfReader.close();
+				} catch (IOException e) {
+					logger.error(e);
+				}
+			}
+		}
+
+		return licenseObj;
+	}
+
 }
