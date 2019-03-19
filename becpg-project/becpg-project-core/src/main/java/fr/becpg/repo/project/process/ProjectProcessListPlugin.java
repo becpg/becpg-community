ec49b62f262f1885726869bbbc5c64e31ebecae4
rabah aoukaba <rabah.aoukaba@becpg.fr>
2019-03-19 09:09:33 +0100

    fix #5399

--- becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/process/ProjectProcessListPlugin.java
+++ becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/process/ProjectProcessListPlugin.java
@@ -12,13 +12,11 @@ import org.alfresco.service.cmr.security.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import fr.becpg.model.BeCPGModel;
 import fr.becpg.model.ProjectModel;
 import fr.becpg.repo.helper.AssociationService;
 import fr.becpg.repo.project.data.ProjectData;
 import fr.becpg.repo.project.data.ProjectState;
 import fr.becpg.repo.repository.AlfrescoRepository;
-import fr.becpg.repo.search.BeCPGQueryBuilder;
 import fr.becpg.repo.web.scripts.process.EntityProcessListPlugin;
 
 @Service("projectProcessListPlugin")
@@ -43,53 +41,45 @@ public class ProjectProcessListPlugin implements EntityProcessListPlugin {
 		FORMATER.setDateFormat(PROCESS_DATETIME_FORMAT);
 		
 		List<Map<String, Object>> ret = new ArrayList<>();
-		
-		List<NodeRef> projectNodeRefs = BeCPGQueryBuilder.createQuery()
-				.ofType(ProjectModel.TYPE_PROJECT)
-				.excludeAspect(BeCPGModel.ASPECT_ENTITY_TPL)
-				.excludeAspect(ContentModel.ASPECT_CHECKED_OUT)
-				.excludeAspect(BeCPGModel.ASPECT_COMPOSITE_VERSION)
-				.excludeProp(ProjectModel.PROP_PROJECT_STATE, ProjectState.Cancelled.toString())
-				.list();
 			
+		List<NodeRef> projectNodeRefs = associationService.getSourcesAssocs(nodeRef, ProjectModel.ASSOC_PROJECT_ENTITY);
+		
 		projectNodeRefs.forEach(pjtNodeRef -> {
-			NodeRef assocRef = associationService.getTargetAssoc(pjtNodeRef, ProjectModel.ASSOC_PROJECT_ENTITY);
-			if(assocRef !=null && assocRef.equals(nodeRef)){
-				
-				ProjectData data = alfrescoRepository.findOne(pjtNodeRef);
-				Map<String, Object> temp = new HashMap<>();
-				
-				temp.put(PROCESS_INSTANCE_NODEREF, pjtNodeRef.toString());
-				temp.put(PROCESS_INSTANCE_TYPE, getType());
-				
-				temp.put(PROCESS_INSTANCE_TITLE, data.getName());
-				
-				String projectTitle = (String) nodeService.getProperty(pjtNodeRef, ContentModel.PROP_TITLE);
-				if(projectTitle != null && !projectTitle.isEmpty()){
-					temp.put(PROCESS_INSTANCE_MESSAGE, projectTitle);
-				}
-				
-				if(data.getStartDate() != null){
-					temp.put(PROCESS_INSTANCE_START_DATE, FORMATER.formatDate(data.getStartDate()));
-				}
-				if(data.getDueDate() != null){
-					temp.put(PROCESS_INSTANCE_DUE_DATE, FORMATER.formatDate(data.getDueDate()));
-				}
-				
-				temp.put(PROCESS_INSTANCE_IS_ACTIVE, data.getProjectState().equals(ProjectState.Completed) ? false : true);
-				temp.put(PROCESS_INSTANCE_STATE, data.getProjectState().toString());
-				
-				NodeRef personRef = null;
-				if(data.getProjectManager()!=null){
-					personRef = data.getProjectManager();
-				} else if(personService.personExists(data.getCreator())){
-					 personRef = personService.getPerson(data.getCreator());
-				}
-				
-				temp.put(PROCESS_INSTANCE_INITIATOR, getPersonModel(personRef));
-				
-				ret.add(temp);
+			
+			ProjectData data = alfrescoRepository.findOne(pjtNodeRef);
+			Map<String, Object> temp = new HashMap<>();
+			
+			temp.put(PROCESS_INSTANCE_NODEREF, pjtNodeRef.toString());
+			temp.put(PROCESS_INSTANCE_TYPE, getType());
+			
+			temp.put(PROCESS_INSTANCE_TITLE, data.getName());
+			
+			String projectTitle = (String) nodeService.getProperty(pjtNodeRef, ContentModel.PROP_TITLE);
+			if(projectTitle != null && !projectTitle.isEmpty()){
+				temp.put(PROCESS_INSTANCE_MESSAGE, projectTitle);
 			}
+			
+			if(data.getStartDate() != null){
+				temp.put(PROCESS_INSTANCE_START_DATE, FORMATER.formatDate(data.getStartDate()));
+			}
+			if(data.getDueDate() != null){
+				temp.put(PROCESS_INSTANCE_DUE_DATE, FORMATER.formatDate(data.getDueDate()));
+			}
+			
+			temp.put(PROCESS_INSTANCE_IS_ACTIVE, data.getProjectState().equals(ProjectState.Completed) ? false : true);
+			temp.put(PROCESS_INSTANCE_STATE, data.getProjectState().toString());
+			
+			NodeRef personRef = null;
+			if(data.getProjectManager()!=null){
+				personRef = data.getProjectManager();
+			} else if(personService.personExists(data.getCreator())){
+				personRef = personService.getPerson(data.getCreator());
+			}
+			
+			temp.put(PROCESS_INSTANCE_INITIATOR, getPersonModel(personRef));
+			
+			ret.add(temp);
+				
 			
 		});
 		
