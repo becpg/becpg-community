ee794b088fba2cb2b22852b7f9a9e789499dae95
Laborie Matthieu <matthieu.laborie@becpg.fr>
2019-05-14 15:47:33 +0200

    fix #5609

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/data/ing/AbstractLabelingComponent.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/data/ing/AbstractLabelingComponent.java
@@ -1,24 +1,28 @@
 /*******************************************************************************
- * Copyright (C) 2010-2018 beCPG. 
- *  
- * This file is part of beCPG 
- *  
- * beCPG is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version. 
- *  
- * beCPG is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * GNU Lesser General Public License for more details. 
- *  
+ * Copyright (C) 2010-2018 beCPG.
+ *
+ * This file is part of beCPG
+ *
+ * beCPG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * beCPG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License along with beCPG. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package fr.becpg.repo.product.data.ing;
 
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 
 import org.alfresco.service.cmr.repository.MLText;
@@ -35,40 +39,59 @@ public abstract class AbstractLabelingComponent extends BeCPGDataObject implemen
 	private static final long serialVersionUID = 270866664168102414L;
 
 	protected Double qty = 0d;
-	
+
 	protected Double volume = 0d;
 
 	protected MLText legalName;
-	
+
 	private boolean isPlural = false;
-	
+
 	private boolean shouldSkip = false;
-	
+
 	protected MLText pluralLegalName;
-	
-	private Set<NodeRef> allergens = new HashSet<NodeRef>();
-	
-	private Set<NodeRef> geoOrigins = new HashSet<NodeRef>();
-	
+
+	private Set<NodeRef> allergens = new HashSet<>();
+
+	private Set<NodeRef> geoOrigins = new HashSet<>();
+
 	public AbstractLabelingComponent() {
 		super();
 	}
 
-	public AbstractLabelingComponent(AbstractLabelingComponent abstractLabelingComponent) 
-	{
+	public AbstractLabelingComponent(AbstractLabelingComponent abstractLabelingComponent) {
 		super(abstractLabelingComponent);
 		this.pluralLegalName = abstractLabelingComponent.pluralLegalName;
-	    this.qty = abstractLabelingComponent.qty;
-	    this.volume = abstractLabelingComponent.volume;
-	    this.legalName = abstractLabelingComponent.legalName;
-	    this.allergens = abstractLabelingComponent.allergens;
-	    this.geoOrigins = abstractLabelingComponent.geoOrigins;
-	    this.isPlural = abstractLabelingComponent.isPlural;
-	    this.shouldSkip = abstractLabelingComponent.shouldSkip;
-	
+		this.qty = abstractLabelingComponent.qty;
+		this.volume = abstractLabelingComponent.volume;
+		this.legalName = abstractLabelingComponent.legalName;
+		this.allergens = abstractLabelingComponent.allergens;
+		this.geoOrigins = abstractLabelingComponent.geoOrigins;
+		this.isPlural = abstractLabelingComponent.isPlural;
+		this.shouldSkip = abstractLabelingComponent.shouldSkip;
+
 	}
 
-	
+	@SuppressWarnings("unchecked")
+	protected <T extends AbstractLabelingComponent> List<T> clone(List<T> list) {
+		List<T> ret = new LinkedList<>();
+		if (list != null) {
+			for (T toAdd : list) {
+				ret.add((T) toAdd.clone());
+			}
+		}
+		return ret;
+	}
+
+	protected Map<NodeRef, AbstractLabelingComponent> clone(Map<NodeRef, AbstractLabelingComponent> list) {
+		Map<NodeRef, AbstractLabelingComponent> ret = new LinkedHashMap<>();
+		if (list != null) {
+			for (Map.Entry<NodeRef, AbstractLabelingComponent> toAdd : list.entrySet()) {
+				ret.put(toAdd.getKey(), toAdd.getValue().clone());
+			}
+		}
+		return ret;
+	}
+
 	@AlfMlText
 	@AlfProp
 	@AlfQname(qname = "bcpg:legalName")
@@ -83,15 +106,14 @@ public abstract class AbstractLabelingComponent extends BeCPGDataObject implemen
 	@Override
 	public String getLegalName(Locale locale) {
 		String ret = MLTextHelper.getClosestValue(legalName, locale);
-		
-		if(ret==null || ret.isEmpty()){
+
+		if ((ret == null) || ret.isEmpty()) {
 			return name;
 		}
-		
+
 		return ret;
 	}
 
-	
 	@AlfMlText
 	@AlfProp
 	@AlfQname(qname = "bcpg:pluralLegalName")
@@ -102,20 +124,19 @@ public abstract class AbstractLabelingComponent extends BeCPGDataObject implemen
 	public void setPluralLegalName(MLText pluralLegalName) {
 		this.pluralLegalName = pluralLegalName;
 	}
-	
-	
+
 	public String getPluralLegalName(Locale locale) {
 		String ret = MLTextHelper.getClosestValue(pluralLegalName, locale);
-		
-		if(ret==null || ret.isEmpty()){
+
+		if ((ret == null) || ret.isEmpty()) {
 			return getLegalName(locale);
 		}
-		
+
 		return ret;
 	}
-	
+
 	public boolean isPlural() {
-		return isPlural && pluralLegalName!=null && !MLTextHelper.isEmpty(pluralLegalName);
+		return isPlural && (pluralLegalName != null) && !MLTextHelper.isEmpty(pluralLegalName);
 	}
 
 	public void setPlural(boolean isPlural) {
@@ -130,13 +151,11 @@ public abstract class AbstractLabelingComponent extends BeCPGDataObject implemen
 	public void setQty(Double qty) {
 		this.qty = qty;
 	}
-	
-	
+
 	@Override
 	public Double getVolume() {
 		return volume;
 	}
-
 
 	public void setVolume(Double volume) {
 		this.volume = volume;
@@ -160,8 +179,8 @@ public abstract class AbstractLabelingComponent extends BeCPGDataObject implemen
 		this.geoOrigins = geoOrigins;
 	}
 
+	@Override
 	public abstract AbstractLabelingComponent clone();
-	
 
 	public boolean shouldSkip() {
 		return shouldSkip;
@@ -175,92 +194,107 @@ public abstract class AbstractLabelingComponent extends BeCPGDataObject implemen
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
-		result = prime * result + ((allergens == null) ? 0 : allergens.hashCode());
-		result = prime * result + ((geoOrigins == null) ? 0 : geoOrigins.hashCode());
-		result = prime * result + (isPlural ? 1231 : 1237);
-		result = prime * result + ((legalName == null) ? 0 : legalName.hashCode());
-		result = prime * result + ((pluralLegalName == null) ? 0 : pluralLegalName.hashCode());
-		result = prime * result + ((qty == null) ? 0 : qty.hashCode());
-		result = prime * result + (shouldSkip ? 1231 : 1237);
-		result = prime * result + ((volume == null) ? 0 : volume.hashCode());
+		result = (prime * result) + ((allergens == null) ? 0 : allergens.hashCode());
+		result = (prime * result) + ((geoOrigins == null) ? 0 : geoOrigins.hashCode());
+		result = (prime * result) + (isPlural ? 1231 : 1237);
+		result = (prime * result) + ((legalName == null) ? 0 : legalName.hashCode());
+		result = (prime * result) + ((pluralLegalName == null) ? 0 : pluralLegalName.hashCode());
+		result = (prime * result) + ((qty == null) ? 0 : qty.hashCode());
+		result = (prime * result) + (shouldSkip ? 1231 : 1237);
+		result = (prime * result) + ((volume == null) ? 0 : volume.hashCode());
 		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (!super.equals(obj))
+		}
+		if (!super.equals(obj)) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
+		}
 		AbstractLabelingComponent other = (AbstractLabelingComponent) obj;
 		if (allergens == null) {
-			if (other.allergens != null)
+			if (other.allergens != null) {
 				return false;
-		} else if (!allergens.equals(other.allergens))
+			}
+		} else if (!allergens.equals(other.allergens)) {
 			return false;
+		}
 		if (geoOrigins == null) {
-			if (other.geoOrigins != null)
+			if (other.geoOrigins != null) {
 				return false;
-		} else if (!geoOrigins.equals(other.geoOrigins))
+			}
+		} else if (!geoOrigins.equals(other.geoOrigins)) {
 			return false;
-		if (isPlural != other.isPlural)
+		}
+		if (isPlural != other.isPlural) {
 			return false;
+		}
 		if (legalName == null) {
-			if (other.legalName != null)
+			if (other.legalName != null) {
 				return false;
-		} else if (!legalName.equals(other.legalName))
+			}
+		} else if (!legalName.equals(other.legalName)) {
 			return false;
+		}
 		if (pluralLegalName == null) {
-			if (other.pluralLegalName != null)
+			if (other.pluralLegalName != null) {
 				return false;
-		} else if (!pluralLegalName.equals(other.pluralLegalName))
+			}
+		} else if (!pluralLegalName.equals(other.pluralLegalName)) {
 			return false;
+		}
 		if (qty == null) {
-			if (other.qty != null)
+			if (other.qty != null) {
 				return false;
-		} else if (!qty.equals(other.qty))
+			}
+		} else if (!qty.equals(other.qty)) {
 			return false;
-		if (shouldSkip != other.shouldSkip)
+		}
+		if (shouldSkip != other.shouldSkip) {
 			return false;
+		}
 		if (volume == null) {
-			if (other.volume != null)
+			if (other.volume != null) {
 				return false;
-		} else if (!volume.equals(other.volume))
+			}
+		} else if (!volume.equals(other.volume)) {
 			return false;
+		}
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "AbstractLabelingComponent [qty=" + qty + ", volume=" + volume + ", legalName=" + legalName + ", nodeRef=" + nodeRef
-				+ ", name=" + name + "]";
+		return "AbstractLabelingComponent [qty=" + qty + ", volume=" + volume + ", legalName=" + legalName + ", nodeRef=" + nodeRef + ", name=" + name
+				+ "]";
 	}
 
 	@Override
 	public int compareTo(LabelingComponent lblComponent) {
 
-		if (lblComponent instanceof CompositeLabeling && ((CompositeLabeling) lblComponent).isGroup()
-				&& !(this instanceof CompositeLabeling && ((CompositeLabeling) this).isGroup())) {
+		if ((lblComponent instanceof CompositeLabeling) && ((CompositeLabeling) lblComponent).isGroup()
+				&& !((this instanceof CompositeLabeling) && ((CompositeLabeling) this).isGroup())) {
 			return 1;
 		}
 
-		if (!(lblComponent instanceof CompositeLabeling && ((CompositeLabeling) lblComponent).isGroup()) && 
-				(this instanceof CompositeLabeling
-				&& ((CompositeLabeling) this).isGroup())) {
+		if (!((lblComponent instanceof CompositeLabeling) && ((CompositeLabeling) lblComponent).isGroup())
+				&& ((this instanceof CompositeLabeling) && ((CompositeLabeling) this).isGroup())) {
 			return -1;
 		}
 
-		if (lblComponent.getQty() != null && this.getQty() != null) {
+		if ((lblComponent.getQty() != null) && (this.getQty() != null)) {
 			return Double.compare(lblComponent.getQty(), this.getQty());
-		} else if (this.getQty() == null && lblComponent.getQty() != null) {
+		} else if ((this.getQty() == null) && (lblComponent.getQty() != null)) {
 			return 1; // after
-		} else if (this.getQty() != null && lblComponent.getQty() == null) {
+		} else if ((this.getQty() != null) && (lblComponent.getQty() == null)) {
 			return -1; // before
 		}
 		return 0;// equals
 	}
-
 
 }
