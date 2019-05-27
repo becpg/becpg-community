0cb85e767181517c1d6d66b154055b5bdad124c7
Laborie Matthieu <matthieu.laborie@becpg.fr>
2019-05-27 10:24:34 +0200

    fix #5500

--- becpg-plm/becpg-plm-core/src/test/java/fr/becpg/test/repo/helper/Nutrient5CTest.java
+++ becpg-plm/becpg-plm-core/src/test/java/fr/becpg/test/repo/helper/Nutrient5CTest.java
@@ -16,6 +16,17 @@ public class Nutrient5CTest {
 		
 		Assert.assertEquals("D",Nutrient5CHelper.buildNutrientClass(12d,
 				Arrays.asList(new Double[]{18d,10d,2d,-1d}),Arrays.asList(new String[]{"E","D","C","B","A"})));
+		
+		
+		Assert.assertEquals(8,Nutrient5CHelper.compute5CScore(1809d, 1.1d, 11d, 5.11d, 593d, 0d, 0d, 10d, 19.3d, "Others"));
+		
+		Assert.assertEquals("C",Nutrient5CHelper.buildNutrientClass(8d,
+				Arrays.asList(new Double[]{18d,10d,2d,-1d}),Arrays.asList(new String[]{"E","D","C","B","A"})));
+		
+		
+		Assert.assertEquals(4,Nutrient5CHelper.compute5CScore(2084d, 2.8d, 22.9d, 4.73d, 672d, 90d, 0d, 4.13d, 5.81d, "Others"));
+		
+		
 	}
 	
 	
