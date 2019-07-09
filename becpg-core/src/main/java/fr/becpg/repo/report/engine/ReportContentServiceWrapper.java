635f4fb537c17dde75f728608d8dd9f3759d78ca
Laborie Matthieu <matthieu.laborie@becpg.fr>
2019-07-09 10:50:35 +0200

    fix #5872

--- becpg-core/src/main/java/fr/becpg/repo/report/engine/ReportContentServiceWrapper.java
+++ becpg-core/src/main/java/fr/becpg/repo/report/engine/ReportContentServiceWrapper.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.alfresco.repo.content.transform.ContentTransformer;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.transaction.AlfrescoTransactionSupport;
+import org.alfresco.repo.transaction.AlfrescoTransactionSupport.TxnReadState;
+import org.alfresco.repo.transaction.RetryingTransactionHelper;
 import org.alfresco.service.cmr.dictionary.InvalidTypeException;
 import org.alfresco.service.cmr.repository.ContentIOException;
 import org.alfresco.service.cmr.repository.ContentReader;
@@ -12,24 +16,56 @@ import org.alfresco.service.cmr.repository.ContentWriter;
 import org.alfresco.service.cmr.repository.InvalidNodeRefException;
 import org.alfresco.service.cmr.repository.NoTransformerException;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.repository.TransformationOptions;
 import org.alfresco.service.namespace.QName;
+import org.alfresco.service.transaction.TransactionService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
+import fr.becpg.model.ReportModel;
+import fr.becpg.repo.report.entity.EntityReportService;
 
 /**
- * 
-Override ou ContentReader
-ExporterComponent
+ *
+ * Override ou ContentReader ExporterComponent
+ *
  * @author matthieu
  *
  */
+@SuppressWarnings("deprecation")
 public class ReportContentServiceWrapper implements ContentService {
 
+	private static Log logger = LogFactory.getLog(ReportContentServiceWrapper.class);
+
 	private ContentService contentService;
+
+	private EntityReportService entityReportService;
+
+	private NodeService nodeService;
+
+	private TransactionService transactionService;
+
+	public void setContentService(ContentService contentService) {
+		this.contentService = contentService;
+	}
+
+	public void setEntityReportService(EntityReportService entityReportService) {
+		this.entityReportService = entityReportService;
+	}
+
 	
+	public void setNodeService(NodeService nodeService) {
+		this.nodeService = nodeService;
+	}
+
+	public void setTransactionService(TransactionService transactionService) {
+		this.transactionService = transactionService;
+	}
+
 	@Override
 	public long getStoreTotalSpace() {
-		return 	contentService.getStoreTotalSpace();
+		return contentService.getStoreTotalSpace();
 	}
 
 	@Override
@@ -44,6 +80,33 @@ public class ReportContentServiceWrapper implements ContentService {
 
 	@Override
 	public ContentReader getReader(NodeRef nodeRef, QName propertyQName) throws InvalidNodeRefException, InvalidTypeException {
+
+		if (ReportModel.TYPE_REPORT.equals(nodeService.getType(nodeRef))) {
+
+			NodeRef entityNodeRef = entityReportService.getEntityNodeRef(nodeRef);
+			if ((entityNodeRef != null) && entityReportService.shouldGenerateReport(entityNodeRef, nodeRef)) {
+
+				RetryingTransactionHelper txnHelper = transactionService.getRetryingTransactionHelper();
+				txnHelper.setForceWritable(true);
+				boolean requiresNew = false;
+				if (AlfrescoTransactionSupport.getTransactionReadState() != TxnReadState.TXN_READ_WRITE) {
+					// We can be in a read-only transaction, so force a new
+					// transaction
+					requiresNew = true;
+				}
+				txnHelper.doInTransaction(() -> AuthenticationUtil.runAs(() -> {
+
+					logger.debug("refreshReport: Entity report is not up to date for " + entityNodeRef);
+
+					entityReportService.generateReport(entityNodeRef, nodeRef);
+
+					return nodeRef;
+
+				}, AuthenticationUtil.getSystemUserName()), false, requiresNew);
+
+			}
+		}
+
 		return contentService.getReader(nodeRef, propertyQName);
 	}
 
@@ -60,20 +123,20 @@ public class ReportContentServiceWrapper implements ContentService {
 	@Override
 	public void transform(ContentReader reader, ContentWriter writer) throws NoTransformerException, ContentIOException {
 		contentService.transform(reader, writer);
-		
+
 	}
 
 	@Override
 	public void transform(ContentReader reader, ContentWriter writer, Map<String, Object> options) throws NoTransformerException, ContentIOException {
-		contentService.transform(reader, writer,options);
-		
+		contentService.transform(reader, writer, options);
+
 	}
 
 	@Override
 	public void transform(ContentReader reader, ContentWriter writer, TransformationOptions options)
 			throws NoTransformerException, ContentIOException {
 		contentService.transform(reader, writer, options);
-		
+
 	}
 
 	@Override
@@ -96,7 +159,7 @@ public class ReportContentServiceWrapper implements ContentService {
 
 	@Override
 	public ContentTransformer getTransformer(String sourceMimetype, String targetMimetype, TransformationOptions options) {
-		return contentService.getTransformer(sourceMimetype, targetMimetype,options);
+		return contentService.getTransformer(sourceMimetype, targetMimetype, options);
 	}
 
 	@Override
