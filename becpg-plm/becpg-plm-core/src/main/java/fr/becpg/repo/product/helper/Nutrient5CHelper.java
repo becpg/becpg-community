fb26fd37d58029dd02974d33ca6f381452171c26
Laborie Matthieu <matthieu.laborie@becpg.fr>
2019-07-09 15:42:15 +0200

    fix #5879

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/helper/Nutrient5CHelper.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/helper/Nutrient5CHelper.java
@@ -83,6 +83,7 @@ public class Nutrient5CHelper {
 	 * @param category
 	 * @return
 	 */
+
 	public static int compute5CScore(Double energyKj, Double satFat, Double totalFat, Double totalSugar, Double sodium, Double percFruitsAndVetgs,
 			Double nspFibre, Double aoacFibre, Double protein, String category) {
 
@@ -90,13 +91,36 @@ public class Nutrient5CHelper {
 		int cScore = 0;
 		int score = 10;
 
+//		DOIT - ON ARRONDIR LES RESULTATS POUR LE CALCUL DU SCORE ?
+//				Exemple : si une teneur en sucres simples est de 9,001, le système doit-il considérer que l'arrondi est 9 et donc, attribuer 1 point
+//				ou doit-il considérer que 9.001>9 et donc attribuer 2 points ?
+//				L’attribution des points pour un nutriment donné se fait sur la base de la teneur de l’aliment dans le
+//				nutriment considéré, avec un arrondi correspondant à un chiffre supplémentaire par rapport à la définition
+//				du seuil d’attribution des points. Pour le calcul du score, pour les fibres les seuils sont définis avec un chiffre
+//				après la virgule. Pour les sucres les seuils sont définis à l’unité ou avec un chiffre après la virgule. Pour les
+//				protéines, hormis pour le seuil supérieur où il n’y a pas de chiffre après la virgule, les autres seuils sont
+//				définis avec un chiffre après la virgule. Pour tous les autres ils sont toujours définis à l’unité, sans chiffre
+//				après la virgule (Energie, AGS, sodium, fruits, légumes, légumineuses, fruits à coque, ratio AGS/lipides
+//				totaux).
+//				Pour l’exemple proposé, le seuil étant à 9g/100g, (il convient de compter 2 points si la teneur en sucres
+//				simples est strictement supérieure à 9 et compter 1 point si la teneur est inférieure ou égale à 9) l’arrondi
+//				se fait à un chiffre après la virgule. Pour une mesure à 9,001, l’arrondi est donc égal à 9,0. De ce fait, 1 seul
+//				point est donc attribué. Si la teneur avait été mesurée à 9,05 ou 9,06, alors l’arrondi aurait été de 9,1 et il
+//				aurait fallu attribuer 2 points. Toujours avec l’exemple des sucres simples : pour un seuil à 4,5 (1 point si la
+//				valeur est strictement supérieure, 0 si elle est inférieure ou égale à 4,5g/100g), l’arrondi est à deux chiffres
+//				après la virgule. Pour un produit à 4,502, l’arrondi est à 4,50, aucun point n’est attribué. Pour un produit à
+//				4,505 ou 4,506, l’arrondi est à 4,51, un point est attribué.
+		
+		
+		
+		
 		double[][] aCategories = getACategory(NutrientCategory.valueOf(category));
 		double[][] cCategories = getCCategory(NutrientCategory.valueOf(category));
 
 		if (energyKj != null) {
 
 			for (double val : aCategories[0]) {
-				if ((energyKj > val) && (val > 0)) {
+				if ((round(energyKj,val) > val) && (val > 0)) {
 					break;
 				}
 				score--;
@@ -108,7 +132,7 @@ public class Nutrient5CHelper {
 			if ((satFat != null) && (totalFat != null)) {
 				score = 10;
 				for (double val : aCategories[1]) {
-					if ((((satFat / totalFat) * 100) > val) && (val > 0)) {
+					if ((((round(satFat,val) / round(totalFat,val)) * 100) > val) && (val > 0)) {
 						break;
 					}
 					score--;
@@ -118,7 +142,7 @@ public class Nutrient5CHelper {
 		} else if (satFat != null) {
 			score = 10;
 			for (double val : aCategories[1]) {
-				if ((satFat > val) && (val > 0)) {
+				if ((round(satFat,val) > val) && (val > 0)) {
 					break;
 				}
 				score--;
@@ -130,7 +154,7 @@ public class Nutrient5CHelper {
 		if (totalSugar != null) {
 			score = 10;
 			for (double val : aCategories[2]) {
-				if ((totalSugar > val) && (val > 0)) {
+				if ((round(totalSugar,val) > val) && (val > 0)) {
 					break;
 				}
 				score--;
@@ -142,7 +166,7 @@ public class Nutrient5CHelper {
 		if (sodium != null) {
 			score = 10;
 			for (double val : aCategories[3]) {
-				if ((sodium > val) && (val > 0)) {
+				if ((round(sodium,val) > val) && (val > 0)) {
 					break;
 				}
 				score--;
@@ -154,7 +178,7 @@ public class Nutrient5CHelper {
 		if (percFruitsAndVetgs != null) {
 			score = 10;
 			for (double val : cCategories[0]) {
-				if ((percFruitsAndVetgs > val) && (val > 0)) {
+				if ((round(percFruitsAndVetgs,val) > val) && (val > 0)) {
 					break;
 				}
 				score--;
@@ -162,10 +186,26 @@ public class Nutrient5CHelper {
 			cScore += score;
 		}
 
+		if ((aScore < 11) || NutrientCategory.Cheeses.equals(NutrientCategory.valueOf(category))
+				|| ((aScore >= 11) && (cScore >= 5) && !NutrientCategory.Beverages.equals(NutrientCategory.valueOf(category)))
+				|| ((aScore >= 11) && (cScore >= 10) && NutrientCategory.Beverages.equals(NutrientCategory.valueOf(category)))) {
+			if (protein != null) {
+				score = 5;
+				for (double val : cCategories[3]) {
+					if ((round(protein,val) > val) && (val > 0)) {
+						break;
+					}
+					score--;
+				}
+				cScore += score;
+			}
+
+		}
+
 		if (nspFibre != null) {
 			score = 5;
 			for (double val : cCategories[1]) {
-				if ((nspFibre > val) && (val > 0)) {
+				if ((round(nspFibre,val) > val) && (val > 0)) {
 					break;
 				}
 				score--;
@@ -176,7 +216,7 @@ public class Nutrient5CHelper {
 		if (aoacFibre != null) {
 			score = 5;
 			for (double val : cCategories[2]) {
-				if ((aoacFibre > val) && (val > 0)) {
+				if ((round(aoacFibre,val) > val) && (val > 0)) {
 					break;
 				}
 				score--;
@@ -184,22 +224,20 @@ public class Nutrient5CHelper {
 			cScore += score;
 		}
 
-		if (!((aScore >= 11) && ((percFruitsAndVetgs != null) && (percFruitsAndVetgs <= 80d))
-				&& !NutrientCategory.Cheeses.equals(NutrientCategory.valueOf(category)))) {
-			if (protein != null) {
-				score = 5;
-				for (double val : cCategories[3]) {
-					if ((protein > val) && (val > 0)) {
-						break;
-					}
-					score--;
-				}
-				cScore += score;
-			}
-		}
-
 		return aScore - cScore;
 
+	}
+
+	
+	
+	
+	
+	private static double round(Double toRound, Double val) {
+		if(val!=null && val.toString().contains(".")) {
+			return Math.round(toRound*100)/100d;
+		}
+
+		return Math.round(toRound*10)/10d;
 	}
 
 	public static String buildNutrientClass(Double score, List<Double> ranges, List<String> clazz) {
