9446d970a07efaf41175b938603d345f498ea01b
Laborie Matthieu <matthieu.laborie@becpg.fr>
2019-07-15 15:17:03 +0200

    JSON Remote format

--- becpg-core/src/main/java/fr/becpg/repo/helper/JsonHelper.java
+++ becpg-core/src/main/java/fr/becpg/repo/helper/JsonHelper.java
@@ -1,9 +1,11 @@
 package fr.becpg.repo.helper;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+import org.alfresco.util.ISO8601DateFormat;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,6 +27,30 @@ public class JsonHelper {
 
 		return criteriaMap;
 
+	}
+	
+
+	public static Object formatValue(Object value) {
+		if (value != null) {
+			if (value instanceof Date) {
+				return ISO8601DateFormat.format((Date) value);
+			} else if (value instanceof Double) {
+				Double d = (Double) value;
+				if (d.isInfinite()) {
+					return 0 == d.compareTo(Double.POSITIVE_INFINITY) ? "23456789012E777" : "-23456789012E777";
+				} else if(d.isNaN()) {
+					return null;
+				}
+			} else if (value instanceof Float) {
+				Float f = (Float) value;
+				if (f.isInfinite()) {
+					return 0 == f.compareTo(Float.POSITIVE_INFINITY) ? "23456789012E777" : "-23456789012E777";
+				} else if(f.isNaN()) {
+					return null;
+				}
+			} 
+		}
+		return value;
 	}
 	
 	
