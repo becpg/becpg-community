9851afb5b737bc612ae665b69c30fd14ec0af613
Laborie Matthieu <matthieu.laborie@becpg.fr>
2020-01-07 10:29:24 +0100

    InProgress #6762 - [Feature] Ng ui

--- becpg-ui/src/app/becpg/components/entity/entity.component.ts
+++ becpg-ui/src/app/becpg/components/entity/entity.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, Input, OnChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { AlfrescoApi } from '@alfresco/js-api';
@@ -13,6 +13,9 @@ import { EntityView } from '../../model/EntityView';
 import { NotificationService } from '@alfresco/adf-core';
 import { DocumentListComponent } from '@alfresco/adf-content-services';
 import { PreviewService } from '../../../services/preview.service';
+import { Observable, of } from 'rxjs';
+import { switchMap, mergeMap } from 'rxjs/operators';
+
 
 
 @Component({
@@ -22,87 +25,35 @@ import { PreviewService } from '../../../services/preview.service';
 })
 export class EntityComponent implements OnInit {
 
-  entity: Entity;
+  entity: Observable<Entity>;
 
   view: EntityView;
-
-  list: EntityList;
-
-  showCreate = true;
-
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1, type: 'entity' },
-        { title: 'Card 2', cols: 1, rows: 1, type: 'list' },
-        { title: 'Card 3', cols: 1, rows: 2, type: 'document' },
-        { title: 'Card 4', cols: 1, rows: 1, type: 'properties' }
-      ];
-    })
-  );
-
-
-
-
-  @Input()
-  showViewer = false;
-  nodeId: string = null;
-
-  @ViewChild('documentList')
-  documentList: DocumentListComponent;
-
 
   constructor(private route: ActivatedRoute, private entityViewService: EntityViewService, private notificationService: NotificationService, private preview: PreviewService, private breakpointObserver: BreakpointObserver, private entityApiService: EntityApiService) { }
 
   ngOnInit() {
+
+
     const id = this.route.snapshot.paramMap.get('id');
+   
+   this.entity = this.route.paramMap.pipe(
+    switchMap(params => {
+      return this.entityApiService.getEntity('workspace://SpacesStore/' + params.get('id'));
+    })
+   );
+
+   this.entity.subscribe(entity => {
     let viewId = this.route.snapshot.paramMap.get('viewId');
     if (viewId == null) {
-      viewId = 'properties';
-    }
-    this.entityApiService.getEntity('workspace://SpacesStore/' + id).then(entity => {
-      this.entity = entity;
-      this.view = this.entityViewService.getView(this.entity, viewId);
-    });
+        viewId = 'properties';
+      }
+    this.view =   this.entityViewService.getView(entity,viewId);
+
+   });
+
 
 
   }
-
-
-
-
-  uploadSuccess(event: any) {
-    this.notificationService.openSnackMessage('File uploaded');
-    this.documentList.reload();
-  }
-
-  showPreview(event) {
-    const entry = event.value.entry;
-    if (entry && entry.isFile) {
-      this.preview.showResource(entry.id);
-    }
-  }
-
-  onGoBack(event: any) {
-    this.showViewer = false;
-    this.nodeId = null;
-  }
-
-
-
-
-
-
 
 
 }
