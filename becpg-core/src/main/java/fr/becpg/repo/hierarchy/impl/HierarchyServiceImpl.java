2fdcbca57a338bf66b15bb95c16b3510a5c05efa
Evelyne Ing <evelyne.ing@becpg.fr>
2020-03-23 14:53:12 +0100

    #6924 - [Bug] Supplier portal possibility to have several accounts

--- becpg-core/src/main/java/fr/becpg/repo/hierarchy/impl/HierarchyServiceImpl.java
+++ becpg-core/src/main/java/fr/becpg/repo/hierarchy/impl/HierarchyServiceImpl.java
@@ -267,7 +267,7 @@ public class HierarchyServiceImpl implements HierarchyService {
 	@Override
 	public NodeRef getOrCreateHierachyFolder(NodeRef entityNodeRef,  QName hierarchyQname , NodeRef destinationNodeRef) {
 		
-		NodeRef hierarchyNodeRef = getHierarchyNodeRef(entityNodeRef, null);
+		NodeRef hierarchyNodeRef = getHierarchyNodeRef(entityNodeRef, hierarchyQname);
 		
 		if (hierarchyNodeRef != null) {
 			destinationNodeRef = getOrCreateHierachyFolder(hierarchyNodeRef, destinationNodeRef);
