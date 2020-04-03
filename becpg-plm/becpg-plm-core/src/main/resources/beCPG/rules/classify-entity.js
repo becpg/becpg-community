06665327696393ae02632119c527977e60775831
Laborie Matthieu <matthieu.laborie@becpg.fr>
2020-04-03 15:51:25 +0200

    Fix #7238 - [Feature] Add helpers for javascript

--- becpg-plm/becpg-plm-core/src/main/resources/beCPG/rules/classify-entity.js
+++ becpg-plm/becpg-plm-core/src/main/resources/beCPG/rules/classify-entity.js
@@ -1,144 +1,9 @@
-/*
- * Helpers
- */
+<import resource="classpath:/beCPG/rules/helpers.js">
 
-function isNullOrEmpty(value) {
-	if (value == null || value == "") {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-function concatName(name, value) {
-	if (name != "" && value != "") {
-		name += " ";
-	}
-	name += value;
-	return name;
-}
-
-function getProp(product, propName) {
-	if (product != null && product != "" && product.properties[propName] != null) {
-		return product.properties[propName];
-	} else {
-		return "";
-	}
-}
-
-function getMLProp(product, propName, locale) {
-	if (product != null && product != "" && bcpg.getMLProperty(product, propName, locale) != null) {
-		return bcpg.getMLProperty(product, propName, locale);
-	} else {
-		return "";
-	}
-}
-
-function getMLConstraint(propValue, propName, locale) {
-	if (propValue != null && propValue != "" && bcpg.getMLConstraint(propValue, propName, locale) != null) {
-		return bcpg.getMLConstraint(propValue, propName, locale);
-	} else {
-		return "";
-	}
-}
-
-function getAssoc(product, assocName, propName) {
-	if (product != null && product != "" && product.assocs[assocName] != null) {
-		if (propName != null && product.assocs[assocName][0].properties[propName] != null && product.assocs[assocName][0].properties[propName] != "") {
-			return product.assocs[assocName][0].properties[propName];
-		}
-		return product.assocs[assocName][0].name;
-	} else {
-		return "";
-	}
-}
-
-function removeForbiddenChar(value) {
-	return value.replace(/[|"<>.*?:+\/]/g, "").replace(/ -/g, "");
-}
-
-function classifyByHierarchy(productNode, folderNode, propHierarchy) {
-	if (folderNode != null) {
-		var action = actions.create("classify-by-hierarchy");
-		action.parameters["destination-folder"] = folderNode;
-		if (propHierarchy) {
-			action.parameters["prop-hierarchy"] = propHierarchy;
-		}
-		action.execute(productNode.nodeRef);
-	}
-}
-
-function isInSite(productNode, siteId) {
-	if (productNode.qnamePath.indexOf("/app:company_home/st:sites/cm:" + siteId + "/") != -1) {
-		return true;
-	} 
-	return false;
-}
-
-function isInUserFolder(productNode){
-	if (productNode.qnamePath.indexOf("/app:company_home/app:user_homes") != -1) {
-		return true;
-	}
-	return false;
-	
-}
-
-function isInFolder(productNode, folderNode) {
-	var i = 0;
-
-	var folder = productNode.parent;
-
-	while (folder.isContainer) {
-		i++;
-		if (i > 10) {
-			break;
-		}
-
-		if (folder.parent === null) {
-			return false;
-		}
-		if (folder.getNodeRef() == folderNode) {
-			return true;
-		}
-
-		folder = folder.parent;
-	}
-
-	return false;
-}
-
-function getDocumentLibraryNodeRef(siteId) {
-	var site = siteService.getSite(siteId);
-	return site.getContainer("documentLibrary");
-}
-
-//
-// Sample method
-//
-function rename(product) {
-	var name = getProp(product, "cm:title");
-
-	if (name != null && name != "") {
-
-		name = removeForbiddenChar(name.trim().toUpperCase());
-		name = concatName(getProp(product, "bcpg:erpCode"), name);
-
-		if (name != "" && product.properties["cm:name"] != name && product.parent.childByNamePath(name) == null) {
-			product.properties["cm:name"] = name;
-			product.save();
-		}
-	}
-}
-
-const SIMULATION_SITE_ID = "simulation";
-const VALID_SITE_ID = "valid";
-const ARCHIVED_SITE_ID = "archived";
-const SUPPLIER_PORTAL_SITE = "supplier-portal";
 
 function main() {
 
 	if (document.hasPermission("Read") && !document.hasAspect("bcpg:entityTplAspect") && !document.hasAspect("cm:workingcopy") && !isInUserFolder(document)) {
-
 		var state = "";
 		if (document.isSubType("bcpg:product")) {
 			state = document.properties["bcpg:productState"];
@@ -150,13 +15,45 @@ function main() {
 
 		if (state == "Valid") {
 			classifyByHierarchy(document, getDocumentLibraryNodeRef(VALID_SITE_ID));
-		} else if ((state == "Simulation" || state == "ToValidate") && !isInSite(document, SUPPLIER_PORTAL_SITE)) {
+		} else if ((state == "Simulation" || state == "ToValidate") && !isInSite(document, SUPPLIER_PORTAL_SITE_ID)) {
 			classifyByHierarchy(document, getDocumentLibraryNodeRef(SIMULATION_SITE_ID));
 		} else if (state == "Archived") {
 			classifyByHierarchy(document, getDocumentLibraryNodeRef(ARCHIVED_SITE_ID));
 		}
 
+		//Rename Sample
+		//rename();
 	}
 }
 
+
 main();
+
+
+/////////////////////////////
+// Add here you custom code
+////////////////////////////
+
+//
+//Sample rename method
+//
+function rename(product) {
+	var name = propValue(product, "cm:title");
+
+	if (!isEmpty(name)) {
+
+		name = concatName(propValue(product, "bcpg:erpCode"), 
+				cleanName(name).toUpperCase());
+		
+		if (product.properties["cm:name"] != name && product.parent.childByNamePath(name) == null) {
+			product.properties["cm:name"] = name;
+			product.save();
+		}
+	}
+}
+
+
+
+
+
+
