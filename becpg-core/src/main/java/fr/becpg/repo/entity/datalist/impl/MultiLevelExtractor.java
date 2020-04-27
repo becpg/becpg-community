6a315eb0afa00158c99b1e94ee27d9d1197f5775
Matthieu Laborie <matthieu.laborie@becpg.fr>
2020-04-27 16:49:20 +0200

    [Setup] Improve startime

--- becpg-core/src/main/java/fr/becpg/repo/entity/datalist/impl/MultiLevelExtractor.java
+++ becpg-core/src/main/java/fr/becpg/repo/entity/datalist/impl/MultiLevelExtractor.java
@@ -32,7 +32,6 @@ import org.alfresco.service.namespace.QName;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import fr.becpg.model.BeCPGModel;
 import fr.becpg.repo.RepoConsts;
 import fr.becpg.repo.entity.datalist.MultiLevelDataListService;
 import fr.becpg.repo.entity.datalist.PaginatedExtractedItems;
