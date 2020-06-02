f566919d6cc183bcc3f51dc29ff8c6b23a2ff647
Matthieu Laborie <matthieu.laborie@becpg.fr>
2020-06-02 18:14:38 +0200

    Fix #7551 - [Bug] Cron not being launch in alfresco 6

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/ecm/AutomaticECOJob.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/ecm/AutomaticECOJob.java
@@ -3,7 +3,6 @@ package fr.becpg.repo.ecm;
 import java.util.List;
 
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
-import org.alfresco.repo.security.authentication.AuthenticationUtil.RunAsWork;
 import org.alfresco.repo.tenant.Tenant;
 import org.alfresco.repo.tenant.TenantAdminService;
 import org.quartz.Job;
@@ -12,9 +11,9 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 /**
- * 
+ *
  * @author matthieu
- * 
+ *
  *         Case 1: - Automatic applied changed on wused (Formulation and
  *         Reports) - Changed with Version Case 2: - Create OM
  */
@@ -34,25 +33,22 @@ public class AutomaticECOJob implements Job {
 		final AutomaticECOService automaticECOService = (AutomaticECOService) jobData.get("automaticECOService");
 		final TenantAdminService tenantAdminService = (TenantAdminService) jobData.get("tenantAdminService");
 
-		AuthenticationUtil.runAsSystem(new RunAsWork<Object>() {
-			public Object doWork() throws Exception {
-				automaticECOService.applyAutomaticEco();
-				return null;
-			}
+		AuthenticationUtil.runAsSystem(() -> {
+			automaticECOService.applyAutomaticEco();
+			return null;
 		});
-		
+
 		if ((tenantAdminService != null) && tenantAdminService.isEnabled()) {
+			@SuppressWarnings("deprecation")
 			List<Tenant> tenants = tenantAdminService.getAllTenants();
 			for (Tenant tenant : tenants) {
 				String tenantDomain = tenant.getTenantDomain();
-				AuthenticationUtil.runAs(new RunAsWork<Object>() {
-					public Object doWork() throws Exception {
-						automaticECOService.applyAutomaticEco();
-						return null;
-					}
+				AuthenticationUtil.runAs(() -> {
+					automaticECOService.applyAutomaticEco();
+					return null;
 				}, tenantAdminService.getDomainUser(AuthenticationUtil.getSystemUserName(), tenantDomain));
 			}
-		} 
+		}
 
 	}
 
