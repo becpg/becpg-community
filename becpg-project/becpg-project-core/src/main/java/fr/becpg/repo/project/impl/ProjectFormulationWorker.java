48739663ff4618c00153c44d106384b30a88d1cd
Matthieu Laborie <matthieu.laborie@becpg.fr>
2020-06-02 18:14:38 +0200

    Fix #7551 - [Bug] Cron not being launch in alfresco 6

--- becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/impl/ProjectFormulationWorker.java
+++ becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/impl/ProjectFormulationWorker.java
@@ -1,18 +1,18 @@
 /*******************************************************************************
- * Copyright (C) 2010-2018 beCPG. 
- *  
- * This file is part of beCPG 
- *  
- * beCPG is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version. 
- *  
- * beCPG is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * GNU Lesser General Public License for more details. 
- *  
+ * Copyright (C) 2010-2018 beCPG.
+ *
+ * This file is part of beCPG
+ *
+ * beCPG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * beCPG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License along with beCPG. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package fr.becpg.repo.project.impl;
@@ -20,18 +20,15 @@ package fr.becpg.repo.project.impl;
 import java.util.Date;
 import java.util.List;
 
-import org.alfresco.model.ContentModel;
 import org.alfresco.repo.policy.BehaviourFilter;
-import org.alfresco.repo.transaction.RetryingTransactionHelper;
-import org.alfresco.repo.transaction.RetryingTransactionHelper.RetryingTransactionCallback;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.transaction.TransactionService;
 import org.alfresco.util.ISO8601DateFormat;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.dao.ConcurrencyFailureException;
 
 import fr.becpg.model.ProjectModel;
-import fr.becpg.repo.formulation.FormulateException;
 import fr.becpg.repo.project.ProjectService;
 import fr.becpg.repo.project.data.ProjectState;
 import fr.becpg.repo.search.BeCPGQueryBuilder;
@@ -39,66 +36,61 @@ import fr.becpg.repo.search.BeCPGQueryBuilder;
 public class ProjectFormulationWorker {
 
 	private static final Log logger = LogFactory.getLog(ProjectFormulationWorker.class);
-	
+
 	private ProjectService projectService;
-	private BehaviourFilter policyBehaviourFilter;
 	private TransactionService transactionService;
-	
+
 	public void setProjectService(ProjectService projectService) {
 		this.projectService = projectService;
 	}
+
 	public void setPolicyBehaviourFilter(BehaviourFilter policyBehaviourFilter) {
-		this.policyBehaviourFilter = policyBehaviourFilter;
 	}
-	
+
 	public void setTransactionService(TransactionService transactionService) {
 		this.transactionService = transactionService;
 	}
-	
-	public void executeFormulation(){
-		
-		final RetryingTransactionHelper txHelper = transactionService.getRetryingTransactionHelper();
-        txHelper.setMaxRetries(1);
-        txHelper.doInTransaction(new RetryingTransactionCallback<Object>(){
-        	
-        	public Object execute() throws Throwable{
-        		
-        		
-        		
-        		BeCPGQueryBuilder queryBuilder = BeCPGQueryBuilder.createQuery()
-        				.ofType(ProjectModel.TYPE_PROJECT)
-        				.andPropEquals(ProjectModel.PROP_PROJECT_STATE, ProjectState.InProgress.toString());
-        		
-        		List<NodeRef> projectNodeRefs = queryBuilder.inDB().list();
-        		
-        		// query
-        		 queryBuilder = BeCPGQueryBuilder.createQuery()
-         				.ofType(ProjectModel.TYPE_PROJECT)
-         				.andPropEquals(ProjectModel.PROP_PROJECT_STATE, ProjectState.Planned.toString())
-         				.andBetween(ProjectModel.PROP_PROJECT_START_DATE, "MIN", ISO8601DateFormat.format(new Date()));
-        		
-        		 projectNodeRefs.addAll(queryBuilder.ftsLanguage().list());
-        		 
-        		try{
-        			policyBehaviourFilter.disableBehaviour(ContentModel.ASPECT_AUDITABLE);
-        			
-        			for(NodeRef projectNodeRef : projectNodeRefs){
-        				try {
-        					if(logger.isDebugEnabled()){
-        						logger.debug("Formulate project " + projectNodeRef);
-        					}
-        					projectService.formulate(projectNodeRef);
-        				} catch (FormulateException e) {
-        					logger.error("Failed to formulate project " + projectNodeRef);
-        				}
-        			}
-        		}
-        		finally{
-        			
-        			policyBehaviourFilter.enableBehaviour(ContentModel.ASPECT_AUDITABLE);
-        		}
-        		return null;
-            }
-        }, false, true);		
+
+	public void executeFormulation() {
+
+		List<NodeRef> projectNodeRefs = transactionService.getRetryingTransactionHelper().doInTransaction(() -> {
+
+			BeCPGQueryBuilder queryBuilder = BeCPGQueryBuilder.createQuery().ofType(ProjectModel.TYPE_PROJECT)
+					.andPropEquals(ProjectModel.PROP_PROJECT_STATE, ProjectState.InProgress.toString());
+
+			List<NodeRef> ret = queryBuilder.inDB().list();
+
+			// query
+			queryBuilder = BeCPGQueryBuilder.createQuery().ofType(ProjectModel.TYPE_PROJECT)
+					.andPropEquals(ProjectModel.PROP_PROJECT_STATE, ProjectState.Planned.toString())
+					.andBetween(ProjectModel.PROP_PROJECT_START_DATE, "MIN", ISO8601DateFormat.format(new Date()));
+
+			ret.addAll(queryBuilder.ftsLanguage().list());
+
+			return ret;
+
+		}, false, true);
+
+		for (NodeRef projectNodeRef : projectNodeRefs) {
+			try {
+				transactionService.getRetryingTransactionHelper().doInTransaction(() -> {
+					if (logger.isDebugEnabled()) {
+						logger.debug("Reformulating project: " + projectNodeRef);
+					}
+					projectService.formulate(projectNodeRef);
+
+					return true;
+
+				}, false, true);
+
+			} catch (Exception e) {
+				if (e instanceof ConcurrencyFailureException) {
+					throw (ConcurrencyFailureException) e;
+				}
+				logger.error("Cannot reformulate project:" + projectNodeRef, e);
+			}
+
+		}
+
 	}
 }
