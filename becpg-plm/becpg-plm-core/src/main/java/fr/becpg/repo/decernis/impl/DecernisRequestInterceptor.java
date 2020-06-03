1f43133ae7ac18eda68c6c4f5ef0838e5d072bc6
Matthieu Laborie <matthieu.laborie@becpg.fr>
2020-06-03 15:27:52 +0200

    Fix #7536  #7538 - [Bug] Decernis issues

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/decernis/impl/DecernisRequestInterceptor.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/decernis/impl/DecernisRequestInterceptor.java
@@ -24,12 +24,11 @@ public class DecernisRequestInterceptor implements ClientHttpRequestInterceptor 
     }
 
     private void traceRequest(HttpRequest request, byte[] body) throws IOException {
-        log.debug("===========================request begin================================================");
+        log.trace("===========================request begin================================================");
         log.trace("URI         : {}", request.getURI());
         log.trace("Method      : {}", request.getMethod());
         log.trace("Headers     : {}", request.getHeaders() );
         log.trace("Request body: {}", new String(body, "UTF-8"));
-        log.debug("==========================request end================================================");
     }
 
     private void traceResponse(ClientHttpResponse response) throws IOException {
@@ -41,12 +40,11 @@ public class DecernisRequestInterceptor implements ClientHttpRequestInterceptor 
             inputStringBuilder.append('\n');
             line = bufferedReader.readLine();
         }
-        log.debug("============================response begin==========================================");
         log.trace("Status code  : {}", response.getStatusCode());
         log.trace("Status text  : {}", response.getStatusText());
         log.trace("Headers      : {}", response.getHeaders());
         log.trace("Response body: {}", inputStringBuilder.toString());
-        log.debug("=======================response end=================================================");
+        log.trace("=======================response end======================================================");
     }
 
 }
--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/data/constraints/RequirementDataType.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/data/constraints/RequirementDataType.java
@@ -15,4 +15,6 @@ public enum RequirementDataType {
 			return null;
 		}
 	} 
+	
+	
 }
