6999cd969bb783c955f31a280b8cf0a671a1c642
pierre colin <pierre.colin@becpg.fr>
2020-06-16 17:01:31 +0200

    Fix #2502 - [Feature] Add Glop service for optimized recipe given constraints

---
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/glop/GlopTargetSpecification.java
@@ -0,0 +1,61 @@
+package fr.becpg.repo.glop;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import fr.becpg.repo.repository.model.BeCPGDataObject;
+
+/**
+ * Specifies a target function for the Glop service
+ * 
+ * @author pierrecolin
+ * @version 1.0
+ * @see fr.becpg.repo.glop.GlopService
+ */
+public class GlopTargetSpecification {
+
+	private BeCPGDataObject target;
+	
+	private String task;
+	
+	/**
+	 * Builds a new target function specification
+	 * 
+	 * @param target the data item involved in the target
+	 * @param task the task to perform with the generated target function
+	 */
+	public GlopTargetSpecification(BeCPGDataObject target, String task) {
+		this.target = target;
+		
+		if (!(task.equals("min") || task.contentEquals("max"))) {
+			throw new IllegalArgumentException("Task must be \"min\" or \"max\"");
+		}
+		
+		this.task = task;
+	}
+	
+	/**
+	 * Returns the data item in the target specification
+	 * 
+	 * @return the data item
+	 */
+	public BeCPGDataObject getTarget() {
+		return target;
+	}
+
+	/**
+	 * Add the target task into a JSON representation. Intended to be used by {@link fr.becpg.repo.glop.impl.GlopServiceImpl}.
+	 * 
+	 * @param obj the JSON to add the target to
+	 * @throws JSONException if the operation failed
+	 */
+	public void putIntoJson(JSONObject obj) throws JSONException {
+		obj.put("task", task);
+	}
+
+	@Override
+	public String toString() {
+		return "GlopTargetSpecification [target=" + target + ", task=" + task + "]";
+	}
+	
+}
