beb4b3fc1a5759f16d77ffa9baedc0d475d4b6fd
Matthieu Laborie <matthieu.laborie@becpg.fr>
2020-07-21 15:37:19 +0200

    Fix #7713 - [Feature] Spel improvements (Allow to add comments, better highlighted, extract/save custom list)

---
+++ becpg-core/src/main/java/fr/becpg/repo/repository/model/DefaultListDataItem.java
@@ -0,0 +1,13 @@
+package fr.becpg.repo.repository.model;
+
+import fr.becpg.repo.repository.annotation.AlfQname;
+import fr.becpg.repo.repository.annotation.AlfType;
+
+@AlfType
+@AlfQname(qname = "bcpg:entityListItem")
+public class DefaultListDataItem extends BeCPGDataObject {
+
+	
+	private static final long serialVersionUID = 162170923610286636L;
+
+}
