50c204cf0fe3c1b13de0b481f04e96366815b7cc
Matthieu Laborie <matthieu.laborie@becpg.fr>
2020-08-19 10:38:58 +0200

    Fix #5064 - [Feature] Reduce versions size

--- becpg-core/src/main/java/fr/becpg/repo/entity/version/BecpgVersionServiceImpl.java
+++ becpg-core/src/main/java/fr/becpg/repo/entity/version/BecpgVersionServiceImpl.java
@@ -13,25 +13,27 @@ import org.apache.commons.logging.LogFactory;
 
 import fr.becpg.model.BeCPGModel;
 
-public class BecpgVersionServiceImpl extends Version2ServiceImpl{
+public class BecpgVersionServiceImpl extends Version2ServiceImpl {
 
 	private static Log logger = LogFactory.getLog(BecpgVersionServiceImpl.class);
-	
-	 @Override
+
+	@Override
 	protected void defaultOnCreateVersion(QName classRef, NodeRef nodeRef, Map<String, Serializable> versionProperties, PolicyScope nodeDetails) {
-		 if(nodeService.hasAspect(nodeRef, BeCPGModel.ASPECT_ENTITYLISTS) ) {
-			 if(classRef!=null && classRef.equals(nodeService.getType(nodeRef))){
-				 logger.info("Skipping node properties for becpg version" +nodeService.getType(nodeRef) +" "+classRef);
-				 
-				 nodeDetails.addProperty(classRef, ContentModel.PROP_NAME , this.nodeService.getProperty(nodeRef, ContentModel.PROP_NAME));
-	             nodeDetails.addProperty(classRef, BeCPGModel.PROP_CODE , this.nodeService.getProperty(nodeRef, BeCPGModel.PROP_CODE));
-			 }
-             
-		 } else {
-			 
-			 logger.info("defaultOnCreateVersion:" +nodeService.getType(nodeRef) +" "+classRef);
-			 
-			 super.defaultOnCreateVersion(classRef, nodeRef, versionProperties, nodeDetails);
-		 }
+		if (logger.isDebugEnabled()) {
+			logger.debug("defaultOnCreateVersion " + nodeService.getType(nodeRef) + " " + classRef + " "
+					+ this.nodeService.getProperty(nodeRef, ContentModel.PROP_NAME));
+		}
+
+		if (nodeService.hasAspect(nodeRef, BeCPGModel.ASPECT_ENTITYLISTS)) {
+			if ((classRef != null) && classRef.equals(nodeService.getType(nodeRef))) {
+				logger.debug("Skipping node properties for becpg version");
+				nodeDetails.addProperty(classRef, ContentModel.PROP_NAME, this.nodeService.getProperty(nodeRef, ContentModel.PROP_NAME));
+				nodeDetails.addProperty(classRef, BeCPGModel.PROP_CODE, this.nodeService.getProperty(nodeRef, BeCPGModel.PROP_CODE));
+			}
+
+		} else {
+
+			super.defaultOnCreateVersion(classRef, nodeRef, versionProperties, nodeDetails);
+		}
 	}
 }
