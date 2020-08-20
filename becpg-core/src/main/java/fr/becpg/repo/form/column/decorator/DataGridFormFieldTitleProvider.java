6578fbf742d54e17ed3ccdf9d64102693164f9fe
Matthieu Laborie <matthieu.laborie@becpg.fr>
2020-08-20 11:37:17 +0200

    InProgress #6803 - [Feature] Add ng form components

--- becpg-core/src/main/java/fr/becpg/repo/form/column/decorator/DataGridFormFieldTitleProvider.java
+++ becpg-core/src/main/java/fr/becpg/repo/form/column/decorator/DataGridFormFieldTitleProvider.java
@@ -1,8 +1,8 @@
 package fr.becpg.repo.form.column.decorator;
 
-import fr.becpg.repo.helper.ExcelHelper.ExcelFieldTitleProvider;
-import fr.becpg.repo.helper.impl.AttributeExtractorServiceImpl;
 import org.alfresco.service.namespace.QName;
+
+import fr.becpg.repo.helper.ExcelHelper.ExcelFieldTitleProvider;
 
 public interface DataGridFormFieldTitleProvider extends ExcelFieldTitleProvider {
     boolean isAllowed(QName field);
