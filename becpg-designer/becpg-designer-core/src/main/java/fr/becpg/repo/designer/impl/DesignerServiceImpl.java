e057323394d988ff260846212ea261b4885b0cf1
Matthieu Laborie <matthieu.laborie@becpg.fr>
2020-09-02 15:09:34 +0200

    [Merge] Fix

--- becpg-designer/becpg-designer-core/src/main/java/fr/becpg/repo/designer/impl/DesignerServiceImpl.java
+++ becpg-designer/becpg-designer-core/src/main/java/fr/becpg/repo/designer/impl/DesignerServiceImpl.java
@@ -47,7 +47,6 @@ import org.alfresco.service.cmr.repository.ContentIOException;
 import org.alfresco.service.cmr.repository.ContentReader;
 import org.alfresco.service.cmr.repository.ContentService;
 import org.alfresco.service.cmr.repository.ContentWriter;
-import org.alfresco.service.cmr.repository.InvalidNodeRefException;
 import org.alfresco.service.cmr.repository.MimetypeService;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
@@ -56,8 +55,6 @@ import org.alfresco.service.namespace.QName;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.xml.sax.SAXException;
@@ -92,8 +89,6 @@ public class DesignerServiceImpl implements DesignerService {
 	private MetaModelVisitor metaModelVisitor;
 
 	private FormModelVisitor formModelVisitor;
-	
-	private FormJsonVisitor formJsonVisitor;
 
 	private DesignerTreeVisitor designerTreeVisitor;
 
@@ -160,15 +155,7 @@ public class DesignerServiceImpl implements DesignerService {
 		this.dictionaryService = dictionaryService;
 	}
 
-        /**
-         * <p>Setter for the field <code>formJsonVisitor</code>.</p>
-         *
-         * @param formJsonVisitor a {@link fr.becpg.repo.designer.impl.FormJsonVisitor} object.
-         */
-        public void setFormJsonVisitor(FormJsonVisitor formJsonVisitor) {
-		this.formJsonVisitor = formJsonVisitor;
-	}
-
+   
 	/**
 	 * <p>Setter for the field <code>configPath</code>.</p>
 	 *
@@ -787,24 +774,5 @@ public class DesignerServiceImpl implements DesignerService {
 
 	}
 
-	/** {@inheritDoc} */
-	@Override
-	public String export(NodeRef nodeRef) {
-		try {
-			 JSONObject ret = new JSONObject();
-			
-			if (nodeService.hasAspect(nodeRef, DesignerModel.ASPECT_CONFIG)) {
-				logger.debug("Write config XML");
-				NodeRef configNodeRef = findConfigNodeRef(nodeRef);
-				formJsonVisitor.visit(configNodeRef, ret);
-	
-			}
-			
-			return ret.toString(3);
-		} catch (JSONException e) {
-			logger.error(e,e);
-		}
-		return null;
-	}
 
 }
