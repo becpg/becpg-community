6c2f76a6067ce244c495b039b6061b2c58401cc5
Matthieu Laborie <matthieu.laborie@becpg.fr>
2020-12-14 09:10:39 +0100

    [Merge] Fix

--- becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/impl/ProjectServiceImpl.java
+++ becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/impl/ProjectServiceImpl.java
@@ -22,8 +22,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
