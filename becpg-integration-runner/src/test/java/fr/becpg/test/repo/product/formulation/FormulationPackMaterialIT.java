166d7b419bfbce35639a3005d3a2dbe9315ae323
Matthieu Laborie <matthieu.laborie@becpg.fr>
2021-01-15 16:29:06 +0100

    Fix #8869 - [Feature] Add level in packaging material formulation

--- becpg-integration-runner/src/test/java/fr/becpg/test/repo/product/formulation/FormulationPackMaterialIT.java
+++ becpg-integration-runner/src/test/java/fr/becpg/test/repo/product/formulation/FormulationPackMaterialIT.java
@@ -252,8 +252,8 @@ public class FormulationPackMaterialIT extends PLMBaseTestCase {
 			PF1.setUnit(ProductUnit.g);
 
 			List<PackMaterialListDataItem> packMaterial = new ArrayList<>();
-			packMaterial.add(new PackMaterialListDataItem(packMaterial1NodeRef, 10d));
-			packMaterial.add(new PackMaterialListDataItem(packMaterial2NodeRef, 20d));
+			packMaterial.add(new PackMaterialListDataItem(packMaterial1NodeRef, 10d, PackagingLevel.Primary));
+			packMaterial.add(new PackMaterialListDataItem(packMaterial2NodeRef, 20d, PackagingLevel.Primary));
 			PF1.setPackMaterialList(packMaterial);
 			PF1NodeRef = alfrescoRepository.create(getTestFolderNodeRef(), PF1).getNodeRef();
 
@@ -269,8 +269,8 @@ public class FormulationPackMaterialIT extends PLMBaseTestCase {
 			SF1.setUnit(ProductUnit.g);
 
 			packMaterial = new ArrayList<>();
-			packMaterial.add(new PackMaterialListDataItem(packMaterial3NodeRef, 30d));
-			packMaterial.add(new PackMaterialListDataItem(packMaterial4NodeRef, 40d));
+			packMaterial.add(new PackMaterialListDataItem(packMaterial3NodeRef, 30d, PackagingLevel.Primary));
+			packMaterial.add(new PackMaterialListDataItem(packMaterial4NodeRef, 40d, PackagingLevel.Primary));
 			SF1.setPackMaterialList(packMaterial);
 			SF1NodeRef = alfrescoRepository.create(getTestFolderNodeRef(), SF1).getNodeRef();
 
@@ -286,7 +286,7 @@ public class FormulationPackMaterialIT extends PLMBaseTestCase {
 			rawMaterial1.setUnit(ProductUnit.g);
 
 			packMaterial = new ArrayList<>();
-			packMaterial.add(new PackMaterialListDataItem(packMaterial5NodeRef, 50d));
+			packMaterial.add(new PackMaterialListDataItem(packMaterial5NodeRef, 50d, PackagingLevel.Primary));
 			rawMaterial1.setPackMaterialList(packMaterial);
 			rawMaterial1NodeRef = alfrescoRepository.create(getTestFolderNodeRef(), rawMaterial1).getNodeRef();
 
