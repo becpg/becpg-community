9b7f47a246d05da4ae7bf0a8fe90c3beac30ca8d
Matthieu Laborie <matthieu.laborie@becpg.fr>
2021-01-22 12:13:13 +0100

    Fix #9145 - [Feature] Add document basket

---
+++ becpg-share/src/main/resources/alfresco/site-webscripts/org/alfresco/components/custom-footer/footer.get.js
@@ -0,0 +1,18 @@
+
+/**
+ * JS Controller for Share's footer component
+ */
+
+function main()
+{
+  // Widget instantiation metadata...
+   var widget = {
+      id : "basket", 
+      name : "beCPG.component.Basket"
+   };
+   model.widgets = [widget];
+}
+
+main();
+
+    
