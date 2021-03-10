28975182da7cc7a9d375d0402f3c1bc99695fcf2
Matthieu Laborie <matthieu.laborie@becpg.fr>
2021-03-10 14:26:02 +0100

    Fix #9552 - [Bug] Content-disposition Header doesn't been writted

--- becpg-core/src/main/java/fr/becpg/repo/helper/AttachmentHelper.java
+++ becpg-core/src/main/java/fr/becpg/repo/helper/AttachmentHelper.java
@@ -1,6 +1,6 @@
 package fr.becpg.repo.helper;
 
-import org.alfresco.repo.webdav.WebDAVHelper;
+import org.springframework.extensions.surf.util.URLEncoder;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 import org.springframework.extensions.webscripts.WebScriptResponse;
 
@@ -14,6 +14,10 @@ public class AttachmentHelper {
 
 	  private static final String HEADER_USER_AGENT     = "User-Agent";
 	 
+	  private AttachmentHelper() {
+		  
+	  }
+	  
 	  /**
 	   * <p>setAttachment.</p>
 	   *
@@ -26,30 +30,64 @@ public class AttachmentHelper {
 	            String headerValue = "attachment";
 	            if (attachFileName != null && attachFileName.length() > 0)
 	            {
-	                if (req == null)
-	                {
-	                    headerValue += "; filename*=UTF-8''" + WebDAVHelper.encodeURL(attachFileName)
-	                            + "; filename=\"" + attachFileName + "\"";
-	                }
-	                else
-	                {
-	                    String userAgent = req.getHeader(HEADER_USER_AGENT);
-	                    boolean isLegacy = (null != userAgent) && (userAgent.contains("MSIE 8") || userAgent.contains("MSIE 7"));
-	                    if (isLegacy)
-	                    {
-	                        headerValue += "; filename=\"" + WebDAVHelper.encodeURL(attachFileName)+"\"";
-	                    }
-	                    else
-	                    {
-	                        headerValue += "; filename=\"" + attachFileName + "\"; filename*=UTF-8''"
-	                                + WebDAVHelper.encodeURL(attachFileName);
-	                    }
-	                }
+	            	 if (req == null)
+		                {
+		                    headerValue += "; filename*=UTF-8''" + URLEncoder.encode(attachFileName)
+		                            + "; filename=\"" + filterNameForQuotedString(attachFileName) + "\"";
+		                }
+		                else
+		                {
+		                    String userAgent = req.getHeader(HEADER_USER_AGENT);
+		                    boolean isLegacy = (null != userAgent) && (userAgent.contains("MSIE 8") || userAgent.contains("MSIE 7"));
+		                    if (isLegacy)
+		                    {
+		                        headerValue += "; filename=\"" + URLEncoder.encode(attachFileName);
+		                    }
+		                    else
+		                    {
+		                        headerValue += "; filename=\"" + filterNameForQuotedString(attachFileName) + "\"; filename*=UTF-8''"
+		                                + URLEncoder.encode(attachFileName);
+		                    }
+		                }
 	            }
 	            
 	            // set header based on filename - will force a Save As from the browse if it doesn't recognize it
 	            // this is better than the default response of the browser trying to display the contents
 	            res.setHeader("Content-Disposition", headerValue);
 	        }
+	  
+	  
+	    
+	    private static String filterNameForQuotedString(String s)
+	    {
+	        StringBuilder sb = new StringBuilder();
+	        for(int i = 0; i < s.length(); i++)
+	        {
+	            char c = s.charAt(i);
+	            if(isValidQuotedStringHeaderParamChar(c))
+	            {
+	                sb.append(c);
+	            }
+	            else
+	            {
+	                sb.append(" ");
+	            }
+	        }
+	        return sb.toString();
+	    }
+	    
+	    private static boolean isValidQuotedStringHeaderParamChar(char c)
+	    {
+	        // see RFC2616 section 2.2: 
+	        // qdtext         = <any TEXT except <">>
+	        // TEXT           = <any OCTET except CTLs, but including LWS>
+	        // CTL            = <any US-ASCII control character (octets 0 - 31) and DEL (127)>
+	        // A CRLF is allowed in the definition of TEXT only as part of a header field continuation.
+	        // Note: we dis-allow header field continuation
+	        return     (c < 256)  // message header param fields must be ISO-8859-1. Lower 256 codepoints of Unicode represent ISO-8859-1
+	                && (c != 127) // CTL - see RFC2616 section 2.2
+	                && (c != '"') // <">
+	                && (c > 31);  // CTL - see RFC2616 section 2.2
+	    }
 	    
 }
