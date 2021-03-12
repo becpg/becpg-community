355dc32e0fa8d84053b2c5d7cc58959a234f3992
valentin leblanc <valentin.leblanc@becpg.fr>
2021-03-12 09:43:38 +0100

    Fix #9434 - [Bug] MLText actvities -> revert the JUnit

--- becpg-integration-runner/src/test/java/fr/becpg/test/repo/activity/DetailActivityContentIT.java
+++ becpg-integration-runner/src/test/java/fr/becpg/test/repo/activity/DetailActivityContentIT.java
@@ -140,7 +140,7 @@ public class DetailActivityContentIT extends AbstractFinishedProductTest {
 		assertEquals("Check if No Activity", 1, getActivities(finishedProductNodeRef, null).size());
 
 		// Add Client to product
-		NodeRef client1NodeRef = transactionService.getRetryingTransactionHelper().doInTransaction(() -> {
+		transactionService.getRetryingTransactionHelper().doInTransaction(() -> {
 			ClientData client1 = new ClientData();
 			client1.setName("Client1");
 			client1 = clientRepository.create(getTestFolderNodeRef(), client1);
@@ -185,7 +185,7 @@ public class DetailActivityContentIT extends AbstractFinishedProductTest {
 				JSONObject dataProp = data.getJSONArray("properties").getJSONObject(0);
 				if (dataProp != null) {
 					assertEquals("Check client title modification", PLMModel.ASSOC_CLIENTS.toString(), dataProp.getString("title"));
-					assertEquals("Check client before modification", "[\"(" + client1NodeRef.toString() + ", Client1)\"]" ,dataProp.getString("before"));
+					assertEquals("Check client before modification", "[]" ,dataProp.getString("before"));
 					assertEquals("Check client after modification", "[\"("+client2NodeRef+", Client2)\"]",dataProp.getString("after"));
 				}
 			}
