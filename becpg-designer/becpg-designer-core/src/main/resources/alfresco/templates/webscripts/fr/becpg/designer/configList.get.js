09fef4be6b5b3e0499a491670fa0bdd252a7cca3
valentin leblanc <valentin.leblanc@becpg.fr>
2021-03-19 09:43:28 +0100

    Fix #8988 - [Bug] Exclude *-config-custom.xml from designer

--- becpg-designer/becpg-designer-core/src/main/resources/alfresco/templates/webscripts/fr/becpg/designer/configList.get.js
+++ becpg-designer/becpg-designer-core/src/main/resources/alfresco/templates/webscripts/fr/becpg/designer/configList.get.js
@@ -9,7 +9,7 @@ function getData()
 			  
 	  var items = search.query(
 			      {
-			         query: '+ASPECT:"dsg:configAspect"',
+			         query: '+ASPECT:"dsg:configAspect" -@\\{http\\://www.alfresco.org/model/content/1.0\\}name:("-config-custom.xml")',
 			         language : 'lucene'
 			      });
 			
