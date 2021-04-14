d1a52653f873fc4095a113dda07826b097e2f94f
Matthieu Laborie <matthieu.laborie@becpg.fr>
2021-04-14 17:41:13 +0200

    [Bug] Fix NPE on task notification when manual date on

--- becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/formulation/TaskOverdueFormulationHandler.java
+++ becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/formulation/TaskOverdueFormulationHandler.java
@@ -32,15 +32,15 @@ import fr.becpg.repo.project.impl.ProjectHelper;
 /**
  * <p>TaskOverdueFormulationHandler class.</p>
  *
- * @author matthieu
+ * @author rabah
  * @version $Id: $Id
  */
 public class TaskOverdueFormulationHandler extends FormulationBaseHandler<ProjectData> {
 
 	private static final Log logger = LogFactory.getLog(TaskOverdueFormulationHandler.class);
-	
+
 	private static final String MAIL_TEMPLATE = "/app:company_home/app:dictionary/app:email_templates/cm:project/cm:task-overdue.ftl";
-	private static final String MAIL_SUBJECT= "project.notification.overdue.subject";	
+	private static final String MAIL_SUBJECT = "project.notification.overdue.subject";
 
 	private BeCPGMailService beCPGMailService;
 	private WorkflowService workflowService;
@@ -90,53 +90,54 @@ public class TaskOverdueFormulationHandler extends FormulationBaseHandler<Projec
 	 */
 	@Override
 	public boolean process(ProjectData projectData) {
-	
 
-		if(ProjectState.InProgress.equals(projectData.getProjectState()) && !projectData.getAspects().contains(BeCPGModel.ASPECT_ENTITY_TPL)){
+		if (ProjectState.InProgress.equals(projectData.getProjectState()) && !projectData.getAspects().contains(BeCPGModel.ASPECT_ENTITY_TPL)) {
 
 			logger.debug("Processing tasks notifications");
-			
-			for(TaskListDataItem task :  projectData.getTaskList()){
-				Boolean notificationsAreEnabled = (task.getInitialNotification() != null 
-						&& task.getNotificationAuthorities() != null 
-						&& !task.getNotificationAuthorities().isEmpty());
 
-				if(!Boolean.TRUE.equals(notificationsAreEnabled)){
-					logger.debug("Task "+task.getTaskName()+" does not have notifications enabled, continue..");
-					continue;
+			for (TaskListDataItem task : projectData.getTaskList()) {
+				if (TaskState.InProgress.equals(task.getTaskState())) {
+					Boolean notificationsAreEnabled = ((task.getInitialNotification() != null) && (task.getNotificationAuthorities() != null)
+							&& !task.getNotificationAuthorities().isEmpty() && (task.getEnd() != null));
+
+					if (Boolean.TRUE.equals(notificationsAreEnabled)) {
+
+						logger.debug("/*-- \tTask " + task.getTaskName() + "\t --*/");
+
+						Date firstNotificationDate = calculateFirstNotificationDate(task);
+						Date currentDate = ProjectHelper.removeTime(new Date());
+						Date nextNotification = calculateNextNotificationDate(task, firstNotificationDate);
+						if (nextNotification != null) {
+							String workflowTaskId = extractWorkflowTask(task);
+
+							if (logger.isDebugEnabled()) {
+								logger.debug("First notification: " + firstNotificationDate);
+								logger.debug("Last notification: " + task.getLastNotification());
+								logger.debug("Next notification: " + nextNotification);
+								logger.debug("workflowTaskId: " + workflowTaskId);
+							}
+
+							if (currentDate.after(nextNotification)) {
+								task.setLastNotification(currentDate);
+								if(logger.isDebugEnabled()) {
+									logger.debug("authoritiesNR: " + task.getNotificationAuthorities());
+								}
+								sendTaskNotificationEmails(task.getNotificationAuthorities(), task, projectData, workflowTaskId);
+							}
+						} else {
+							logger.debug("No new notification scheduled, skipping..");
+						}
+					} else {
+						logger.debug("Task " + task.getTaskName() + " does not have notifications enabled, continue..");
+
+					}
+				} else {
+					logger.debug("Task " + task.getTaskName() + " is not in progress, skipping..");
 				}
 
-				if(!TaskState.InProgress.equals(task.getTaskState())){
-					logger.debug("Task "+task.getTaskName()+" is not in progress, skipping..");
-					continue;
-				}
-
-				logger.debug("/*-- \tTask "+task.getTaskName()+"\t --*/");
-
-				Date firstNotificationDate = calculateFirstNotificationDate(task);
-				Date currentDate = ProjectHelper.removeTime(new Date());
-				Date nextNotification = calculateNextNotificationDate(task, firstNotificationDate);
-				String workflowTaskId = extractWorkflowTask(task);
-				
-				if(logger.isDebugEnabled()){
-					logger.debug("First notification: "+firstNotificationDate);
-					logger.debug("Last notification: "+task.getLastNotification());
-					logger.debug("Next notification: "+nextNotification);
-					logger.debug("workflowTaskId: "+workflowTaskId);
-				}
-				if(nextNotification == null){
-					logger.debug("No new notification scheduled, skipping..");
-					continue;
-				}
-
-				if(currentDate.after(nextNotification)){
-					task.setLastNotification(currentDate);
-					logger.debug("authoritiesNR: "+task.getNotificationAuthorities());
-					sendTaskNotificationEmails(task.getNotificationAuthorities(), task, projectData, workflowTaskId);
-				}
 			}
 		} else {
-			logger.debug("Project "+projectData.getName()+" not in progress (status: "+projectData.getProjectState()+")");
+			logger.debug("Project " + projectData.getName() + " not in progress (status: " + projectData.getProjectState() + ")");
 		}
 		return true;
 	}
@@ -148,10 +149,10 @@ public class TaskOverdueFormulationHandler extends FormulationBaseHandler<Projec
 	 * @param firstNotificationDate a {@link java.util.Date} object.
 	 * @return a {@link java.util.Date} object.
 	 */
-	public Date calculateNextNotificationDate(TaskListDataItem task, Date firstNotificationDate){
-		if(task.getLastNotification() == null || task.getLastNotification().before(firstNotificationDate)){
+	public Date calculateNextNotificationDate(TaskListDataItem task, Date firstNotificationDate) {
+		if ((task.getLastNotification() == null) || task.getLastNotification().before(firstNotificationDate)) {
 			return firstNotificationDate;
-		} else if(task.getNotificationFrequency() != null && task.getNotificationFrequency() > 0){
+		} else if ((task.getNotificationFrequency() != null) && (task.getNotificationFrequency() > 0)) {
 			Calendar cal = Calendar.getInstance();
 			cal.setTime(task.getLastNotification());
 			cal.add(Calendar.DAY_OF_MONTH, task.getNotificationFrequency());
@@ -169,7 +170,7 @@ public class TaskOverdueFormulationHandler extends FormulationBaseHandler<Projec
 	 * @param task a {@link fr.becpg.repo.project.data.projectList.TaskListDataItem} object.
 	 * @return a {@link java.util.Date} object.
 	 */
-	public Date calculateFirstNotificationDate(TaskListDataItem task){
+	public Date calculateFirstNotificationDate(TaskListDataItem task) {
 		Calendar firstNotificationCal = Calendar.getInstance();
 		firstNotificationCal.setTime(task.getEnd());
 		firstNotificationCal.add(Calendar.DAY_OF_MONTH, task.getInitialNotification());
@@ -184,23 +185,21 @@ public class TaskOverdueFormulationHandler extends FormulationBaseHandler<Projec
 	 * @param project a {@link fr.becpg.repo.project.data.ProjectData} object.
 	 * @param workflowTaskId a {@link java.lang.String} object.
 	 */
-	public void sendTaskNotificationEmails(List<NodeRef> authorities, TaskListDataItem task, ProjectData project, String workflowTaskId){
+	public void sendTaskNotificationEmails(List<NodeRef> authorities, TaskListDataItem task, ProjectData project, String workflowTaskId) {
 		Map<String, Object> templateArgs = new HashMap<>();
 		templateArgs.put("date", new Date());
 		templateArgs.put("task", task.getTaskName());
 		templateArgs.put("project", project.getName());
 		templateArgs.put("dueDate", task.getEnd());
 		templateArgs.put("taskId", workflowTaskId);
-		
+
 		authorities = projectService.extractResources(project.getNodeRef(), authorities);
 		Map<String, Object> argsMap = new HashMap<>();
 		argsMap.put("args", templateArgs);
-		beCPGMailService.sendMail(authorities, 
-									projectNotificationService.createSubject(project.getNodeRef(), task.getNodeRef(), I18NUtil.getMessage(MAIL_SUBJECT)),
-									MAIL_TEMPLATE, 
-									argsMap, true);	 
+		beCPGMailService.sendMail(authorities,
+				projectNotificationService.createSubject(project.getNodeRef(), task.getNodeRef(), I18NUtil.getMessage(MAIL_SUBJECT)), MAIL_TEMPLATE,
+				argsMap, true);
 	}
-
 
 	/**
 	 * <p>extractWorkflowTask.</p>
@@ -208,28 +207,27 @@ public class TaskOverdueFormulationHandler extends FormulationBaseHandler<Projec
 	 * @param task a {@link fr.becpg.repo.project.data.projectList.TaskListDataItem} object.
 	 * @return a {@link java.lang.String} object.
 	 */
-	public String extractWorkflowTask(TaskListDataItem task){
-		
-		if(StringUtils.isEmpty(task.getWorkflowInstance())){
+	public String extractWorkflowTask(TaskListDataItem task) {
+
+		if (StringUtils.isEmpty(task.getWorkflowInstance())) {
 			logger.debug("Workflow instance is empty");
 			return null;
 		}
-		
+
 		WorkflowTaskQuery taskQuery = new WorkflowTaskQuery();
 		taskQuery.setProcessId(task.getWorkflowInstance());
 		taskQuery.setTaskState(WorkflowTaskState.IN_PROGRESS);
 		List<WorkflowTask> foundTasks = workflowService.queryTasks(taskQuery, false);
 
-		if(foundTasks.isEmpty()){
+		if (foundTasks.isEmpty()) {
 			logger.debug("found no tasks matching..");
 			return null;
 		}
 
 		List<WorkflowTask> filteredTasks = foundTasks.stream()
-				.filter(found -> task.getNodeRef().equals(found.getProperties().get(ProjectModel.ASSOC_WORKFLOW_TASK)))
-				.collect(Collectors.toList());
+				.filter(found -> task.getNodeRef().equals(found.getProperties().get(ProjectModel.ASSOC_WORKFLOW_TASK))).collect(Collectors.toList());
 
-		if(filteredTasks.isEmpty()){
+		if (filteredTasks.isEmpty()) {
 			return null;
 		}
 
