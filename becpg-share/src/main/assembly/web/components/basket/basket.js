604a77602aa48b87548d8645eebcbc84f067db60
Matthieu Laborie <matthieu.laborie@becpg.fr>
2021-06-14 11:01:58 +0200

    Fix  #9242 - [Feature] Add delete from basket action

--- becpg-share/src/main/assembly/web/components/basket/basket.js
+++ becpg-share/src/main/assembly/web/components/basket/basket.js
@@ -5,6 +5,10 @@
 	var Dom = YAHOO.util.Dom;
 
 	var $html = Alfresco.util.encodeHTML;
+	
+	var REMOVE_EVENTCLASS= Alfresco.util.generateDomId(null,"remove-from-basket");
+
+
 
 
 	/**
@@ -29,7 +33,6 @@
 			Alfresco.component.Base,
 			{
 
-
 				/**
 				 * Fired by YUI when parent element is available for
 				 * scripting.
@@ -37,9 +40,11 @@
 				 * @method onReady
 				 */
 				onReady: function Basket_onReady() {
+ 
+					var me = this;
 
 					this.services.basket = new beCPG.service.Basket();
-
+					
 
 					this.widgets.basketMenu = Alfresco.util.createYUIButton(this, "basket-button", this.onBasketMenu, {
 						type: "menu",
@@ -47,13 +52,28 @@
 						lazyloadmenu: false,
 						disabled: true
 					});
-
+					
 					this.onBasketChanged();
+					
+					 var fnRemoveHandler = function fnRemoveHandler(layer, args)
+			         {
+			           	
+			         	 var anchor = args[1].anchor,
+			              rel = anchor.getAttribute("rel");
+			              me.services.basket.toggle({"nodeRef":rel});
+                          
+                          anchor.parentNode.parentNode.removeChild(anchor.parentNode);
+							
+              			  return true;
+			         };
+
+					YAHOO.Bubbling.addDefaultAction(REMOVE_EVENTCLASS, fnRemoveHandler);
+					
 				},
-
 				onBasketMenu: function(sType, aArgs) {
+					
 					var domEvent = aArgs[0], eventTarget = aArgs[1];
-
+					
 					// Check mandatory docList module is present
 
 					// Get the function related to the clicked item
@@ -72,10 +92,15 @@
 				},
 
 				onActionShowBasket: function(records) {
-					var popupHtml = "<div class=\"basket-popup\"> <ul>";
+					
+					var popupHtml = "<div  class=\"basket-popup\"> <ul>";
+
 					for (var i = 0; i < records.length; i++) {
-						popupHtml += "<li><span class=\"content\">" + $html(records[i].displayName) + "</span></li>";
+						popupHtml += "<li><span class=\"content\">" + $html(records[i].displayName) + "</span>";
+						popupHtml += '<a href="#" rel="' + records[i].nodeRef + '" class="'+REMOVE_EVENTCLASS+' basket-delete" title="'
+	           							+ Alfresco.util.message("basket.remove.tip") + '">&nbsp;</a></li>';
 					}
+
 					popupHtml += "<ul></div>";
 
 					Alfresco.util.PopupManager.displayPrompt(
@@ -87,11 +112,10 @@
 						});
 
 				},
-
 				onBasketChanged: function() {
 					if (Alfresco.doclib && Alfresco.doclib.Actions) {
 						var count = this.services.basket.count();
-
+						
 
 						var menuItems = this.widgets.basketMenu.getMenu().getItems();
 						for (var m in menuItems) {
@@ -100,7 +124,9 @@
 						}
 						if (count > 0) {
 							Dom.removeClass(this.id + "-basket", "hidden");
+							
 							this.widgets.basketMenu.set("disabled", false);
+							
 						} else {
 							Dom.addClass(this.id + "-basket", "hidden");
 							this.widgets.basketMenu.set("disabled", true);
