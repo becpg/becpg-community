b99323480816552d576ef5eaf8b57707620a1c2f
valentin leblanc <valentin.leblanc@becpg.fr>
2021-06-22 15:29:04 +0200

    Fix #10500 - [Bug] Fix missing activities

--- becpg-core/src/main/java/fr/becpg/repo/activity/extractor/ActivityListExtractor.java
+++ becpg-core/src/main/java/fr/becpg/repo/activity/extractor/ActivityListExtractor.java
@@ -18,6 +18,7 @@
 package fr.becpg.repo.activity.extractor;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -193,16 +194,31 @@ public class ActivityListExtractor extends SimpleExtractor {
 									if (activityProperty.has(EntityActivityService.BEFORE)) {
 										Object beforeProperty = activityProperty.get(EntityActivityService.BEFORE);
 										if ((beforeProperty instanceof JSONArray) && (((JSONArray) beforeProperty).length() > 0)) {
-											postProperty.put(EntityActivityService.BEFORE, checkProperty(beforeProperty, propertyDef));
+											
+											Object afterProperty = activityProperty.get(EntityActivityService.AFTER);
+											
+											if ((afterProperty instanceof JSONArray) && (((JSONArray) afterProperty).length() > 0)) {
+												adaptProperty((JSONArray) beforeProperty, (JSONArray) afterProperty);
+											}
+											
+											postProperty.put(EntityActivityService.BEFORE, checkProperty((JSONArray) beforeProperty, propertyDef));
 										} else {
 											postProperty.put(EntityActivityService.BEFORE, beforeProperty);
 										}
 									}
+									
 									// AfterProperty
 									if (activityProperty.has(EntityActivityService.AFTER)) {
 										Object afterProperty = activityProperty.get(EntityActivityService.AFTER);
 										if ((afterProperty instanceof JSONArray) && (((JSONArray) afterProperty).length() > 0)) {
-											postProperty.put(EntityActivityService.AFTER, checkProperty(afterProperty, propertyDef));
+											
+											Object beforeProperty = activityProperty.get(EntityActivityService.BEFORE);
+											
+											if ((beforeProperty instanceof JSONArray) && (((JSONArray) beforeProperty).length() > 0)) {
+												adaptProperty((JSONArray) afterProperty, (JSONArray) beforeProperty);
+											}
+											
+											postProperty.put(EntityActivityService.AFTER, checkProperty((JSONArray) afterProperty, propertyDef));
 										} else {
 											postProperty.put(EntityActivityService.AFTER, afterProperty);
 										}
@@ -234,6 +250,26 @@ public class ActivityListExtractor extends SimpleExtractor {
 
 	}
 
+	private void adaptProperty(JSONArray propToAdapt, JSONArray propRef) throws JSONException {
+
+		if (propToAdapt.get(0) == JSONObject.NULL && propRef.get(0) instanceof JSONArray) {
+
+			JSONArray newArray = new JSONArray();
+
+			for (int k = 0; k < ((JSONArray) propRef.get(0)).length(); k++) {
+				newArray.put("");
+			}
+
+			propToAdapt.put(0, newArray);
+
+		} else if (propToAdapt.get(0) instanceof JSONArray && propRef.get(0) instanceof JSONArray
+				&& ((JSONArray) propToAdapt.get(0)).length() < ((JSONArray) propRef.get(0)).length()) {
+			for (int k = 0; k < ((JSONArray) propRef.get(0)).length() - ((JSONArray) propToAdapt.get(0)).length(); k++) {
+				((JSONArray) propToAdapt.get(0)).put("");
+			}
+		}
+	}
+
 	/**
 	 * <p>checkProperty.</p>
 	 *
@@ -241,9 +277,8 @@ public class ActivityListExtractor extends SimpleExtractor {
 	 * @param propertyDef a {@link org.alfresco.service.cmr.dictionary.PropertyDefinition} object.
 	 * @return a {@link org.json.JSONArray} object.
 	 */
-	public JSONArray checkProperty(Object property, PropertyDefinition propertyDef) {
+	public JSONArray checkProperty(JSONArray propertyArray, PropertyDefinition propertyDef) {
 		boolean updateProperty = true;
-		JSONArray propertyArray = (JSONArray) property;
 		JSONArray postproperty = new JSONArray();
 		for (int i = 0; i < propertyArray.length(); i++) {
 			try {
