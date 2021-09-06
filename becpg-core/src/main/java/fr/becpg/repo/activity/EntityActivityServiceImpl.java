77a36af3094140bd7694ae2a1be58128ee92e913
Matthieu Laborie <matthieu.laborie@becpg.fr>
2021-09-06 17:12:47 +0200

    [Setup] Fix junit test

--- becpg-core/src/main/java/fr/becpg/repo/activity/EntityActivityServiceImpl.java
+++ becpg-core/src/main/java/fr/becpg/repo/activity/EntityActivityServiceImpl.java
@@ -1336,6 +1336,8 @@ public class EntityActivityServiceImpl implements EntityActivityService {
 		return transactionService.getRetryingTransactionHelper().doInTransaction(() -> {
 			BeCPGQueryBuilder queryBuilder = BeCPGQueryBuilder.createQuery().ofType(BeCPGModel.TYPE_ENTITY_V2)
 					.excludeVersions()
+					.inDB()
+					.ftsLanguage()
 					.maxResults(RepoConsts.MAX_RESULTS_UNLIMITED);
 			return queryBuilder.list();
 		}, false, true);
