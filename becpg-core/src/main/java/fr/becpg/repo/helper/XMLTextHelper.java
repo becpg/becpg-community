b7493bff5f07920c4166dd64b876f33e77d82f22
valentin leblanc <valentin.leblanc@becpg.fr>
2021-09-10 17:22:06 +0200

    Fix #11054 - [Bug] Strip non valid XML characters in remote and report

---
+++ becpg-core/src/main/java/fr/becpg/repo/helper/XMLTextHelper.java
@@ -0,0 +1,31 @@
+package fr.becpg.repo.helper;
+
+public class XMLTextHelper {
+
+	private XMLTextHelper() {
+		//Singleton
+	}
+	
+	public static String stripNonValidXMLCharacters(String in) {
+		StringBuilder out = new StringBuilder(); // Used to hold the output.
+		char current; // Used to reference the current character.
+
+		if (in == null || ("".equals(in))) {
+			return ""; // vacancy test.
+		}
+		for (int i = 0; i < in.length(); i++) {
+	          current = in.charAt(i); // NOTE: No IndexOutOfBoundsException caught here; it should not happen.
+	          if ((current == 0x9) ||
+	              (current == 0xA) ||
+	              (current == 0xD) ||
+	              ((current >= 0x20) && (current <= 0xD7FF)) ||
+	              ((current >= 0xE000) && (current <= 0xFFFD)) ||
+	              ((current >= 0x10000) && (current <= 0x10FFFF))) {
+	        	  out.append(current);
+	          }
+	      }
+	      
+	      return out.toString();
+	  }
+	
+}
