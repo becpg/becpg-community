3ed877a88a7012ac9a9addcdd8724ab378f3b68a
Matthieu Laborie <matthieu.laborie@becpg.fr>
2021-11-09 18:04:58 +0100

    [Merge] Fix

--- becpg-core/src/main/java/fr/becpg/repo/activity/extractor/ActivityListExtractor.java
+++ becpg-core/src/main/java/fr/becpg/repo/activity/extractor/ActivityListExtractor.java
@@ -160,7 +160,7 @@ public class ActivityListExtractor extends SimpleExtractor {
 						}
 	
 						if (((entityType != null)
-								&& (postLookup.has(EntityActivityService.PROP_DATALIST_TYPE) && (securityService.computeAccessMode(entityNodeRef, entityType,
+								&& (postLookup.has(EntityActivityService.PROP_DATALIST_TYPE) && (securityService.computeAccessMode(entityType,
 										postLookup.getString(EntityActivityService.PROP_DATALIST_TYPE)) == SecurityService.NONE_ACCESS)))
 								|| ((charactNodeRef != null) && (permissionService.hasPermission(charactNodeRef, "Read") != AccessStatus.ALLOWED))) {
 	
@@ -182,7 +182,7 @@ public class ActivityListExtractor extends SimpleExtractor {
 								QName propertyName = QName.createQName(activityProperty.getString(EntityActivityService.PROP_TITLE));
 	
 								if ((entityType != null)
-										&& (securityService.computeAccessMode(entityNodeRef, entityType, propertyName) != SecurityService.NONE_ACCESS)) {
+										&& (securityService.computeAccessMode(entityType, propertyName) != SecurityService.NONE_ACCESS)) {
 									// Property Title
 									PropertyDefinition propertyDef = dictionaryService.getProperty(propertyName);
 									ClassAttributeDefinition propDef = entityDictionaryService.getPropDef(propertyName);
