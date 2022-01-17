7241affbfa72eb9a345e92b9bdd5675fa89257bc
Matthieu Laborie <matthieu.laborie@becpg.fr>
2022-01-17 16:06:01 +0100

    Fix #12167 - [Bug] Fix boolean search filter

--- becpg-core/src/main/java/fr/becpg/repo/search/impl/AdvSearchServiceImpl.java
+++ becpg-core/src/main/java/fr/becpg/repo/search/impl/AdvSearchServiceImpl.java
@@ -216,7 +216,8 @@ public class AdvSearchServiceImpl implements AdvSearchService {
 								// no need to add the term
 								if (!propValue.isEmpty()) {
 
-									String from, to;
+									String from;
+									String to;
 									int sepindex = propValue.indexOf("|");
 									if (propName.endsWith("-date-range")) {
 										// date range found
@@ -290,7 +291,7 @@ public class AdvSearchServiceImpl implements AdvSearchService {
 									queryBuilder.andBetween(QName.createQName(propName, namespaceService), "0", propValue);
 
 								}
-							} else {
+							} else if(!propName.endsWith("-entry")) {
 								// beCPG - bug fix : pb with operator -,
 								// AND, OR
 								// poivre AND -noir
@@ -358,10 +359,6 @@ public class AdvSearchServiceImpl implements AdvSearchService {
 		}
 
 		if (!NodeRef.isNodeRef(hierarchyName)) {
-
-			// TODO use HierarchyService, not generic
-			// " +PATH:\"/app:company_home/%s//*\" +TYPE:\"bcpg:linkedValue\"
-			// +@bcpg\\:lkvValue:\"%s\" ";
 
 			BeCPGQueryBuilder queryBuilder = BeCPGQueryBuilder.createQuery()
 					.inSubPath(RepoConsts.PATH_SYSTEM + "/" + RepoConsts.PATH_PRODUCT_HIERARCHY + "/"
