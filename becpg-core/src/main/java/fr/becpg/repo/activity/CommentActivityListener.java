b747cc1898c1fcd2dfb5ec5ce7b2817de7d96823
valentin leblanc <valentin.leblanc@becpg.fr>
2022-06-24 15:15:40 +0200

    Fix #12159 - [Feature] Update adhoc task from comment update

--- becpg-core/src/main/java/fr/becpg/repo/activity/CommentActivityListener.java
+++ becpg-core/src/main/java/fr/becpg/repo/activity/CommentActivityListener.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,11 +21,15 @@ import org.alfresco.service.cmr.security.PersonService;
 import org.alfresco.service.cmr.version.Version;
 import org.alfresco.service.cmr.version.VersionHistory;
 import org.alfresco.service.cmr.version.VersionService;
+import org.alfresco.service.cmr.workflow.WorkflowPath;
 import org.alfresco.service.cmr.workflow.WorkflowService;
+import org.alfresco.service.cmr.workflow.WorkflowTask;
+import org.alfresco.service.cmr.workflow.WorkflowTaskQuery;
 import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.commons.text.StringEscapeUtils;
 import org.htmlcleaner.ContentToken;
 import org.htmlcleaner.HtmlCleaner;
 import org.htmlcleaner.TagNode;
@@ -80,7 +85,7 @@ public class CommentActivityListener implements InitializingBean, EntityActivity
 
 			String comment = contentService.getReader(commentNodeRef, ContentModel.PROP_CONTENT).getContentString();
 
-			sendCommentNotification(comment, entityNodeRef);
+			sendCommentNotification(comment, entityNodeRef, commentNodeRef);
 
 		} else if (ActivityType.Version.equals(activityListDataItem.getActivityType())) {
 
@@ -92,13 +97,13 @@ public class CommentActivityListener implements InitializingBean, EntityActivity
 				Version version = versionHistory.getVersion(versionLabel);
 
 				if (version != null) {
-					sendCommentNotification(version.getDescription(), entityNodeRef);
+					sendCommentNotification(version.getDescription(), entityNodeRef, null);
 				}
 			}
 		}
 	}
 
-	private void sendCommentNotification(String comment, NodeRef entityNodeRef) {
+	private void sendCommentNotification(String comment, NodeRef entityNodeRef, NodeRef commentNodeRef) {
 
 		if (comment != null && !comment.isBlank()) {
 
@@ -131,22 +136,45 @@ public class CommentActivityListener implements InitializingBean, EntityActivity
 			
 			for (String username : usernames) {
 
-				NodeRef packageRef = packageMgr.create(null);
+				if (commentNodeRef != null) {
+					String workflowInstanceId = (String) nodeService.getProperty(commentNodeRef, ContentModel.PROP_DESCRIPTION);
+					if (workflowInstanceId == null || workflowInstanceId.isBlank()) {
+						NodeRef packageRef = packageMgr.create(null);
 
-				Map<QName, Serializable> params = new HashMap<>();
+						Map<QName, Serializable> params = new HashMap<>();
 
-				params.put(WorkflowModel.ASSOC_PACKAGE, packageRef);
-				params.put(WorkflowModel.PROP_WORKFLOW_DESCRIPTION, commentText);
-				params.put(WorkflowModel.ASSOC_ASSIGNEE,
-						(Serializable) Arrays.asList(personService.getPerson(username)));
-				params.put(WorkflowModel.PROP_WORKFLOW_PRIORITY, 2);
-				params.put(WorkflowModel.PROP_SEND_EMAIL_NOTIFICATIONS, true);
+						params.put(WorkflowModel.ASSOC_PACKAGE, packageRef);
+						params.put(WorkflowModel.PROP_WORKFLOW_DESCRIPTION, commentText);
+						params.put(WorkflowModel.ASSOC_ASSIGNEE, (Serializable) Arrays.asList(personService.getPerson(username)));
+						params.put(WorkflowModel.PROP_WORKFLOW_PRIORITY, 2);
+						params.put(WorkflowModel.PROP_SEND_EMAIL_NOTIFICATIONS, true);
 
-				nodeService.addChild(packageRef, entityNodeRef, WorkflowModel.ASSOC_PACKAGE_CONTAINS, QName
-						.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, QName.createValidLocalName(itemName)));
+						nodeService.addChild(packageRef, entityNodeRef, WorkflowModel.ASSOC_PACKAGE_CONTAINS, QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, QName.createValidLocalName(itemName)));
 
-				workflowService.startWorkflow("activiti$activitiAdhoc:1:4", params);
-
+						WorkflowPath workflowPath = workflowService.startWorkflow("activiti$activitiAdhoc:1:4", params);
+						
+						if (workflowPath != null) {
+							nodeService.setProperty(commentNodeRef, ContentModel.PROP_DESCRIPTION, workflowPath.getInstance().getId());
+						}
+					} else {
+			            WorkflowTaskQuery tasksQuery = new WorkflowTaskQuery();
+			            tasksQuery.setTaskState(null);
+			            tasksQuery.setActive(null);
+			            tasksQuery.setProcessId(workflowInstanceId);
+			            List<WorkflowTask> tasks = workflowService.queryTasks(tasksQuery, true);
+						
+			            if (tasks != null) {
+			            	for (WorkflowTask task : tasks) {
+			            		if ("wf:adhocTask".equals(task.getName())) {
+			            			Map<QName, Serializable> taskProperties = task.getProperties();
+			            			taskProperties.put(WorkflowModel.PROP_DESCRIPTION, commentText);
+			            			workflowService.updateTask(task.getId(), taskProperties, null, null);
+			            			break;
+			            		}
+			            	}
+			            }
+					}
+				}
 			}
 		}
 	}
@@ -159,7 +187,7 @@ public class CommentActivityListener implements InitializingBean, EntityActivity
 		
 		for (Object child : node.getChildren()) {
 			if (child instanceof ContentToken) {
-				contentBuilder.append(((ContentToken) child).getContent());
+				contentBuilder.append(StringEscapeUtils.unescapeHtml4(((ContentToken) child).getContent()));
 			} else if (child instanceof TagNode) {
 				parseHtmlContent((TagNode) child, contentBuilder);
 			}
