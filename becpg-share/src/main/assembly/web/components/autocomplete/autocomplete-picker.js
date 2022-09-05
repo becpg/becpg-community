077dcd1494fcdc876f8e33152a95bf8cb78f40c5
valentin leblanc <valentin.leblanc@becpg.fr>
2022-09-05 14:58:23 +0200

    Fix #14512 - [Bug] Fix process view assocs form

--- becpg-share/src/main/assembly/web/components/autocomplete/autocomplete-picker.js
+++ becpg-share/src/main/assembly/web/components/autocomplete/autocomplete-picker.js
@@ -140,7 +140,7 @@
                             var me = this, previewTooltip = null, q = "";
 
                             // Start by loading new Item
-                            if (!(this.options.multipleSelectMode || this.isAssoc))
+                            if (this.options.multipleSelectMode || this.isAssoc)
                             {
                                 me.loadItems();
                             }
@@ -849,10 +849,6 @@
                         	 basket.innerHTML = "";
                         	}
                         	this.options.currentValue = args[1];
-                        	 if (this.options.multipleSelectMode || this.isAssoc)
-                             {
-                        		 this.loadItems();
-                             }
                         },
 
                         beforeFormRuntimeInit : function(layer, args)
