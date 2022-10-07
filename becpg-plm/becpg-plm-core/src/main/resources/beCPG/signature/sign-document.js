820b0030ef410190825e3b6b6ca1352135a8dfdb
valentin leblanc <valentin.leblanc@becpg.fr>
2022-10-07 19:19:10 +0200

    InProgress #14844 - [Feature] Improve signature on supplier portal

--- becpg-plm/becpg-plm-core/src/main/resources/beCPG/signature/sign-document.js
+++ becpg-plm/becpg-plm-core/src/main/resources/beCPG/signature/sign-document.js
@@ -1,3 +1,25 @@
+function extractRecipients(entity) {
+	var recipients = [];
+			
+	for (var i in entity.assocs["sign:recipients"]) {
+		
+		var authority = entity.assocs["sign:recipients"][i];
+		
+		if (authority.type == "{http://www.alfresco.org/model/content/1.0}authorityContainer") {
+			
+			var members = authority.childAssociations["cm:member"];
+			
+			for (var index in members) {
+				recipients.push(members[index]);
+			}
+		} else {
+			recipients.push(authority);
+		}
+	}
+	
+	return recipients;
+}
+
 function main() {
 	
 	var docDeliverable;
@@ -12,7 +34,24 @@ function main() {
 
 	var document = search.findNode(docDeliverable.content);
 
-	bSign.signDocument(document);
+	document = bSign.signDocument(document);
+	
+	var suppliers = extractRecipients(document);
+	
+	if (document.properties["sign:status"] == "Signed") {
+		
+		var documentName = document.properties["cm:name"];
+		var shareId = bcpg.shareContent(document);
+		
+		var templateArgs = {};
+		var templateModel = {};
+
+		templateArgs['documentName'] = documentName;
+		templateArgs['shareId'] = shareId;
+		templateModel['args'] = templateArgs;
+		
+		bcpg.sendMail(suppliers, bcpg.getMessage("plm.supplier.portal.sign.mail.title", documentName), "/app:company_home/app:dictionary/app:email_templates/cm:workflownotification/cm:signature-notify-email.ftl", templateModel, true);
+	}
 	
 }
 
--- becpg-plm/becpg-plm-core/src/main/resources/beCPG/supplier/validateProjectEntity.js
+++ becpg-plm/becpg-plm-core/src/main/resources/beCPG/supplier/validateProjectEntity.js
@@ -1,12 +1,7 @@
 function main() {
 	var projectEntity = null;
 
-	var projectNodeRef = search.findNode(project.nodeRef);
-	
-	if (project.entities != null && project.entities.size() > 0 && projectNodeRef.assocs["bcpg:supplierAccountRef"] && projectNodeRef.assocs["bcpg:supplierAccountRef"].length > 0) {
-		
-		var supplier = projectNodeRef.assocs["bcpg:supplierAccountRef"][0];
-		
+	if (project.entities != null && project.entities.size() > 0) {
 		projectEntity = search.findNode(project.entities.get(0));
 		/*
 		  if no autoMerge date is specified  merge and validate entity 
@@ -14,37 +9,6 @@ function main() {
 		projectEntity = bSupplier.validateProjectEntity(projectEntity);
 
 		project.entities.add(projectEntity.nodeRef);
-		
-		var suppliers = [];
-		
-		suppliers.push(supplier);
-		
-		var notificationSignedDocument = null;
-
-		for (var i = 0; i < project.deliverableList.size(); i++) {
-			var deliverable = project.deliverableList.get(i);
-			
-			if (deliverable.name == "notificationSignedDocument") {
-				notificationSignedDocument = deliverable;
-			}
-		}
-		
-		if (notificationSignedDocument && notificationSignedDocument.content) {
-			
-			var signedDocument = search.findNode(notificationSignedDocument.content);
-			
-			var documentName = signedDocument.properties["cm:name"];
-			var shareId = bcpg.shareContent(signedDocument);
-			
-			var templateArgs = {};
-			var templateModel = {};
-	
-			templateArgs['documentName'] = documentName;
-			templateArgs['shareId'] = shareId;
-			templateModel['args'] = templateArgs;
-			
-			bcpg.sendMail(suppliers, bcpg.getMessage("plm.supplier.portal.sign.mail.title", documentName), "/app:company_home/app:dictionary/app:email_templates/cm:workflownotification/cm:signature-notify-email.ftl", templateModel, true);
-		}
 		
 	}
 }
