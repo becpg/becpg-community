6f000a36e110f521eb3a461e6c8b96ca793a78a9
valentin leblanc <valentin.leblanc@becpg.fr>
2022-11-07 15:53:05 +0100

    Fix #14808 - [Bug] Add patch for diluentAspect

---
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/admin/patch/RemoveDiluentAspectPatch.java
@@ -0,0 +1,221 @@
+package fr.becpg.repo.admin.patch;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.alfresco.repo.batch.BatchProcessWorkProvider;
+import org.alfresco.repo.batch.BatchProcessor;
+import org.alfresco.repo.batch.BatchProcessor.BatchProcessWorker;
+import org.alfresco.repo.domain.node.NodeDAO;
+import org.alfresco.repo.domain.patch.PatchDAO;
+import org.alfresco.repo.domain.qname.QNameDAO;
+import org.alfresco.repo.policy.BehaviourFilter;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.rule.RuleService;
+import org.alfresco.service.namespace.QName;
+import org.alfresco.util.Pair;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.extensions.surf.util.I18NUtil;
+
+import fr.becpg.model.BeCPGModel;
+
+/**
+ * <p>RemoveDiluentAspectPatch class.</p>
+ *
+ * @author matthieu
+ * @version $Id: $Id
+ */
+public class RemoveDiluentAspectPatch extends AbstractBeCPGPatch {
+
+	private static final QName ASPECT_DILUENT = QName.createQName(BeCPGModel.BECPG_URI, "diluentAspect");
+	private static final Log logger = LogFactory.getLog(RemoveDiluentAspectPatch.class);
+	private static final String MSG_SUCCESS = "patch.bcpg.plm.RemoveDiluentAspectPatch.result";
+
+	private NodeDAO nodeDAO;
+	private PatchDAO patchDAO;
+	private QNameDAO qnameDAO;
+	private BehaviourFilter policyBehaviourFilter;
+	private RuleService ruleService;
+
+	private final int batchThreads = 3;
+	private final int batchSize = 40;
+	private final long count = batchThreads * batchSize;
+
+	/** {@inheritDoc} */
+	@Override
+	protected String applyInternal() throws Exception {
+
+		AuthenticationUtil.setAdminUserAsFullyAuthenticatedUser();
+
+		BatchProcessWorkProvider<NodeRef> workProvider = new BatchProcessWorkProvider<NodeRef>() {
+			final List<NodeRef> result = new ArrayList<>();
+
+			final long maxNodeId = getNodeDAO().getMaxNodeId();
+
+			long minSearchNodeId = 0;
+			long maxSearchNodeId = count;
+
+			final Pair<Long, QName> val = getQnameDAO().getQName(ASPECT_DILUENT);
+
+			@Override
+			public int getTotalEstimatedWorkSize() {
+				return result.size();
+			}
+			
+			@Override
+			public long getTotalEstimatedWorkSizeLong() {
+				return getTotalEstimatedWorkSize();
+			}
+
+			@Override
+			public Collection<NodeRef> getNextWork() {
+				if (val != null) {
+					Long typeQNameId = val.getFirst();
+
+					result.clear();
+
+					while (result.isEmpty() && (minSearchNodeId < maxNodeId)) {
+
+						List<Long> nodeids = getPatchDAO().getNodesByAspectQNameId(typeQNameId, minSearchNodeId, maxSearchNodeId);
+
+						for (Long nodeid : nodeids) {
+							NodeRef.Status status = getNodeDAO().getNodeIdStatus(nodeid);
+							if (!status.isDeleted()) {
+								result.add(status.getNodeRef());
+							}
+						}
+						minSearchNodeId = minSearchNodeId + count;
+						maxSearchNodeId = maxSearchNodeId + count;
+					}
+				}
+
+				return result;
+			}
+		};
+
+		BatchProcessor<NodeRef> batchProcessor = new BatchProcessor<>("RemoveDiluentAspectPatch", transactionService.getRetryingTransactionHelper(),
+				workProvider, batchThreads, batchSize, applicationEventPublisher, logger, 1000);
+
+		BatchProcessWorker<NodeRef> worker = new BatchProcessWorker<NodeRef>() {
+
+			@Override
+			public void afterProcess() throws Throwable {
+				ruleService.disableRules();
+			}
+
+			@Override
+			public void beforeProcess() throws Throwable {
+				ruleService.enableRules();
+			}
+
+			@Override
+			public String getIdentifier(NodeRef entry) {
+				return entry.toString();
+			}
+
+			@Override
+			public void process(NodeRef nodeRef) throws Throwable {
+
+				AuthenticationUtil.setAdminUserAsFullyAuthenticatedUser();
+				policyBehaviourFilter.disableBehaviour();
+
+				if (nodeService.exists(nodeRef)) {
+					nodeService.removeAspect(nodeRef, ASPECT_DILUENT);
+				} else {
+					logger.warn("nodeRef doesn't exist : " + nodeRef);
+				}
+
+			}
+
+		};
+
+		batchProcessor.processLong(worker, true);
+
+		return I18NUtil.getMessage(MSG_SUCCESS);
+	}
+
+	/**
+	 * <p>Getter for the field <code>nodeDAO</code>.</p>
+	 *
+	 * @return a {@link org.alfresco.repo.domain.node.NodeDAO} object.
+	 */
+	public NodeDAO getNodeDAO() {
+		return nodeDAO;
+	}
+
+	/**
+	 * <p>Setter for the field <code>nodeDAO</code>.</p>
+	 *
+	 * @param nodeDAO a {@link org.alfresco.repo.domain.node.NodeDAO} object.
+	 */
+	public void setNodeDAO(NodeDAO nodeDAO) {
+		this.nodeDAO = nodeDAO;
+	}
+
+	/**
+	 * <p>Getter for the field <code>patchDAO</code>.</p>
+	 *
+	 * @return a {@link org.alfresco.repo.domain.patch.PatchDAO} object.
+	 */
+	public PatchDAO getPatchDAO() {
+		return patchDAO;
+	}
+
+	/**
+	 * <p>Setter for the field <code>patchDAO</code>.</p>
+	 *
+	 * @param patchDAO a {@link org.alfresco.repo.domain.patch.PatchDAO} object.
+	 */
+	public void setPatchDAO(PatchDAO patchDAO) {
+		this.patchDAO = patchDAO;
+	}
+
+	/**
+	 * <p>Getter for the field <code>qnameDAO</code>.</p>
+	 *
+	 * @return a {@link org.alfresco.repo.domain.qname.QNameDAO} object.
+	 */
+	public QNameDAO getQnameDAO() {
+		return qnameDAO;
+	}
+
+	/**
+	 * <p>Setter for the field <code>qnameDAO</code>.</p>
+	 *
+	 * @param qnameDAO a {@link org.alfresco.repo.domain.qname.QNameDAO} object.
+	 */
+	public void setQnameDAO(QNameDAO qnameDAO) {
+		this.qnameDAO = qnameDAO;
+	}
+
+	/**
+	 * <p>Setter for the field <code>policyBehaviourFilter</code>.</p>
+	 *
+	 * @param policyBehaviourFilter a {@link org.alfresco.repo.policy.BehaviourFilter} object.
+	 */
+	public void setPolicyBehaviourFilter(BehaviourFilter policyBehaviourFilter) {
+		this.policyBehaviourFilter = policyBehaviourFilter;
+	}
+
+	/**
+	 * <p>Getter for the field <code>ruleService</code>.</p>
+	 *
+	 * @return a {@link org.alfresco.service.cmr.rule.RuleService} object.
+	 */
+	public RuleService getRuleService() {
+		return ruleService;
+	}
+
+	/**
+	 * <p>Setter for the field <code>ruleService</code>.</p>
+	 *
+	 * @param ruleService a {@link org.alfresco.service.cmr.rule.RuleService} object.
+	 */
+	public void setRuleService(RuleService ruleService) {
+		this.ruleService = ruleService;
+	}
+
+}
