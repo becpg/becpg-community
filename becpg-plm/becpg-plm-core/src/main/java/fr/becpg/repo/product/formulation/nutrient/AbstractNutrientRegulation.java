7d3dfd4b1f2931a3fad72214d42dbd0170075abe
Matthieu Laborie <matthieu.laborie@becpg.fr>
2022-11-16 13:55:08 +0100

    Fix #14347 - [Feature] Update brazilian regulation

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/nutrient/AbstractNutrientRegulation.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/nutrient/AbstractNutrientRegulation.java
@@ -23,11 +23,11 @@ import fr.becpg.common.csv.CSVReader;
  */
 public abstract class AbstractNutrientRegulation implements NutrientRegulation {
 
-	private String path;
-	
 	/** Constant <code>logger</code> */
 	protected static Log logger = LogFactory.getLog(AbstractNutrientRegulation.class);
-	
+
+
+
 	protected class NutrientDefinition {
 		private Integer sort;
 		private Integer depthLevel;
@@ -43,8 +43,8 @@ public abstract class AbstractNutrientRegulation implements NutrientRegulation {
 			return depthLevel;
 		}
 
-		public NutrientDefinition(String nutCode, Integer sort, Integer depthLevel, Boolean mandatory, Boolean optional, Boolean bold, Double gda,
-				Double ul, String unit, Boolean showGDAPerc) {
+		public NutrientDefinition(Integer sort, Integer depthLevel, Boolean mandatory, Boolean optional, Boolean bold, Double gda, Double ul,
+				String unit, Boolean showGDAPerc) {
 			super();
 			this.sort = sort;
 			this.depthLevel = depthLevel;
@@ -80,7 +80,7 @@ public abstract class AbstractNutrientRegulation implements NutrientRegulation {
 		public Double getUl() {
 			return ul;
 		}
-		
+
 		public String getUnit() {
 			return unit;
 		}
@@ -93,12 +93,16 @@ public abstract class AbstractNutrientRegulation implements NutrientRegulation {
 
 	private Map<String, NutrientDefinition> definitions = null;
 
+
+
+	private String path;
+	
 	/**
 	 * <p>Constructor for AbstractNutrientRegulation.</p>
 	 *
 	 * @param path a {@link java.lang.String} object.
 	 */
-	public AbstractNutrientRegulation(String path) {
+	protected AbstractNutrientRegulation(String path) {
 		this.path = path;
 
 	}
@@ -108,20 +112,21 @@ public abstract class AbstractNutrientRegulation implements NutrientRegulation {
 		ClassPathResource resource = new ClassPathResource(path);
 		try (InputStream in = resource.getInputStream()) {
 			try (InputStreamReader inReader = new InputStreamReader(resource.getInputStream())) {
-				try (CSVReader csvReader = new CSVReader(inReader, ';','"',1)) {
+				try (CSVReader csvReader = new CSVReader(inReader, ';', '"', 1)) {
 					String[] line = null;
 					// nutCode charactName sort depthLevel mandatory optionnal bold gda ul
 					while ((line = csvReader.readNext()) != null) {
-						definitions.put(line[0], new NutrientDefinition(line[0], parseInt(line[2]), parseInt(line[3]), "true".equals(line[4]),
-								"true".equals(line[5]), "true".equals(line[6]), parseDouble(line[7]), parseDouble(line[8]), line[9], "true".equals(line[10])));
+						definitions.put(line[0],
+								new NutrientDefinition(parseInt(line[2]), parseInt(line[3]), "true".equals(line[4]), "true".equals(line[5]),
+										"true".equals(line[6]), parseDouble(line[7]), parseDouble(line[8]), line[9], "true".equals(line[10])));
 					}
 				}
 			}
 		} catch (IOException e) {
-			logger.error(e,e);
+			logger.error(e, e);
 		}
 	}
-	
+
 	private Double parseDouble(String value) {
 		if (value != null && !value.trim().isEmpty()) {
 			return Double.valueOf(value.trim().replace(",", "."));
@@ -142,7 +147,7 @@ public abstract class AbstractNutrientRegulation implements NutrientRegulation {
 	@Override
 	public NutrientDefinition getNutrientDefinition(String nutCode) {
 		//Defers loading
-		if(definitions == null) {
+		if (definitions == null) {
 			loadRegulation();
 		}
 		return definitions.get(nutCode);
@@ -161,17 +166,17 @@ public abstract class AbstractNutrientRegulation implements NutrientRegulation {
 		if (nutrientTypeCode != null && !nutrientTypeCode.isEmpty()) {
 			String regulUnit = nutUnit;
 			NutrientDefinition def = getNutrientDefinition(nutrientTypeCode);
-			if(def != null){
+			if (def != null) {
 				regulUnit = def.getUnit();
 			}
-			if(logger.isDebugEnabled()){
+			if (logger.isDebugEnabled()) {
 				logger.debug("round : nutrientTypeCode " + nutrientTypeCode + " value " + value + " nutUnit " + nutUnit + " regulUnit " + regulUnit);
 			}
 			return roundByCode(convertValue(value, nutUnit, regulUnit), nutrientTypeCode);
 		}
 		return roundValue(value, 1d);
 	}
-	
+
 	/** {@inheritDoc} */
 	@Override
 	public String displayValue(Double value, Double roundedValue, String nutrientTypeCode, Locale locale) {
@@ -180,7 +185,7 @@ public abstract class AbstractNutrientRegulation implements NutrientRegulation {
 		}
 		return formatDouble(roundedValue, locale);
 	}
-	
+
 	/**
 	 * <p>formatDouble.</p>
 	 *
@@ -188,17 +193,16 @@ public abstract class AbstractNutrientRegulation implements NutrientRegulation {
 	 * @param locale a {@link java.util.Locale} object.
 	 * @return a {@link java.lang.String} object.
 	 */
-	protected String formatDouble(Double value, Locale locale){
-		if(value != null){
+	protected String formatDouble(Double value, Locale locale) {
+		if (value != null) {
 			DecimalFormatSymbols symbols = new DecimalFormatSymbols(locale);
 			DecimalFormat df = new DecimalFormat("#,###.######", symbols);
-	        return df.format(value);
-		}
-		else{
+			return df.format(value);
+		} else {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * <p>roundValue.</p>
 	 *
@@ -207,126 +211,101 @@ public abstract class AbstractNutrientRegulation implements NutrientRegulation {
 	 * @return a {@link java.lang.Double} object.
 	 */
 	protected Double roundValue(Double value, Double delta) {
-		if(value != null){
+		if (value != null) {
 			// round by delta (eg: 0.5)
 			double roundedValue = (delta * Math.round(value / delta));
 			// sometime roundedValue is 0.3000000001 when 0.1 * 3 --> we round twice
 			return (Math.round(roundedValue * 1000d) / 1000d);
-		}
-		else{
+		} else {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * <p>isVitamin.</p>
 	 *
 	 * @param nutrientTypeCode a {@link java.lang.String} object.
 	 * @return a boolean.
 	 */
-	protected boolean isVitamin(String nutrientTypeCode){
-		if(nutrientTypeCode!=null){
-			if(nutrientTypeCode.equals(NutrientCode.FolicAcid)
-				|| nutrientTypeCode.equals(NutrientCode.VitA)
-				|| nutrientTypeCode.equals(NutrientCode.VitC)
-				|| nutrientTypeCode.equals(NutrientCode.VitD)
-				|| nutrientTypeCode.equals(NutrientCode.VitE)
-				|| nutrientTypeCode.equals(NutrientCode.VitK1)
-				|| nutrientTypeCode.equals(NutrientCode.VitK2)
-				|| nutrientTypeCode.equals(NutrientCode.VitB1)
-				|| nutrientTypeCode.equals(NutrientCode.VitB2)
-				|| nutrientTypeCode.equals(NutrientCode.VitB3)
-				|| nutrientTypeCode.equals(NutrientCode.PantoAcid)
-				|| nutrientTypeCode.equals(NutrientCode.VitB6)
-				|| nutrientTypeCode.equals(NutrientCode.VitB12)
-				|| nutrientTypeCode.equals(NutrientCode.Thiamin)
-				|| nutrientTypeCode.equals(NutrientCode.Riboflavin)
-				|| nutrientTypeCode.equals(NutrientCode.Niacin)
-				|| nutrientTypeCode.equals(NutrientCode.Folate)
-				|| nutrientTypeCode.equals(NutrientCode.FolateDFE)
-				|| nutrientTypeCode.equals(NutrientCode.Biotin)
-				|| nutrientTypeCode.equals(NutrientCode.Choline)
-				|| nutrientTypeCode.equals(NutrientCode.Retinol)
-				|| nutrientTypeCode.equals(NutrientCode.BetaCarotene)
-				|| nutrientTypeCode.equals(NutrientCode.BetaCrypt)
-				|| nutrientTypeCode.equals(NutrientCode.Lycopene)
-				|| nutrientTypeCode.equals(NutrientCode.AlphaCarot)
-				|| nutrientTypeCode.equals(NutrientCode.ProvitaminA)){
+	protected boolean isVitamin(String nutrientTypeCode) {
+		if (nutrientTypeCode != null) {
+			if (nutrientTypeCode.equals(NutrientCode.FolicAcid) || nutrientTypeCode.equals(NutrientCode.VitA)
+					|| nutrientTypeCode.equals(NutrientCode.VitC) || nutrientTypeCode.equals(NutrientCode.VitD)
+					|| nutrientTypeCode.equals(NutrientCode.VitE) || nutrientTypeCode.equals(NutrientCode.VitK1)
+					|| nutrientTypeCode.equals(NutrientCode.VitK2) || nutrientTypeCode.equals(NutrientCode.VitB1)
+					|| nutrientTypeCode.equals(NutrientCode.VitB2) || nutrientTypeCode.equals(NutrientCode.VitB3)
+					|| nutrientTypeCode.equals(NutrientCode.PantoAcid) || nutrientTypeCode.equals(NutrientCode.VitB6)
+					|| nutrientTypeCode.equals(NutrientCode.VitB12) || nutrientTypeCode.equals(NutrientCode.Thiamin)
+					|| nutrientTypeCode.equals(NutrientCode.Riboflavin) || nutrientTypeCode.equals(NutrientCode.Niacin)
+					|| nutrientTypeCode.equals(NutrientCode.Folate) || nutrientTypeCode.equals(NutrientCode.FolateDFE)
+					|| nutrientTypeCode.equals(NutrientCode.Biotin) || nutrientTypeCode.equals(NutrientCode.Choline)
+					|| nutrientTypeCode.equals(NutrientCode.Retinol) || nutrientTypeCode.equals(NutrientCode.BetaCarotene)
+					|| nutrientTypeCode.equals(NutrientCode.BetaCrypt) || nutrientTypeCode.equals(NutrientCode.Lycopene)
+					|| nutrientTypeCode.equals(NutrientCode.AlphaCarot) || nutrientTypeCode.equals(NutrientCode.ProvitaminA)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	/**
 	 * <p>isMineral.</p>
 	 *
 	 * @param nutrientTypeCode a {@link java.lang.String} object.
 	 * @return a boolean.
 	 */
-	protected boolean isMineral(String nutrientTypeCode){
-		if(nutrientTypeCode!=null){
-			if(nutrientTypeCode.equals(NutrientCode.Calcium)
-				|| nutrientTypeCode.equals(NutrientCode.Sodium)
-				|| nutrientTypeCode.equals(NutrientCode.Potassium)
-				|| nutrientTypeCode.equals(NutrientCode.Iron)	
-				|| nutrientTypeCode.equals(NutrientCode.Salt)
-				|| nutrientTypeCode.equals(NutrientCode.Copper)
-				|| nutrientTypeCode.equals(NutrientCode.Phosphorus)
-				|| nutrientTypeCode.equals(NutrientCode.Magnesium)
-				|| nutrientTypeCode.equals(NutrientCode.Zinc)
-				|| nutrientTypeCode.equals(NutrientCode.Iodine)
-				|| nutrientTypeCode.equals(NutrientCode.Selenium)
-				|| nutrientTypeCode.equals(NutrientCode.Fluoride)
-				|| nutrientTypeCode.equals(NutrientCode.Manganese)
-				|| nutrientTypeCode.equals(NutrientCode.Chromium)
-				|| nutrientTypeCode.equals(NutrientCode.Starch)
-				|| nutrientTypeCode.equals(NutrientCode.Molybdenum)
-				|| nutrientTypeCode.equals(NutrientCode.Chloride)){
+	protected boolean isMineral(String nutrientTypeCode) {
+		if (nutrientTypeCode != null) {
+			if (nutrientTypeCode.equals(NutrientCode.Calcium) || nutrientTypeCode.equals(NutrientCode.Sodium)
+					|| nutrientTypeCode.equals(NutrientCode.Potassium) || nutrientTypeCode.equals(NutrientCode.Iron)
+					|| nutrientTypeCode.equals(NutrientCode.Salt) || nutrientTypeCode.equals(NutrientCode.Copper)
+					|| nutrientTypeCode.equals(NutrientCode.Phosphorus) || nutrientTypeCode.equals(NutrientCode.Magnesium)
+					|| nutrientTypeCode.equals(NutrientCode.Zinc) || nutrientTypeCode.equals(NutrientCode.Iodine)
+					|| nutrientTypeCode.equals(NutrientCode.Selenium) || nutrientTypeCode.equals(NutrientCode.Fluoride)
+					|| nutrientTypeCode.equals(NutrientCode.Manganese) || nutrientTypeCode.equals(NutrientCode.Chromium)
+					|| nutrientTypeCode.equals(NutrientCode.Starch) || nutrientTypeCode.equals(NutrientCode.Molybdenum)
+					|| nutrientTypeCode.equals(NutrientCode.Chloride)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	/** {@inheritDoc} */
-	public Double convertValue(Double value, String nutUnit, String regulUnit){
-		
-		if (value != null && nutUnit != null && regulUnit != null ) { 
+	public Double convertValue(Double value, String nutUnit, String regulUnit) {
+
+		if (value != null && nutUnit != null && regulUnit != null) {
 			// convert mg to g
 			double coef = 1d;
-			if(nutUnit.startsWith("mg") && regulUnit.startsWith("g")){
+			if (nutUnit.startsWith("mg") && regulUnit.startsWith("g")) {
 				coef = 1000;
-			}else if(nutUnit.startsWith("µg") && regulUnit.startsWith("g")){
+			} else if (nutUnit.startsWith("µg") && regulUnit.startsWith("g")) {
 				coef = 1000000;
-			}else if(nutUnit.startsWith("µg") && regulUnit.startsWith("mg")){
+			} else if (nutUnit.startsWith("µg") && regulUnit.startsWith("mg")) {
 				coef = 1000;
-			}else if(nutUnit.startsWith("g") && regulUnit.startsWith("mg")){
+			} else if (nutUnit.startsWith("g") && regulUnit.startsWith("mg")) {
 				coef = 0.001;
-			}
-			else if(nutUnit.startsWith("g") && regulUnit.startsWith("µg")){
+			} else if (nutUnit.startsWith("g") && regulUnit.startsWith("µg")) {
 				coef = 0.000001;
-			}
-			else if(nutUnit.startsWith("mg") && regulUnit.startsWith("µg")){
+			} else if (nutUnit.startsWith("mg") && regulUnit.startsWith("µg")) {
 				coef = 0.001;
 			}
-			if(logger.isDebugEnabled()){
-				logger.debug("value " +  value + " coef " + coef);
+			if (logger.isDebugEnabled()) {
+				logger.debug("value " + value + " coef " + coef);
 			}
 			return value / coef;
-		}
-		else{
+		} else {
 			return value;
 		}
 	}
 
 	/*
-	(***) NOTES FOR ROUNDING % Daily Value (DV):
-
-	    To calculate % DV, divide either the actual (unrounded) quantitative amount or the declared (rounded) amount by the appropriate RDI or DRV. Use whichever amount will provide the greatest consistency on the food label and prevent unnecessary consumer confusion 109.9(d)(7)(2).
-	    When %DV values fall between two whole numbers, rounding shall be as follows:
-	        for values exactly halfway between two whole numbers or higher (e.g., 2.5 to 2.990 the values shall round up (e.g., 3%)
-	        for values less than halfway between two whole numbers (e.g., 2.01 to 2.49) the values shall round down (e.g., 2%).
+	 * (***) NOTES FOR ROUNDING % Daily Value (DV):
+	 * 
+	 * To calculate % DV, divide either the actual (unrounded) quantitative amount or the declared (rounded) amount by the appropriate RDI or DRV. Use whichever amount will provide the greatest
+	 * consistency on the food label and prevent unnecessary consumer confusion 109.9(d)(7)(2). When %DV values fall between two whole numbers, rounding shall be as follows: for values exactly halfway
+	 * between two whole numbers or higher (e.g., 2.5 to 2.990 the values shall round up (e.g., 3%) for values less than halfway between two whole numbers (e.g., 2.01 to 2.49) the values shall round
+	 * down (e.g., 2%).
 	 */
 	/** {@inheritDoc} */
 	@Override
@@ -334,9 +313,8 @@ public abstract class AbstractNutrientRegulation implements NutrientRegulation {
 		if (value == null) {
 			return null;
 		}
-		return  roundValue(value, 1d);
+		return roundValue(value, 1d);
 	}
-	
 
 	/**
 	 * <p>roundByCode.</p>
@@ -346,7 +324,7 @@ public abstract class AbstractNutrientRegulation implements NutrientRegulation {
 	 * @return a {@link java.lang.Double} object.
 	 */
 	protected abstract Double roundByCode(Double value, String nutrientTypeCode);
-	
+
 	/**
 	 * <p>displayValueByCode.</p>
 	 *
