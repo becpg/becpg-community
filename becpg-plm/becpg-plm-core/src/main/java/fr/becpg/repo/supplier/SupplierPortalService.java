7f98a2623eb63ba1a36e6b0a926439aefb6b6c7a
valentin leblanc <valentin.leblanc@becpg.fr>
2022-11-25 15:21:57 +0100

    Fix #15539 - [Bug] Remove unused method in supplier portal service

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/supplier/SupplierPortalService.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/supplier/SupplierPortalService.java
@@ -12,7 +12,6 @@ public interface SupplierPortalService {
 
 	NodeRef createSupplierProject(NodeRef entityNodeRef, NodeRef projectTemplateNodeRef, List<NodeRef> supplierAccountNodeRefs);
 	NodeRef getSupplierNodeRef(NodeRef entityNodeRef);
-	NodeRef getSupplierDestFolder(NodeRef supplierNodeRef);
 	NodeRef getOrCreateSupplierDestFolder(NodeRef supplierNodeRef, List<NodeRef> resources);
 	String createName(NodeRef entityNodeRef, NodeRef supplierNodeRef, String nameFormat, Date currentDate);
 	
