1c5276c1b6810a48562fdadf86dd7de703c2571c
valentin leblanc <valentin.leblanc@becpg.fr>
2022-12-09 12:04:02 +0100

    Fix #15721 - [Bug] Clear repository caches when needed

---
+++ becpg-core/src/main/java/fr/becpg/repo/dictionary/constraint/LinkedListPolicy.java
@@ -0,0 +1,56 @@
+/*
+ * 
+ */
+package fr.becpg.repo.dictionary.constraint;
+
+import java.util.Set;
+
+import org.alfresco.repo.node.NodeServicePolicies;
+import org.alfresco.repo.policy.JavaBehaviour;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+
+import fr.becpg.model.BeCPGModel;
+import fr.becpg.repo.policy.AbstractBeCPGPolicy;
+import fr.becpg.repo.repository.AlfrescoRepository;
+import fr.becpg.repo.repository.RepositoryEntity;
+
+/**
+ * <p>LinkedListPolicy class.</p>
+ *
+ * @author valentin
+ * @version $Id: $Id
+ */
+public class LinkedListPolicy extends AbstractBeCPGPolicy implements NodeServicePolicies.OnDeleteNodePolicy {
+
+	private AlfrescoRepository<RepositoryEntity> alfrescoRepository;
+	
+	public void setAlfrescoRepository(AlfrescoRepository<RepositoryEntity> alfrescoRepository) {
+		this.alfrescoRepository = alfrescoRepository;
+	}
+	
+	/**
+	 * <p>doInit.</p>
+	 */
+	public void doInit() {
+		policyComponent.bindClassBehaviour(NodeServicePolicies.OnDeleteNodePolicy.QNAME, BeCPGModel.TYPE_LINKED_VALUE, new JavaBehaviour(this,
+				"onDeleteNode"));
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void onDeleteNode(ChildAssociationRef childAssocRef, boolean isNodeArchived) {
+		queueNode(childAssocRef.getChildRef());
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	protected boolean doBeforeCommit(String key, Set<NodeRef> pendingNodes) {
+		if(!pendingNodes.isEmpty()){
+			alfrescoRepository.clearCaches(LinkedListPolicy.class.getName());
+		}
+		
+		return true;
+	}
+	
+}
