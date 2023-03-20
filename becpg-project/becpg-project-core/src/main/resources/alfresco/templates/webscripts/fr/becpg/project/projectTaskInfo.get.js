d6ab1e5ca1b51ef2e7efbd06710c886c1b96c09e
valentin leblanc <valentin.leblanc@becpg.fr>
2023-03-20 10:32:35 +0100

    Fix #16517 - [Bug] Fix deliverable content from url in task view

--- becpg-project/becpg-project-core/src/main/resources/alfresco/templates/webscripts/fr/becpg/project/projectTaskInfo.get.js
+++ becpg-project/becpg-project-core/src/main/resources/alfresco/templates/webscripts/fr/becpg/project/projectTaskInfo.get.js
@@ -24,19 +24,54 @@ function main()
    model.task = task;
    model.deliverables = [];
    model.urlMap = {};
+   model.contentMap = {};
    
    
-   if(task != null &&  task.sourceAssocs["pjt:dlTask"]!=null){
-      for(var i = 0; i < task.sourceAssocs["pjt:dlTask"].length; i++){
-          var deliverable = task.sourceAssocs["pjt:dlTask"][i];
-          if(deliverable.properties["pjt:dlScriptExecOrder"] == null || 
-                  deliverable.properties["pjt:dlScriptExecOrder"] == "None"){
-              var url = bProject.getDeliverableUrl(deliverable);
-              model.urlMap[deliverable.nodeRef.toString()] = url;
-              model.deliverables.push(deliverable);
-          }
-      }
-   }
+	if (task != null && task.sourceAssocs["pjt:dlTask"] != null) {
+		for (var i = 0; i < task.sourceAssocs["pjt:dlTask"].length; i++) {
+			var deliverable = task.sourceAssocs["pjt:dlTask"][i];
+			if (deliverable.properties["pjt:dlScriptExecOrder"] == null ||
+				deliverable.properties["pjt:dlScriptExecOrder"] == "None") {
+				model.contentMap[deliverable.nodeRef.toString()] = [];
+				
+				var delContents = deliverable.assocs["pjt:dlContent"];
+				
+				if (delContents) {
+					for (var j = 0; j < delContents.length; j++) {
+						var delContent = delContents[j];
+						model.contentMap[deliverable.nodeRef.toString()].push(delContent);
+					}
+				}
+				
+				var url = bProject.getDeliverableUrl(deliverable);
+				
+				if (url) {
+					
+					var hasContentFromUrl = false;
+					
+					if (url.startsWith("content:")) {
+						var contentRef = url.substring(url.indexOf("content:") + "content:".length);
+						if (contentRef) {
+							var regex = new RegExp(".+://.+/.+");
+							if (contentRef.match(regex)) {
+								var contentNode = search.findNode(contentRef);
+								if (contentNode) {
+									hasContentFromUrl = true;
+									model.contentMap[deliverable.nodeRef.toString()].push(contentNode);
+								}
+							}
+						}
+					}
+					
+					if (!hasContentFromUrl) {
+						model.urlMap[deliverable.nodeRef.toString()] = url;
+					}
+				}
+
+				model.deliverables.push(deliverable);
+			}
+		}
+	}
    
 }
 
