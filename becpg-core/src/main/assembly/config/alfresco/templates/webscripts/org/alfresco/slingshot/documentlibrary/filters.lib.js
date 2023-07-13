c049bab9726d95cbd2f6f729a7e9c6e74454cd55
valentin leblanc <valentin.leblanc@becpg.fr>
2023-07-13 15:50:41 +0200

    Fix #16603 - [Feature] Add eanCode to product catalog search

--- becpg-core/src/main/assembly/config/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/filters.lib.js
+++ becpg-core/src/main/assembly/config/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/filters.lib.js
@@ -289,7 +289,7 @@ var Filters =
          filterParams.query += " " + (Filters.TYPE_MAP[parsedArgs.type] || "");
          if(args.searchTerm!=null &&  args.searchTerm != "" ){
          	filterParams.query += " AND (@cm\\:name:\""+args.searchTerm+"\" OR @cm\\:title:\""+args.searchTerm+"\" OR  " +
-         			"@bcpg\\:erpCode:\""+args.searchTerm+"\" OR  @bcpg\\:code:\""+args.searchTerm+"\" OR  @cm\\:description:\""+args.searchTerm+"\")";	
+         			"@bcpg\\:erpCode:\""+args.searchTerm+"\" OR  @bcpg\\:code:\""+args.searchTerm+"\" OR  @bcpg\\:eanCode:\""+args.searchTerm+"\" OR  @cm\\:description:\""+args.searchTerm+"\")";	
          }
       }
 
