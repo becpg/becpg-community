b09fe1505bc9435840b10a350bba681a94e7cd60
valentin leblanc <valentin.leblanc@becpg.fr>
2023-08-04 12:29:21 +0200

    Fix #17913 - [Bug] Do not update channel modifed date if channel list is modified

--- becpg-core/src/main/java/fr/becpg/repo/entity/catalog/EntityCatalogObserver.java
+++ becpg-core/src/main/java/fr/becpg/repo/entity/catalog/EntityCatalogObserver.java
@@ -1,5 +1,7 @@
 package fr.becpg.repo.entity.catalog;
 
+import java.util.Set;
+
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.QName;
 
@@ -7,6 +9,6 @@ public interface EntityCatalogObserver {
 
 	void notifyAuditedFieldChange(String string, NodeRef entityNodeRef);
 
-	boolean acceptCatalogEvents(QName type, NodeRef entityNodeRef);
-
+	boolean acceptCatalogEvents(QName type, NodeRef entityNodeRef, Set<NodeRef> listNodeRefs);
+	
 }
