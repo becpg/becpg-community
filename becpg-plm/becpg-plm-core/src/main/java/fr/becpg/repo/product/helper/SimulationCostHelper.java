5a10ef776c67796ff5c167cdfa82b32f281ab5b8
Matthieu Laborie <matthieu.laborie@becpg.fr>
2023-11-09 15:47:12 +0100

    Fix #18495 - [Bug] Bug in multiple kit when simulating cost

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/helper/SimulationCostHelper.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/helper/SimulationCostHelper.java
@@ -256,7 +256,6 @@ public class SimulationCostHelper implements InitializingBean {
 					} else {
 						totalQty += qty;
 					}
-					break;
 				} else if (subProductData instanceof PackagingKitData) {
 					totalQty = qty
 							* getPackagingListQty(subProductData, componentNodeRef, ((PackagingKitData) subProductData).getPalletBoxesPerPallet(),null);
