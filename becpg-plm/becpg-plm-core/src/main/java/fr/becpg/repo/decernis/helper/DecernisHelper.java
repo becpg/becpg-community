5ea0ed95e9f3697ac86f8e5b8ad4a71efd7f487e
Valentin Leblanc <valentin.leblanc@becpg.fr>
2023-11-27 17:59:07 +0100

    Fix #19640 - [Bug] Fix Decernis number format

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/decernis/helper/DecernisHelper.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/decernis/helper/DecernisHelper.java
@@ -1,11 +1,17 @@
 package fr.becpg.repo.decernis.helper;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class DecernisHelper {
 	
-	private static final DecimalFormat decimalFormat = new DecimalFormat("#.########################"); // 24 decimal places
+	private static final DecimalFormat decimalFormat = new DecimalFormat("#.########################", new DecimalFormatSymbols(Locale.ENGLISH)); // 24 decimal places
 
+	private DecernisHelper() {
+		
+	}
+	
 	public static Double truncateDoubleValue(Double ingQtyPerc) {
 		if (ingQtyPerc == null) {
 			return null;
