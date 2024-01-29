ebf67ea942c1c035c953514e2dae4f2f008afa26
Matthieu Laborie <matthieu.laborie@becpg.fr>
2024-01-29 11:10:01 +0100

    [Bug] Fix unit test

--- becpg-integration-runner/src/test/java/fr/becpg/test/FormulationTestSuite.java
+++ becpg-integration-runner/src/test/java/fr/becpg/test/FormulationTestSuite.java
@@ -8,6 +8,7 @@ import fr.becpg.test.repo.product.formulation.CharactDetailsFormulationIT;
 import fr.becpg.test.repo.product.formulation.CompareFormulationIT;
 import fr.becpg.test.repo.product.formulation.FormulationCalcILWithYieldOnCompIT;
 import fr.becpg.test.repo.product.formulation.FormulationCheckPropsOfCompIT;
+import fr.becpg.test.repo.product.formulation.FormulationCostsIT;
 import fr.becpg.test.repo.product.formulation.FormulationFullIT;
 import fr.becpg.test.repo.product.formulation.FormulationGenericRawMaterialIT;
 import fr.becpg.test.repo.product.formulation.FormulationIT;
@@ -21,7 +22,7 @@ import fr.becpg.test.repo.product.formulation.LabelingFormulationIT;
 @SuiteClasses(value = { CharactDetailsFormulationIT.class, CompareFormulationIT.class, FormulationCalcILWithYieldOnCompIT.class,
 		FormulationCheckPropsOfCompIT.class, FormulationFullIT.class, FormulationGenericRawMaterialIT.class, FormulationMultiLevelILIT.class,
 		FormulationProductWithoutQtyIT.class, FormulationTareIT.class, FormulationIT.class, FormulationWithIngRequirementsIT.class,
-		LabelingFormulationIT.class })
+		LabelingFormulationIT.class, FormulationCostsIT.class, LabelingFormulationIT.class })
 public class FormulationTestSuite {
 
 }
