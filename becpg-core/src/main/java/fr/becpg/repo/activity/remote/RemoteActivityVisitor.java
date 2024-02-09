0897b1fb201f731061016060defdb5a984123d53
fredboxebeld <frederic.boxebeld@becpg.fr>
2024-02-09 16:13:05 +0100

    InProgress #17800 - [Merge] commit to change branch

--- becpg-core/src/main/java/fr/becpg/repo/activity/remote/RemoteActivityVisitor.java
+++ becpg-core/src/main/java/fr/becpg/repo/activity/remote/RemoteActivityVisitor.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import org.alfresco.repo.domain.activities.ActivityFeedEntity;
 
+import fr.becpg.common.BeCPGException;
+
 /**
  * 
  */
 public interface RemoteActivityVisitor {
 
-	public void visit(List<ActivityFeedEntity> feedEntries, OutputStream result) throws Exception;
+	public void visit(List<ActivityFeedEntity> feedEntries, OutputStream result) throws BeCPGException;
 
 	public String getContentType();
 
