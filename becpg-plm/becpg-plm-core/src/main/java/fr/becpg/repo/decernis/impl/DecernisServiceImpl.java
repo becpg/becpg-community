ad7c155e39b6cfd5378a067ec1ac9697550712ac
Valentin Leblanc <valentin.leblanc@becpg.fr>
2024-02-29 15:25:21 +0100

    Fix #18751 - [Feature] Fix ing regulatory filter and display

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/decernis/impl/DecernisServiceImpl.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/decernis/impl/DecernisServiceImpl.java
@@ -237,7 +237,7 @@ public class DecernisServiceImpl implements DecernisService, FormulationChainPlu
 
 		String citation = items.stream().map(item -> item.getCitation().getDefaultValue()).distinct().sorted().collect(Collectors.joining(", "));
 		String usages = items.stream().map(item -> item.getUsages().getDefaultValue()).distinct().sorted().collect(Collectors.joining(", "));
-		String restrictionLevels = items.stream().map(item -> item.getRestrictionLevels().getDefaultValue()).distinct().sorted().collect(Collectors.joining(", "));
+		String restrictionLevels = items.stream().map(item -> item.getRestrictionLevels().getDefaultValue()).filter(r -> r != null && !r.equals("-")).distinct().sorted().collect(Collectors.joining(", "));
 		String resultIndicators = items.stream().map(item -> item.getResultIndicator().getDefaultValue()).distinct().sorted().collect(Collectors.joining(", "));
 
 		mergedItem.setResultIndicator(new MLText(resultIndicators));
