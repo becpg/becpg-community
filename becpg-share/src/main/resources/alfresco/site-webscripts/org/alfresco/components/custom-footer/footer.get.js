7a7a9d305f7f6b7468184a58f1e18f7273cd463e
Matthieu Laborie <matthieu.laborie@becpg.fr>
2024-04-02 08:11:28 +0200

    InProgress #10243 - [Feature] Add Watson widget

--- becpg-share/src/main/resources/alfresco/site-webscripts/org/alfresco/components/custom-footer/footer.get.js
+++ becpg-share/src/main/resources/alfresco/site-webscripts/org/alfresco/components/custom-footer/footer.get.js
@@ -3,16 +3,28 @@
  * JS Controller for Share's footer component
  */
 
-function main()
-{
-  // Widget instantiation metadata...
-   var widget = {
-      id : "basket", 
-      name : "beCPG.component.Basket"
-   };
-   model.widgets = [widget];
+function main() {
+
+	var basket = {
+		id: "basket",
+		name: "beCPG.component.Basket"
+	};
+	model.widgets = [basket];
+
+	model.isAIEnable = (user!=null && user.capabilities["isAIUser"] != null && user.capabilities["isAIUser"] == true) || false;
+
+	if (model.isAIEnable) {
+
+		//https://gweb-cloud-chat-prod.appspot.com/chat/bot
+
+		var watson = {
+			id: "Watson",
+			name: "beCPG.component.Watson"
+		};
+		model.widgets.push(watson);
+
+	}
 }
 
 main();
 
-    
