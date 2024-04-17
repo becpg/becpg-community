fde641544b4e6ef494e04441f67b052ce3bc6922
Matthieu Laborie <matthieu.laborie@becpg.fr>
2024-04-17 12:45:23 +0200

    InProgress #10243 - [Feature] Add Watson widget

--- becpg-share/src/main/assembly/web/components/entity-suggestions/entity-suggestions.js
+++ becpg-share/src/main/assembly/web/components/entity-suggestions/entity-suggestions.js
@@ -198,7 +198,7 @@
 			var owner = args[1].anchor, me = this;
 			var remoteEntity = JSON.parse(decodeURIComponent(owner.getAttribute("data-entity")));
 
-			var url = Alfresco.constants.URL_CONTEXT + "proxy/ai/api/suggestions/apply";
+			var url = Alfresco.constants.URL_CONTEXT + "proxy/ai/api/suggestions/apply?ticket=" + me.options.ticket;
 
 			Alfresco.util.Ajax
 				.jsonPost(
