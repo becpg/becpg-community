0d4f7379c50b9e5c99b0bf6385db8c3ffd116eb4
Matthieu Laborie <matthieu.laborie@becpg.fr>
2024-04-19 16:39:57 +0200

    Fix #10243 - [Feature] Better becpg-ai Integration

--- becpg-share/src/main/java/fr/becpg/web/connector/BecpgTicketAuthenticator.java
+++ becpg-share/src/main/java/fr/becpg/web/connector/BecpgTicketAuthenticator.java
@@ -1,13 +1,31 @@
 package fr.becpg.web.connector;
 
-import org.springframework.extensions.webscripts.connector.AlfrescoAuthenticator;
+import org.springframework.extensions.surf.exception.AuthenticationException;
+import org.springframework.extensions.webscripts.connector.AbstractAuthenticator;
 import org.springframework.extensions.webscripts.connector.ConnectorSession;
+import org.springframework.extensions.webscripts.connector.Credentials;
 
-public class BecpgTicketAuthenticator extends AlfrescoAuthenticator {
+public class BecpgTicketAuthenticator extends AbstractAuthenticator {
+
+
+	public static final String PARAM_TICKETNAME_TICKET = "ticket";
 
 	@Override
 	public boolean isAuthenticated(String endpoint, ConnectorSession connectorSession) {
-		return true;
+		 return (connectorSession.getParameter(PARAM_TICKETNAME_TICKET) != null) ||
+	               (connectorSession.getCookieNames().length != 0);
+	}
+
+	@Override
+	public ConnectorSession authenticate(String endpoint, Credentials credentials, ConnectorSession connectorSession) throws AuthenticationException {
+//		 if (connectorSession != null)
+//         {
+//             connectorSession.setParameter(PARAM_TICKETNAME_TICKET, ticket);
+//             
+//             // signal that this succeeded
+//             cs = connectorSession;
+//         }
+		return connectorSession;
 	}
 
 }
--- becpg-share/src/main/java/fr/becpg/web/connector/BecpgTicketConnector.java
+++ becpg-share/src/main/java/fr/becpg/web/connector/BecpgTicketConnector.java
@@ -1,16 +1,12 @@
 package fr.becpg.web.connector;
 
-import org.springframework.extensions.config.Config;
 import org.springframework.extensions.config.RemoteConfigElement.ConnectorDescriptor;
-import org.springframework.extensions.webscripts.RequestCachingConnector;
 import org.springframework.extensions.webscripts.connector.AlfrescoAuthenticator;
 import org.springframework.extensions.webscripts.connector.ConnectorContext;
-import org.springframework.extensions.webscripts.connector.Credentials;
+import org.springframework.extensions.webscripts.connector.HttpConnector;
 import org.springframework.extensions.webscripts.connector.RemoteClient;
 
-public class BecpgTicketConnector extends RequestCachingConnector {
-
-	public static final String PARAM_TICKETNAME_TICKET = "ticket";
+public class BecpgTicketConnector extends HttpConnector {
 
 	public BecpgTicketConnector(ConnectorDescriptor descriptor, String endpoint) {
 		super(descriptor, endpoint);
@@ -18,56 +14,33 @@ public class BecpgTicketConnector extends RequestCachingConnector {
 
 	@Override
 	protected void applyRequestAuthentication(RemoteClient remoteClient, ConnectorContext context) {
-		String currentAuthToken = getCurrentAuthToken(remoteClient, context);
+		String currentAuthToken = getCurrentAuthToken(context);
 
 		if (currentAuthToken != null) {
 			remoteClient.setTicket(currentAuthToken);
-			remoteClient.setTicketName(PARAM_TICKETNAME_TICKET);
+			remoteClient.setTicketName(BecpgTicketAuthenticator.PARAM_TICKETNAME_TICKET);
 		}
 	}
 
-	public String getCurrentAuthToken(RemoteClient remoteClient, ConnectorContext context) {
+	public String getCurrentAuthToken(ConnectorContext context) {
+		String ticket = null;
 
 		if (context != null) {
 			//beCPG ticket is passed in URI
-			String becpgTicket = context.getParameters().get(PARAM_TICKETNAME_TICKET);
-			if (becpgTicket != null) {
-				return becpgTicket;
-			}
+			ticket = context.getParameters().get(BecpgTicketAuthenticator.PARAM_TICKETNAME_TICKET);
+
 		}
 
-		//Else connector are authenticating again'st Alfresco
-		String alfTicket = null;
-		String userName = "guest";
-		String instanceName = null;
-
 		if (getCredentials() != null) {
-
-			userName = (String) getCredentials().getProperty(Credentials.CREDENTIAL_USERNAME);
 
 			// if this connector is managing session info
 			if (getConnectorSession() != null) {
 				// apply alfresco ticket from connector session - i.e. previous login attempt
-				alfTicket = getConnectorSession().getParameter(AlfrescoAuthenticator.CS_PARAM_ALF_TICKET);
+				ticket = getConnectorSession().getParameter(AlfrescoAuthenticator.CS_PARAM_ALF_TICKET);
 			}
 		}
 
-		Config remoteConfig = remoteClient.getConfigService().getConfig("Remote");
-		if (remoteConfig != null && remoteConfig.hasConfigElement("becpg-instance")) {
-			instanceName = remoteConfig.getConfigElementValue("becpg-instance");
-		}
-
-		return getCurrentAuthToken(alfTicket, userName, instanceName);
-	}
-
-	public String getCurrentAuthToken(String alfTicket, String userName, String instanceName) {
-		String currentUserName = (instanceName != null ? instanceName.replace("|true", "") : "default") + "$" + userName;
-		if (!currentUserName.contains("@") || instanceName == null || !instanceName.contains("|true")) {
-			currentUserName += "@default";
-		}
-		currentUserName += "#" + alfTicket;
-
-		return java.util.Base64.getEncoder().encodeToString(currentUserName.getBytes());
+		return ticket;
 	}
 
 }
