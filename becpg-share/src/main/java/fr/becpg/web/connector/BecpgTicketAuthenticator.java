fdb886c18a5a7ec26b730f3ef1c9b09927456d60
Matthieu Laborie <matthieu.laborie@becpg.fr>
2024-04-19 12:18:01 +0200

    Fix #10243 - [Feature] Better becpg-ai Integration

--- becpg-share/src/main/java/fr/becpg/web/connector/BecpgTicketAuthenticator.java
+++ becpg-share/src/main/java/fr/becpg/web/connector/BecpgTicketAuthenticator.java
@@ -1,7 +1,13 @@
 package fr.becpg.web.connector;
 
 import org.springframework.extensions.webscripts.connector.AlfrescoAuthenticator;
+import org.springframework.extensions.webscripts.connector.ConnectorSession;
 
-public class BecpgTicketAuthenticator extends AlfrescoAuthenticator{
+public class BecpgTicketAuthenticator extends AlfrescoAuthenticator {
+
+	@Override
+	public boolean isAuthenticated(String endpoint, ConnectorSession connectorSession) {
+		return true;
+	}
 
 }
