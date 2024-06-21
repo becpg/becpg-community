bb0626c2548f3b9b679d274cb6d53e5f1da6d274
Matthieu Laborie <matthieu.laborie@becpg.fr>
2024-06-21 11:08:56 +0200

    Fix #22850 - [Bug] Fix cache of security rules is being reseted

--- becpg-core/src/main/java/fr/becpg/model/SecurityModel.java
+++ becpg-core/src/main/java/fr/becpg/model/SecurityModel.java
@@ -30,47 +30,51 @@ public final class SecurityModel {
 	//
 	// Namespace
 	//
+	
+	private SecurityModel(){
+		//DO Nothing
+	}
 
 	/** Security Model URI */
-	public final static String SECURITY_URI = "http://www.bcpg.fr/model/security/1.0";
+	public static final String SECURITY_URI = "http://www.bcpg.fr/model/security/1.0";
 
 	/** Security Model Prefix */
-	public final static String SECURITY_PREFIX = "sec";
+	public static final String SECURITY_PREFIX = "sec";
 
 	//
 	// Security Model Definitions
 	//
 	/** Constant <code>MODEL</code> */
-	public final static QName MODEL = QName.createQName(SECURITY_URI, "secmodel");
+	public static final QName MODEL = QName.createQName(SECURITY_URI, "secmodel");
 
 	/** Constant <code>TYPE_ACL_ENTRY</code> */
-	public final static QName TYPE_ACL_ENTRY = QName.createQName(SECURITY_URI, "aclEntry");
+	public static final QName TYPE_ACL_ENTRY = QName.createQName(SECURITY_URI, "aclEntry");
 
 	/** Constant <code>TYPE_ACL_GROUP</code> */
-	public final static QName TYPE_ACL_GROUP = QName.createQName(SECURITY_URI, "aclGroup");
+	public static final QName TYPE_ACL_GROUP = QName.createQName(SECURITY_URI, "aclGroup");
 
 	/** Constant <code>PROP_ACL_GROUP_NODE_TYPE</code> */
-	public final static QName PROP_ACL_GROUP_NODE_TYPE = QName.createQName(SECURITY_URI, "nodeType");
-	
+	public static final QName PROP_ACL_GROUP_NODE_TYPE = QName.createQName(SECURITY_URI, "nodeType");
+
 	/** Constant <code>PROP_ACL_GROUP_NODE_TYPE</code> */
-	public final static QName PROP_ACL_GROUP_IS_LOCAL_PERMISSION= QName.createQName(SECURITY_URI, "isLocalPermission");
+	public static final QName PROP_ACL_GROUP_IS_LOCAL_PERMISSION = QName.createQName(SECURITY_URI, "isLocalPermission");
 
 	/** Constant <code>ASSOC_GROUPS_ASSIGNEE</code> */
-	public final static QName ASSOC_GROUPS_ASSIGNEE = QName.createQName(SECURITY_URI, "groupsAssignee");
+	public static final QName ASSOC_GROUPS_ASSIGNEE = QName.createQName(SECURITY_URI, "groupsAssignee");
 
 	/** Constant <code>PROP_ACL_PROPNAME</code> */
-	public final static QName PROP_ACL_PROPNAME = QName.createQName(SECURITY_URI, "propName");
+	public static final QName PROP_ACL_PROPNAME = QName.createQName(SECURITY_URI, "propName");
 
 	/** Constant <code>PROP_ACL_PERMISSION</code> */
-	public final static QName PROP_ACL_PERMISSION = QName.createQName(SECURITY_URI, "aclPermission");
+	public static final QName PROP_ACL_PERMISSION = QName.createQName(SECURITY_URI, "aclPermission");
 
 	/** Constant <code>ASPECT_SECURITY</code> */
-	public static final QName ASPECT_SECURITY= QName.createQName(SECURITY_URI, "securityAspect");
-	
+	public static final QName ASPECT_SECURITY = QName.createQName(SECURITY_URI, "securityAspect");
+
 	/** Constant <code>ASSOC_SECURITY_REF</code> */
-	public static final QName ASSOC_SECURITY_REF= QName.createQName(SECURITY_URI, "securityRef");
-	
+	public static final QName ASSOC_SECURITY_REF = QName.createQName(SECURITY_URI, "securityRef");
+
 	public static final QName ASSOC_READ_GROUPS = QName.createQName(SECURITY_URI, "readGroups");
-	
+
 	public static final QName PROP_IS_DEFAULT_READ_ONLY = QName.createQName(SECURITY_URI, "isDefaultReadOnly");
 }
