cf769be15c264dfb5757d84cb20552804200df4c
Matthieu Laborie <matthieu.laborie@becpg.fr>
2024-08-14 11:13:43 +0200

    Fix #17980 - [Feature] Export using POST method

--- becpg-project/becpg-project-share/src/main/assembly/web/components/project/project-list-toolbar.js
+++ becpg-project/becpg-project-share/src/main/assembly/web/components/project/project-list-toolbar.js
@@ -251,9 +251,7 @@
 
                               var MAX_RESULTS_UNLIMITED = -1;
 							
- 							 beCPG.util.launchAsyncDownload("export.xlsx", "export.xlsx", dt._getDataUrl(MAX_RESULTS_UNLIMITED) + "&format=xlsx&metadata=" + encodeURIComponent(YAHOO.lang.JSON
-                                    .stringify(requestParams)));  
-                 
+ 							 beCPG.util.launchAsyncDownload("export.xlsx", "export.xlsx", dt._getDataUrl(MAX_RESULTS_UNLIMITED) + "&format=xlsx", requestParams);  
 
                            },
                            scope : this
