fc493e704bf38bf004b70d62325956ce5434f589
Matthieu Laborie <matthieu.laborie@becpg.fr>
2024-08-20 15:05:14 +0200

    [Merge] Fix

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/supplier/impl/SupplierPortalServiceImpl.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/supplier/impl/SupplierPortalServiceImpl.java
@@ -32,6 +32,7 @@ import org.alfresco.service.namespace.QName;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.extensions.surf.util.I18NUtil;
 import org.springframework.stereotype.Service;
 
