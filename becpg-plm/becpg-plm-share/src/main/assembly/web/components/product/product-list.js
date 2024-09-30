60e19c6c80802632d377f6a4c347980234ba5d1e
Matthieu Laborie <matthieu.laborie@becpg.fr>
2024-09-30 15:25:37 +0200

    Fix #24306 - [Bug] Update product-list export form

--- becpg-plm/becpg-plm-share/src/main/assembly/web/components/product/product-list.js
+++ becpg-plm/becpg-plm-share/src/main/assembly/web/components/product/product-list.js
@@ -4,15 +4,6 @@
  */
  
 (function() {
-    /**
-     * YUI Library aliases
-     */
-    var Dom = YAHOO.util.Dom;
-
-    /**
-     * Alfresco Slingshot aliases
-     */
-    var $html = Alfresco.util.encodeHTML;
 
     /**
      * ProductList constructor.
@@ -31,24 +22,5 @@
      */
     YAHOO.extend(beCPG.component.ProductList, beCPG.module.EntityDataGrid);
 
-    /**
-     * Augment prototype with main class implementation, ensuring overwrite is enabled
-     */
-    YAHOO.lang.augmentObject(beCPG.component.ProductList.prototype, {
-                
-        /**
-         * Data Item created event handler
-         * 
-         * @method onDataItemCreated
-         * @param layer {object} Event fired
-         * @param args {array} Event parameters (depends on event type)
-         */
-        onDataItemCreated: function(layer, args) {
-            var obj = args[1];
-            if (obj && obj.nodeRef !== null) {
-                this.queryExecutionId = null;
-                window.location = beCPG.util.entityURL(this.options.siteId, obj.nodeRef, "pjt:project", null, "View-properties");
-            }
-        }
-    }, true);
+ 
 })();
