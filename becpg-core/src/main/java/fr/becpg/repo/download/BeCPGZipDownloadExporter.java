21dd4a1281e43b7fc120ad3995d4f40e9d560d14
Valentin Leblanc <valentin.leblanc@becpg.fr>
2024-10-23 10:39:24 +0200

    Fix #24193 - [Bug] Fix zip download

--- becpg-core/src/main/java/fr/becpg/repo/download/BeCPGZipDownloadExporter.java
+++ becpg-core/src/main/java/fr/becpg/repo/download/BeCPGZipDownloadExporter.java
@@ -241,19 +241,18 @@ public class BeCPGZipDownloadExporter extends BaseExporter
 	}
 
 	private NodeRef findMatchingChild(NodeRef parentRef, Path subPathToMatch) {
-		
+		if (parentRef == null || !nodeService.exists(parentRef)) {
+			return null;
+		}
 		String firstPartToMatch = subPathToMatch.size() == 1 ? subPathToMatch.toString() : subPathToMatch.subPath(0, 0).toString();
-		
-		if (nodeService.exists(parentRef)) {
-			int parentPath = nodeService.getPath(parentRef).size();
-			for (ChildAssociationRef childAssoc : nodeService.getChildAssocs(parentRef)) {
-				NodeRef childRef = childAssoc.getChildRef();
-				Path currentPath = nodeService.getPath(childRef);
-				Path subPath = currentPath.subPath(parentPath, currentPath.size() - 1);
-				String firstPart = subPath.size() == 1 ? subPath.toString() : subPath.subPath(0, 0).toString();
-				if (firstPart.equals(firstPartToMatch)) {
-					return childRef;
-				}
+		int parentPath = nodeService.getPath(parentRef).size();
+		for (ChildAssociationRef childAssoc : nodeService.getChildAssocs(parentRef)) {
+			NodeRef childRef = childAssoc.getChildRef();
+			Path currentPath = nodeService.getPath(childRef);
+			Path subPath = currentPath.subPath(parentPath, currentPath.size() - 1);
+			String firstPart = subPath.size() == 1 ? subPath.toString() : subPath.subPath(0, 0).toString();
+			if (firstPart.equals(firstPartToMatch)) {
+				return childRef;
 			}
 		}
 		return null;
