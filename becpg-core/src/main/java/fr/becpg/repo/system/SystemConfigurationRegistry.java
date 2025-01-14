5088cceb725f0c9bb56924d03102f9fa6dea685e
Matthieu Laborie <matthieu.laborie@becpg.fr>
2025-01-14 16:32:36 +0100

    Fix #16958 - [Bug] Unsure supported locales is loaded at startup

--- becpg-core/src/main/java/fr/becpg/repo/system/SystemConfigurationRegistry.java
+++ becpg-core/src/main/java/fr/becpg/repo/system/SystemConfigurationRegistry.java
@@ -5,7 +5,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("systemConfigurationRegistry")
 public class SystemConfigurationRegistry {
 	   private static SystemConfigurationService instance;
 
