ee37fe52d6a6301a6111c5796bbb4700528b729c
Matthieu Laborie <matthieu.laborie@becpg.fr>
2025-02-14 12:09:18 +0100

    [Bug] Fix patch

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/admin/patch/InitRepoNotificationPatch.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/admin/patch/InitRepoNotificationPatch.java
@@ -29,34 +29,36 @@ public class InitRepoNotificationPatch extends AbstractBeCPGPatch {
 			NodeRef charactsNodeRef = repoService.getFolderByPath(systemNodeRef, RepoConsts.PATH_CHARACTS);
 			NodeRef listContainer = charactsNodeRef == null ? null : nodeService.getChildByName(charactsNodeRef, BeCPGModel.ASSOC_ENTITYLISTS, RepoConsts.CONTAINER_DATALISTS);
 			NodeRef notificationFolder = listContainer == null ? null : nodeService.getChildByName(listContainer, ContentModel.ASSOC_CONTAINS, RepoConsts.PATH_NOTIFICATIONS);
-			NodeRef requirementsNotification = nodeService.getChildByName(notificationFolder, ContentModel.ASSOC_CONTAINS, RepoConsts.FORMULATION_ERRORS_NOTIFICATION);
-			
-			if (requirementsNotification != null) {
-				nodeService.setProperty(requirementsNotification, QName.createQName(BeCPGModel.BECPG_URI, "nrConditions"), "{\"query\":\"+@{http://www.bcpg.fr/model/becpg/1.0}rclDataType:\\\"Formulation\\\"\"}");
-			}
-			
-			NodeRef inProgressProjectsNotification = nodeService.getChildByName(notificationFolder, ContentModel.ASSOC_CONTAINS, RepoConsts.IN_PROGRESS_PROJECTS_NOTIFICATION);
-			
-			if (inProgressProjectsNotification != null) {
-				nodeService.setProperty(inProgressProjectsNotification, QName.createQName(BeCPGModel.BECPG_URI, "nrConditions"), "{\"query\":\"+@{http://www.bcpg.fr/model/project/1.0}projectState:\\\"InProgress\\\"\"}");
-			}
-			
-			NodeRef validatedProductsNotification = nodeService.getChildByName(notificationFolder, ContentModel.ASSOC_CONTAINS, RepoConsts.VALIDATED_PRODUCTS_NOTIFICATION);
-			
-			if (validatedProductsNotification != null) {
-				nodeService.setProperty(validatedProductsNotification, QName.createQName(BeCPGModel.BECPG_URI, "nrConditions"), "{\"query\":\"+@{http://www.bcpg.fr/model/becpg/1.0}productState:\\\"Valid\\\"\"}");
-			}
-			
-			NodeRef validatedAndUpdatedProductsNotification = nodeService.getChildByName(notificationFolder, ContentModel.ASSOC_CONTAINS, RepoConsts.VALIDATED_AND_UPDATED_PRODUCTS_NOTIFICATION);
-			
-			if (validatedAndUpdatedProductsNotification != null) {
-				nodeService.setProperty(validatedAndUpdatedProductsNotification, QName.createQName(BeCPGModel.BECPG_URI, "nrConditions"), "{\"query\":\"+@{http://www.bcpg.fr/model/becpg/1.0}productState:\\\"Valid\\\"\"}");
-			}
-			
-			NodeRef archivedProductsNotification = nodeService.getChildByName(notificationFolder, ContentModel.ASSOC_CONTAINS, RepoConsts.ARCHIVED_PRODUCTS_NOTIFICATION);
-			
-			if (archivedProductsNotification != null) {
-				nodeService.setProperty(archivedProductsNotification, QName.createQName(BeCPGModel.BECPG_URI, "nrConditions"), "{\"query\":\"+@{http://www.bcpg.fr/model/becpg/1.0}productState:\\\"Archived\\\"\"}");
+			if(notificationFolder !=null) {
+				NodeRef requirementsNotification = nodeService.getChildByName(notificationFolder, ContentModel.ASSOC_CONTAINS, RepoConsts.FORMULATION_ERRORS_NOTIFICATION);
+				
+				if (requirementsNotification != null) {
+					nodeService.setProperty(requirementsNotification, QName.createQName(BeCPGModel.BECPG_URI, "nrConditions"), "{\"query\":\"+@{http://www.bcpg.fr/model/becpg/1.0}rclDataType:\\\"Formulation\\\"\"}");
+				}
+				
+				NodeRef inProgressProjectsNotification = nodeService.getChildByName(notificationFolder, ContentModel.ASSOC_CONTAINS, RepoConsts.IN_PROGRESS_PROJECTS_NOTIFICATION);
+				
+				if (inProgressProjectsNotification != null) {
+					nodeService.setProperty(inProgressProjectsNotification, QName.createQName(BeCPGModel.BECPG_URI, "nrConditions"), "{\"query\":\"+@{http://www.bcpg.fr/model/project/1.0}projectState:\\\"InProgress\\\"\"}");
+				}
+				
+				NodeRef validatedProductsNotification = nodeService.getChildByName(notificationFolder, ContentModel.ASSOC_CONTAINS, RepoConsts.VALIDATED_PRODUCTS_NOTIFICATION);
+				
+				if (validatedProductsNotification != null) {
+					nodeService.setProperty(validatedProductsNotification, QName.createQName(BeCPGModel.BECPG_URI, "nrConditions"), "{\"query\":\"+@{http://www.bcpg.fr/model/becpg/1.0}productState:\\\"Valid\\\"\"}");
+				}
+				
+				NodeRef validatedAndUpdatedProductsNotification = nodeService.getChildByName(notificationFolder, ContentModel.ASSOC_CONTAINS, RepoConsts.VALIDATED_AND_UPDATED_PRODUCTS_NOTIFICATION);
+				
+				if (validatedAndUpdatedProductsNotification != null) {
+					nodeService.setProperty(validatedAndUpdatedProductsNotification, QName.createQName(BeCPGModel.BECPG_URI, "nrConditions"), "{\"query\":\"+@{http://www.bcpg.fr/model/becpg/1.0}productState:\\\"Valid\\\"\"}");
+				}
+				
+				NodeRef archivedProductsNotification = nodeService.getChildByName(notificationFolder, ContentModel.ASSOC_CONTAINS, RepoConsts.ARCHIVED_PRODUCTS_NOTIFICATION);
+				
+				if (archivedProductsNotification != null) {
+					nodeService.setProperty(archivedProductsNotification, QName.createQName(BeCPGModel.BECPG_URI, "nrConditions"), "{\"query\":\"+@{http://www.bcpg.fr/model/becpg/1.0}productState:\\\"Archived\\\"\"}");
+				}
 			}
 		}
 		
