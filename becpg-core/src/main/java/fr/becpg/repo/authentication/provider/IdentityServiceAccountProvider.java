8e230faea1f4c7fa0cac5fb91aaac2e98690d5e7
Valentin Leblanc <valentin.leblanc@becpg.fr>
2025-03-27 10:49:14 +0100

    Fix #27038 - [Feature] Improve IDS service

--- becpg-core/src/main/java/fr/becpg/repo/authentication/provider/IdentityServiceAccountProvider.java
+++ becpg-core/src/main/java/fr/becpg/repo/authentication/provider/IdentityServiceAccountProvider.java
@@ -3,11 +3,13 @@ package fr.becpg.repo.authentication.provider;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.alfresco.repo.security.authentication.identityservice.IdentityServiceException;
 import org.alfresco.service.cmr.security.AuthorityService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
@@ -32,6 +34,8 @@ import fr.becpg.repo.authentication.BeCPGUserAccount;
  */
 @Service
 public class IdentityServiceAccountProvider {
+
+	private static final String GET_USER_ID_ERROR = "Could not find userId from identity service for user: ";
 
 	private static Log logger = LogFactory.getLog(IdentityServiceAccountProvider.class);
 
@@ -83,6 +87,15 @@ public class IdentityServiceAccountProvider {
 	 * @return a boolean
 	 */
 	public boolean registerAccount(BeCPGUserAccount userAccount) {
+		if (logger.isDebugEnabled()) {
+        	logger.debug("registerAccount in IDS for username: " + userAccount.getUserName());
+        }
+		if (getUserId(userAccount.getUserName()) != null) {
+			if(logger.isDebugEnabled()){
+				logger.debug("user already exists in IDS: " + userAccount.getUserName());
+			}
+			return false;
+		}
 		try {
 			HttpClientBuilder builder = HttpClientBuilder.create();
 
@@ -106,15 +119,9 @@ public class IdentityServiceAccountProvider {
 				}
 				try (CloseableHttpResponse createResp = httpClient.execute(request)) {
 					String response = EntityUtils.toString(createResp.getEntity());
-					if (response.toLowerCase().contains("user exists")) {
-						if(logger.isDebugEnabled()){
-							logger.debug("user exists response: " + response);
-						}
-						return false;
-					}
 					if (!HttpStatus.valueOf(createResp.getStatusLine().getStatusCode()).is2xxSuccessful()) {
-						throw new IllegalStateException(response);
-					} else  if(logger.isDebugEnabled()){
+						throw new IdentityServiceException(response);
+					} else if(logger.isDebugEnabled()){
 						logger.debug(response);
 					}
 				}
@@ -122,59 +129,144 @@ public class IdentityServiceAccountProvider {
 			}
 		} catch (IOException e) {
 			logger.error(e, e);
-			return false;
+            throw new IdentityServiceException("Could not register user in IDS", e);
 		}
 		return true;
 	}
 	
+	public void deleteAccount(String username) {
+		if (logger.isDebugEnabled()) {
+        	logger.debug("deleteAccount in IDS for username: " + username);
+        }
+		String userId = getUserId(username);
+		if (userId == null) {
+			if(logger.isDebugEnabled()){
+				logger.debug("user already deleted in IDS: " + username);
+			}
+			return;
+		}
+		try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+			HttpDelete request = new HttpDelete(authServerUrl + "/admin/realms/" + realm + "/users/" + userId);
+			request.setHeader("Authorization", "Bearer " + getAdminAccessToken());
+			try (CloseableHttpResponse response = httpClient.execute(request)) {
+				if (!HttpStatus.valueOf(response.getStatusLine().getStatusCode()).is2xxSuccessful()) {
+					throw new IdentityServiceException("Failed to delete user: " + username);
+				}
+			}
+		} catch (IOException e) {
+			logger.error(e, e);
+			throw new IdentityServiceException("Could not delete user in IDS", e);
+		}
+	}
+
+
+	public boolean updateUser(BeCPGUserAccount userAccount) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("updateUser in IDS for username: " + userAccount.getUserName());
+		}
+		String userId = getUserId(userAccount.getUserName());
+		if (userId == null) {
+			throw new IllegalStateException(GET_USER_ID_ERROR + userAccount.getUserName());
+		}
+		try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+			HttpPut request = new HttpPut(authServerUrl + "/admin/realms/" + realm + "/users/" + userId);
+			request.setHeader("Content-Type", "application/json;charset=UTF-8");
+			request.setHeader("Authorization", "Bearer " + getAdminAccessToken());
+
+			JSONObject userRepresentation = new JSONObject();
+			if (userAccount.getEmail() != null && !userAccount.getEmail().isBlank()) {
+				userRepresentation.put("email", userAccount.getEmail());
+			}
+			if (userAccount.getFirstName() != null && !userAccount.getFirstName().isBlank()) {
+				userRepresentation.put("firstName", userAccount.getFirstName());
+			}
+			if (userAccount.getLastName() != null && !userAccount.getLastName().isBlank()) {
+				userRepresentation.put("lastName", userAccount.getLastName());
+			}
+			
+			request.setEntity(new StringEntity(userRepresentation.toString(), "UTF-8"));
+			try (CloseableHttpResponse response = httpClient.execute(request)) {
+				if (!HttpStatus.valueOf(response.getStatusLine().getStatusCode()).is2xxSuccessful()) {
+					String reponseBody = "";
+					if (response.getEntity() != null) {
+						reponseBody = EntityUtils.toString(response.getEntity());
+					}
+					throw new IdentityServiceException("Failed to update user: " + userAccount.getUserName() + ", reponseBody: " + reponseBody);
+				}
+			}
+		} catch (IOException e) {
+			logger.error(e, e);
+			throw new IdentityServiceException("Error while updating user in IDS", e);
+		}
+		return true;
+	}
+	
+	public void updatePassword(String username, String newPassword) {
+		if (logger.isDebugEnabled()) {
+        	logger.debug("generatePassword in IDS for username: " + username);
+        }
+	    try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+	        String userId = getUserId(username);
+	        if (userId == null) {
+	            throw new IllegalStateException(GET_USER_ID_ERROR + username);
+	        }
+			HttpPut request = new HttpPut(authServerUrl + "/admin/realms/" + realm + "/users/" + userId + "/reset-password");
+	        request.setHeader("Content-Type", "application/json;charset=UTF-8");
+	        request.setHeader("Authorization", "Bearer " + getAdminAccessToken());
+	
+	        JSONObject credential = new JSONObject();
+	        credential.put("type", "password");
+	        credential.put("value", newPassword);
+	        credential.put("temporary", true);
+	
+	        request.setEntity(new StringEntity(credential.toString(), "UTF-8"));
+	        try (CloseableHttpResponse response = httpClient.execute(request)) {
+	            if (!HttpStatus.valueOf(response.getStatusLine().getStatusCode()).is2xxSuccessful()) {
+	            	String responseBody = EntityUtils.toString(response.getEntity());
+	            	throw new IdentityServiceException("Error while updating password in IDS for user: " + username + ", responseBody: " + responseBody);
+	            }
+	        }
+	    } catch (IOException e) {
+	        logger.error(e, e);
+	        throw new IdentityServiceException("Error while updating password in IDS for user: " + username, e);
+	    }
+	}
+
+
 	private String getUserId(String username) {
+		if (logger.isDebugEnabled()) {
+        	logger.debug("getUserId in IDS for username: " + username);
+        }
 		try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
             HttpGet request = new HttpGet(authServerUrl + "/admin/realms/" + realm + "/users?username=" + username);
             request.setHeader("Content-Type", "application/json;charset=UTF-8");
             request.setHeader("Authorization", "Bearer " + getAdminAccessToken());
 
             try (CloseableHttpResponse response = httpClient.execute(request)) {
-                if (HttpStatus.valueOf(response.getStatusLine().getStatusCode()).is2xxSuccessful()) {
-                    JSONArray users = new JSONArray(EntityUtils.toString(response.getEntity()));
-                    
+                String reponseBody = EntityUtils.toString(response.getEntity());
+                if (logger.isDebugEnabled()) {
+                	logger.debug("reponseBody from getUserId: " + reponseBody);
+                }
+				if (HttpStatus.valueOf(response.getStatusLine().getStatusCode()).is2xxSuccessful()) {
+                    JSONArray users = new JSONArray(reponseBody);
                     for (int i = 0 ; i < users.length(); i ++) {
                     	JSONObject user = users.getJSONObject(i);
 						if (user.getString("username").equals(username)) {
                     		return user.getString("id");
                     	}
                     }
+                    return null;
                 } else {
-                	throw new IllegalStateException("Cannot get userId from identity service");
+                	throw new IdentityServiceException("Error while fetching userId from identity service: " + reponseBody + " for user: " + username);
                 }
             }
         } catch (IOException e) {
             logger.error(e, e);
+            throw new IdentityServiceException("Error while fetching userId from identity service for user: " + username, e);
         }
-		throw new IllegalStateException("Cannot get userId from identity service");
 	}
 	
-	public boolean generatePassword(String username, String newPassword) {
-        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
-            HttpPut request = new HttpPut(authServerUrl + "/admin/realms/" + realm + "/users/" + getUserId(username) + "/reset-password");
-            request.setHeader("Content-Type", "application/json;charset=UTF-8");
-            request.setHeader("Authorization", "Bearer " + getAdminAccessToken());
-
-            JSONObject credential = new JSONObject();
-            credential.put("type", "password");
-            credential.put("value", newPassword);
-            credential.put("temporary", true);
-
-            request.setEntity(new StringEntity(credential.toString(), "UTF-8"));
-            try (CloseableHttpResponse response = httpClient.execute(request)) {
-                return HttpStatus.valueOf(response.getStatusLine().getStatusCode()).is2xxSuccessful();
-            }
-        } catch (IOException e) {
-            logger.error(e, e);
-            return false;
-        }
-    }
-	
-	 private String getAdminAccessToken() throws IOException {
+	private String getAdminAccessToken() throws IOException {
         try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
             HttpPost request = new HttpPost(authServerUrl + "/realms/" + realm + "/protocol/openid-connect/token");
             ArrayList<BasicNameValuePair> parameters = new ArrayList<>();
@@ -191,12 +283,12 @@ public class IdentityServiceAccountProvider {
                     	return auth.getString("access_token");
                     } else {
 						logger.error(auth.toString());
-						throw new IllegalStateException("Cannot get access_token from identityService");
+						throw new IllegalStateException("Error while fetching access_token from identityService");
 					}
                 }
             }
         }
-        throw new IOException("Failed to obtain admin access token");
+        throw new IOException("Error while fetching access_token from identityService");
     }
 
 	/** {@inheritDoc} */
