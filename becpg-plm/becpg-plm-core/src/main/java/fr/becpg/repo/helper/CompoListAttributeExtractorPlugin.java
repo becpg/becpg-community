14709da27d7b70464be0ebc93466f95c73f07ad3
Valentin Leblanc <valentin.leblanc@becpg.fr>
2025-04-01 15:23:15 +0200

    Fix #25405 - [Bug] Fix compoList activity className

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/helper/CompoListAttributeExtractorPlugin.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/helper/CompoListAttributeExtractorPlugin.java
@@ -5,8 +5,6 @@ import java.util.Collections;
 
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.QName;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,41 +12,37 @@ import fr.becpg.model.PLMModel;
 import fr.becpg.repo.helper.impl.CharactAttributeExtractorPlugin;
 import fr.becpg.repo.system.SystemConfigurationService;
 
+/**
+ * <p>CompoListAttributeExtractorPlugin class.</p>
+ *
+ * @author matthieu
+ */
 @Service
 public class CompoListAttributeExtractorPlugin extends CharactAttributeExtractorPlugin {
 	
-	private static Log logger = LogFactory.getLog(CompoListAttributeExtractorPlugin.class);
-
 	@Autowired
 	private AssociationService associationService;
 	
 	@Autowired
 	private SystemConfigurationService systemConfigurationService;
 	
+	/** {@inheritDoc} */
 	@Override
 	public String extractPropName(QName type, NodeRef nodeRef) {
 		NodeRef product = associationService.getTargetAssoc(nodeRef, PLMModel.ASSOC_COMPOLIST_PRODUCT);
 		return super.extractExpr(product, systemConfigurationService.confValue("beCPG.product.name.format"));
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	public Collection<QName> getMatchingTypes() {
 		return Collections.singletonList(PLMModel.TYPE_COMPOLIST);
 	}
 	
+	/** {@inheritDoc} */
 	@Override
 	public Integer getPriority() {
 		return 1;
-	}
-
-	@Override
-	public String extractMetadata(QName type, NodeRef nodeRef) {
-		NodeRef product = associationService.getTargetAssoc(nodeRef, PLMModel.ASSOC_COMPOLIST_PRODUCT);
-		if(product == null) {
-			logger.warn("No compoList product for :"+nodeRef);
-			return "product";
-		}
-		return super.extractMetadata(nodeService.getType(product), product);
 	}
 
 }
