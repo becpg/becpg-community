4c92705e2883fb4b48ece3df52da23b3991c8ec7
Valentin Leblanc <valentin.leblanc@becpg.fr>
2025-04-01 15:23:15 +0200

    Fix #25405 - [Bug] Add compoList extractor plugin

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/helper/CompoListAttributeExtractorPlugin.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/helper/CompoListAttributeExtractorPlugin.java
@@ -3,6 +3,8 @@ package fr.becpg.repo.helper;
 import java.util.Collection;
 import java.util.Collections;
 
+import javax.annotation.Nonnull;
+
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.QName;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,35 +14,39 @@ import fr.becpg.model.PLMModel;
 import fr.becpg.repo.helper.impl.CharactAttributeExtractorPlugin;
 import fr.becpg.repo.system.SystemConfigurationService;
 
+/**
+ * <p>CompoListAttributeExtractorPlugin class.</p>
+ *
+ * @author matthieu
+ */
 @Service
 public class CompoListAttributeExtractorPlugin extends CharactAttributeExtractorPlugin {
-
+	
 	@Autowired
 	private AssociationService associationService;
 	
 	@Autowired
 	private SystemConfigurationService systemConfigurationService;
 	
+	/** {@inheritDoc} */
 	@Override
-	public String extractPropName(QName type, NodeRef nodeRef) {
+	@Nonnull
+	public String extractPropName(@Nonnull QName type, @Nonnull NodeRef nodeRef) {
 		NodeRef product = associationService.getTargetAssoc(nodeRef, PLMModel.ASSOC_COMPOLIST_PRODUCT);
 		return super.extractExpr(product, systemConfigurationService.confValue("beCPG.product.name.format"));
 	}
 
+	/** {@inheritDoc} */
 	@Override
+	@Nonnull
 	public Collection<QName> getMatchingTypes() {
 		return Collections.singletonList(PLMModel.TYPE_COMPOLIST);
 	}
 	
+	/** {@inheritDoc} */
 	@Override
 	public Integer getPriority() {
-		return 1;
-	}
-
-	@Override
-	public String extractMetadata(QName type, NodeRef nodeRef) {
-		NodeRef product = associationService.getTargetAssoc(nodeRef, PLMModel.ASSOC_COMPOLIST_PRODUCT);
-		return super.extractMetadata(nodeService.getType(product), product);
+		return 2;
 	}
 
 }
