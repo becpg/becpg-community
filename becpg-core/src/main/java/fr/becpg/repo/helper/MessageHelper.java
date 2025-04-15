2cca9a900c48079d53bc2f0ee94e177043a8ea83
Valentin Leblanc <valentin.leblanc@becpg.fr>
2025-04-15 16:59:55 +0200

    Fix unit tests

--- becpg-core/src/main/java/fr/becpg/repo/helper/MessageHelper.java
+++ becpg-core/src/main/java/fr/becpg/repo/helper/MessageHelper.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import org.alfresco.repo.i18n.MessageService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.extensions.surf.util.I18NUtil;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,18 +22,30 @@ public class MessageHelper implements InitializingBean {
 	}
 	
     public static String getMessage(String messageKey) {
+    	if (instance == null || instance.messageService == null) {
+    		return I18NUtil.getMessage(messageKey);
+    	}
     	return instance.messageService.getMessage(messageKey);
     }
 
     public static String getMessage(final String messageKey, final Locale locale) {
+    	if (instance == null || instance.messageService == null) {
+    		return I18NUtil.getMessage(messageKey, locale);
+    	}
     	return instance.messageService.getMessage(messageKey, locale);
     }
 
     public static String getMessage(String messageKey, Object... params) {
+    	if (instance == null || instance.messageService == null) {
+    		return I18NUtil.getMessage(messageKey, params);
+    	}
     	return instance.messageService.getMessage(messageKey, params);
     }
 
     public static String getMessage(String messageKey, Locale locale, Object... params) {
+    	if (instance == null || instance.messageService == null) {
+    		return I18NUtil.getMessage(messageKey, locale, params);
+    	}
     	return instance.messageService.getMessage(messageKey, locale, params);
     }
 
