2e90a0e27ffa94d6153726c01e805926c019ea55
Alexandre Masanes <alexandre.masanes@becpg.fr>
2025-05-16 14:23:22 +0200

    Fix #18349 - [Bug] add format() transformer for deliverable url generation

--- becpg-integration-runner/src/test/java/fr/becpg/test/project/ProjectScriptHelperIT.java
+++ becpg-integration-runner/src/test/java/fr/becpg/test/project/ProjectScriptHelperIT.java
@@ -3,15 +3,19 @@ package fr.becpg.test.project;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.alfresco.model.ContentModel;
 import org.alfresco.repo.jscript.ScriptNode;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.QName;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.extensions.surf.util.URLEncoder;
 
 import fr.becpg.model.BeCPGModel;
+import fr.becpg.repo.RepoConsts;
+import fr.becpg.repo.entity.EntityService;
 import fr.becpg.repo.product.data.RawMaterialData;
 import fr.becpg.repo.project.data.ProjectData;
 import fr.becpg.repo.project.data.projectList.DeliverableListDataItem;
@@ -27,6 +31,9 @@ public class ProjectScriptHelperIT extends RepoBaseTestCase {
 	@Autowired
 	ProjectScriptHelper projectScriptHelper;
 
+	@Autowired
+	EntityService entityService;
+
 	@Test
 	public void testCreateDeliverableUrl() {
 
@@ -39,6 +46,8 @@ public class ProjectScriptHelperIT extends RepoBaseTestCase {
 			projectData.setEntities(List.of(alfrescoRepository.create(getTestFolderNodeRef(), RawMaterialData.build().withName("MP1")).getNodeRef(),
 					alfrescoRepository.create(getTestFolderNodeRef(), RawMaterialData.build().withName("MP2")).getNodeRef()));
 
+			projectData.getEntities().forEach(nodeRef -> entityService.getDocumentsFolder(nodeRef, true));
+			
 			alfrescoRepository.create(getTestFolderNodeRef(), projectData);
 
 			// {nodeRef} --> replace with project nodeRef
@@ -49,28 +58,66 @@ public class ProjectScriptHelperIT extends RepoBaseTestCase {
 			// {assocName|@type} --> replace with association property
 			// {assocName|xpath:./path} --> replace with nodeRef found in relative assoc path
 
-			projectData.withDeliverableList(List.of(DeliverableListDataItem.build().withTasks(List.of(projectData.getTaskList().get(0).getNodeRef()))
+			final List<NodeRef> tasks = List.of(projectData.getTaskList().get(0).getNodeRef());
+			
+			projectData.withDeliverableList(List.of(DeliverableListDataItem.build().withTasks(tasks)
 
 					.withUrl(
-							"https://entity?nodeRef={nodeRef}&code={nodeRef|bcpg:code}&state={nodeRef|pjt:projectState}&entities={pjt:projectEntity}")));
+							"https://entity?nodeRef={nodeRef}&code={nodeRef|bcpg:code}&state={nodeRef|pjt:projectState}&entities={pjt:projectEntity}"),
+					DeliverableListDataItem.build().withTasks(tasks)
 
-			return alfrescoRepository.save(projectData);
+					.withUrl(
+							"https://entity?state={nodeRef|pjt:projectState?format(.%s)}"),
+					DeliverableListDataItem.build().withTasks(tasks)
 
+					.withUrl(
+							"https://entity?documents={nodeRef|xpath:./cm:" + RepoConsts.PATH_DOCUMENTS + "}&entityDocuments={pjt:projectEntity|xpath:./cm:" + RepoConsts.PATH_DOCUMENTS + "}"),
+					DeliverableListDataItem.build().withTasks(tasks)
+
+					.withUrl(
+							"https://entity?type={nodeRef|@type}&entityTypes={pjt:projectEntity|@type}"),
+					DeliverableListDataItem.build().withTasks(tasks)
+
+					.withUrl(
+							"https://entity?entityNames={pjt:projectEntity|cm:name}")));
+			
+			projectData = (ProjectData) alfrescoRepository.save(projectData);
+			
+			entityService.getDocumentsFolder(projectData.getNodeRef(), true);
+
+			return projectData;
 		});
 
 		inReadTx(() -> {
 			
 			ProjectData projectData = (ProjectData) alfrescoRepository.findOne(ret.getNodeRef());
 
-			String url = projectScriptHelper.getDeliverableUrl(new ScriptNode(ret.getDeliverableList().get(0).getNodeRef(), serviceRegistry));
-
-			String url2 = "https://entity?nodeRef=" + URLEncoder.encodeUriComponent(ret.getNodeRef().toString()) + "&code=" +projectData.getCode()
-					+ "&state=" + projectData.getState().toString() + "&entities="
-					+ URLEncoder.encodeUriComponent(projectData.getEntities().stream().map(NodeRef::toString).collect(Collectors.joining(",")));
-
+			final String[] expectedUrls = {
+					"https://entity?nodeRef=" + URLEncoder.encodeUriComponent(ret.getNodeRef().toString()) + "&code="
+							+ projectData.getCode() + "&state=" + projectData.getState().toString() + "&entities="
+							+ URLEncoder.encodeUriComponent(projectData.getEntities().stream().map(NodeRef::toString)
+									.collect(Collectors.joining(","))),
+					"https://entity?state=." + projectData.getState().toString(),
+					"https://entity?documents="
+							+ URLEncoder.encodeUriComponent(
+									entityService.getDocumentsFolder(projectData.getNodeRef(), false).toString())
+							+ "&entityDocuments="
+							+ URLEncoder.encodeUriComponent(projectData.getEntities().stream()
+									.map(nodeRef -> entityService.getDocumentsFolder(nodeRef, false).toString())
+									.collect(Collectors.joining(","))),
+					"https://entity?type=" + nodeService.getType(projectData.getNodeRef()).getLocalName()
+							+ "&entityTypes="
+							+ URLEncoder.encodeUriComponent(projectData.getEntities().stream().map(nodeService::getType)
+									.map(QName::getLocalName).collect(Collectors.joining(","))),
+					"https://entity?entityNames=" + URLEncoder.encodeUriComponent(projectData.getEntities().stream()
+							.map(nodeRef -> nodeService.getProperty(nodeRef, ContentModel.PROP_NAME).toString())
+							.collect(Collectors.joining(","))) };
 			
-			Assert.assertEquals(url, url2);
-
+			for (final DeliverableListDataItem deliverable : ret.getDeliverableList()) {
+				String url = projectScriptHelper.getDeliverableUrl(new ScriptNode(deliverable.getNodeRef(), serviceRegistry));
+			
+				Assert.assertEquals(url, expectedUrls[ret.getDeliverableList().indexOf(deliverable)]);
+			}
 			return null;
 		});
 
