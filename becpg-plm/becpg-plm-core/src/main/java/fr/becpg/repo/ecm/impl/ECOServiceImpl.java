3749c7435bcf7941cd6993fce8d357d6cdb065f5
Valentin Leblanc <valentin.leblanc@becpg.fr>
2025-05-28 10:10:57 +0200

    Fix #24656 - [Bug] Fix multiples issues

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/ecm/impl/ECOServiceImpl.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/ecm/impl/ECOServiceImpl.java
@@ -975,7 +975,7 @@ public class ECOServiceImpl implements ECOService {
 								Entry<NodeRef, MultiLevelListData> entry = iterator.next();
 								NodeRef nodeRef = entry.getValue().getEntityNodeRef();
 								String productState = (String) nodeService.getProperty(nodeRef, PLMModel.PROP_PRODUCT_STATE);
-								if (!wUsedStatesList.contains(productState)) {
+								if (productState == null || !wUsedStatesList.contains(productState)) {
 									iterator.remove();
 								}
 							}
