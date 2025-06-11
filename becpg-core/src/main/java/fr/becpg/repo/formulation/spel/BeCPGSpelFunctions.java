8014c4902b8cad999bff8897f9e4e0e9cab0e4c9
Matthieu Laborie <matthieu.laborie@becpg.fr>
2025-06-11 16:39:23 +0200

    Fix #28176 - [Feature] Add beCPG.findDuplicates($range) spel helper

--- becpg-core/src/main/java/fr/becpg/repo/formulation/spel/BeCPGSpelFunctions.java
+++ becpg-core/src/main/java/fr/becpg/repo/formulation/spel/BeCPGSpelFunctions.java
@@ -10,6 +10,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
