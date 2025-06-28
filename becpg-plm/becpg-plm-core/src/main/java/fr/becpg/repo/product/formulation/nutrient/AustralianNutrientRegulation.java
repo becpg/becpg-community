0a87516e990785a55836d43a11a30071d108f40b
Arthur Azambre <arthur.azambre@becpg.fr>
2025-06-28 17:20:19 +0200

    Fix #27527 - [Bug] Update Australian nutrient regulation

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/nutrient/AustralianNutrientRegulation.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/nutrient/AustralianNutrientRegulation.java
@@ -27,6 +27,37 @@ public class AustralianNutrientRegulation extends AbstractNutrientRegulation {
 		if(value != null){
 			if (nutrientTypeCode.equals(NutrientCode.Energykcal) || nutrientTypeCode.equals(NutrientCode.EnergykJ)) {
 				return roundValue(value,1d);
+			} else if ((nutrientTypeCode.equals(NutrientCode.Protein) ||
+					nutrientTypeCode.equals(NutrientCode.CarbohydrateWithFiber) ||
+					nutrientTypeCode.equals(NutrientCode.CarbohydrateByDiff) ||
+					nutrientTypeCode.equals(NutrientCode.Sugar)) && value > 1d) {
+				BigDecimal bd = BigDecimal.valueOf(value);
+				bd = bd.setScale(0, RoundingMode.CEILING);
+				return bd.doubleValue();
+			} else if (nutrientTypeCode.equals(NutrientCode.Fat) || nutrientTypeCode.equals(NutrientCode.FatSaturated)) {
+				if (value < 5d) {
+					BigDecimal bd = BigDecimal.valueOf(value);
+					BigDecimal bd2 = new BigDecimal("2");
+					bd = bd.multiply(bd2).setScale(0, RoundingMode.CEILING);
+					return bd.divide(bd2).doubleValue();
+				} else {
+					BigDecimal bd = BigDecimal.valueOf(value);
+					bd = bd.setScale(0, RoundingMode.CEILING);
+					return bd.doubleValue();
+				}
+			} else if (nutrientTypeCode.equals(NutrientCode.Sodium)) {
+				if (value > 5d) {
+					if (value <= 140d) {
+						BigDecimal bd = BigDecimal.valueOf(value);
+						BigDecimal bd2 = new BigDecimal("2");
+						bd = bd.multiply(bd2).setScale(-1, RoundingMode.CEILING);
+						return bd.divide(bd2).doubleValue();
+					} else {
+						BigDecimal bd = BigDecimal.valueOf(value);
+						bd = bd.setScale(-1, RoundingMode.CEILING);
+						return bd.doubleValue();
+					}
+				}
 			}
 		}
 		
