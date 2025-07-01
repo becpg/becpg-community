2e7f9c6dd22e0c8020381b0a25f4db56af60f56d
Arthur Azambre <arthur.azambre@becpg.fr>
2025-07-01 16:15:39 +0200

    Fix #12507 - [Feature] Japan and Taiwan displayValue fix

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/nutrient/TaiwanNutrientRegulation.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/nutrient/TaiwanNutrientRegulation.java
@@ -45,8 +45,7 @@ public class TaiwanNutrientRegulation extends AbstractNutrientRegulation {
 	@Override
 	protected String displayValueByCode(Double value, Double roundedValue, String nutrientTypeCode, String measurementPrecision, Locale locale) {
 
-		if (measurementPrecision == null || //
-				NutMeasurementPrecision.LessThan.toString().equals(measurementPrecision) //
+		if (NutMeasurementPrecision.LessThan.toString().equals(measurementPrecision) //
 						&& (value != null && roundedValue != null && nutrientTypeCode != null //
 								&& (nutrientTypeCode.equals(NutrientCode.Energykcal) && value <= 4
 										|| ((nutrientTypeCode.equals(NutrientCode.Fat) || nutrientTypeCode.equals(NutrientCode.CarbohydrateByDiff)
