891d059ce02278401e368b6eff280ab675b2e2fb
Valentin Leblanc <valentin.leblanc@becpg.fr>
2025-07-22 15:28:07 +0200

    Fix #22696 - [Feature] Add Toxico systemic and max values on bcpg:ing

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/toxicology/impl/ToxicologyServiceImpl.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/toxicology/impl/ToxicologyServiceImpl.java
@@ -1,19 +1,16 @@
 package fr.becpg.repo.toxicology.impl;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Locale;
+import java.util.Objects;
 
-import org.alfresco.model.ContentModel;
 import org.alfresco.repo.batch.BatchProcessor;
-import org.alfresco.repo.model.Repository;
+import org.alfresco.repo.node.MLPropertyInterceptor;
 import org.alfresco.repo.transaction.AlfrescoTransactionSupport;
+import org.alfresco.service.cmr.repository.MLText;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
-import org.alfresco.service.cmr.repository.StoreRef;
-import org.alfresco.service.namespace.QName;
 import org.alfresco.util.transaction.TransactionListenerAdapter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -23,14 +20,14 @@ import org.springframework.stereotype.Service;
 import fr.becpg.model.BeCPGModel;
 import fr.becpg.model.PLMModel;
 import fr.becpg.model.ToxType;
-import fr.becpg.repo.PlmRepoConsts;
 import fr.becpg.repo.RepoConsts;
 import fr.becpg.repo.batch.BatchInfo;
 import fr.becpg.repo.batch.BatchQueueService;
 import fr.becpg.repo.batch.BatchStep;
 import fr.becpg.repo.batch.EntityListBatchProcessWorkProvider;
-import fr.becpg.repo.helper.RepoService;
+import fr.becpg.repo.helper.MLTextHelper;
 import fr.becpg.repo.search.BeCPGQueryBuilder;
+import fr.becpg.repo.toxicology.ToxHelper;
 import fr.becpg.repo.toxicology.ToxicologyService;
 
 /**
@@ -45,50 +42,42 @@ public class ToxicologyServiceImpl implements ToxicologyService {
 
 	@Autowired
 	private NodeService nodeService;
-	
-	@Autowired
-	private Repository repository;
-	
-	@Autowired
-	private RepoService repoService;
-	
+
 	@Autowired
 	private BatchQueueService batchQueueService;
-	
+
 	@Override
 	public Double getMaxValue(NodeRef ingNodeRef, NodeRef toxNodeRef) {
-		NodeRef toxIngNodeRef = getToxIngNodeRef(ingNodeRef, toxNodeRef);
-		if (!nodeService.exists(toxIngNodeRef)) {
-			logger.debug("ToxIng does not exist for ing: " + ingNodeRef + " and tox:" + toxNodeRef);
-			return null;
-		}
-		return (Double) nodeService.getProperty(toxIngNodeRef, PLMModel.PROP_TOX_ING_MAX_VALUE);
+		String maxValues = (String) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_MAX_VALUES);
+		String toxName = extractToxName(toxNodeRef);
+		return ToxHelper.extractToxValue(maxValues, toxName);
 	}
 
 	/** {@inheritDoc} */
 	@Override
 	public void updateToxIngAfterIngUpdate(NodeRef ingNodeRef) {
-		AlfrescoTransactionSupport.bindListener(new TransactionListenerAdapter() {
-			@Override
-			public void afterCommit() {
-				List<NodeRef> toxList = BeCPGQueryBuilder.createQuery().inDB().ofType(PLMModel.TYPE_TOX)
-						.maxResults(RepoConsts.MAX_RESULTS_UNLIMITED)
-						.list();
-				BatchInfo batchInfo = new BatchInfo("updateOrCreateToxIng-" + ingNodeRef.getId(), "becpg.batch.updateOrCreateToxIng");
-				batchInfo.setRunAsSystem(true);
-				BatchStep<NodeRef> batchStep = new BatchStep<>();
-				batchStep.setWorkProvider(new EntityListBatchProcessWorkProvider<>(toxList));
-				batchStep.setProcessWorker(new BatchProcessor.BatchProcessWorkerAdaptor<NodeRef>() {
-					@Override
-					public void process(NodeRef toxNodeRef) throws Throwable {
-						if (nodeService.exists(toxNodeRef) && nodeService.exists(ingNodeRef)) {
-							updateOrCreateToxIng(ingNodeRef, toxNodeRef);
-						}
-					}
-				});
-				batchQueueService.queueBatch(batchInfo, List.of(batchStep));
+		if (Boolean.TRUE.equals(nodeService.getProperty(ingNodeRef, BeCPGModel.PROP_IS_DELETED))) {
+			return;
+		}
+		List<NodeRef> toxList = BeCPGQueryBuilder.createQuery().inDB().ofType(PLMModel.TYPE_TOX)
+				.maxResults(RepoConsts.MAX_RESULTS_UNLIMITED).list()
+				.stream().filter(n -> !Boolean.TRUE.equals(nodeService.getProperty(n, BeCPGModel.PROP_IS_DELETED))).toList();
+		for (NodeRef toxNodeRef : toxList) {
+			String toxName = extractToxName(toxNodeRef);
+			Double systemicValue = computeSystemicValue(ingNodeRef, toxNodeRef, toxName);
+			String currentSystemicValues = (String) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_SYSTEMIC_VALUES);
+			String newSystemicValues = ToxHelper.appendToxValue(currentSystemicValues, toxName, systemicValue);
+			if (!Objects.equals(newSystemicValues, currentSystemicValues)) {
+				nodeService.setProperty(ingNodeRef, PLMModel.PROP_ING_TOX_SYSTEMIC_VALUES, newSystemicValues);
 			}
-		});
+			
+			Double maxValue = computeMaxValue(ingNodeRef, toxNodeRef, toxName, systemicValue);
+			String currentMaxValues = (String) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_MAX_VALUES);
+			String newMaxValues = ToxHelper.appendToxValue(currentMaxValues, toxName, maxValue);
+			if (!Objects.equals(newMaxValues, currentMaxValues)) {
+				nodeService.setProperty(ingNodeRef, PLMModel.PROP_ING_TOX_MAX_VALUES, newMaxValues);
+			}
+		}
 	}
 
 	/** {@inheritDoc} */
@@ -97,19 +86,18 @@ public class ToxicologyServiceImpl implements ToxicologyService {
 		AlfrescoTransactionSupport.bindListener(new TransactionListenerAdapter() {
 			@Override
 			public void afterCommit() {
-				List<NodeRef> toxIngList = BeCPGQueryBuilder.createQuery().inDB().ofType(PLMModel.TYPE_TOX_ING)
+				List<NodeRef> ingList = BeCPGQueryBuilder.createQuery().inDB().ofType(PLMModel.TYPE_ING)
 						.maxResults(RepoConsts.MAX_RESULTS_UNLIMITED)
-						.andPropEquals(PLMModel.PROP_TOX_ING_TOX, toxNodeRef.toString())
 						.list();
-				BatchInfo batchInfo = new BatchInfo("updateToxIng-" + toxNodeRef.getId(), "becpg.batch.updateToxIng");
+				BatchInfo batchInfo = new BatchInfo("updateToxIngAfterToxUpdate-" + extractToxName(toxNodeRef), "becpg.batch.updateToxIng");
 				batchInfo.setRunAsSystem(true);
 				BatchStep<NodeRef> batchStep = new BatchStep<>();
-				batchStep.setWorkProvider(new EntityListBatchProcessWorkProvider<>(toxIngList));
+				batchStep.setWorkProvider(new EntityListBatchProcessWorkProvider<>(ingList));
 				batchStep.setProcessWorker(new BatchProcessor.BatchProcessWorkerAdaptor<NodeRef>() {
 					@Override
-					public void process(NodeRef toxIngNodeRef) throws Throwable {
-						if (nodeService.exists(toxIngNodeRef)) {
-							updateToxIng(toxIngNodeRef);
+					public void process(NodeRef ingNodeRef) throws Throwable {
+						if (nodeService.exists(ingNodeRef) && nodeService.exists(toxNodeRef)) {
+							updateIngFromTox(ingNodeRef, toxNodeRef);
 						}
 					}
 				});
@@ -120,132 +108,125 @@ public class ToxicologyServiceImpl implements ToxicologyService {
 
 	/** {@inheritDoc} */
 	@Override
-	public void deleteToxIngBeforeIngDelete(NodeRef ingNodeRef) {
-		List<NodeRef> toxIngList = BeCPGQueryBuilder.createQuery().inDB()
-				.ofType(PLMModel.TYPE_TOX_ING)
-				.andPropEquals(PLMModel.PROP_TOX_ING_ING, ingNodeRef.toString())
-				.maxResults(RepoConsts.MAX_RESULTS_UNLIMITED)
-				.list();
-		AlfrescoTransactionSupport.bindListener(new TransactionListenerAdapter() {
-			@Override
-			public void afterCommit() {
-				// make sure the node is actually deleted
-				if (!nodeService.exists(ingNodeRef)) {
-					deleteToxIngListInBatch(toxIngList, ingNodeRef.getId());
-				}
-			}
-		});
-	}
-
-	/** {@inheritDoc} */
-	@Override
 	public void deleteToxIngBeforeToxDelete(NodeRef toxNodeRef) {
-		List<NodeRef> toxIngList = BeCPGQueryBuilder.createQuery().inDB()
-				.ofType(PLMModel.TYPE_TOX_ING)
-				.andPropEquals(PLMModel.PROP_TOX_ING_TOX, toxNodeRef.toString())
+		List<NodeRef> ingList = BeCPGQueryBuilder.createQuery().inDB()
+				.ofType(PLMModel.TYPE_ING)
 				.maxResults(RepoConsts.MAX_RESULTS_UNLIMITED)
 				.list();
+		String toxName = extractToxName(toxNodeRef);
 		AlfrescoTransactionSupport.bindListener(new TransactionListenerAdapter() {
 			@Override
 			public void afterCommit() {
 				// make sure the node is actually deleted
 				if (!nodeService.exists(toxNodeRef)) {
-					deleteToxIngListInBatch(toxIngList, toxNodeRef.getId());
+					removeToxFromIngList(toxName, ingList);
 				}
 			}
+
 		});
 	}
 	
-	private void deleteToxIngListInBatch(List<NodeRef> toxIngList, String id) {
-		BatchInfo batchInfo = new BatchInfo("deleteToxIng-" + id + "-" + System.currentTimeMillis(), "becpg.batch.deleteToxIng");
+	private void removeToxFromIngList(String toxName, List<NodeRef> ingList) {
+		BatchInfo batchInfo = new BatchInfo("deleteToxIng-" + toxName, "becpg.batch.deleteToxIng");
 		batchInfo.setRunAsSystem(true);
 		BatchStep<NodeRef> batchStep = new BatchStep<>();
-		batchStep.setWorkProvider(new EntityListBatchProcessWorkProvider<>(toxIngList));
+		batchStep.setWorkProvider(new EntityListBatchProcessWorkProvider<>(ingList));
 		batchStep.setProcessWorker(new BatchProcessor.BatchProcessWorkerAdaptor<NodeRef>() {
 			@Override
-			public void process(NodeRef toxIngNodeRef) throws Throwable {
-				if (nodeService.exists(toxIngNodeRef)) {
-					logger.debug("Deleting toxIngNodeRef: " + toxIngNodeRef);
-					nodeService.addAspect(toxIngNodeRef, ContentModel.ASPECT_TEMPORARY, null);
-					nodeService.deleteNode(toxIngNodeRef);
+			public void process(NodeRef ingNodeRef) throws Throwable {
+				if (nodeService.exists(ingNodeRef)) {
+					String currentSystemicValues = (String) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_SYSTEMIC_VALUES);
+					if (currentSystemicValues != null) {
+						String newSystemicValues = ToxHelper.removeToxValue(currentSystemicValues, toxName);
+						if (!Objects.equals(newSystemicValues, currentSystemicValues)) {
+							nodeService.setProperty(ingNodeRef, PLMModel.PROP_ING_TOX_SYSTEMIC_VALUES, newSystemicValues);
+						}
+					}
+					String currentMaxValues = (String) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_MAX_VALUES);
+					if (currentMaxValues != null) {
+						String newMaxValues = ToxHelper.removeToxValue(currentMaxValues, toxName);
+						if (!Objects.equals(newMaxValues, currentMaxValues)) {
+							nodeService.setProperty(ingNodeRef, PLMModel.PROP_ING_TOX_MAX_VALUES, newMaxValues);
+						}
+					}
 				}
 			}
 		});
 		batchQueueService.queueBatch(batchInfo, List.of(batchStep));
 	}
 
-	private void updateOrCreateToxIng(NodeRef ingNodeRef, NodeRef toxNodeRef) {
-		if (Boolean.TRUE.equals(nodeService.getProperty(ingNodeRef, BeCPGModel.PROP_IS_DELETED))
-				|| Boolean.TRUE.equals(nodeService.getProperty(toxNodeRef, BeCPGModel.PROP_IS_DELETED))) {
+	private String extractToxName(NodeRef toxNodeRef) {
+		boolean wasMLAware = MLPropertyInterceptor.setMLAware(true);
+		try {
+			return MLTextHelper.getClosestValue((MLText) nodeService.getProperty(toxNodeRef, BeCPGModel.PROP_CHARACT_NAME), Locale.getDefault());
+		} finally {
+			MLPropertyInterceptor.setMLAware(wasMLAware);
+		}
+	}
+
+	private Double computeMaxValue(NodeRef ingNodeRef, NodeRef toxNodeRef, String toxName, Double systemicValue) {
+		Boolean calculateMax = (Boolean) nodeService.getProperty(toxNodeRef, PLMModel.PROP_TOX_CALCULATE_MAX);
+		if (Boolean.TRUE.equals(calculateMax)) {
+			Double maxValue = computeMaxValue(ingNodeRef, toxNodeRef);
+			if (maxValue == null) {
+				maxValue = systemicValue;
+			} else if (systemicValue != null) {
+				maxValue = Math.min(maxValue, systemicValue);
+			}
+			logger.debug("Calculate max value from ingNodeRef: " + ingNodeRef + " and toxNodeRef: " + toxNodeRef + ", maxValue=" + maxValue);
+			if (maxValue != null)  {
+				return maxValue;
+			}
+		}
+		return null;
+	}
+
+	private Double computeSystemicValue(NodeRef ingNodeRef, NodeRef toxNodeRef, String toxName) {
+		Boolean calculateSystemic = (Boolean) nodeService.getProperty(toxNodeRef, PLMModel.PROP_TOX_CALCULATE_SYSTEMIC);
+		Double systemicValue = null;
+		if (Boolean.TRUE.equals(calculateSystemic)) {
+			Double podMax = (Double) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_POD_SYSTEMIC);
+			Double mosMoe = (Double) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_MOS_MOE);
+			Double finalQuantity = (Double) nodeService.getProperty(toxNodeRef, PLMModel.PROP_TOX_VALUE);
+			String absorptionType = (String) nodeService.getProperty(toxNodeRef, PLMModel.PROP_TOX_ABSORPTION_TYPE);
+			Double dermalAbsorption = (Double) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_DERMAL_ABSORPTION);
+			Double oralAbsorption = (Double) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_ORAL_ABSORPTION);
+			Double finalAbsorption = dermalAbsorption;
+			if ("Oral".equals(absorptionType)) {
+				finalAbsorption = oralAbsorption;
+			} else if ("Worst".equals(absorptionType)) {
+				finalAbsorption = 100d;
+			}
+			if (podMax != null && finalAbsorption != null && finalAbsorption != 0 && mosMoe != null && mosMoe != 0 && finalQuantity != null) {
+				systemicValue = (podMax * 60 / (finalQuantity * finalAbsorption / 100 * mosMoe)) * 100;
+				logger.debug("Calculate systemic value from ingNodeRef: " + ingNodeRef + " and toxNodeRef: " + toxNodeRef + ", systemicValue=" + systemicValue);
+			}
+		}
+		return systemicValue;
+	}
+	
+	private void updateIngFromTox(NodeRef ingNodeRef, NodeRef toxNodeRef) {
+		if (Boolean.TRUE.equals(nodeService.getProperty(ingNodeRef, BeCPGModel.PROP_IS_DELETED))) {
 			return;
 		}
-		NodeRef toxIngNodeRef = getToxIngNodeRef(ingNodeRef, toxNodeRef);
-		if (!nodeService.exists(toxIngNodeRef)) {
-			logger.debug("Create toxIng node from ingNodeRef: " + ingNodeRef + " and toxNodeRef: " + toxNodeRef);
-			NodeRef listContainer = getCharactListContainer();
-			NodeRef toxIngFolder = nodeService.getChildByName(listContainer, ContentModel.ASSOC_CONTAINS, PlmRepoConsts.PATH_TOX_ING);
-			Map<QName, Serializable> props = new HashMap<>();
-			props.put(ContentModel.PROP_NODE_UUID, toxIngNodeRef.getId());
-			props.put(PLMModel.PROP_TOX_ING_ING, ingNodeRef);
-			props.put(PLMModel.PROP_TOX_ING_TOX, toxNodeRef);
-			toxIngNodeRef = nodeService
-					.createNode(toxIngFolder, ContentModel.ASSOC_CONTAINS, ContentModel.ASSOC_CONTAINS, PLMModel.TYPE_TOX_ING, props).getChildRef();
+		String currentSystemicValues = (String) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_SYSTEMIC_VALUES);
+		String currentMaxValues = (String) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_MAX_VALUES);
+		if ((currentSystemicValues == null || currentSystemicValues.isBlank()) && (currentMaxValues == null || currentMaxValues.isBlank()))  {
+			return;
 		}
-		updateToxIng(toxIngNodeRef);
-	}
-
-	private NodeRef getToxIngNodeRef(NodeRef ingNodeRef, NodeRef toxNodeRef) {
-		String toxIngId = ingNodeRef.getId().substring(0, 18) + toxNodeRef.getId().substring(0, 18);
-		return new NodeRef(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, toxIngId);
-	}
-
-	private NodeRef getCharactListContainer() {
-		NodeRef companyHomeNodeRef = repository.getCompanyHome();
-		NodeRef systemNodeRef = repoService.getFolderByPath(companyHomeNodeRef, RepoConsts.PATH_SYSTEM);
-		NodeRef charactsNodeRef = repoService.getFolderByPath(systemNodeRef, RepoConsts.PATH_CHARACTS);
-		return nodeService.getChildByName(charactsNodeRef, BeCPGModel.ASSOC_ENTITYLISTS, RepoConsts.CONTAINER_DATALISTS);
-	}
-
-	private void updateToxIng(NodeRef toxIngNodeRef) {
-		NodeRef toxNodeRef = (NodeRef) nodeService.getProperty(toxIngNodeRef, PLMModel.PROP_TOX_ING_TOX);
-		NodeRef ingNodeRef = (NodeRef) nodeService.getProperty(toxIngNodeRef, PLMModel.PROP_TOX_ING_ING);
-		if (toxNodeRef != null && nodeService.exists(toxNodeRef) && ingNodeRef != null && nodeService.exists(ingNodeRef)) {
-			Boolean calculateSystemic = (Boolean) nodeService.getProperty(toxNodeRef, PLMModel.PROP_TOX_CALCULATE_SYSTEMIC);
-			if (Boolean.TRUE.equals(calculateSystemic)) {
-				Double podMax = (Double) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_POD_SYSTEMIC);
-				Double mosMoe = (Double) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_MOS_MOE);
-				Double finalQuantity = (Double) nodeService.getProperty(toxNodeRef, PLMModel.PROP_TOX_VALUE);
-				String absorptionType = (String) nodeService.getProperty(toxNodeRef, PLMModel.PROP_TOX_ABSORPTION_TYPE);
-				Double dermalAbsorption = (Double) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_DERMAL_ABSORPTION);
-				Double oralAbsorption = (Double) nodeService.getProperty(ingNodeRef, PLMModel.PROP_ING_TOX_ORAL_ABSORPTION);
-				Double finalAbsorption = dermalAbsorption;
-				if ("Oral".equals(absorptionType)) {
-					finalAbsorption = oralAbsorption;
-				} else if ("Worst".equals(absorptionType)) {
-					finalAbsorption = 100d;
-				}
-				if (podMax != null && finalAbsorption != null && finalAbsorption != 0 && mosMoe != null && mosMoe != 0 && finalQuantity != null) {
-					Double systemicValue = (podMax * 60 / (finalQuantity * finalAbsorption / 100 * mosMoe)) * 100;
-					logger.debug("Calculate systemic value from ingNodeRef: " + ingNodeRef + " and toxNodeRef: " + toxNodeRef + ", systemicValue=" + systemicValue);
-					nodeService.setProperty(toxIngNodeRef, PLMModel.PROP_TOX_ING_SYSTEMIC_VALUE, systemicValue);
-				}
-			}
-
-			Boolean calculateMax = (Boolean) nodeService.getProperty(toxNodeRef, PLMModel.PROP_TOX_CALCULATE_MAX);
-			if (Boolean.TRUE.equals(calculateMax)) {
-				Double maxValue = computeMaxValue(ingNodeRef, toxNodeRef);
-				Double systemicValue = (Double) nodeService.getProperty(toxIngNodeRef, PLMModel.PROP_TOX_ING_SYSTEMIC_VALUE);
-				if (maxValue == null) {
-					maxValue = systemicValue;
-				} else if (systemicValue != null) {
-					maxValue = Math.min(maxValue, systemicValue);
-				}
-				logger.debug("Calculate max value from ingNodeRef: " + ingNodeRef + " and toxNodeRef: " + toxNodeRef + ", maxValue=" + maxValue);
-				nodeService.setProperty(toxIngNodeRef, PLMModel.PROP_TOX_ING_MAX_VALUE, maxValue);
-			}
+		String toxName = extractToxName(toxNodeRef);
+		Double systemicValue = computeSystemicValue(ingNodeRef, toxNodeRef, toxName);
+		String newSystemicValues = ToxHelper.appendToxValue(currentSystemicValues, toxName, systemicValue);
+		if (!Objects.equals(currentSystemicValues, newSystemicValues)) {
+			nodeService.setProperty(ingNodeRef, PLMModel.PROP_ING_TOX_SYSTEMIC_VALUES, newSystemicValues);
+		}
+		Double maxValue = computeMaxValue(ingNodeRef, toxNodeRef, toxName, systemicValue);
+		String newMaxValues = ToxHelper.appendToxValue(currentMaxValues, toxName, maxValue);
+		if (!Objects.equals(currentMaxValues, newMaxValues)) {
+			nodeService.setProperty(ingNodeRef, PLMModel.PROP_ING_TOX_MAX_VALUES, newMaxValues);
 		}
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	private Double computeMaxValue(NodeRef ingNodeRef, NodeRef toxNodeRef) {
 		List<String> toxTypes = (List<String>) nodeService.getProperty(toxNodeRef, PLMModel.PROP_TOX_TYPES);
