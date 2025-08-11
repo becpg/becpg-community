4fc515d8c0cba1923271ebd42a36c131d50ed1b3
Matthieu Laborie <matthieu.laborie@becpg.fr>
2025-08-11 18:05:26 +0200

    Fix #29197 - [Bug] Fix NPE in Hazard classification

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/clp/HazardClassificationFormulationHandler.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/clp/HazardClassificationFormulationHandler.java
@@ -304,6 +304,10 @@ public class HazardClassificationFormulationHandler extends FormulationBaseHandl
 			for (IngListDataItem ing : formulatedProduct.getIngList()) {
 
 				Double quantityPercentage = ing.getQtyPerc();
+				
+				if (quantityPercentage == null || quantityPercentage == 0) {
+					continue;
+				}
 
 				IngItem ingItem = (IngItem) alfrescoRepository.findOne(ing.getIng());
 
