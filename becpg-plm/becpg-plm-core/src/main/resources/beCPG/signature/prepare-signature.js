569736e865617043b7403920c711412a910a11d6
Matthieu Laborie <matthieu.laborie@becpg.fr>
2025-09-22 12:31:51 +0200

    Fix #27543 - [Bug] Error deleting old generated signature report

--- becpg-plm/becpg-plm-core/src/main/resources/beCPG/signature/prepare-signature.js
+++ becpg-plm/becpg-plm-core/src/main/resources/beCPG/signature/prepare-signature.js
@@ -1,30 +1,39 @@
+
+//Todo @Valentin should be an helper because -prepare can change and it's really internal
+function findUrlDeliverable() {
+
+    var keyName = deliverable.name.replace(" - prepare", "");
+
+    for (var i = 0; i < project.deliverableList.size(); i++) {
+        var del = project.deliverableList.get(i);
+        if (del.name == keyName + " - url") {
+            if (del.content) {
+                return del;
+            }
+            return null;
+        }
+    }
+
+    return null;
+}
+
+
 function main() {
 
-	var urlDeliverable = null;
-	
-	var keyName = deliverable.name.replace(" - prepare", "");
-	
-	for (var i = 0; i < project.deliverableList.size(); i++) {
-		var del = project.deliverableList.get(i);
-		if (del.name == keyName + " - url") {
-			urlDeliverable = del;
-			break;
-		}
-	}
+    if (task.resources != null && !task.resources.isEmpty()) {
+        var urlDeliverable = findUrlDeliverable()
+        if (urlDeliverable != null) {
+            var document = search.findNode(del.content);
+            if (document != null) {
+                var recipients = [];
+                recipients.push(search.findNode(task.resources.get(0)));
 
-	if (task.resources != null && !task.resources.isEmpty() && urlDeliverable && urlDeliverable.content) {
-		var recipient = search.findNode(task.resources.get(0));
-		var document = search.findNode(urlDeliverable.content);
-	
-		var recipients = [];
-		recipients.push(recipient);
-	
-		bSign.prepareForSignature(document, recipients);
-	
-		var signatureUrl = bSign.getSignatureView(document, recipient, task.nodeRef);
-	
-		urlDeliverable.url = signatureUrl;
-	}
+                bSign.prepareForSignature(document, recipients);
+
+                urlDeliverable.url = bSign.getSignatureView(document, recipient, task.nodeRef);
+            }
+        }
+    }
 
 }
 
