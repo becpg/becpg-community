887f08cae615689d19a159ee9448e5d97db6924c
Matthieu Laborie <matthieu.laborie@becpg.fr>
2025-12-11 17:57:41 +0100

    Fix #24194 [Bug]- Make ProjectHelper date calculations timezone independent

--- becpg-integration-runner/src/test/java/fr/becpg/test/project/ProjectHelperTest.java
+++ becpg-integration-runner/src/test/java/fr/becpg/test/project/ProjectHelperTest.java
@@ -1,6 +1,8 @@
 package fr.becpg.test.project;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,28 +11,49 @@ import fr.becpg.repo.project.impl.ProjectHelper;
 
 public class ProjectHelperTest {
 
+	@Test
+	public void testRemoveTimeTimeZoneIndependent() {
+		Date now = new Date();
+		TimeZone original = TimeZone.getDefault();
+		try {
+			TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+			Date date1 = ProjectHelper.removeTime(now);
+
+			TimeZone.setDefault(TimeZone.getTimeZone("GMT+2"));
+			Date date2 = ProjectHelper.removeTime(now);
+
+			Assert.assertEquals("removeTime should be timezone independent", date1.getTime(), date2.getTime());
+		} finally {
+			TimeZone.setDefault(original);
+		}
+	}
 	
 	@Test
-	@SuppressWarnings("deprecation")
 	public void testCalculateNextDate() {
-		Date startDate = new Date(2019,9,26, 10,10);
-		Date endDate = new Date(2020,12,14);
+		TimeZone gmt = TimeZone.getTimeZone("GMT");
+		Calendar cal = Calendar.getInstance(gmt);
+		cal.set(2019, Calendar.OCTOBER, 26, 10, 10, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		Date startDate = cal.getTime();
+		
+		cal.set(2021, Calendar.JANUARY, 14, 0, 0, 0);
+		Date endDate = cal.getTime();
+		
 		startDate = ProjectHelper.removeTime(startDate);
 		
-		Assert.assertEquals(0, startDate.getHours());
-		Assert.assertEquals(0, startDate.getMinutes());
+		Calendar check = Calendar.getInstance(gmt);
+		check.setTime(startDate);
+		Assert.assertEquals(0, check.get(Calendar.HOUR_OF_DAY));
+		Assert.assertEquals(0, check.get(Calendar.MINUTE));
 		
 		int duration = ProjectHelper.calculateTaskDuration(startDate, endDate);
 		
-		Assert.assertEquals(320,duration);
+		Assert.assertEquals(319, duration);
 		
 		Date nextDate = ProjectHelper.calculateNextDate(startDate, duration+1, true);
 		
-		
-		
 		Assert.assertNotNull(nextDate);
 		Assert.assertEquals(endDate, nextDate);
-		
 	}
 	
 }
