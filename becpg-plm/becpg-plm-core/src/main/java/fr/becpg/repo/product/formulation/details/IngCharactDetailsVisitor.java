11f3d8259c1bb699a1fcc5df65f02fb3283b4d8d
Matthieu Laborie <matthieu.laborie@becpg.fr>
2026-01-13 10:30:04 +0100

    Fix #30123 - [Bug] Fix ingredient detail percentages in multi-level view

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/details/IngCharactDetailsVisitor.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/product/formulation/details/IngCharactDetailsVisitor.java
@@ -15,63 +15,66 @@ import fr.becpg.repo.repository.model.SimpleCharactDataItem;
  * @author matthieu
  */
 public class IngCharactDetailsVisitor extends SimpleCharactDetailsVisitor {
-	
+
 	/** {@inheritDoc} */
 	@Override
 	protected boolean shouldFormulateInVolume(CharactDetailsVisitorContext context, ProductData partProduct, SimpleCharactDataItem simpleCharact) {
 		return false;
 	}
-	
 
 	/** {@inheritDoc} */
 	@Override
-	protected void provideAdditionalValues(ProductData rootProduct, ProductData formulatedProduct, 
-	                                       SimpleCharactDataItem simpleCharact, String unit, 
-	                                       Double qtyUsed, Double netQty, 
-	                                       CharactDetailsValue currentCharactDetailsValue) {
-	    IngListDataItem ingListDataItem = (IngListDataItem) simpleCharact;
+	protected void provideAdditionalValues(ProductData rootProduct, ProductData formulatedProduct, SimpleCharactDataItem simpleCharact, String unit,
+			Double qtyUsed, Double netQty, CharactDetailsValue currentCharactDetailsValue) {
+		IngListDataItem ingListDataItem = (IngListDataItem) simpleCharact;
 
-	    // Add qty with yield at final product level (Qty with yield %)
-	    if (ingListDataItem.getQtyPercWithYield() != null) {
-	        CharactDetailAdditionalValue qtyWithYieldValue = new CharactDetailAdditionalValue(
-	            I18NUtil.getMessage("bcpg_bcpgmodel.property.bcpg_ingListQtyPercWithYield.title"),
-	            FormulationHelper.calculateValue(0d, qtyUsed, ingListDataItem.getQtyPercWithYield(), netQty),
-	            unit
-	        );
-	        currentCharactDetailsValue.getAdditionalValues().add(qtyWithYieldValue);
-	    }
-	    
-	    if (ingListDataItem.getQtyPercWithSecondaryYield() != null) {
-	        CharactDetailAdditionalValue qtyWithSecondaryYieldValue = new CharactDetailAdditionalValue(
-	            I18NUtil.getMessage("bcpg_bcpgmodel.property.bcpg_ingListQtyPercWithSecondaryYield.title"),
-	            FormulationHelper.calculateValue(0d, qtyUsed, ingListDataItem.getQtyPercWithSecondaryYield(), netQty),
-	            unit
-	        );
-	        currentCharactDetailsValue.getAdditionalValues().add(qtyWithSecondaryYieldValue);
-	    }
+		Double qtyPerc = ingListDataItem.getQtyPerc();
 
-	    // Array of percentage values
-	    Double[] qtyPercValues = {
-	        ingListDataItem.getQtyPerc1(),
-	        ingListDataItem.getQtyPerc2(),
-	        ingListDataItem.getQtyPerc3(),
-	        ingListDataItem.getQtyPerc4(),
-	        ingListDataItem.getQtyPerc5()
-	    };
+		// Calculate qty with yield based on root product's yield applied to the base qtyPerc
+		Double rootYield = rootProduct.getYield();
+		if ((rootYield != null) && (rootYield != 0d) && (qtyPerc != null)) {
+			Double qtyPercWithYield = qtyPerc / (rootYield / 100d);
 
-	    // Loop through percentage values
-	    for (int i = 0; i < qtyPercValues.length; i++) {
-	        Double qtyPerc = qtyPercValues[i];
-	        if (qtyPerc != null) {
-	            String titleKey = String.format("bcpg_bcpgmodel.property.bcpg_ingListQtyPerc%d.title", i + 1);
-	            CharactDetailAdditionalValue additionalValue = new CharactDetailAdditionalValue(
-	                I18NUtil.getMessage(titleKey),
-	                FormulationHelper.calculateValue(0d, qtyUsed, qtyPerc, netQty),
-	                unit
-	            );
-	            currentCharactDetailsValue.getAdditionalValues().add(additionalValue);
-	        }
-	    }
+			CharactDetailAdditionalValue qtyWithYieldValue = new CharactDetailAdditionalValue(
+					I18NUtil.getMessage("bcpg_bcpgmodel.property.bcpg_ingListQtyPercWithYield.title"),
+					FormulationHelper.calculateValue(0d, qtyUsed, qtyPercWithYield, netQty), unit);
+			currentCharactDetailsValue.getAdditionalValues().add(qtyWithYieldValue);
+		}
+
+		// Calculate qty with secondary yield based on root product's secondary yield
+		Double rootSecondaryYield = rootProduct.getSecondaryYield();
+		if ((rootSecondaryYield != null) && (rootSecondaryYield != 0d) && (qtyPerc != null)) {
+			// Apply both yields if primary yield exists, otherwise just secondary yield
+			Double baseValue = qtyPerc;
+			if ((rootYield != null) && (rootYield != 0d)) {
+				baseValue = qtyPerc / (rootYield / 100d);
+			}
+			Double qtyPercWithSecondaryYield = baseValue / (rootSecondaryYield / 100d);
+			CharactDetailAdditionalValue qtyWithSecondaryYieldValue = new CharactDetailAdditionalValue(
+					I18NUtil.getMessage("bcpg_bcpgmodel.property.bcpg_ingListQtyPercWithSecondaryYield.title"),
+					FormulationHelper.calculateValue(0d, qtyUsed, qtyPercWithSecondaryYield, netQty), unit);
+			currentCharactDetailsValue.getAdditionalValues().add(qtyWithSecondaryYieldValue);
+		}
+
+		// Array of percentage values
+		Double[] qtyPercValues = { ingListDataItem.getQtyPerc1(), ingListDataItem.getQtyPerc2(), ingListDataItem.getQtyPerc3(),
+				ingListDataItem.getQtyPerc4(), ingListDataItem.getQtyPerc5() };
+
+		// Loop through percentage values
+		for (int i = 0; i < qtyPercValues.length; i++) {
+			Double qtyPercPerValue = qtyPercValues[i];
+			if (qtyPercPerValue != null) {
+				String titleKey = String.format("bcpg_bcpgmodel.property.bcpg_ingListQtyPerc%d.title", i + 1);
+				CharactDetailAdditionalValue additionalValue = new CharactDetailAdditionalValue(I18NUtil.getMessage(titleKey),
+						FormulationHelper.calculateValue(0d, qtyUsed, qtyPercPerValue, netQty), unit);
+				currentCharactDetailsValue.getAdditionalValues().add(additionalValue);
+			}
+		}
 	}
-	
+
+	@Override
+	protected boolean applyYield() {
+		return false;
+	}
+
 }
