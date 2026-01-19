a9b436705e6ae2cc4999ab15538e71ddb78c2293
Matthieu Laborie <matthieu.laborie@becpg.fr>
2026-01-19 11:06:27 +0100

    Fix #32062 - [Bug] Unable to merge an entity if a node is locked

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/ecm/impl/ECOServiceImpl.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/ecm/impl/ECOServiceImpl.java
@@ -317,7 +317,7 @@ public class ECOServiceImpl implements ECOService {
 
 					JSONObject lockInfo = new JSONObject();
 
-					lockInfo.put("lockType", "versioning");
+					lockInfo.put(EntityVersionService.LOCK_TYPE_PARAM, EntityVersionService.LOCK_TYPE_VERSIONING);
 					lockInfo.put("sourceNodeRef", ecoData.getNodeRef());
 					lockInfo.put("sourceInfo", batchInfo.toJson());
 
