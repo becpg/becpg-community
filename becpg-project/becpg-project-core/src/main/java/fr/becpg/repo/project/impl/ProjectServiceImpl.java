bfe3a3a7f693adee16496f604aba2d7de215b968
Matthieu Laborie <matthieu.laborie@becpg.fr>
2026-02-23 14:15:45 +0100

    Fix #32002 - [Bug] Fix project task role extraction in security group check

--- becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/impl/ProjectServiceImpl.java
+++ becpg-project/becpg-project-core/src/main/java/fr/becpg/repo/project/impl/ProjectServiceImpl.java
@@ -657,10 +657,15 @@ public class ProjectServiceImpl extends DefaultSecurityServicePlugin implements 
 	@Override
 	public boolean checkIsInSecurityGroup(NodeRef nodeRef, List<NodeRef> groups) {
 		if (nodeRef != null) {
+			NodeRef projectNodeRef = nodeRef;
+			if (ProjectModel.TYPE_TASK_LIST.equals(nodeService.getType(nodeRef))) {
+				projectNodeRef = entityListDAO.getEntity(nodeRef);
+			}
+			
 			for (NodeRef groupNodeRef : groups) {
 				String authorityName = authorityDAO.getAuthorityName(groupNodeRef);
 				if (ProjectHelper.isRoleAuhtority(authorityName)) {
-					List<NodeRef> resources = extractResources(nodeRef, Arrays.asList(groupNodeRef));
+					List<NodeRef> resources = extractResources(projectNodeRef, Arrays.asList(groupNodeRef));
 					if (resources.contains(personService.getPerson(AuthenticationUtil.getFullyAuthenticatedUser()))) {
 						return true;
 					}
