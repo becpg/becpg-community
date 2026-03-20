6bf67a492b736f9f09b27901d2acdf2c94003968
Matthieu Laborie <matthieu.laborie@becpg.fr>
2026-03-20 10:25:17 +0100

    Fix #22670 - [Bug] Align data list context in report extractor
    (cherry picked from commit 1887e0b17f25cddaaee14324dd65cc7500d7c625)

--- becpg-core/src/main/java/fr/becpg/repo/report/entity/impl/DefaultEntityReportExtractor.java
+++ becpg-core/src/main/java/fr/becpg/repo/report/entity/impl/DefaultEntityReportExtractor.java
@@ -811,7 +811,7 @@ public class DefaultEntityReportExtractor implements EntityReportExtractorPlugin
 	 */
 	protected void loadDataListItemAttributes(BeCPGDataObject dataListItem, Element nodeElt, DefaultExtractorContext context,
 			List<QName> hiddentAttributes) {
-		loadDataListItemAttributes(dataListItem, nodeElt, context, hiddentAttributes, true);
+		context.doInDataListContext(() -> loadDataListItemAttributes(dataListItem, nodeElt, context, hiddentAttributes, true));
 	}
 
 	/**
