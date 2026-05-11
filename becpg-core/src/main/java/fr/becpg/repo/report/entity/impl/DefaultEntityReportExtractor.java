dd3a5e13fd03841fc5f83f9cf18a5b4d869e2f2f
Valentin Leblanc <valentin.leblanc@becpg.fr>
2026-05-11 11:28:01 +0200

    Fix #34136 - [Fix] Fix datalist image extraction in report datasource

--- becpg-core/src/main/java/fr/becpg/repo/report/entity/impl/DefaultEntityReportExtractor.java
+++ becpg-core/src/main/java/fr/becpg/repo/report/entity/impl/DefaultEntityReportExtractor.java
@@ -906,10 +906,7 @@ public class DefaultEntityReportExtractor implements EntityReportExtractorPlugin
 					&& entityDictionaryService.isSubClass(nodeService.getType(nodeRef), BeCPGModel.TYPE_ENTITYLIST_ITEM)
 					&& context.isPrefOn(EntityReportParameters.PARAM_EXTRACT_DATALIST_IMAGE, Boolean.FALSE)) {
 				String imgId = String.format(DATALIST_IMG_ID, nodeRef.getId());
-				if (!context.getExtractedImages().contains(nodeRef)) {
-					context.getExtractedImages().add(nodeRef);
-					extractImage(nodeRef, imgId, nodeElt, context);
-				}
+				extractImage(nodeRef, imgId, nodeElt, context);
 			}
 
 			// do not display system properties
@@ -1377,10 +1374,7 @@ public class DefaultEntityReportExtractor implements EntityReportExtractorPlugin
 				List<AssociationRef> avatorAssocs = nodeService.getTargetAssocs(creatorNodeRef, ContentModel.ASSOC_AVATAR);
 				if (!avatorAssocs.isEmpty()) {
 					NodeRef avatarNodeRef = avatorAssocs.get(0).getTargetRef();
-					if (!context.getExtractedImages().contains(avatarNodeRef)) {
-						context.getExtractedImages().add(avatarNodeRef);
-						extractImage(creatorNodeRef, avatarNodeRef, AVATAR_IMG_ID, imgsElt, context, null);
-					}
+					extractImage(creatorNodeRef, avatarNodeRef, AVATAR_IMG_ID, imgsElt, context, null);
 				}
 			}
 		}
