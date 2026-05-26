0f25d6e31dedb45ef1f66e5a43266cf943fabf38
Valentin Leblanc <valentin.leblanc@becpg.fr>
2026-05-26 11:08:18 +0200

    Fix #34166 - [Bug] Extend admin module webscript access to system managers

--- becpg-core/src/main/java/fr/becpg/repo/web/scripts/admin/MonitorWebScript.java
+++ becpg-core/src/main/java/fr/becpg/repo/web/scripts/admin/MonitorWebScript.java
@@ -97,7 +97,7 @@ public class MonitorWebScript extends DeclarativeWebScript {
 
 	private String becpgSchema;
 
-	private AuthorityService authorityService;
+	protected AuthorityService authorityService;
 
 	private BatchQueueService batchQueueService;
 	
