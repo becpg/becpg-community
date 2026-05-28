bdb7167528ca755cbaf724f56c803e3cb844f528
Valentin Leblanc <valentin.leblanc@becpg.fr>
2026-05-28 10:51:16 +0200

    Fix #11315 - [Bug] Check permissions before accessing branches in view

--- becpg-core/src/main/java/fr/becpg/repo/entity/version/EntityVersionServiceImpl.java
+++ becpg-core/src/main/java/fr/becpg/repo/entity/version/EntityVersionServiceImpl.java
@@ -555,33 +555,30 @@ public class EntityVersionServiceImpl implements EntityVersionService {
 		List<EntityVersion> entityVersions = new ArrayList<>();
 		if (!nodeService.hasAspect(entityNodeRef, ContentModel.ASPECT_WORKING_COPY)
 				&& !nodeService.hasAspect(entityNodeRef, BeCPGModel.ASPECT_COMPOSITE_VERSION)) {
-			AuthenticationUtil.runAsSystem(() -> {
-				VersionHistory versionHistory = versionService.getVersionHistory(entityNodeRef);
+			VersionHistory versionHistory = versionService.getVersionHistory(entityNodeRef);
+			
+			if (versionHistory != null) {
+				List<ChildAssociationRef> versionAssocs = getVersionAssocs(entityNodeRef);
 				
-				if (versionHistory != null) {
-					List<ChildAssociationRef> versionAssocs = getVersionAssocs(entityNodeRef);
-					
-					NodeRef branchFromNodeRef = getBranchFromNodeRef(entityNodeRef);
-					
-					Optional<Version> lowestVersion = versionHistory.getAllVersions().stream()
-							.min(((o1, o2) -> ((Double) Double.parseDouble(o1.getVersionLabel())).compareTo(Double.parseDouble(o2.getVersionLabel()))));
-					
-					for (Version version : versionHistory.getAllVersions()) {
-						NodeRef entityVersionNodeRef = getEntityVersion(versionAssocs, version);
-						EntityVersion entityVersion = null;
-						if (entityVersionNodeRef != null && !nodeService.hasAspect(entityVersionNodeRef, ContentModel.ASPECT_TEMPORARY)) {
-							entityVersion = new EntityVersion(version, entityNodeRef, entityVersionNodeRef, branchFromNodeRef);
-						} else {
-							entityVersion = new EntityVersion(version, entityNodeRef, getEntityVersion(version), branchFromNodeRef);
-						}
-						if (RepoConsts.INITIAL_VERSION.equals(version.getVersionLabel()) || lowestVersion.isPresent() && version == lowestVersion.get()) {
-							entityVersion.setCreatedDate((Date) nodeService.getProperty(entityNodeRef, ContentModel.PROP_CREATED));
-						}
-						entityVersions.add(entityVersion);
+				NodeRef branchFromNodeRef = getBranchFromNodeRef(entityNodeRef);
+				
+				Optional<Version> lowestVersion = versionHistory.getAllVersions().stream()
+						.min(((o1, o2) -> ((Double) Double.parseDouble(o1.getVersionLabel())).compareTo(Double.parseDouble(o2.getVersionLabel()))));
+				
+				for (Version version : versionHistory.getAllVersions()) {
+					NodeRef entityVersionNodeRef = getEntityVersion(versionAssocs, version);
+					EntityVersion entityVersion = null;
+					if (entityVersionNodeRef != null && !nodeService.hasAspect(entityVersionNodeRef, ContentModel.ASPECT_TEMPORARY)) {
+						entityVersion = new EntityVersion(version, entityNodeRef, entityVersionNodeRef, branchFromNodeRef);
+					} else {
+						entityVersion = new EntityVersion(version, entityNodeRef, getEntityVersion(version), branchFromNodeRef);
 					}
+					if (RepoConsts.INITIAL_VERSION.equals(version.getVersionLabel()) || lowestVersion.isPresent() && version == lowestVersion.get()) {
+						entityVersion.setCreatedDate((Date) nodeService.getProperty(entityNodeRef, ContentModel.PROP_CREATED));
+					}
+					entityVersions.add(entityVersion);
 				}
-				return null;
-			});
+			}
 		}
 
 		return entityVersions;
@@ -720,7 +717,8 @@ public class EntityVersionServiceImpl implements EntityVersionService {
 
 		List<NodeRef> ret = new ArrayList<>();
 		// Look for childs
-		for (NodeRef associationRef : associationService.getSourcesAssocs(entityNodeRef, BeCPGModel.ASSOC_BRANCH_FROM_ENTITY)) {
+		List<NodeRef> sourcesAssocs = associationService.getSourcesAssocs(entityNodeRef, BeCPGModel.ASSOC_BRANCH_FROM_ENTITY, false, null, null, true);
+		for (NodeRef associationRef : sourcesAssocs) {
 			if (!VersionHelper.isVersion(associationRef)
 					&& !nodeService.hasAspect(associationRef, BeCPGModel.ASPECT_COMPOSITE_VERSION)) {
 				NodeRef tmpNodeRef = associationRef;
