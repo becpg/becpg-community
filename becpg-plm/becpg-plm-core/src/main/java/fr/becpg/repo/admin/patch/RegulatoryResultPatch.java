c20084986d323fb9487a8270b46221cb597a2ce3
Roman Kupin <roman.kupin@becpg.fr>
2026-06-18 17:03:25 +0200

    InProgress #10229 - [Bug] Disabled integrity checker for RegulatoryModePatch, fixed copypaste inconsistencies for batch name

--- becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/admin/patch/RegulatoryResultPatch.java
+++ becpg-plm/becpg-plm-core/src/main/java/fr/becpg/repo/admin/patch/RegulatoryResultPatch.java
@@ -94,7 +94,7 @@ public class RegulatoryResultPatch extends AbstractBeCPGPatch {
 			}
 		};
 
-		BatchProcessor<NodeRef> batchProcessor = new BatchProcessor<>("ErrorLogPatch", transactionService.getRetryingTransactionHelper(),
+		BatchProcessor<NodeRef> batchProcessor = new BatchProcessor<>("RegulatoryResultPatch", transactionService.getRetryingTransactionHelper(),
 				workProvider, BATCH_THREADS, BATCH_SIZE, applicationEventPublisher, logger, 1000);
 
 		BatchProcessWorker<NodeRef> worker = new BatchProcessWorker<>() {
