cacbd2044ee9c9bad9e782380d9cee629f5eb18f
Valentin Leblanc <valentin.leblanc@becpg.fr>
2026-07-24 14:17:00 +0200

    Fix #35748 - [Bug] Prevent failing notification rules from blocking subsequent notifications

--- becpg-core/src/main/java/fr/becpg/repo/notification/impl/NotificationRuleServiceImpl.java
+++ becpg-core/src/main/java/fr/becpg/repo/notification/impl/NotificationRuleServiceImpl.java
@@ -152,29 +152,41 @@ public class NotificationRuleServiceImpl implements NotificationRuleService {
 		for (NodeRef notificationNodeRef : getAllNotificationRule()) {
 			NotificationRuleListDataItem notification = alfrescoRepository.findOne(notificationNodeRef);
 			if ((notification != null) && !shouldSkip(notification)) {
+				try {
+					initializeNotification(notification);
 
-				initializeNotification(notification);
+					SearchRuleFilter filter = buildSearchFilter(notification);
+					SearchRuleResult searchResult = searchRuleService.search(filter);
 
-				SearchRuleFilter filter = buildSearchFilter(notification);
-				SearchRuleResult searchResult = searchRuleService.search(filter);
+					if (!isEmptyResult(notification, searchResult, filter)) {
 
-				if (!isEmptyResult(notification, searchResult, filter)) {
+						Map<String, Object> templateArgs = buildTemplateArgs(notification, filter);
+						Map<NodeRef, Object> entities = buildEntities(searchResult.getResults(), notification);
 
-					Map<String, Object> templateArgs = buildTemplateArgs(notification, filter);
-					Map<NodeRef, Object> entities = buildEntities(searchResult.getResults(), notification);
+						Consumer<NodeRef> mailSender = createMailSender(notification, templateArgs, entities, filter, searchResult);
 
-					Consumer<NodeRef> mailSender = createMailSender(notification, templateArgs, entities, filter, searchResult);
+						if (CollectionUtils.isNotEmpty(notification.getReportTpls())) {
+							processReportTemplates(notification, searchResult, mailSender);
+						} else {
+							mailSender.accept(null);
+						}
 
-					if (CollectionUtils.isNotEmpty(notification.getReportTpls())) {
-						processReportTemplates(notification, searchResult, mailSender);
+						executeScriptIfExists(notification, searchResult.getResults(), templateArgs);
 					} else {
-						mailSender.accept(null);
+						logNoObjects(notification, filter);
+
 					}
-
-					executeScriptIfExists(notification, searchResult.getResults(), templateArgs);
-				} else {
-					logNoObjects(notification, filter);
-
+				} catch (Throwable e) {
+					if (RetryingTransactionHelper.extractRetryCause(e) != null) {
+						throw e;
+					}
+					logger.error("Error while sending notification " + notificationNodeRef, e);
+					try {
+						notification.setErrorLog(e.getMessage() != null ? e.getMessage() : e.toString());
+						alfrescoRepository.save(notification);
+					} catch (Exception ex) {
+						logger.error("Error while saving notification error details " + notificationNodeRef, ex);
+					}
 				}
 			}
 		}
